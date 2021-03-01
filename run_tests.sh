ec57139825cb710683bd9c6d6cac66cd096654eb
Maxim Strutovsky <strutovsky.m.a@gmail.com>
2021-03-01 10:54:12 +0300

    Kept one experiments script

--- run_tests.sh
+++ run_tests.sh
@@ -3,11 +3,11 @@ declare -a datasets=('adult' 'breast_cancer' 'CIPublicHighway' 'EpicMeds' 'EpicV
 declare -a separators=(';' ',' ',' '|' '|' ',' '|' ',')
 declare -a header_presence=('false' 'true' 'true' 'true' 'true' 'true' 'true' 'true')
 
-declare -a seeds=(1000 2000 10000)
+declare -a seeds=(10000)
 
 cd build/target
 
-declare experiment_name=01_08
+declare experiment_name=02_08_nocaching
 mkdir $experiment_name
 for j in "${seeds[@]}"
 do
@@ -15,11 +15,10 @@ mkdir $experiment_name/$j
 for i in $(seq 0 7)
 do
 	#cat 01_07/$j/results_${datasets[i]}.txt | grep 'TIME' > 01_07/$j/results_${datasets[i]}.txt
-	for k in $(seq 1 10)
+	for k in $(seq 1 1)
 	do
 		touch $experiment_name/$j/results_${datasets[i]}.txt
-		#echo --algo='pyro' --data="${datasets[i]}".csv --sep="${separators[i]}" --hasHeader="${header_presence[i]}" --seed=$j 
-		./fdtester_run --algo='pyro' --data="${datasets[i]}".csv --sep="${separators[i]}" --hasHeader="${header_presence[i]}" --seed=$j >> $experiment_name/$j/results_${datasets[i]}.txt
+		./fdtester_run --algo='pyro' --data="${datasets[i]}".csv --sep="${separators[i]}" --hasHeader="${header_presence[i]}" --seed=$j | grep 'TIME' >> $experiment_name/$j/results_${datasets[i]}.txt
 	done
 done
 done
