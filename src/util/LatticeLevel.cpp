740fdd6ac85419e6e6b36dfc5d71eebfbfe5e519
Maxim Strutovsky <strutovsky.m.a@gmail.com>
2021-03-17 09:59:32 +0300

    TANE four-space indent fix

--- src/util/LatticeLevel.cpp
+++ src/util/LatticeLevel.cpp
@@ -8,49 +8,49 @@
 using std::move, std::min, std::shared_ptr, std::vector, std::cout, std::endl, std::sort, std::make_shared;
 
 void LatticeLevel::add(std::unique_ptr<LatticeVertex> vertex) {
-  vertices.emplace(vertex->getVertical().getColumnIndices(), std::move(vertex));
+    vertices.emplace(vertex->getVertical().getColumnIndices(), std::move(vertex));
 }
 
 LatticeVertex const* LatticeLevel::getLatticeVertex(const boost::dynamic_bitset<>& columnIndices) const {
-  auto it = vertices.find(columnIndices);
-  if (it != vertices.end()){
-    return it->second.get();
-  }
-  else{
-    return nullptr;
-  }
+    auto it = vertices.find(columnIndices);
+    if (it != vertices.end()){
+        return it->second.get();
+    }
+    else{
+        return nullptr;
+    }
 }
 
 void LatticeLevel::generateNextLevel(std::vector<std::unique_ptr<LatticeLevel>>& levels) {
     unsigned int arity = levels.size() - 1;
     assert(arity >= 1);
     std::cout << "-------------Creating level " << arity + 1 << "...-----------------\n";
-    LatticeLevel *currentLevel = levels[arity].get();
+
+    LatticeLevel* currentLevel = levels[arity].get();
 
     std::vector<LatticeVertex *> currentLevelVertices;
-    for (const auto&[map_key, vertex] : currentLevel->getVertices()) {
+    for (const auto& [map_key, vertex] : currentLevel->getVertices()) {
         currentLevelVertices.push_back(vertex.get());
     }
-
 
     std::sort(currentLevelVertices.begin(), currentLevelVertices.end(), LatticeVertex::comparator);
     LatticeLevel nextLevel(arity + 1);
 
+    for (unsigned int vertexIndex1 = 0; vertexIndex1 < currentLevelVertices.size(); vertexIndex1++){
+        LatticeVertex* vertex1 = currentLevelVertices[vertexIndex1];
 
-    for (unsigned int vertexIndex1 = 0; vertexIndex1 < currentLevelVertices.size(); vertexIndex1++) {
-        LatticeVertex *vertex1 = currentLevelVertices[vertexIndex1];
         if (vertex1->getRhsCandidates().none() && !vertex1->getIsKeyCandidate()) {
             continue;
         }
 
-        for (unsigned int vertexIndex2 = vertexIndex1 + 1; vertexIndex2 < currentLevelVertices.size(); vertexIndex2++) {
-            LatticeVertex *vertex2 = currentLevelVertices[vertexIndex2];
+        for (unsigned int vertexIndex2 = vertexIndex1 + 1; vertexIndex2 < currentLevelVertices.size(); vertexIndex2++){
+            LatticeVertex* vertex2 = currentLevelVertices[vertexIndex2];
 
-            if (!vertex1->comesBeforeAndSharePrefixWith(*vertex2)) {
+            if (!vertex1->comesBeforeAndSharePrefixWith(*vertex2)){
                 break;
             }
 
-            if (!vertex1->getRhsCandidates().intersects(vertex1->getRhsCandidates()) && !vertex2->getIsKeyCandidate()) {
+            if (!vertex1->getRhsCandidates().intersects(vertex1->getRhsCandidates()) && !vertex2->getIsKeyCandidate()){
                 continue;
             }
 
@@ -66,16 +66,15 @@ void LatticeLevel::generateNextLevel(std::vector<std::unique_ptr<LatticeLevel>>&
             childVertex->setKeyCandidate(vertex1->getIsKeyCandidate() && vertex2->getIsKeyCandidate());
             childVertex->setInvalid(vertex1->getIsInvalid() || vertex2->getIsInvalid());
 
-            for (unsigned int i = 0, skipIndex = parentIndices.find_first();
-                 i < arity - 1; i++, skipIndex = parentIndices.find_next(skipIndex)) {
+            for (unsigned int i = 0, skipIndex = parentIndices.find_first(); i < arity - 1; i++, skipIndex = parentIndices.find_next(skipIndex)) {
                 parentIndices[skipIndex] = false;
-                LatticeVertex const *parentVertex = currentLevel->getLatticeVertex(parentIndices);
+                LatticeVertex const* parentVertex = currentLevel->getLatticeVertex(parentIndices);
 
-                if (parentVertex == nullptr) {
+                if (parentVertex == nullptr){
                     goto continueMidOuter;
                 }
                 childVertex->getRhsCandidates() &= parentVertex->getConstRhsCandidates();
-                if (childVertex->getRhsCandidates().none()) {
+                if (childVertex->getRhsCandidates().none()){
                     goto continueMidOuter;
                 }
                 childVertex->getParents().push_back(parentVertex);
@@ -83,6 +82,10 @@ void LatticeLevel::generateNextLevel(std::vector<std::unique_ptr<LatticeLevel>>&
 
                 childVertex->setKeyCandidate(childVertex->getIsKeyCandidate() && parentVertex->getIsKeyCandidate());
                 childVertex->setInvalid(childVertex->getIsInvalid() || parentVertex->getIsInvalid());
+
+                if (!childVertex->getIsKeyCandidate() && childVertex->getRhsCandidates().none()){
+                    goto continueMidOuter;
+                }
             }
 
             childVertex->getParents().push_back(vertex1);
@@ -93,21 +96,22 @@ void LatticeLevel::generateNextLevel(std::vector<std::unique_ptr<LatticeLevel>>&
             continueMidOuter:
             continue;
         }
-
-        levels.push_back(std::make_unique<LatticeLevel>(nextLevel));
     }
+
+    levels.push_back(std::make_unique<LatticeLevel>(nextLevel));
 }
 
 void LatticeLevel::clearLevelsBelow(std::vector<std::unique_ptr<LatticeLevel>>& levels, unsigned int arity) {
     // Clear the levels from the level list
     auto it = levels.begin();
-    for (unsigned int i = 0; i < std::min((unsigned int) levels.size(), arity); i++) {
+
+    for (unsigned int i = 0; i < std::min((unsigned int)levels.size(), arity); i++) {
         (*(it++))->getVertices().clear();
     }
 
     //Clear child references
     if (arity < levels.size()) {
-        for (auto&[map_key, retainedVertex] : levels[arity]->getVertices()) {
+        for (auto& [map_key, retainedVertex] : levels[arity]->getVertices()){
             retainedVertex->getParents().clear();
         }
     }
