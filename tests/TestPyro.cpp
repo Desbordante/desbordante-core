c1a8acbe48904695ac1541def384de29239d83b7
Maxim Strutovsky <strutovsky.m.a@gmail.com>
2021-04-01 13:31:19 +0300

    Upgraded verification system

--- tests/TestPyro.cpp
+++ tests/TestPyro.cpp
@@ -5,24 +5,26 @@
 #include "gtest/gtest.h"
 
 #include "algorithms/Pyro.h"
+#include "Datasets.h"
 
 using ::testing::ContainerEq, ::testing::Eq;
 using std::vector, std::string, std::cout, std::endl;
 
-TEST(PyroTester, heavyDatasetsHash) {
-    vector<unsigned int> hashes = {20873, 58641, 32696, 19617, 24082, 55285, 34874, 62210};
-    vector<string> datasets = {"adult.csv", "breast_cancer.csv", "CIPublicHighway.csv", "EpicMeds.csv",
-                               "EpicVitals.csv", "iowa1kk.csv", "LegacyPayors.csv", "neighbors100k.csv"};
-    vector<char> separators = {';', ',', ',', '|', '|', ',', '|', ','};
-    vector<bool> hasHeader = {false, true, true, true, true, true, true, true};
+
+
+class PyroTest : public LightDatasets, public HeavyDatasets, public ::testing::Test {};
+
+TEST_F(PyroTest, ReturnsSameHashOnHeavyDatasets) {
 
     auto path = fs::current_path().append("inputData");
 
     try {
-        for (size_t i = 0; i < datasets.size(); i++) {
-            auto pyro = Pyro(path / datasets[i], separators[i], hasHeader[i], 0, 0, -1);
+        for (size_t i = 0; i < HeavyDatasets::datasetQuantity(); i++) {
+            auto pyro = Pyro(path / HeavyDatasets::dataset(i), HeavyDatasets::separator(i),
+                             HeavyDatasets::hasHeader(i), 0, 0, -1);
             pyro.execute();
-            ASSERT_EQ(pyro.FDAlgorithm::fletcher16(), hashes[i]) << "Pyro result hash changed at " << datasets[i];
+            EXPECT_EQ(pyro.FDAlgorithm::fletcher16(), HeavyDatasets::hash(i))
+                << "Pyro result hash changed for " << HeavyDatasets::dataset(i);
         }
     }
     catch (std::runtime_error& e) {
