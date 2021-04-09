2bcb4c44a080e5fd3b359079d1f3d22e7760c797
Grismat <smirnalexandr@rambler.ru>
2021-04-09 18:38:28 +0300

    added random walk method

--- src/algorithms/DFD.cpp
+++ src/algorithms/DFD.cpp
@@ -4,6 +4,9 @@
 
 #include "DFD.h"
 
+#include <algorithm>
+#include <random>
+
 #include "../model/ColumnLayoutRelationData.h"
 #include "../model/RelationalSchema.h"
 #include "../util/PositionListIndex.h"
@@ -24,7 +27,7 @@ unsigned long long DFD::execute() {
         //if current column is unique
         if (columnPLI->getNumNonSingletonCluster() == 0) {
             possibleRHSs.erase(columnIter);
-            auto lhs = Vertical(**columnIter);
+            auto lhs = Vertical(**columnIter);//наверно стоит убрать, потому что при первом запуске же будет мув
 
             for (auto rhs : possibleRHSs) {
                 this->registerFD(lhs, *rhs); //TODO ptrs???
@@ -32,4 +35,16 @@ unsigned long long DFD::execute() {
         }
     }
 
+    //second loop of DFD
+    for (auto rhs : possibleRHSs) {
+        //тут строим новую решетку, соответственно нужно обнулить/завести некоторые структуры данных
+        findLHSs(rhs, schema);
+    }
+}
+
+shared_ptr<Vertical> DFD::randomWalk(std::list<shared_ptr<Vertical>> const& verticalList) {
+    std::uniform_int_distribution<> dis(0, std::distance(verticalList.begin(), verticalList.end()) - 1);
+    auto iterator = verticalList.begin();
+    std::advance(iterator, dis(this->gen));
+    return *iterator;
 }
--- src/algorithms/DFD.h
+++ src/algorithms/DFD.h
@@ -4,14 +4,20 @@
 
 #pragma once
 
+#include <random>
+
 #include "FDAlgorithm.h"
 
 class DFD : public FDAlgorithm {
 private:
-    //TODO
+    std::random_device rd; // для генерации случайных чисел
+    std::mt19937 gen;
+
+    void findLHSs(shared_ptr<Column> rhs, shared_ptr<RelationalSchema> schema); //TODO: нужен ли второй параметр?; мб переименовать типа findDeps
+    shared_ptr<Vertical> randomWalk(std::list<shared_ptr<Vertical>> const& verticalList);
 public:
     explicit DFD(std::filesystem::path const& path, char separator = ',', bool hasHeader = true)
-        : FDAlgorithm(path, separator, hasHeader) {}
+        : FDAlgorithm(path, separator, hasHeader), gen(rd()) {}
 
     unsigned long long execute() override;
 };
