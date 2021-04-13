a6e45c5d4fcd91d138e0c310882e3e3990f4b9c3
Grismat <smirnalexandr@rambler.ru>
2021-04-13 22:12:54 +0300

    "latest version without hashtable"

--- src/algorithms/DFD.cpp
+++ src/algorithms/DFD.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <random>
 
+#include "LatticeNode.h"
 #include "../model/ColumnLayoutRelationData.h"
 #include "../model/RelationalSchema.h"
 #include "../util/PositionListIndex.h"
@@ -42,9 +43,56 @@ unsigned long long DFD::execute() {
     }
 }
 
-shared_ptr<Vertical> DFD::randomWalk(std::list<shared_ptr<Vertical>> const& verticalList) {
-    std::uniform_int_distribution<> dis(0, std::distance(verticalList.begin(), verticalList.end()) - 1);
-    auto iterator = verticalList.begin();
+shared_ptr<LatticeNode> DFD::takeRandom(std::list<shared_ptr<LatticeNode>> const& nodeList) {
+    std::uniform_int_distribution<> dis(0, std::distance(nodeList.begin(), nodeList.end()) - 1);
+    auto iterator = nodeList.begin();
     std::advance(iterator, dis(this->gen));
     return *iterator;
+}
+
+void DFD::findLHSs(shared_ptr<Column> rhs, shared_ptr<RelationalSchema> schema) {
+    /*std::list<shared_ptr<Vertical>> seeds;
+
+    //initialize seeds nodes
+    for (auto column : schema->getColumns()) {
+        //add to seeds all columns except rhs
+        if (column->getIndex() != rhs->getIndex())
+            seeds.emplace_back(std::make_shared<Vertical>(*column)); //TO DO проверить
+    }*/
+
+    std::vector<shared_ptr<LatticeNode>> minimalDeps; //TODO мб их определять либо в функции execute, либо полями класса
+    std::vector<shared_ptr<LatticeNode>> maximalNonDeps;
+
+    std::list<shared_ptr<LatticeNode>> seeds; //TODO лист или вектор?
+
+    for (auto column : schema->getColumns()) {
+        //add to seeds all columns except rhs
+        if (column->getIndex() != rhs->getIndex()) {
+            seeds.emplace_back(std::make_shared<LatticeNode>(*column)); //TODO проверить
+        }
+    }
+
+    while (!seeds.empty()) {
+        shared_ptr<LatticeNode> node = takeRandom(seeds);
+        do {
+            if (node->isVisited() && node->isCandidate()) {
+                if (node->isMinimalDependency()) {
+                    minimalDeps.push_back(node);
+                } else if (node->isMaximalNonDependency()) {
+                    maximalNonDeps.push_back(node);
+                }
+                node.updateDependencyType();
+            } else {
+                if (!inferCategory(node)) {
+                    computePartitions(node);
+                }
+            }
+
+            node = pickNextNode();
+        } while (node != nullptr);
+    }
+}
+
+shared_ptr<LatticeNode> pickNextNode(shared_ptr<LatticeNode> node) {
+
 }
--- src/algorithms/DFD.h
+++ src/algorithms/DFD.h
@@ -7,14 +7,25 @@
 #include <random>
 
 #include "FDAlgorithm.h"
+#include "LatticeNode.h"
 
 class DFD : public FDAlgorithm {
 private:
+    /*enum class NodeCategory {
+        dependency,
+        minimalDependency,
+        candidateMinimalDependency,
+        nonDependency,
+        maximalNonDependency,
+        candidateMaximalNonDependency
+    };*/
+
     std::random_device rd; // для генерации случайных чисел
     std::mt19937 gen;
 
     void findLHSs(shared_ptr<Column> rhs, shared_ptr<RelationalSchema> schema); //TODO: нужен ли второй параметр?; мб переименовать типа findDeps
-    shared_ptr<Vertical> randomWalk(std::list<shared_ptr<Vertical>> const& verticalList);
+    shared_ptr<LatticeNode> takeRandom(std::list<shared_ptr<LatticeNode>> const& nodeList);
+
 public:
     explicit DFD(std::filesystem::path const& path, char separator = ',', bool hasHeader = true)
         : FDAlgorithm(path, separator, hasHeader), gen(rd()) {}
