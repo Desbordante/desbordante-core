9e7a9e32c95568741b9371e74fde07f0fad91b27
Grismat <smirnalexandr@rambler.ru>
2021-04-20 17:55:11 +0300

    LatticeObservations class updated

--- src/util/LatticeObservations.h
+++ src/util/LatticeObservations.h
@@ -7,6 +7,16 @@
 #include <unordered_map>
 #include "Vertical.h"
 
+
+namespace std {
+    template<>
+    struct hash<Vertical> {
+        size_t operator()(Vertical const &k) const {
+            return k.getColumnIndices().to_ulong();
+        }
+    };
+}
+
 enum class NodeCategory {
         dependency,
         minimalDependency,
@@ -19,10 +29,18 @@ enum class NodeCategory {
 class LatticeObservations : public std::unordered_map<Vertical, NodeCategory> {
 public:
 
+    //TODO ctors
+
     bool isCandidate(shared_ptr<Vertical> const& vertical);
     bool isVisited(shared_ptr<Vertical> const& vertical) { return this->find(*vertical) != this->end();} //нужна ли?
 
     NodeCategory updateDependencyCategory(shared_ptr<Vertical>  const& vertical);
     NodeCategory updateNonDependencyCategory(shared_ptr<Vertical>  const& vertical);
 
+    bool inferCategory(shared_ptr<Vertical> const& node);
+
+    bool getCategory(shared_ptr<Vertical> node); //TODO чтобы не возиться с end
+
 };
+
+
