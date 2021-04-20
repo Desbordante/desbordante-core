4acad5b4fddde1d46ee3545bee1f3a97138888a0
Grismat <smirnalexandr@rambler.ru>
2021-04-20 17:53:51 +0300

    first implementation of NonDependenciesMap class

---
+++ src/algorithms/DFD/PruningSets/NonDependenciesMap.cpp
@@ -0,0 +1,54 @@
+//
+// Created by alex on 20.04.2021.
+//
+
+#include "NonDependenciesMap.h"
+
+NonDependenciesMap::NonDependenciesMap(shared_ptr<RelationalSchema> schema) {
+    for (auto const& column : schema->getColumns()) {
+        this->insert(std::make_pair(Vertical(*column), std::unordered_set<shared_ptr<Vertical>>()));
+    }
+}
+
+void NonDependenciesMap::addNewNonDependency(shared_ptr<Vertical> node) {
+    using std::unordered_set;
+
+    for (auto const& column : node->getColumns()) {
+        unordered_set<shared_ptr<Vertical>>& verticalSet = this->find(Vertical(*column))->second;
+        for (auto const& vertical : verticalSet) {
+            if (node->contains(*vertical)) {
+                verticalSet.erase(vertical);    //удаляем подмножества
+            }
+        }
+        verticalSet.insert(node);
+    }
+}
+
+vector<shared_ptr<Vertical>>
+NonDependenciesMap::getUncheckedSupersets(shared_ptr<Vertical> node, LatticeObservations const& observations) {
+    std::vector<shared_ptr<Vertical>> uncheckedSubsets;
+
+    for (auto& subsetNode : node->getParents()) {
+        if (observations.find(*subsetNode) == observations.end() &&
+            !canBePruned(*subsetNode)
+        ) {
+            uncheckedSubsets.push_back(std::move(subsetNode));
+        }
+    }
+
+    return uncheckedSubsets;
+}
+
+bool NonDependenciesMap::canBePruned(const Vertical &node) {
+    using std::unordered_set;
+
+    for (auto const& column : node.getColumns()) {
+        unordered_set<shared_ptr<Vertical>>& verticalContainingColumnSet = this->find(Vertical(*column))->second;
+        for (auto const& vertical : verticalContainingColumnSet) {
+            if (vertical->contains(node)) { //TODO contains проверяет на равенство?
+                return true;
+            }
+        }
+    }
+    return false;
+}
