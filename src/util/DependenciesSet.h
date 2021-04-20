9f6edde251464660ff4809fa1bf9a827cbcf01cf
Grismat <smirnalexandr@rambler.ru>
2021-04-20 11:55:00 +0300

    first implementation of DependenciesSet class

---
+++ src/util/DependenciesSet.h
@@ -0,0 +1,38 @@
+//
+// Created by alexandrsmirn
+//
+
+#pragma once
+
+#include <unordered_map>
+#include <unordered_set>
+
+#include "Vertical.h"
+#include "LatticeObservations.h"
+
+/*namespace std {
+    template<>
+    struct hash<Vertical> {
+        size_t operator()(Vertical const &k) const {
+            return k.getColumnIndices().to_ulong();
+        }
+    };
+}*/
+
+namespace std {
+    template<>
+    struct hash<shared_ptr<Vertical>> {
+        size_t operator()(shared_ptr<Vertical> const &k) const {
+            return k->getColumnIndices().to_ulong();
+        }
+    };
+}
+
+class DependenciesSet : public std::unordered_map<Vertical, std::unordered_set<shared_ptr<Vertical>>> { //TODO указателями или просто объектами?
+public:
+    DependenciesSet(shared_ptr<RelationalSchema> schema);
+
+    void addNewDependency(shared_ptr<Vertical> node);
+    std::vector<shared_ptr<Vertical>> getUncheckedSubsets(shared_ptr<Vertical> node, LatticeObservations const& observations);
+    bool canBePruned(Vertical const& node);
+};
