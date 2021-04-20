8e4c0f100eea09dbf52b7f44505dbaa8f97d1fd0
Grismat <smirnalexandr@rambler.ru>
2021-04-20 17:53:51 +0300

    first implementation of NonDependenciesMap class

---
+++ src/algorithms/DFD/PruningSets/NonDependenciesMap.h
@@ -0,0 +1,28 @@
+//
+// Created by alexandrsmirn
+//
+
+#pragma once
+
+#include "Vertical.h"
+#include "LatticeObservations.h"
+
+namespace std {
+    template<>
+    struct hash<shared_ptr<Vertical>> {
+        size_t operator()(shared_ptr<Vertical> const &k) const {
+            return k->getColumnIndices().to_ulong();
+        }
+    };
+}
+
+class NonDependenciesMap : public std::unordered_map<Vertical, std::unordered_set<shared_ptr<Vertical>>>{
+public:
+    explicit NonDependenciesMap(shared_ptr<RelationalSchema> schema);
+    NonDependenciesMap() = default;
+
+    void addNewNonDependency(shared_ptr<Vertical> node);
+    vector<shared_ptr<Vertical>> getUncheckedSupersets(shared_ptr<Vertical> node, LatticeObservations const& observations);
+    bool canBePruned(Vertical const& node);
+};
+
