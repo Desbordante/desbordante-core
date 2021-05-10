f8e9929561100b8ddd36dde1b573dcc33391918e
Michael Polyntsov <Arno9148@gmail.com>
2021-05-10 18:12:41 +0300

    Use DiffSet alias for Vertical to improve readability

--- src/algorithms/FastFDs.h
+++ src/algorithms/FastFDs.h
@@ -19,6 +19,7 @@ public:
     unsigned long long execute() override;
 private:
     using OrderingComparator = std::function<bool (Column const&, Column const&)>;
+    using DiffSet = Vertical;
 
     // Computes all difference sets of `relation_` by complementing agree sets
     void genDiffSets();
@@ -26,26 +27,26 @@ private:
     /* Computes minimal difference sets
      * of `relation_` modulo `col`
      */
-    std::vector<Vertical> getDiffSetsMod(Column const& col) const;
+    std::vector<DiffSet> getDiffSetsMod(Column const& col) const;
     /* Returns initial ordering,
      * the total ordering of { schema_->getColumns() \ `attribute` } according to `diff_sets`
      */
-    std::set<Column, OrderingComparator> getInitOrdering(std::vector<Vertical> const& diff_sets,
+    std::set<Column, OrderingComparator> getInitOrdering(std::vector<DiffSet> const& diff_sets,
                                                          Column const& attribute) const;
     /* Returns next ordering,
      * the total ordering of { B in schema_->getColumns() | B > `attribute` (in `cur_ordering`) }
      * according to `diff_sets`
      */
-    std::set<Column, OrderingComparator> getNextOrdering(std::vector<Vertical> const& diff_sets,
+    std::set<Column, OrderingComparator> getNextOrdering(std::vector<DiffSet> const& diff_sets,
                                                          Column const& attribute,
                                                          std::set<Column, OrderingComparator> const& cur_ordering) const;
-    void findCovers(Column const& attribute, std::vector<Vertical> const& diff_sets_mod,
-                    std::vector<Vertical> const& cur_diff_sets, Vertical const& path,
+    void findCovers(Column const& attribute, std::vector<DiffSet> const& diff_sets_mod,
+                    std::vector<DiffSet> const& cur_diff_sets, Vertical const& path,
                     std::set<Column, OrderingComparator> const& ordering);
     /* Returns true if `cover` is the minimal cover of `diff_sets_mod`,
      * false otherwise
      */
-    bool coverMinimal(Vertical const& cover, std::vector<Vertical> const& diff_sets_mod) const;
+    bool coverMinimal(Vertical const& cover, std::vector<DiffSet> const& diff_sets_mod) const;
     /* Returns true if `candidate` covers `sets`,
      * false otherwise
      */
@@ -57,12 +58,12 @@ private:
      * `l_col` and `r_col` cover the same number of sets but
      * `l_col` index less than `r_col` index
      */
-    bool orderingComp(std::vector<Vertical> const& diff_sets,
+    bool orderingComp(std::vector<DiffSet> const& diff_sets,
                       Column const& l_col, Column const& r_col) const;
     bool columnContainsOnlyEqualValues(Column const& column) const;
 
     std::unique_ptr<ColumnLayoutRelationData> relation_;
     RelationalSchema const* schema_;
-    std::vector<Vertical> diff_sets_;
+    std::vector<DiffSet> diff_sets_;
 };
 
