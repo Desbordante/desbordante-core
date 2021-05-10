54937b446abbc52aeafdab18a427685822e8d572
Michael Polyntsov <Arno9148@gmail.com>
2021-05-11 00:43:39 +0300

    Add IdentifierSet and AgreeSetFactory tests to TestUtil.cpp

--- tests/TestUtil.cpp
+++ tests/TestUtil.cpp
@@ -5,6 +5,8 @@
 
 #include "ColumnLayoutRelationData.h"
 #include "ListAgreeSetSample.h"
+#include "IdentifierSet.h"
+#include "AgreeSetFactory.h"
 
 using ::testing::ContainerEq, ::testing::Eq;
 using std::deque, std::vector, std::cout, std::endl, std::unique_ptr;
@@ -94,4 +96,131 @@ TEST(testingBitsetToLonglong, first){
     ASSERT_EQ(res_vector.size(), 1);
     for (auto long_long_repr : res_vector)
         ASSERT_EQ(encoded_num, long_long_repr);
+}
+
+TEST(IdentifierSetTest, Computation) {
+    std::set<std::string> id_sets;
+    std::set<std::string> id_sets_ans = {
+        "[(A, 0), (B, 1), (C, 1), (D, 1), (E, 1), (F, 1)]",
+        "[(A, 1), (B, 1), (C, 1), (D, 2), (E, 0), (F, 1)]",
+        "[(A, 1), (B, 1), (C, 2), (D, 1), (E, 1), (F, 0)]",
+        "[(A, 1), (B, 2), (C, 2), (D, 1), (E, 1), (F, 1)]",
+        "[(A, 1), (B, 2), (C, 1), (D, 2), (E, 1), (F, 1)]",
+        "[(A, 1), (B, 2), (C, 2), (D, 2), (E, 1), (F, 1)]"
+    };
+
+    try {
+        auto path = fs::current_path().append("inputData").append("BernoulliRelation.csv");
+        CSVParser parser(path);
+        auto relation = ColumnLayoutRelationData::createFrom(parser, false);
+
+        for (unsigned i = 0; i < relation->getNumRows(); ++i) {
+            id_sets.insert(IdentifierSet(relation.get(), i).toString());
+        }
+    }
+    catch (std::runtime_error const& e) {
+        cout << "Exception raised in test: " << e.what() << endl;
+        FAIL();
+    }
+    ASSERT_THAT(id_sets_ans, ContainerEq(id_sets));
+}
+
+
+TEST(IdentifierSetTest, Intersection) {
+    std::set<std::string> intersection_actual; // id set intersection result
+    std::set<std::string> intersection_ans = {
+        "[A D F]",
+        "[A B]",
+        "[D E F]",
+        "[A E]",
+        "[C E F]",
+        "[E F]",
+        "[B C F]",
+        "[A B E F]",
+        "[A C D E]",
+        "[B D E]",
+        "[A B C E F]",
+        "[A F]",
+        "[A B D E F]",
+        "[A C D F]",
+        "[A C E]"
+    };
+
+    try {
+        auto path = fs::current_path().append("inputData").append("BernoulliRelation.csv");
+        CSVParser parser(path);
+        auto relation = ColumnLayoutRelationData::createFrom(parser, false);
+        std::vector<IdentifierSet> id_sets;
+
+        for (unsigned i = 0; i < relation->getNumRows(); ++i) {
+            id_sets.emplace_back(relation.get(), i);
+        }
+
+        auto back_it = std::prev(id_sets.end());
+        for (auto p = id_sets.begin(); p != back_it; ++p) {
+            for (auto q = std::next(p); q != id_sets.end(); ++q) {
+                intersection_actual.insert(p->intersect(*q).toString());
+            }
+        }
+    }
+    catch (std::runtime_error const& e) {
+        cout << "Exception raised in test: " << e.what() << endl;
+        FAIL();
+    }
+    ASSERT_THAT(intersection_ans, ContainerEq(intersection_actual));
+}
+
+
+template<AgreeSetsGenMethod method>
+void testAgreeSetFactory() {
+    std::set<std::string> agree_sets_actual; // id set intersection result
+    std::set<std::string> agree_sets_ans = {
+        "[A D F]",
+        "[A B]",
+        "[D E F]",
+        "[A E]",
+        "[C E F]",
+        "[E F]",
+        "[B C F]",
+        "[A B E F]",
+        "[]",
+        "[A C D E]",
+        "[B D E]",
+        "[A B C E F]",
+        "[A F]",
+        "[A B D E F]",
+        "[A C D F]",
+        "[A C E]"
+    };
+
+    try {
+        auto path = fs::current_path().append("inputData").append("BernoulliRelation.csv");
+        CSVParser parser(path);
+        auto relation = ColumnLayoutRelationData::createFrom(parser, false);
+        AgreeSetFactory factory(relation.get());
+        for (AgreeSet const& agree_set : factory.genAgreeSets<method>()) {
+            agree_sets_actual.insert(agree_set.toString());
+        }
+    }
+    catch (std::runtime_error const& e) {
+        cout << "Exception raised in test: " << e.what() << endl;
+        FAIL();
+    }
+    ASSERT_THAT(agree_sets_ans, ContainerEq(agree_sets_actual));
+}
+
+TEST(AgreeSetFactoryTest, UsingVectorOfIDSets) {
+    testAgreeSetFactory<AgreeSetsGenMethod::kUsingVectorOfIDSets>();
+}
+
+TEST(AgreeSetFactoryTest, UsingMapOfIDSets) {
+    testAgreeSetFactory<AgreeSetsGenMethod::kUsingMapOfIDSets>();
+}
+
+TEST(AgreeSetFactoryTest, UsingGetAgreeSet) {
+    testAgreeSetFactory<AgreeSetsGenMethod::kUsingGetAgreeSet>();
+}
+
+TEST(AgreeSetFactoryTest, UsingMCAndGetAgreeSet) {
+    testAgreeSetFactory<AgreeSetsGenMethod::kUsingMCAndGetAgreeSet>();
 }
