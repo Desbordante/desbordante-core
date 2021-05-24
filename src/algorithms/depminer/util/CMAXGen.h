ad6c359c146ddffab43b01a05552e9224f49fd0a
eduardG <almart_mail@mail.ru>
2021-05-24 21:13:57 +0300

    Depminer Fixed

--- src/algorithms/depminer/util/CMAXGen.h
+++ src/algorithms/depminer/util/CMAXGen.h
@@ -17,15 +17,18 @@ class CMAXGen{
 private:
     std::set<CMAXSet> cmaxSets;
     std::set<MAXSet> maxSets;
-    std::shared_ptr<RelationalSchema> schema;
+    const RelationalSchema* schema;
     void MaxSetsGenerate(std::set<Vertical> agreeSets);
     void CMaxSetsGenerate();
 public:
-    CMAXGen(std::shared_ptr<RelationalSchema> schema) : schema(schema){};
+    CMAXGen(const RelationalSchema* schema) : schema(schema){};
     CMAXGen() = default;
     ~CMAXGen() = default;
     void execute(std::set<Vertical> agreeSets);
     std::set<CMAXSet> getCmaxSets(){
         return this->cmaxSets;
+    }
+    std::set<MAXSet> getMaxSets(){
+        return this->maxSets;
     }
 };
