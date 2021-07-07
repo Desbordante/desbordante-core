282b328f938c861cbca6a5316cff8ad0357ad5ad
Ilia Shuckin <theilia509@gmail.com>
2021-07-07 19:06:42 +0300

    remove unstipped pli

--- src/model/ColumnLayoutRelationData.cpp
+++ src/model/ColumnLayoutRelationData.cpp
@@ -9,7 +9,6 @@
 
 #include "ColumnLayoutRelationData.h"
 #include "easylogging++.h"
-#include "UnstrippedPositionListIndex.h"
 
 
 ColumnLayoutRelationData::ColumnLayoutRelationData(std::unique_ptr<RelationalSchema> schema,
@@ -99,72 +98,6 @@ std::unique_ptr<ColumnLayoutRelationData> ColumnLayoutRelationData::createFrom(
         auto column = Column(schema.get(), fileInput.getColumnName(i), i);
         schema->appendColumn(std::move(column));
         auto pli = PositionListIndex::createFor(columnVectors[i], schema->isNullEqualNull());
-        columnData.emplace_back(schema->getColumn(i), std::move(pli));
-    }
-
-    schema->init();
-
-    return std::make_unique<ColumnLayoutRelationData>(std::move(schema), std::move(columnData));
-}
-
-std::unique_ptr<ColumnLayoutRelationData> ColumnLayoutRelationData::createUnstrippedFrom(CSVParser &fileInput, bool isNullEqNull) {
-    return createUnstrippedFrom(fileInput, isNullEqNull, -1, -1);
-}
-
-std::unique_ptr<ColumnLayoutRelationData> ColumnLayoutRelationData::createUnstrippedFrom(
-        CSVParser &fileInput, bool isNullEqNull, int maxCols, long maxRows) {
-    auto schema = std::make_unique<RelationalSchema>(fileInput.getRelationName(), isNullEqNull);
-    std::unordered_map<std::string, int> valueDictionary;
-    int nextValueId = 1;
-    const int nullValueId = -1;
-    int numColumns = fileInput.getNumberOfColumns();
-    if (maxCols > 0) numColumns = std::min(numColumns, maxCols);
-    std::vector<std::vector<int>> columnVectors = std::vector<std::vector<int>>(numColumns);
-    int rowNum = 0;
-    std::vector<std::string> row;
-
-    while (fileInput.getHasNext()){
-        row = fileInput.parseNext();
-
-        if (row.empty() && numColumns == 1) {
-            row.emplace_back("");
-        } else if ((int)row.size() != numColumns) {
-            LOG(WARNING) << "Skipping incomplete rows";
-            continue;
-        }
-
-        if (maxRows <= 0 || rowNum < maxRows){
-            int index = 0;
-            for (std::string& field : row){
-                if (field.empty()){
-                    columnVectors[index].push_back(nullValueId);
-                } else {
-                    auto location = valueDictionary.find(field);
-                    int valueId;
-                    if (location == valueDictionary.end()){
-                        valueDictionary[field] = nextValueId;
-                        valueId = nextValueId;
-                        nextValueId++;
-                    } else {
-                        valueId = location->second;
-                    }
-                    columnVectors[index].push_back(valueId);
-                }
-                index++;
-                if (index >= numColumns) break;
-            }
-        } else {
-            //TODO: Подумать что тут сделать
-            assert(0);
-        }
-        rowNum++;
-    }
-
-    std::vector<ColumnData> columnData;
-    for (int i = 0; i < numColumns; ++i) {
-        auto column = Column(schema.get(), fileInput.getColumnName(i), i);
-        schema->appendColumn(std::move(column));
-        auto pli = UnstrippedPositionListIndex::createUnstrippedFor(columnVectors[i], schema->isNullEqualNull());
         columnData.emplace_back(schema->getColumn(i), std::move(pli));
     }
 
