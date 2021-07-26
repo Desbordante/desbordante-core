a054023f5245964df6b4313ed4708195c49d5649
Kirill-Stupakov <kirill.stupakov.0@gmail.com>
2021-07-26 22:34:22 +0400

    Added allowed file formats list

--- web-app/server/src/routes/algsInfo.js
+++ web-app/server/src/routes/algsInfo.js
@@ -3,11 +3,12 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
 
+    const allowedFileFormats = ["text/csv", "application/vnd.ms-excel"]
     const allowedAlgorithms = ["FastFDs", "Pyro", "TaneX"];
     const allowedSeparators = [",", "\\t", "\\n"];
     const maxFileSize = 50000000;
    
-    res.send(JSON.stringify({ allowedAlgorithms, allowedSeparators, maxFileSize }));
+    res.send(JSON.stringify({ allowedFileFormats, allowedAlgorithms, allowedSeparators, maxFileSize }));
 });
 
 module.exports = router;
