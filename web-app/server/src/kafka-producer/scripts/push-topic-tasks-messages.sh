eae3c0d65eafc303c226dc1da3ae18c799911973
vs9lh <zr9ihi@gmail.com>
2021-07-28 16:51:01 +0300

    Deleted unnecessary files

--- web-app/server/src/kafka-producer/scripts/push-topic-tasks-messages.sh
+++
@@ -1,3 +0,0 @@
-docker exec -it kafka /opt/bitnami/kafka/bin/kafka-console-producer.sh \
-    --broker-list localhost:9092 \
-    --topic tasks
--- web-app/server/src/kafka-producer/scripts/view-topic-tasks-messages.sh
+++
@@ -1,4 +0,0 @@
-docker exec -it kafka /opt/bitnami/kafka/bin/kafka-console-consumer.sh \
-  --bootstrap-server localhost:9092 \
-  --from-beginning \
-  --topic tasks
