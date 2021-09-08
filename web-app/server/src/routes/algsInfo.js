cc4505ea8b2cfc2bdb29bdfa7ee7d7a8c65f3599
vs9lh <zr9ihi@gmail.com>
2021-09-08 19:52:43 +0300

    Add: Add new information about algorithms

--- web-app/server/src/routes/algsInfo.js
+++ web-app/server/src/routes/algsInfo.js
@@ -9,8 +9,15 @@ router.get('/', function(req, res, next) {
     // TODO: Choose more suitable datasets
     const availableDatasets = ["BernoulliRelation.csv", "WDC_age.csv", "TestLong.csv", "TestWide.csv", "WDC_game.csv"];
     const maxFileSize = 50000000;
+    const algorithmsInfo = [
+        { name: "Pyro",     props: { errorThreshold: true,  maxLHS: true,   threads: true  } },
+        { name: "TaneX",    props: { errorThreshold: true,  maxLHS: true,   threads: false } },
+        { name: "FastFDs",  props: { errorThreshold: false, maxLHS: false,  threads: true  } },
+        { name: "FD mine",  props: { errorThreshold: false, maxLHS: false,  threads: false } },
+        { name: "DFD",      props: { errorThreshold: false, maxLHS: false,  threads: false } }
+     ]
    
-    res.send(JSON.stringify({ allowedFileFormats, allowedAlgorithms, allowedSeparators, availableDatasets, maxFileSize }));
+    res.send(JSON.stringify({ allowedFileFormats, allowedAlgorithms, algorithmsInfo, allowedSeparators, availableDatasets, maxFileSize }));
 });
 
 module.exports = router;
