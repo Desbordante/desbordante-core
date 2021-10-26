c0667f3489520483e57a14f5023b46af2b074c4b
skrinker <guzel28sit@gmail.com>
2021-10-26 10:39:58 +0300

    added csv function

--- web-app/client/src/components/Snippet/Snippet.tsx
+++ web-app/client/src/components/Snippet/Snippet.tsx
@@ -13,57 +13,60 @@ interface Props {
 function convertCSVToArray(inputData: string, delimiter: string) {
   const objPattern = new RegExp(
     "(\\" +
-      delimiter +
-      "|\\r?\\n|\\r|^)" +
-      "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
-      "([^\"\\" +
-      delimiter +
-      "\\r\\n]*))",
-    "gi",
+    delimiter +
+    "|\\r?\\n|\\r|^)" +
+    "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+    "([^\"\\" +
+    delimiter +
+    "\\r\\n]*))",
+    "gi"
   );
 
   let arrMatches: any;
   const arrData: string[][] = [[]];
-
-  while ((arrMatches = objPattern.exec(inputData))) {
+  arrMatches = objPattern.exec(inputData);
+  while (arrMatches !== null) {
     const strMatchedDelimiter = arrMatches[1];
-    if (
-      strMatchedDelimiter.length &&
-      strMatchedDelimiter !== delimiter
-    ) {
+    if (strMatchedDelimiter.length && strMatchedDelimiter !== delimiter) {
       arrData.push([]);
     }
 
     let strMatchedValue;
 
     if (arrMatches[2]) {
-      strMatchedValue = arrMatches[2].replace(new RegExp("\"\"", "g"), "\"");
+      strMatchedValue = arrMatches[2].replace(new RegExp('""', "g"), '"');
     } else {
       strMatchedValue = arrMatches[3];
     }
 
     arrData[arrData.length - 1].push(strMatchedValue);
+    arrMatches = objPattern.exec(inputData);
   }
 
   return arrData;
 }
 
 const Snippet: React.FC<Props> = ({ file }) => {
-  const [text, setText] = useState("");
+  // const [text, setText] = useState("");
 
   const [table, setTable] = useState<string[][]>();
 
   useEffect(() => {
     async function readFile() {
       const buffer = await file!.text();
-      setText(buffer);
-      setTable(convertCSVToArray(text, ","));
-      console.table(table);
+      // setText(buffer);
+      setTable(convertCSVToArray(buffer, ","));
+      console.log(table);
     }
     readFile();
   }, []);
 
-  return <div>{table}</div>;
+  {
+    table!.map((value) => {
+      return <td>{value}</td>;
+    });
+  }
+  return <div></div>;
 };
 
 export default Snippet;
