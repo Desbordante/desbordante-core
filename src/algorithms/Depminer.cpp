c78a91798e65e69b1b41ec7d5163c9ecdb72e27c
eduardG <almart_mail@mail.ru>
2021-11-11 12:00:09 +0300

    Moved CMAXSet generation to Depminer class

--- src/algorithms/Depminer.cpp
+++ src/algorithms/Depminer.cpp
@@ -30,9 +30,7 @@ unsigned long long Depminer::execute(){
     std::unordered_set<Vertical> agreeSets = agreeSetFactory.genAgreeSets();
 
     //maximal sets
-    CMAXGen cmaxSets = CMAXGen(schema);
-    cmaxSets.execute(agreeSets);
-    std::vector<CMAXSet> cmaxVec = cmaxSets.getCmaxSets();
+    std::vector<CMAXSet> cmaxVec = generateCMAXSets(agreeSets);
     
     //LHS
     auto lhsTime = std::chrono::system_clock::now();
@@ -49,6 +47,63 @@ unsigned long long Depminer::execute(){
     std::chrono::milliseconds elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startTime);
     return elapsed_milliseconds.count();
 }
+
+std::vector<CMAXSet> Depminer::generateCMAXSets(std::unordered_set<Vertical>& agreeSets){
+    auto startTime = std::chrono::system_clock::now();
+
+    std::vector<CMAXSet> cmaxSets;
+
+    for(auto& column : this->schema->getColumns()){
+        MAXSet result(*column);
+
+        // finding all sets, which doesn't contain column
+        for(Vertical ag : agreeSets){
+            if(!ag.contains(*column)){
+                result.addCombination(ag);
+            }
+        }
+
+        // finding max sets
+        std::unordered_set<Vertical> superSets;
+        std::unordered_set<Vertical> setsDelete;
+        bool toAdd = true;
+
+        for(Vertical set : result.getCombinations()){
+            for(Vertical superSet : superSets){
+                if(set.contains(superSet)){
+                    setsDelete.insert(superSet);
+                }
+                if(toAdd){
+                    toAdd = !superSet.contains(set);
+                }
+            }
+            for(auto toDelete : setsDelete){
+                superSets.erase(toDelete);
+            }
+            if(toAdd){
+                superSets.insert(set);
+            }else{
+                toAdd = true;
+            }
+            setsDelete.clear();
+        }
+        
+        // Inverting MaxSet
+        std::unordered_set<Vertical> resultSuperSets;
+        for(Vertical const & combination : superSets){
+            resultSuperSets.insert(combination.invert());
+        }
+        result.makeNewCombinations(resultSuperSets);
+        cmaxSets.push_back(result);
+    }
+
+    auto elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startTime);
+    std::cout << "TIME TO GENERATE CMAXSETS: " << elapsed_milliseconds.count() << std::endl;
+    std::cout << "TOTAL CMAX SETS: " << cmaxSets.size() << "\n";
+
+    return std::move(cmaxSets);
+}
+
 
 void Depminer::lhsForColumn(const std::unique_ptr<Column>& column, std::vector<CMAXSet> const & cmaxSets){
         std::unordered_set<Vertical> li;
