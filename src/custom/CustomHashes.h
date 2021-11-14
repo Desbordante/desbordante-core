6bfecfaaf9f07cc8a5504bee4fee11ab278c8a2f
Mstrutov <strutovsky.m.a@gmail.com>
2021-11-14 17:25:50 +0300

    Implement safe vertical hashing appropriate for wide datasets
    The problem with the previous hashing method is that just using the to_ulong method on a bitset of size > 16 yields an overflow exception. This makes Desbordante inapplicable to wide datasets, as every algorithm uses hashing data structures.
    
    The solution is to implement an alternative hashing method, which takes into account only the first 16 bits of a bitset. Such an approach, however, possibly could slow down the hashing structures, therefore for now it is not the default option. One can activate it with an addition of `-DSAFE_VERTICAL_HASHING=ON` option to a CMake call. It is implemented as a compile time option, as a runtime implementation would probably have a heavier impact on the hashing performance.

--- src/custom/CustomHashes.h
+++ src/custom/CustomHashes.h
@@ -3,11 +3,48 @@
 #include "RelationalSchema.h"
 #include "Vertical.h"
 
+class CustomHashing {
+private:
+    enum class BitsetHashingMethod {
+        kTryConvertToUlong,
+        kTrimAndConvertToUlong
+    };
+
+    static constexpr BitsetHashingMethod defaultHashingMethod =
+#ifdef SAFE_VERTICAL_HASHING
+        BitsetHashingMethod::kTrimAndConvertToUlong;
+#else
+        BitsetHashingMethod::kTryConvertToUlong;
+#endif
+
+    template <auto bitsetHashingMethod = defaultHashingMethod>
+    static size_t bitset_hash(boost::dynamic_bitset<> const& bitset);
+
+    friend std::hash<Vertical>;
+    friend std::hash<Column>;
+};
+
+template <>
+inline size_t CustomHashing::bitset_hash<CustomHashing::BitsetHashingMethod::kTryConvertToUlong>
+        (boost::dynamic_bitset<> const& bitset) {
+
+    return bitset.to_ulong();
+}
+
+template <>
+inline size_t CustomHashing::bitset_hash<CustomHashing::BitsetHashingMethod::kTrimAndConvertToUlong>
+        (boost::dynamic_bitset<> const& bitset) {
+
+    boost::dynamic_bitset<> copyBitset = bitset;
+    copyBitset.resize(std::numeric_limits<unsigned long>::digits);
+    return copyBitset.to_ulong();
+}
+
 namespace std {
     template<>
     struct hash<Vertical> {
         size_t operator()(Vertical const& k) const {
-            return k.getColumnIndicesRef().to_ulong();
+            return CustomHashing::bitset_hash(k.getColumnIndicesRef());
         }
     };
 
@@ -16,37 +53,35 @@ namespace std {
         size_t operator()(Column const& k) const {
             boost::dynamic_bitset<> columnIndex(k.getSchema()->getNumColumns());
             columnIndex.set(k.getIndex());
-            return columnIndex.to_ulong();
+            return CustomHashing::bitset_hash(columnIndex);
         }
     };
 
     template<>
     struct hash<std::shared_ptr<Vertical>> {
         size_t operator()(std::shared_ptr<Vertical> const& k) const {
-            return k->getColumnIndices().to_ulong();
+            return std::hash<Vertical>()(*k);
         }
     };
 
     template<>
     struct hash<std::shared_ptr<Column>> {
         size_t operator()(std::shared_ptr<Column> const& k) const {
-            boost::dynamic_bitset<> columnIndex(k->getSchema()->getNumColumns());
-            columnIndex.set(k->getIndex());
-            return columnIndex.to_ulong();
+            return std::hash<Column>()(*k);
         }
     };
 
     template<class T>
     struct hash<std::pair<Vertical, T>> {
         size_t operator()(std::pair<Vertical, T> const& k) const {
-            return k.first.getColumnIndices().to_ulong();
+            return std::hash<Vertical>()(k.first);
         }
     };
 
     template<class T>
     struct hash<std::pair<std::shared_ptr<Vertical>, T>> {
         size_t operator()(std::pair<std::shared_ptr<Vertical>, T> const& k) const {
-            return k.first->getColumnIndices().to_ulong();
+            return std::hash<Vertical>()(*k.first);
         }
     };
 }
