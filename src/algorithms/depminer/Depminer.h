35b008ba750e712606f533f61d7fe84b217ceb2d
eduardG <almart_mail@mail.ru>
2021-11-15 18:01:49 +0300

    Fixed Depminer.h and Depminer.cpp

--- src/algorithms/depminer/Depminer.h
+++ src/algorithms/depminer/Depminer.h
@@ -2,15 +2,15 @@
 
 #include "CSVParser.h"
 #include "FDAlgorithm.h"
-#include "depminer/MAXSet.h"
+#include "depminer/CMAXSet.h"
 
 class Depminer : public FDAlgorithm {
 private:
-    void lhsForColumn(const std::unique_ptr<Column>& column, std::vector<CMAXSet> const & cmaxSets);
-    CMAXSet genFirstLevel(std::vector<CMAXSet> const & cmaxSets, Column attribute, std::unordered_set<Vertical>& li);
-    std::unordered_set<Vertical> genNextLevel(std::unordered_set<Vertical> const& li);
-    bool checkJoin(Vertical const& _p, Vertical const& _q);
-    std::vector<CMAXSet> generateCMAXSets(std::unordered_set<Vertical>& agreeSets);
+    void lhsForColumn(std::unique_ptr<Column> const& column, std::vector<CMAXSet> const& cmaxSets);
+    CMAXSet genFirstLevel(std::vector<CMAXSet> const& cmaxSets, Column attribute, std::unordered_set<Vertical>& li) const;
+    std::unordered_set<Vertical> genNextLevel(std::unordered_set<Vertical> const& li) const;
+    static bool checkJoin(Vertical const& _p, Vertical const& _q);
+    std::vector<CMAXSet> generateCMAXSets(std::unordered_set<Vertical> const& agreeSets);
     double progressStep;
 public:
     
