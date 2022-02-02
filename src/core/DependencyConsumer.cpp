2dc0020c43650ebbd19967baff629217d61a1e0d
Michael Polyntsov <Arno9148@gmail.com>
2022-02-02 22:26:28 +0300

    Fix Pyro datarace errors
    One datarace was in SearchSpace::discover() with std::cout usage from
    several threads. Replaced std::cout with thread-safe LOG(). Registering
    mined uccs and fds from different threads also lead to the datarace.
    Synchronized registration to fix it.

--- src/core/DependencyConsumer.cpp
+++ src/core/DependencyConsumer.cpp
@@ -1,13 +1,15 @@
 
 #include "DependencyConsumer.h"
 
-PartialFD DependencyConsumer::registerFd(Vertical const& lhs, Column const& rhs, double error, double score) const {
+PartialFD DependencyConsumer::registerFd(Vertical const& lhs, Column const& rhs,
+                                         double error, double score) const {
     PartialFD partialFd(lhs, rhs, error, score);
     fdConsumer_(partialFd);
     return partialFd;
 }
 
-PartialKey DependencyConsumer::registerUcc(Vertical const& keyVertical, double error, double score) const {
+PartialKey DependencyConsumer::registerUcc(Vertical const& keyVertical,
+                                           double error, double score) const {
     PartialKey partialKey(keyVertical, error, score);
     uccConsumer_(partialKey);
     return partialKey;
