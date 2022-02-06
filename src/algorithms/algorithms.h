78c05cff9f22d56ff7adbc55cdc9253759bc6bbc
Michael Polyntsov <Arno9148@gmail.com>
2022-02-06 15:39:31 +0300

    Move algo instance creation logic to new module
    Separate module for algorithm instance creation helps to avoid code
    duplication between places where you need to create algorithm objects,
    e.g. between main() of cli desbordante app and backend of desbordante
    web-app. Also this approach is a lot more scalable in terms of adding
    new algorithms.

---
+++ src/algorithms/algorithms.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/* Functional dependency mining algorithms */
+#include "algorithms/depminer/Depminer.h"
+#include "algorithms/DFD/DFD.h"
+#include "algorithms/FastFDs.h"
+#include "algorithms/FDep/FDep.h"
+#include "algorithms/Fd_mine.h"
+#include "algorithms/Pyro.h"
+#include "algorithms/TaneX.h"
+
--- src/main.cpp
+++ src/main.cpp
@@ -9,28 +9,40 @@
 #include <boost/program_options.hpp>
 #include <easylogging++.h>
 
-#include "algorithms/Pyro.h"
-#include "algorithms/TaneX.h"
-#include "algorithms/DFD/DFD.h"
-#include "algorithms/FDep/FDep.h"
-#include "algorithms/Fd_mine.h"
-#include "algorithms/FastFDs.h"
-#include "algorithms/depminer/Depminer.h"
+#include "algo_factory.h"
 
 namespace po = boost::program_options;
 
 INITIALIZE_EASYLOGGINGPP
 
-bool CheckOptions(std::string const& alg, double error) {
-    if (alg != "pyro" && alg != "tane" && alg != "fastfds" && alg != "fdmine" && alg != "dfd" &&
-        alg != "depminer" && alg != "fdep") {
-        std::cout << "ERROR: no matching algorithm. Available algorithms are:\n\tpyro\n\ttane.\n"
-                  << std::endl;
+template<typename BetterEnumType>
+static std::string EnumToAvailableValues() {
+    std::stringstream avail_values;
+
+    avail_values << '[';
+
+    for (auto const& name : BetterEnumType::_names()) {
+        avail_values << name << '|';
+    }
+
+    avail_values.seekp(-1, avail_values.cur);
+    avail_values << ']';
+
+    return avail_values.str();
+}
+
+static bool CheckOptions(std::string const& alg, double error) {
+    if (!algos::Algo::_is_valid(alg.c_str())) {
+        std::cout << "ERROR: no matching algorithm."
+                     " Available algorithms are:\n" +
+                     EnumToAvailableValues<algos::Algo>() + '\n';
         return false;
     }
+
     if (error > 1 || error < 0) {
-        std::cout << "ERROR: error should be between 0 and 1.\n" << std::endl;
+        std::cout << "ERROR: error should be between 0 and 1.\n";
     }
+
     return true;
 }
 
@@ -44,29 +56,42 @@ int main(int argc, char const* argv[]) {
     unsigned int max_lhs = -1;
     unsigned int parallelism = 0;
 
+    std::string const algo_desc = "algorithm to use. Available algorithm:\n" +
+                                  EnumToAvailableValues<algos::Algo>() +
+                                  " for FD mining.";
+
     po::options_description desc("Allowed options");
-    desc.add_options()("help", "print help")("algo", po::value<std::string>(&alg),
-                                             "algorithm [pyro|tane|fastfds|fdmine|dfd|fdep]")(
-        "data", po::value<std::string>(&dataset), "path to CSV file, relative to ./inputData")(
-        "sep", po::value<char>(&separator), "CSV separator")(
-        "has_header", po::value<bool>(&has_header),
-        "CSV header presence flag [true|false]. Default true")(
-        "seed", po::value<int>(&seed), "RNG seed")("error", po::value<double>(&error),
-                                                   "error for AFD algorithms. Default 0.01")(
-        "maxLHS", po::value<unsigned int>(&max_lhs),
-        (std::string("max considered LHS size. Default: ") + std::to_string((unsigned int)-1))
-            .c_str())("threads", po::value<unsigned int>(&parallelism));
+    desc.add_options()
+        ("help", "print this message and exit")
+        ("mine", "type of dependency to mine")
+        ("algo", po::value<std::string>(&alg),
+         algo_desc.c_str())
+        ("data", po::value<std::string>(&dataset),
+         "path to CSV file, relative to ./inputData")
+        ("sep", po::value<char>(&separator)->default_value(separator),
+         "CSV separator")
+        ("hasHeader", po::value<bool>(&has_header)->default_value(has_header),
+         "CSV header presence flag [true|false]")
+        ("seed", po::value<int>(&seed), "RNG seed")
+        ("error", po::value<double>(&error)->default_value(error),
+         "error for AFD algorithms")
+        ("maxLHS", po::value<unsigned int>(&max_lhs)->default_value(max_lhs),
+         "max considered LHS size")
+        ("threads", po::value<unsigned int>(&parallelism),
+         "number of threads to use")
+        ;
 
     po::variables_map vm;
     try {
         po::store(po::parse_command_line(argc, argv, desc), vm);
         po::notify(vm);
-    } catch (po::error& e) {
+    } catch (po::error &e) {
         std::cout << e.what() << std::endl;
         return 0;
     }
 
-    if (vm.count("help")) {
+    if (vm.count("help"))
+    {
         std::cout << desc << std::endl;
         return 0;
     }
@@ -74,12 +99,13 @@ int main(int argc, char const* argv[]) {
     el::Loggers::configureFromGlobal("logging.conf");
 
     std::transform(alg.begin(), alg.end(), alg.begin(),
-                   [](unsigned char c) { return std::tolower(c); });
+                   [](unsigned char c){ return std::tolower(c); });
 
     if (!CheckOptions(alg, error)) {
         std::cout << desc << std::endl;
         return 1;
     }
+
     std::cout << "Input: algorithm \"" << alg
               << "\" with seed " << std::to_string(seed)
               << ", error \"" << std::to_string(error)
@@ -87,26 +113,13 @@ int main(int argc, char const* argv[]) {
               << "\" and dataset \"" << dataset
               << "\" with separator \'" << separator
               << "\'. Header is " << (has_header ? "" : "not ") << "present. " << std::endl;
-    auto path = std::filesystem::current_path() / "inputData" / dataset;
 
-    std::unique_ptr<FDAlgorithm> algorithm_instance;
-    if (alg == "pyro") {
-        algorithm_instance =
-            std::make_unique<Pyro>(path, separator, has_header, seed, error, max_lhs, parallelism);
-    } else if (alg == "tane") {
-        algorithm_instance = std::make_unique<Tane>(path, separator, has_header, error, max_lhs);
-    } else if (alg == "dfd") {
-        algorithm_instance = std::make_unique<DFD>(path, separator, has_header, parallelism);
-    } else if (alg == "fdmine") {
-        algorithm_instance = std::make_unique<Fd_mine>(path, separator, has_header);
-    } else if (alg == "fastfds") {
-        algorithm_instance =
-            std::make_unique<FastFDs>(path, separator, has_header, max_lhs, parallelism);
-    } else if (alg == "depminer") {
-        algorithm_instance = std::make_unique<Depminer>(path, separator, has_header);
-    } else if (alg == "fdep") {
-        algorithm_instance = std::make_unique<FDep>(path, separator, has_header);
-    }
+
+
+    std::unique_ptr<FDAlgorithm> algorithm_instance =
+        algos::CreateAlgorithmInstance(alg, dataset, separator, has_header, error, max_lhs,
+                                       parallelism, seed);
+
     try {
         unsigned long long elapsed_time = algorithm_instance->Execute();
         std::cout << "> ELAPSED TIME: " << elapsed_time << std::endl;
