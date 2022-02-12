nikita-talalai/Desbordante

Issue #23 - set up logging via easylogging
- replace std::cout with LOG(<LEVEL>) everywhere, excluding main.cpp and tests
- optimize iostream imports
- change logging level in several places so that INFO would provide the most general info and would be suitable for unit test output
- enable test logging to make CI logs more useful and readable

src/algorithms/DFD/DFD.cpp
@@ -6,6 +6,8 @@
 #include "RelationalSchema.h"
 #include "PositionListIndex.h"
 #include "LatticeTraversal/LatticeTraversal.h"
+#include "logging/easylogging++.h"
+
 
 unsigned long long DFD::executeInternal() {
     partitionStorage = std::make_unique<PartitionStorage>(relation_.get(),
@@ -52,7 +54,7 @@ unsigned long long DFD::executeInternal() {
                 registerFD(minimalDependencyLHS, *rhs);
             }
             addProgress(progressStep);
-            std::cout << static_cast<int>(getProgress().second) << "%\n";
+            LOG(INFO) << static_cast<int>(getProgress().second);
         });
     }
 
@@ -62,9 +64,8 @@ unsigned long long DFD::executeInternal() {
     auto elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startTime);
     long long aprioriMillis = elapsed_milliseconds.count();
 
-    //std::cout << "====JSON-FD========\r\n" << getJsonFDs() << std::endl;
-    std::cout << "> FD COUNT: " << fdCollection_.size() << std::endl;
-    std::cout << "> HASH: " << PliBasedFDAlgorithm::fletcher16() << std::endl;
+    LOG(INFO) << "> FD COUNT: " << fdCollection_.size();
+    LOG(INFO) << "> HASH: " << PliBasedFDAlgorithm::fletcher16();
 
     return aprioriMillis;
 }

src/algorithms/FastFDs.cpp
@@ -58,7 +58,7 @@ unsigned long long FastFDs::executeInternal() {
 
     auto task = [this](std::unique_ptr<Column> const& column) {
         if (columnContainsOnlyEqualValues(*column)) {
-            LOG(INFO) << "Registered FD: " << schema_->emptyVertical->toString()
+            LOG(DEBUG) << "Registered FD: " << schema_->emptyVertical->toString()
                       << "->" << column->toString();
             registerFD(Vertical(), *column);
             return;
@@ -120,7 +120,7 @@ void FastFDs::findCovers(Column const& attribute, vector<DiffSet> const& diff_se
 
     if (cur_diff_sets.empty()) {
         if (coverMinimal(path, diff_sets_mod)) {
-            LOG(INFO) << "Registered FD: " << path.toString()
+            LOG(DEBUG) << "Registered FD: " << path.toString()
                       << "->" << attribute.toString();
             registerFD(path, attribute);
             return;

src/algorithms/Fd_mine.cpp
@@ -4,6 +4,8 @@
 #include <queue>
 #include <vector>
 
+#include "logging/easylogging++.h"
+
 unsigned long long Fd_mine::executeInternal() {
     // 1
     schema = relation_->getSchema();
@@ -246,17 +248,17 @@ void Fd_mine::display() {
             if (!rhs[j] || (rhs[j] && lhs[j])) {
                 continue;
             }
-            std::cout << "Discovered FD: ";
+            LOG(DEBUG) << "Discovered FD: ";
             for (size_t i = 0; i < lhs.size(); i++) {
                 if (lhs[i]) {
-                    std::cout << schema->getColumn(i)->getName() << " ";
+                    LOG(DEBUG) << schema->getColumn(i)->getName() << " ";
                 }
             }
-            std::cout << "-> " << schema->getColumn(j)->getName() << "\n";
+            LOG(DEBUG) << "-> " << schema->getColumn(j)->getName();
             registerFD(Vertical(schema, lhs), *schema->getColumn(j));
             fd_counter++;
         }
     }
-    std::cout << "TOTAL FDs " << fd_counter << "\n";
+    LOG(DEBUG) << "TOTAL FDs " << fd_counter;
 }
 

src/algorithms/Pyro.cpp
@@ -4,12 +4,12 @@
 
 #include "FdG1Strategy.h"
 #include "KeyG1Strategy.h"
+#include "logging/easylogging++.h"
 #include "Pyro.h"
 
 std::mutex searchSpacesMutex;
 
 unsigned long long Pyro::executeInternal() {
-    using std::cout;
     auto startTime = std::chrono::system_clock::now();
 
     auto schema = relation_->getSchema();
@@ -100,20 +100,15 @@ unsigned long long Pyro::executeInternal() {
 
     setProgress(100);
     auto elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startTime);
-
-    LOG(DEBUG) << boost::format{"FdG1 error calculation: %1% ms"} % (FdG1Strategy::nanos_ / 1000000);
-    std::cout << "Init time: " << initTimeMillis << "ms" << std::endl;
-    std::cout << "Time: " << elapsed_milliseconds.count() << " milliseconds" << std::endl;
-    std::cout << "Error calculation count: " << totalErrorCalcCount << std::endl;
-    std::cout << "Total ascension time: " << totalAscension << "ms" << std::endl;
-    std::cout << "Total trickle time: " << totalTrickle << "ms" << std::endl;
-    std::cout << "Total intersection time: "
-              << util::PositionListIndex::micros / 1000 << "ms" << std::endl;
-    /*std::cout << "====RESULTS-FD====\r\n" << fdsToString();
-    std::cout << "====RESULTS-UCC====\r\n" << uccsToString();
-    std::cout << "====JSON-FD========\r\n" << FDAlgorithm::getJsonFDs() << std::endl;*/
-
-    std::cout << "HASH: " << PliBasedFDAlgorithm::fletcher16() << std::endl;
+    LOG(INFO) << boost::format{"FdG1 error calculation: %1% ms"} % (FdG1Strategy::nanos_ / 1000000);
+    LOG(INFO) << "Init time: " << initTimeMillis << "ms";
+    LOG(INFO) << "Time: " << elapsed_milliseconds.count() << " milliseconds";
+    LOG(INFO) << "Error calculation count: " << totalErrorCalcCount;
+    LOG(INFO) << "Total ascension time: " << totalAscension << "ms";
+    LOG(INFO) << "Total trickle time: " << totalTrickle << "ms";
+    LOG(INFO) << "Total intersection time: "
+              << util::PositionListIndex::micros / 1000 << "ms";
+    LOG(INFO) << "HASH: " << PliBasedFDAlgorithm::fletcher16();
     return elapsed_milliseconds.count();
 }
 

src/algorithms/TaneX.cpp
@@ -1,7 +1,6 @@
 #include "TaneX.h"
 
 #include <chrono>
-#include <iostream>
 #include <iomanip>
 #include <list>
 #include <memory>
@@ -12,6 +11,7 @@
 #include "RelationalSchema.h"
 #include "LatticeLevel.h"
 #include "LatticeVertex.h"
+#include "logging/easylogging++.h"
 
 double Tane::calculateZeroAryFdError(ColumnData const* rhs,
                                      ColumnLayoutRelationData const* relationData) {
@@ -36,28 +36,19 @@ void Tane::registerFD(Vertical const& lhs, Column const* rhs,
                       [[maybe_unused]] double error,
                       [[maybe_unused]] RelationalSchema const* schema) {
     dynamic_bitset<> lhs_bitset = lhs.getColumnIndices();
-    /*std::cout << "Discovered FD: ";
-    for (size_t i = lhs_bitset.find_first(); i != dynamic_bitset<>::npos; i = lhs_bitset.find_next(i)) {
-        std::cout << schema->getColumn(i)->getName() << " ";
-    }
-    std::cout << "-> " << rhs->getName() << " - error equals " << error << std::endl;*/
     PliBasedFDAlgorithm::registerFD(lhs, *rhs);
     countOfFD++;
 }
-
-/*void Tane::registerFD(Vertical const* lhs, Column const* rhs, double error, RelationalSchema const* schema) {
-    registerFD(*lhs, rhs, error, schema);
-}*/
 
 void Tane::registerUCC([[maybe_unused]] Vertical const& key,
                        [[maybe_unused]] double error,
                        [[maybe_unused]] RelationalSchema const* schema)  {
     /*dynamic_bitset<> key_bitset = key.getColumnIndices();
-    std::cout << "Discovered UCC: ";
+    LOG(INFO) << "Discovered UCC: ";
     for (int i = key_bitset.find_first(); i != -1; i = key_bitset.find_next(i)) {
-        std::cout << schema->getColumn(i)->getName() << " ";
+        LOG(INFO) << schema->getColumn(i)->getName() << " ";
     }
-    std::cout << "- error equals " << error << std::endl;*/
+    LOG(INFO) << "- error equals " << error << std::endl;*/
     countOfUCC++;
 }
 
@@ -66,15 +57,15 @@ void Tane::registerUCC([[maybe_unused]] Vertical const& key,
 unsigned long long Tane::executeInternal() {
     RelationalSchema const* schema = relation_->getSchema();
 
-    std::cout << schema->getName() << " has " << relation_->getNumColumns() << " columns, "
+    LOG(INFO) << schema->getName() << " has " << relation_->getNumColumns() << " columns, "
          << relation_->getNumRows() << " rows, and a maximum NIP of " << std::setw(2)
-         << relation_->getMaximumNip() << "." << std::endl;
+         << relation_->getMaximumNip() << ".";
 
     for (auto& column : schema->getColumns()) {
         double avgPartners = relation_->getColumnData(column->getIndex()).
                 getPositionListIndex()->getNepAsLong() * 2.0 / relation_->getNumRows();
-        std::cout << "* " << column->toString() << ": every tuple has " << std::setw(2)
-             << avgPartners << " partners on average." << std::endl;
+        LOG(INFO) << "* " << column->toString() << ": every tuple has " << std::setw(2)
+             << avgPartners << " partners on average.";
     }
     auto startTime = std::chrono::system_clock::now();
     double progressStep = 100.0 / (schema->getNumColumns() + 1);
@@ -153,8 +144,8 @@ unsigned long long Tane::executeInternal() {
         //aprioriMillis += elapsed_milliseconds.count();
 
         util::LatticeLevel* level = levels[arity].get();
-        std::cout << "Checking " << level->getVertices().size() << " "
-                  << arity << "-ary lattice vertices." << std::endl;
+        LOG(TRACE) << "Checking " << level->getVertices().size() << " "
+                  << arity << "-ary lattice vertices.";
         if (level->getVertices().empty()) {
             break;
         }
@@ -258,7 +249,6 @@ unsigned long long Tane::executeInternal() {
 
         }
 
-        //cout << "Pruned level: " << level->getArity() << ". " << level->getVertices().size() << " vertices" << endl;
         //TODO: printProfilingData
         addProgress(progressStep);
     }
@@ -267,14 +257,13 @@ unsigned long long Tane::executeInternal() {
     std::chrono::milliseconds elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startTime);
     aprioriMillis += elapsed_milliseconds.count();
 
-    std::cout << "Time: " << aprioriMillis << " milliseconds" << std::endl;
-    std::cout << "Intersection time: " << util::PositionListIndex::micros / 1000
-              << "ms" << std::endl;
-    std::cout << "Total intersections: " << util::PositionListIndex::intersectionCount << std::endl;
-    std::cout << "Total FD count: " << countOfFD << std::endl;
-    std::cout << "Total UCC count: " << countOfUCC << std::endl;
-    // std::cout << "===== FD JSON ========" << getJsonFDs() << std::endl;
-    std::cout << "HASH: " << fletcher16() << std::endl;
+    LOG(INFO) << "Time: " << aprioriMillis << " milliseconds";
+    LOG(INFO) << "Intersection time: " << util::PositionListIndex::micros / 1000
+              << "ms";
+    LOG(INFO) << "Total intersections: " << util::PositionListIndex::intersectionCount;
+    LOG(INFO) << "Total FD count: " << countOfFD;
+    LOG(INFO) << "Total UCC count: " << countOfUCC;
+    LOG(INFO) << "HASH: " << fletcher16();
 
     return aprioriMillis;
 }

src/core/SearchSpace.cpp
@@ -1,6 +1,9 @@
-#include "logging/easylogging++.h"
 #include "SearchSpace.h"
+
 #include <queue>
+
+#include "logging/easylogging++.h"
+
 // TODO: extra careful with const& -> shared_ptr conversions via make_shared-smart pointer may delete the object - pass empty deleter [](*) {}
 
 void SearchSpace::discover() {
@@ -241,7 +244,7 @@ bool SearchSpace::ascend(DependencyCandidate const &launchPad) {
             break;
         }
     }
-    
+
     //std::cout << static_cast<std::string>(traversalCandidate) << std::endl;
 
     if (!error) {
@@ -283,7 +286,7 @@ bool SearchSpace::ascend(DependencyCandidate const &launchPad) {
 
 void SearchSpace::checkEstimate([[maybe_unused]] DependencyStrategy* strategy,
                                 [[maybe_unused]] DependencyCandidate const& traversalCandidate) {
-    std::cout << "Stepped into method 'checkEstimate' - not implemented yet being a debug method\n";
+    LOG(DEBUG) << "Stepped into method 'checkEstimate' - not implemented yet being a debug method\n";
 }
 
 void SearchSpace::trickleDown(Vertical const& mainPeak, double mainPeakError) {
@@ -649,14 +652,13 @@ bool SearchSpace::isKnownNonDependency(Vertical const& vertical,
 }
 
 void SearchSpace::printStats() const {
-    using std::cout, std::endl;
-    cout << "Trickling down from: " << tricklingDownFrom / 1000000 << endl;
-    cout << "Trickling down: " << tricklingDown / 1000000 - tricklingDownFrom / 1000000 << endl;
-    cout << "Trickling down nested:" << tricklingDownPart / 1000000 << endl;
-    cout << "Num nested: " << numNested / 1000000 << endl;
-    cout << "Ascending: " << ascending / 1000000<< endl;
-    cout << "Polling: " << pollingLaunchPads / 1000000 << endl;
-    cout << "Returning launch pad: " << returningLaunchPad / 1000000 << endl;
+    LOG(INFO) << "Trickling down from: " << tricklingDownFrom / 1000000;
+    LOG(INFO) << "Trickling down: " << tricklingDown / 1000000 - tricklingDownFrom / 1000000;
+    LOG(INFO) << "Trickling down nested:" << tricklingDownPart / 1000000;
+    LOG(INFO) << "Num nested: " << numNested / 1000000;
+    LOG(INFO) << "Ascending: " << ascending / 100000;
+    LOG(INFO) << "Polling: " << pollingLaunchPads / 1000000;
+    LOG(INFO) << "Returning launch pad: " << returningLaunchPad / 1000000;
 }
 
 void SearchSpace::ensureInitialized() {

Desbordante/desbordante-core

src/algorithms/DFD/DFD.cpp
@@ -6,6 +6,8 @@
 #include "RelationalSchema.h"
 #include "PositionListIndex.h"
 #include "LatticeTraversal/LatticeTraversal.h"
+#include "logging/easylogging++.h"
+
 
 unsigned long long DFD::ExecuteInternal() {
     partition_storage_ = std::make_unique<PartitionStorage>(relation_.get(),
@@ -53,7 +55,7 @@ unsigned long long DFD::ExecuteInternal() {
                 RegisterFd(minimal_dependency_lhs, *rhs);
             }
             AddProgress(progress_step);
-            std::cout << static_cast<int>(GetProgress().second) << "%\n";
+            LOG(INFO) << static_cast<int>(GetProgress().second);
         });
     }
 
@@ -64,9 +66,8 @@ unsigned long long DFD::ExecuteInternal() {
         std::chrono::system_clock::now() - start_time);
     long long apriori_millis = elapsed_milliseconds.count();
 
-    //std::cout << "====JSON-FD========\r\n" << GetJsonFDs() << std::endl;
-    std::cout << "> FD COUNT: " << fd_collection_.size() << std::endl;
-    std::cout << "> HASH: " << PliBasedFDAlgorithm::Fletcher16() << std::endl;
+    LOG(INFO) << "> FD COUNT: " << fd_collection_.size();
+    LOG(INFO) << "> HASH: " << PliBasedFDAlgorithm::Fletcher16();
 
     return apriori_millis;
 }

src/algorithms/FastFDs.cpp
@@ -58,7 +58,7 @@ unsigned long long FastFDs::ExecuteInternal() {
 
     auto task = [this](std::unique_ptr<Column> const& column) {
         if (ColumnContainsOnlyEqualValues(*column)) {
-            LOG(INFO) << "Registered FD: " << schema_->empty_vertical_->ToString()
+            LOG(DEBUG) << "Registered FD: " << schema_->empty_vertical_->ToString()
                       << "->" << column->ToString();
             RegisterFd(Vertical(), *column);
             return;
@@ -120,7 +120,7 @@ void FastFDs::FindCovers(Column const& attribute, vector<DiffSet> const& diff_se
 
     if (cur_diff_sets.empty()) {
         if (CoverMinimal(path, diff_sets_mod)) {
-            LOG(INFO) << "Registered FD: " << path.ToString()
+            LOG(DEBUG) << "Registered FD: " << path.ToString()
                       << "->" << attribute.ToString();
             RegisterFd(path, attribute);
             return;

src/algorithms/Fd_mine.cpp
@@ -4,6 +4,8 @@
 #include <queue>
 #include <vector>
 
+#include "logging/easylogging++.h"
+
 unsigned long long Fd_mine::ExecuteInternal() {
     // 1
     schema_ = relation_->GetSchema();
@@ -255,17 +257,17 @@ void Fd_mine::Display() {
             if (!rhs[j] || (rhs[j] && lhs[j])) {
                 continue;
             }
-            std::cout << "Discovered FD: ";
+            LOG(DEBUG) << "Discovered FD: ";
             for (size_t i = 0; i < lhs.size(); i++) {
                 if (lhs[i]) {
-                    std::cout << schema_->GetColumn(i)->GetName() << " ";
+                    LOG(DEBUG) << schema_->GetColumn(i)->GetName() << " ";
                 }
             }
-            std::cout << "-> " << schema_->GetColumn(j)->GetName() << "\n";
+            LOG(DEBUG) << "-> " << schema_->GetColumn(j)->GetName();
             RegisterFd(Vertical(schema_, lhs), *schema_->GetColumn(j));
             fd_counter++;
         }
     }
-    std::cout << "TOTAL FDs " << fd_counter << "\n";
+    LOG(DEBUG) << "TOTAL FDs " << fd_counter;
 }
 

src/algorithms/Pyro.cpp
@@ -4,12 +4,12 @@
 
 #include "FdG1Strategy.h"
 #include "KeyG1Strategy.h"
+#include "logging/easylogging++.h"
 #include "Pyro.h"
 
 std::mutex searchSpacesMutex;
 
 unsigned long long Pyro::ExecuteInternal() {
-    using std::cout;
     auto start_time = std::chrono::system_clock::now();
 
     auto schema = relation_->GetSchema();
@@ -105,20 +105,16 @@ unsigned long long Pyro::ExecuteInternal() {
     auto elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(
         std::chrono::system_clock::now() - start_time);
 
-    LOG(DEBUG) << boost::format{"FdG1 error calculation: %1% ms"} %
-                      (FdG1Strategy::nanos_ / 1000000);
-    std::cout << "Init time: " << init_time_millis << "ms" << std::endl;
-    std::cout << "Time: " << elapsed_milliseconds.count() << " milliseconds" << std::endl;
-    std::cout << "Error calculation count: " << total_error_calc_count << std::endl;
-    std::cout << "Total ascension time: " << total_ascension << "ms" << std::endl;
-    std::cout << "Total trickle time: " << total_trickle << "ms" << std::endl;
-    std::cout << "Total intersection time: "
-              << util::PositionListIndex::micros_ / 1000 << "ms" << std::endl;
-    /*std::cout << "====RESULTS-FD====\r\n" << FDsToString();
-    std::cout << "====RESULTS-UCC====\r\n" << UCCsToString();
-    std::cout << "====JSON-FD========\r\n" << FDAlgorithm::GetJsonFDs() << std::endl;*/
-
-    std::cout << "HASH: " << PliBasedFDAlgorithm::Fletcher16() << std::endl;
+    LOG(INFO) << boost::format{"FdG1 error calculation: %1% ms"} %
+                     (FdG1Strategy::nanos_ / 1000000);
+    LOG(INFO) << "Init time: " << init_time_millis << "ms";
+    LOG(INFO) << "Time: " << elapsed_milliseconds.count() << " milliseconds";
+    LOG(INFO) << "Error calculation count: " << total_error_calc_count;
+    LOG(INFO) << "Total ascension time: " << total_ascension << "ms";
+    LOG(INFO) << "Total trickle time: " << total_trickle << "ms";
+    LOG(INFO) << "Total intersection time: "
+              << util::PositionListIndex::micros_ / 1000 << "ms";
+    LOG(INFO) << "HASH: " << PliBasedFDAlgorithm::Fletcher16();
     return elapsed_milliseconds.count();
 }
 

src/algorithms/TaneX.cpp
@@ -1,7 +1,6 @@
 #include "TaneX.h"
 
 #include <chrono>
-#include <iostream>
 #include <iomanip>
 #include <list>
 #include <memory>
@@ -12,6 +11,7 @@
 #include "RelationalSchema.h"
 #include "LatticeLevel.h"
 #include "LatticeVertex.h"
+#include "logging/easylogging++.h"
 
 double Tane::CalculateZeroAryFdError(ColumnData const* rhs,
                                      ColumnLayoutRelationData const* relation_data) {
@@ -35,42 +35,34 @@ void Tane::RegisterFd(Vertical const& lhs, Column const* rhs,
                       [[maybe_unused]] double error,
                       [[maybe_unused]] RelationalSchema const* schema) {
     dynamic_bitset<> lhs_bitset = lhs.GetColumnIndices();
-    /*std::cout << "Discovered FD: ";
-    for (size_t i = lhs_bitset.find_first(); i != dynamic_bitset<>::npos; i = lhs_bitset.find_next(i)) {
-        std::cout << schema->GetColumn(i)->GetName() << " ";
-    }
-    std::cout << "-> " << rhs->GetName() << " - error equals " << error << std::endl;*/
     PliBasedFDAlgorithm::RegisterFd(lhs, *rhs);
     count_of_fd_++;
 }
 
-/*void Tane::RegisterFd(Vertical const* lhs, Column const* rhs, double error, RelationalSchema const* schema) {
-    RegisterFd(*lhs, rhs, error, schema);
-}*/
-
-void Tane::RegisterUcc([[maybe_unused]] Vertical const& key, [[maybe_unused]] double error,
-                       [[maybe_unused]] RelationalSchema const* schema) {
-    /*dynamic_bitset<> key_bitset = key.GetColumnIndices();
-    std::cout << "Discovered UCC: ";
+void Tane::RegisterUcc([[maybe_unused]] Vertical const& key,
+                       [[maybe_unused]] double error,
+                       [[maybe_unused]] RelationalSchema const* schema)  {
+    /*dynamic_bitset<> key_bitset = key.getColumnIndices();
+    LOG(INFO) << "Discovered UCC: ";
     for (int i = key_bitset.find_first(); i != -1; i = key_bitset.find_next(i)) {
-        std::cout << schema->GetColumn(i)->GetName() << " ";
+        LOG(INFO) << schema->GetColumn(i)->GetName() << " ";
     }
-    std::cout << "- error equals " << error << std::endl;*/
+    LOG(INFO) << "- error equals " << error << std::endl;*/
     count_of_ucc_++;
 }
 
 unsigned long long Tane::ExecuteInternal() {
     RelationalSchema const* schema = relation_->GetSchema();
 
-    std::cout << schema->GetName() << " has " << relation_->GetNumColumns() << " columns, "
+    LOG(INFO) << schema->GetName() << " has " << relation_->GetNumColumns() << " columns, "
               << relation_->GetNumRows() << " rows, and a maximum NIP of " << std::setw(2)
-              << relation_->GetMaximumNip() << "." << std::endl;
+              << relation_->GetMaximumNip() << ".";
 
     for (auto& column : schema->GetColumns()) {
         double avg_partners = relation_->GetColumnData(column->GetIndex()).
             GetPositionListIndex()->GetNepAsLong() * 2.0 / relation_->GetNumRows();
-        std::cout << "* " << column->ToString() << ": every tuple has " << std::setw(2)
-                  << avg_partners << " partners on average." << std::endl;
+        LOG(INFO) << "* " << column->ToString() << ": every tuple has " << std::setw(2)
+                  << avg_partners << " partners on average.";
     }
     auto start_time = std::chrono::system_clock::now();
     double progress_step = 100.0 / (schema->GetNumColumns() + 1);
@@ -151,8 +143,8 @@ unsigned long long Tane::ExecuteInternal() {
         //apriori_millis_ += elapsed_milliseconds.count();
 
         util::LatticeLevel* level = levels[arity].get();
-        std::cout << "Checking " << level->GetVertices().size() << " "
-                  << arity << "-ary lattice vertices." << std::endl;
+        LOG(TRACE) << "Checking " << level->GetVertices().size() << " "
+                  << arity << "-ary lattice vertices.";
         if (level->GetVertices().empty()) {
             break;
         }
@@ -260,7 +252,6 @@ unsigned long long Tane::ExecuteInternal() {
             }
         }
 
-        //cout << "Pruned level: " << level->GetArity() << ". " << level->GetVertices().size() << " vertices_" << endl;
         //TODO: printProfilingData
         AddProgress(progress_step);
     }
@@ -271,15 +262,14 @@ unsigned long long Tane::ExecuteInternal() {
                                                               start_time);
     apriori_millis_ += elapsed_milliseconds.count();
 
-    std::cout << "Time: " << apriori_millis_ << " milliseconds" << std::endl;
-    std::cout << "Intersection time: " << util::PositionListIndex::micros_ / 1000
-              << "ms" << std::endl;
-    std::cout << "Total intersections: " << util::PositionListIndex::intersection_count_
+    LOG(INFO) << "Time: " << apriori_millis_ << " milliseconds";
+    LOG(INFO) << "Intersection time: " << util::PositionListIndex::micros_ / 1000
+              << "ms";
+    LOG(INFO) << "Total intersections: " << util::PositionListIndex::intersection_count_
               << std::endl;
-    std::cout << "Total FD count: " << count_of_fd_ << std::endl;
-    std::cout << "Total UCC count: " << count_of_ucc_ << std::endl;
-    // std::cout << "===== FD JSON ========" << GetJsonFDs() << std::endl;
-    std::cout << "HASH: " << Fletcher16() << std::endl;
+    LOG(INFO) << "Total FD count: " << count_of_fd_;
+    LOG(INFO) << "Total UCC count: " << count_of_ucc_;
+    LOG(INFO) << "HASH: " << Fletcher16();
 
     return apriori_millis_;
 }

src/core/SearchSpace.cpp
@@ -1,6 +1,9 @@
-#include "logging/easylogging++.h"
 #include "SearchSpace.h"
+
 #include <queue>
+
+#include "logging/easylogging++.h"
+
 // TODO: extra careful with const& -> shared_ptr conversions via make_shared-smart pointer may delete the object - pass empty deleter [](*) {}
 
 void SearchSpace::Discover() {
@@ -297,7 +300,7 @@ bool SearchSpace::Ascend(DependencyCandidate const& launch_pad) {
 
 void SearchSpace::CheckEstimate([[maybe_unused]] DependencyStrategy* strategy,
                                 [[maybe_unused]] DependencyCandidate const& traversal_candidate) {
-    std::cout << "Stepped into method 'checkEstimate' - not implemented yet being a debug method\n";
+    LOG(DEBUG) << "Stepped into method 'checkEstimate' - not implemented yet being a debug method\n";
 }
 
 void SearchSpace::TrickleDown(Vertical const& main_peak, double main_peak_error) {
@@ -681,15 +684,13 @@ bool SearchSpace::IsKnownNonDependency(Vertical const& vertical,
 }
 
 void SearchSpace::PrintStats() const {
-    using std::cout, std::endl;
-    cout << "Trickling down from: " << trickling_down_from_ / 1000000 << endl;
-    cout << "Trickling down: " << trickling_down_ / 1000000 - trickling_down_from_ / 1000000
-         << endl;
-    cout << "Trickling down nested:" << trickling_down_part_ / 1000000 << endl;
-    cout << "Num nested: " << num_nested_ / 1000000 << endl;
-    cout << "Ascending: " << ascending_ / 1000000 << endl;
-    cout << "Polling: " << polling_launch_pads_ / 1000000 << endl;
-    cout << "Returning launch pad: " << returning_launch_pad_ / 1000000 << endl;
+    LOG(INFO) << "Trickling down from: " << trickling_down_from_ / 1000000;
+    LOG(INFO) << "Trickling down: " << trickling_down_ / 1000000 - trickling_down_from_ / 1000000;
+    LOG(INFO) << "Trickling down nested:" << trickling_down_part_ / 1000000;
+    LOG(INFO) << "Num nested: " << num_nested_ / 1000000;
+    LOG(INFO) << "Ascending: " << ascending_ / 100000;
+    LOG(INFO) << "Polling: " << polling_launch_pads_ / 1000000;
+    LOG(INFO) << "Returning launch pad: " << returning_launch_pad_ / 1000000;
 }
 
 void SearchSpace::EnsureInitialized() {