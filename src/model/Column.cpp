76606feb029f522969027e1a8e4992c644d0dc51
Michael Polyntsov <Arno9148@gmail.com>
2022-02-19 22:12:44 +0300

    Implement operator== for RelationalSchema
    In typo mining workflow several algorithms work on the same table and
    can have different RelationalSchema objects so we cannot just compare
    pointers to RelationalSchema. Implement operator== for RelationalSchema
    objects to address this issue.

--- src/model/Column.cpp
+++ src/model/Column.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 bool Column::operator<(const Column& rhs) const {
     // use assert here to check if Columns belong to the same schema?
-    return index_ > rhs.index_ && schema_ == rhs.schema_;
+    return index_ > rhs.index_ && *schema_ == *rhs.schema_;
 }
 
 bool Column::operator>(const Column& rhs) const {
-    return index_ < rhs.index_ && schema_ == rhs.schema_;
+    return index_ < rhs.index_ && *schema_ == *rhs.schema_;
 }
 
 bool Column::operator==(const Column& rhs) const {
     if (this == &rhs) return true;
-    return index_ == rhs.index_ && schema_ == rhs.schema_;
+    return index_ == rhs.index_ && *schema_ == *rhs.schema_;
 }
 
 bool Column::operator!=(const Column& rhs) const {
