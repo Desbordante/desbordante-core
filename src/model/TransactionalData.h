4f7062ff73d18a841c656100fe8dd78368851ab5
Alexandr Smirnov <smirnalexandr@rambler.ru>
2022-03-15 20:57:53 +0500

    Fix PR issues: logic

--- src/model/TransactionalData.h
+++ src/model/TransactionalData.h
@@ -19,17 +19,21 @@ private:
                                                                  unsigned tid_col_index = 0,
                                                                  unsigned item_col_index = 1);
 
-    static std::unique_ptr<TransactionalData>  CreateFromTabular(CSVParser& file_input,
-                                                                 bool has_tid);
+    static std::unique_ptr<TransactionalData> CreateFromTabular(CSVParser& file_input,
+                                                                bool has_tid);
+
+    TransactionalData(std::vector<std::string> item_universe,
+                      std::unordered_map<unsigned, Itemset> transactions)
+        : item_universe_(std::move(item_universe)), transactions_(std::move(transactions)) {}
+
 public:
     TransactionalData() = delete;
 
     TransactionalData(TransactionalData const&) = delete;
     TransactionalData& operator=(TransactionalData const&) = delete;
 
-    TransactionalData(std::vector<std::string>&& item_universe,
-                      std::unordered_map<unsigned, Itemset>&& transactions)
-        : item_universe_(std::move(item_universe)), transactions_(std::move(transactions)) {}
+    TransactionalData(TransactionalData&& other) = default;
+    TransactionalData& operator=(TransactionalData&& other) = default;
 
     std::vector<std::string> const& GetItemUniverse() const noexcept { return item_universe_; }
     std::unordered_map<unsigned, Itemset> const& GetTransactions() const noexcept {
