90570f1ed6591504941bf2c0154b948b4ace48d9
Michael Polyntsov <Arno9148@gmail.com>
2022-03-22 18:54:56 +0300

    Implement column type deduction and ColumnTypedData

---
+++ src/model/ColumnLayoutTypedRelationData.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "CSVParser.h"
+#include "RelationData.h"
+#include "TypedColumnData.h"
+
+namespace model {
+
+using TypedRelationData = AbstractRelationData<TypedColumnData>;
+
+class ColumnLayoutTypedRelationData final : public TypedRelationData {
+public:
+    using TypedRelationData::AbstractRelationData;
+
+    unsigned int GetNumRows() const final {
+        if (column_data_.empty()) {
+            return 0;
+        } else {
+            return column_data_.front().GetNumRows();
+        }
+    }
+
+    static std::unique_ptr<ColumnLayoutTypedRelationData> CreateFrom(CSVParser& file_input,
+                                                                     bool is_null_eq_null,
+                                                                     int max_cols = -1,
+                                                                     long max_rows = -1);
+};
+
+}  // namespace model
