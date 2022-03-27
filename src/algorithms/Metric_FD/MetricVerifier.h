133ae56faeb890a1bd7c63a11fd64d7e32159cce
Aviu00 <aviu00@protonmail.com>
2022-03-27 17:46:40 +0300

    Implement levenshtein metric for metric FD verification

--- src/algorithms/Metric_FD/MetricVerifier.h
+++ src/algorithms/Metric_FD/MetricVerifier.h
@@ -5,21 +5,39 @@
 #include "ColumnLayoutTypedRelationData.h"
 #include "ColumnLayoutRelationData.h"
 
+namespace algos {
+
+BETTER_ENUM(Metric, char,
+            integer = 0,
+            floating_point,
+            levenshtein
+);
+
 class MetricVerifier : public algos::Primitive {
 private:
-    using Config = FDAlgorithm::Config;
-    Config config_;
-
     std::shared_ptr<model::ColumnLayoutTypedRelationData> typed_relation_;
     std::shared_ptr<ColumnLayoutRelationData> relation_; //temporarily parsing 2 times
 
     bool CompareNumericValues
         (util::PLI::Cluster const& cluster, model::TypedColumnData const& col) const;
-    static bool PositiveDoubleLessOrEq(long double l, long double r);
+    bool CompareStringValues
+        (util::PLI::Cluster const& cluster, model::TypedColumnData const& col) const;
 public:
+    struct Config {
+        std::filesystem::path data{}; /* Path to input file */
+        char separator = ',';         /* Separator for csv */
+        bool has_header = true;       /* Indicates if input file has header */
+        bool is_null_equal_null = true; /* Is NULL value equals another NULL value */
+        std::string metric;
+        long double parameter;
+        std::vector<unsigned int> lhs_indices;
+        unsigned int rhs_index;
+    };
+
+    Metric metric_;
     std::vector<unsigned int> lhs_indices_;
     unsigned int rhs_index_;
-    long double parameter_; //long double temporarily
+    double parameter_;
 
     bool metric_fd_holds = false;
 
@@ -27,15 +45,16 @@ public:
 
     explicit MetricVerifier(Config const& config)
         : Primitive(config.data, config.separator, config.has_header, {}),
-          lhs_indices_(config.GetSpecialParam<std::vector<unsigned int>>("lhs_indices")),
-          rhs_index_(config.GetSpecialParam<unsigned int>("rhs_index")),
-          parameter_(config.GetSpecialParam<long double>("parameter")),
-          config_(config) {
+          metric_(Metric::_from_string(config.metric.c_str())),
+          lhs_indices_(config.lhs_indices),
+          rhs_index_(config.rhs_index),
+          parameter_(config.parameter) {
         relation_ =
-            ColumnLayoutRelationData::CreateFrom(input_generator_, config_.is_null_equal_null);
-        CSVParser generator2(config_.data, config_.separator, config_.has_header);
-        typed_relation_ = model::ColumnLayoutTypedRelationData::CreateFrom(generator2,
-                                                                           config_.is_null_equal_null);
+            ColumnLayoutRelationData::CreateFrom(input_generator_, config.is_null_equal_null);
+        CSVParser generator2(config.data, config.separator, config.has_header);
+        typed_relation_ =
+            model::ColumnLayoutTypedRelationData::CreateFrom(generator2, config.is_null_equal_null);
     }
 };
 
+} // namespace algos
--- tests/TestMetricVerifier.cpp
+++ tests/TestMetricVerifier.cpp
@@ -1,13 +1,15 @@
-//#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include "MetricVerifier.h"
 #include "AlgoFactory.h"
 
+namespace tests {
+
 struct MetricVerifyingParams {
     algos::StdParamsMap params;
 
-    MetricVerifyingParams(long double min_parameter,
+    MetricVerifyingParams(std::string metric,
+                          long double min_parameter,
                           std::vector<unsigned int> lhs_indices,
                           unsigned int rhs_index,
                           std::string const& dataset,
@@ -20,29 +22,30 @@ struct MetricVerifyingParams {
                   {"separator", separator},
                   {"has_header", has_header},
                   {"is_null_equal_null", true},
-                  {"max_lhs", static_cast<unsigned int>(0)},
-                  {"threads", static_cast<ushort>(0)}}) {}
+                  {"metric", metric}}) {}
 };
 
 class TestMetricVerifying : public ::testing::TestWithParam<MetricVerifyingParams> {};
 
-static std::unique_ptr<MetricVerifier> CreateMetricVerifier(algos::StdParamsMap map) {
+static std::unique_ptr<algos::MetricVerifier> CreateMetricVerifier(algos::StdParamsMap map) {
     auto metric_verrifier =
-        algos::details::CreateMetricVerifier(map);
-    auto casted = dynamic_cast<MetricVerifier*>(metric_verrifier.release());
-    return std::unique_ptr<MetricVerifier>(casted);
+        algos::CreateAlgorithmInstance(algos::AlgoMiningType::metric, algos::Algo::metric, std::move(map));
+    auto casted = dynamic_cast<algos::MetricVerifier*>(metric_verrifier.release());
+    return std::unique_ptr<algos::MetricVerifier>(casted);
 }
 
-static bool GetResult(MetricVerifier& metric_verifier) {
+static bool GetResult(algos::MetricVerifier& metric_verifier) {
     metric_verifier.Execute();
     return metric_verifier.metric_fd_holds;
 }
 
 TEST_P(TestMetricVerifying, DefaultTest) {
-    std::unique_ptr<MetricVerifier> verifier = CreateMetricVerifier(GetParam().params);
+    std::unique_ptr<algos::MetricVerifier> verifier = CreateMetricVerifier(GetParam().params);
 
     ASSERT_TRUE(GetResult(*verifier.get()));
-    verifier->parameter_ -= 1e-8;
+    verifier->parameter_ -= verifier->metric_._value == algos::Metric::floating_point
+                            ? 1e-9
+                            : 1;
     if (verifier->parameter_ < 0) return;
     ASSERT_FALSE(GetResult(*verifier.get()));
 }
@@ -50,14 +53,19 @@ TEST_P(TestMetricVerifying, DefaultTest) {
 INSTANTIATE_TEST_SUITE_P(
     MetricVerifierTestSuite, TestMetricVerifying,
     ::testing::Values(
-        MetricVerifyingParams(2, {0, 1}, 2, "TestLong.csv", ',', true),
-        MetricVerifyingParams(1, {0}, 1, "TestLong.csv", ',', true),
-        MetricVerifyingParams(4, {1}, 0, "TestLong.csv", ',', true),
-        MetricVerifyingParams(5, {0}, 2, "TestLong.csv", ',', true),
-        MetricVerifyingParams(0, {2}, 1, "TestLong.csv", ',', true),
-        MetricVerifyingParams(20500, {0}, 4, "TestMetric.csv", ',', true),
-        MetricVerifyingParams(1059, {1}, 4, "TestMetric.csv", ',', true),
-        MetricVerifyingParams(1, {1, 0}, 4, "TestMetric.csv", ',', true),
-        MetricVerifyingParams(4.5724231, {0}, 2, "TestMetric.csv", ',', true),
-        MetricVerifyingParams(7.53, {0}, 3, "TestMetric.csv", ',', true)
-        ));
+        MetricVerifyingParams("integer", 2, {0, 1}, 2, "TestLong.csv", ',', true),
+        MetricVerifyingParams("integer", 1, {0}, 1, "TestLong.csv", ',', true),
+        MetricVerifyingParams("integer", 4, {1}, 0, "TestLong.csv", ',', true),
+        MetricVerifyingParams("integer", 5, {0}, 2, "TestLong.csv", ',', true),
+        MetricVerifyingParams("integer", 0, {2}, 1, "TestLong.csv", ',', true),
+        MetricVerifyingParams("integer", 20500, {0}, 4, "TestMetric.csv", ',', true),
+        MetricVerifyingParams("integer", 1059, {1}, 4, "TestMetric.csv", ',', true),
+        MetricVerifyingParams("integer", 1, {1, 0}, 4, "TestMetric.csv", ',', true),
+        MetricVerifyingParams("floating_point", 4.5724231, {0}, 2, "TestMetric.csv", ',', true),
+        MetricVerifyingParams("floating_point", 7.53, {0}, 3, "TestMetric.csv", ',', true),
+        MetricVerifyingParams("levenshtein", 2, {0}, 5, "TestMetric.csv", ',', true),
+        MetricVerifyingParams("levenshtein", 5, {1}, 5, "TestMetric.csv", ',', true),
+        MetricVerifyingParams("levenshtein", 4, {0}, 6, "TestMetric.csv", ',', true)
+    ));
+
+} // namespace tests
