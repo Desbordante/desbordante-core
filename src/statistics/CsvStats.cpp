b2ad8c1a092791f61fee059248b1fc9c98cacdc6
Nikita Talalai <nikita.talalai.2002@gmail.com>
2022-05-12 02:55:50 +0300

    Implement all statisctics
    Create a new type CsvStats. It can compute such statistics as
    sum, average mean, min, max, quantiles, deviation etc. It works well
    for not mixed columns.

--- src/statistics/CsvStats.cpp
+++ src/statistics/CsvStats.cpp
@@ -1,8 +1,25 @@
 #include "CsvStats.h"
 
+#include <math.h>
+
+#include <algorithm>
+#include <cmath>
+#include <iterator>
+#include <type_traits>
+#include <unordered_set>
+#include <utility>
+
+#include "AbstractColumnData.h"
 #include "BuiltIn.h"
+#include "DoubleType.h"
+#include "EmptyType.h"
+#include "IntType.h"
+#include "NullType.h"
 #include "NumericType.h"
+#include "StringType.h"
 #include "Type.h"
+#include "TypedColumnData.h"
+#include "UndefinedType.h"
 
 static inline std::vector<mo::TypedColumnData> CreateColumnData(std::string_view data, char sep,
                                                                 bool has_header) {
@@ -17,120 +34,232 @@ static inline std::vector<mo::TypedColumnData> CreateColumnData(std::string_view
 CsvStats::CsvStats(std::string_view data, char sep, bool has_header)
     : col_data_(CreateColumnData(data, sep, has_header)) {}
 
-/*std::optional<std::variant<mo::Int, mo::Double>> CsvStats::GetSum(int index) {
+std::optional<std::pair<std::byte const*, mo::Type const*>> CsvStats::GetMin(int index) {
     mo::TypedColumnData const& col = col_data_[index];
-    if (col.IsNumeric()) {
-        mo::INumericType const& type = static_cast<mo::INumericType const&>(col.GetType());
-        std::byte* sum(type.Allocate());
-        type.GetValue<std::variant<mo::Int, mo::Double>>(sum) = mo::Int(0);
-        for (std::byte const* value : col.GetData()) {
-            type.Add(sum, value, sum);
-        }
-        return type.GetValue<std::variant<mo::Int, mo::Double>>(sum);
+    mo::TypeId type_id = col.GetTypeId();
+    if (type_id == +mo::TypeId::kEmpty || type_id == +mo::TypeId::kNull ||
+        type_id == +mo::TypeId::kUndefined) {
+        return {};
     }
-    return {};
-};*/
+    mo::Type const& type = static_cast<mo::Type const&>(col.GetType());
+    auto data = col.GetData();
+    std::byte const* min;
 
-std::byte* CsvStats::GetSum(int index) {
-    mo::TypedColumnData const& col = col_data_[index];
-    if (col.IsNumeric()) {
-        mo::INumericType const& type = static_cast<mo::INumericType const&>(col.GetType());
-        //mo::DoubleType type;
-
-        std::byte* sum(type.Allocate());
-        //type.GetValue<std::variant<mo::Int, mo::Double>>(sum) = mo::Int(0);
-        for (std::byte const* value : col.GetData()) {
-            type.Add(sum, value, sum);
+    int counter = 0;
+    for (unsigned i = 0; i < data.size(); ++i) {
+        if (!col.IsNullOrEmpty(i)) {
+            if (counter == 0) {
+                min = data[i];  // for first init of min with elem of data
+                ++counter;
+            }
+            if (type.Compare(data[i], min) == mo::CompareResult::kLess) {
+                min = data[i];
+            }
         }
+    }
+    return std::make_pair(min, &col.GetType());
+}
 
-        return sum;
+std::optional<std::pair<std::byte const*, mo::Type const*>> CsvStats::GetMax(int index) {
+    mo::TypedColumnData const& col = col_data_[index];
+    mo::TypeId type_id = col.GetTypeId();
+    if (type_id == +mo::TypeId::kEmpty || type_id == +mo::TypeId::kNull ||
+        type_id == +mo::TypeId::kUndefined) {
+        return {};
+    }
+    mo::Type const& type = static_cast<mo::Type const&>(col.GetType());
+    auto data = col.GetData();
+    std::byte const* max;
+
+    int counter = 0;
+    for (unsigned i = 0; i < data.size(); ++i) {
+        if (!col.IsNullOrEmpty(i)) {
+            if (counter == 0) {
+                max = data[i];  // for first init of max with elem of data
+                ++counter;
+            }
+            if (type.Compare(data[i], max) == mo::CompareResult::kGreater) {
+                max = data[i];
+            }
+        }
+    }
+    return std::make_pair(max, &col.GetType());
+}
+
+std::optional<std::pair<std::byte const*, mo::Type const*>> CsvStats::GetSum(int index) {
+    mo::TypedColumnData const& col = col_data_[index];
+    mo::TypeId type_id = col.GetTypeId();
+    if (type_id == +mo::TypeId::kEmpty || type_id == +mo::TypeId::kNull ||
+        type_id == +mo::TypeId::kUndefined) {
+        return {};
+    }
+    if (col.IsNumeric()) {
+        auto data = col.GetData();
+        mo::INumericType const& type = static_cast<mo::INumericType const&>(col.GetType());
+        std::byte* sum(type.Allocate());
+        for (unsigned i = 0; i < data.size(); ++i) {
+            if (!col.IsNullOrEmpty(i)) {
+                type.Add(sum, data[i], sum);
+            }
+        }
+        return std::make_pair(sum, &col.GetType());
     }
     return {};
 };
 
-std::byte* CsvStats::GetAvg(int index) {
+std::optional<std::pair<std::byte const*, mo::Type const*>> CsvStats::GetAvg(int index) {
     mo::TypedColumnData const& col = col_data_[index];
+    mo::TypeId type_id = col.GetTypeId();
+    if (type_id == +mo::TypeId::kEmpty || type_id == +mo::TypeId::kNull ||
+        type_id == +mo::TypeId::kUndefined) {
+        return {};
+    }
     if (col.IsNumeric()) {
         mo::DoubleType type;
-        mo::INumericType const& ctype = static_cast<mo::INumericType const&>(col.GetType());
-        std::byte* sum = GetSum(index);
+        mo::INumericType const& colType = static_cast<mo::INumericType const&>(col.GetType());
+        std::byte const* sum = GetSum(index)->first;
+        std::byte const* left;
+        if (col.GetType() != type) {
+            left = type.MakeValue(colType.GetValue<mo::Int>(sum));
+        } else {
+            left = sum;
+            sum = nullptr;
+        }
         std::byte* avg = type.Allocate();
-        std::byte* left = type.MakeValue(ctype.GetValue<mo::Int>(sum));
-        std::byte* right = type.MakeValue(col.GetNumRows() - col.GetNumNulls());
-        type.Div(left, right, avg);
-        return avg;
+        std::byte const* countOfNums = type.MakeValue(this->Count(index));
+        type.Div(left, countOfNums, avg);
+
+        type.Free(sum);
+        type.Free(left);
+        type.Free(countOfNums);
+
+        return std::make_pair(avg, &type);
     }
     return {};
 }
 
-std::byte* CsvStats::GetSTD(int index)
-{
+std::optional<std::pair<std::byte const*, mo::Type const*>> CsvStats::GetSTD(int index) {
     mo::TypedColumnData const& col = col_data_[index];
+    mo::TypeId type_id = col.GetTypeId();
+    if (type_id == +mo::TypeId::kEmpty || type_id == +mo::TypeId::kNull ||
+        type_id == +mo::TypeId::kUndefined) {
+        return {};
+    }
     if (col.IsNumeric()) {
+        auto data = col.GetData();
         mo::DoubleType type;
-        mo::INumericType const& cType = static_cast<mo::INumericType const&>(col.GetType());
-        std::byte* avg = GetAvg(index);
-        std::byte* nAvg = type.Allocate();
-        type.Negate(avg, nAvg);
+        mo::INumericType const& colType = static_cast<mo::INumericType const&>(col.GetType());
+        std::byte const* avg = GetAvg(index)->first;
+        std::byte* negAvg = type.Allocate();
+        type.Negate(avg, negAvg);
         std::byte* sum = type.Allocate();
         std::byte* dif = type.Allocate();
-        for (std::byte const* value : col.GetData()) {
-            if(col.GetTypeId() != static_cast<char>(mo::TypeId::kDouble)) {
-                value = type.MakeValue(cType.GetValue<mo::Int>(value));
+        std::byte* tmp;
+        bool notDoubleType = col.GetType() != type;
+        for (unsigned i = 0; i < data.size(); ++i) {
+            if (!col.IsNullOrEmpty(i)) {
+                if (notDoubleType) {
+                    tmp = type.MakeValue(colType.GetValue<mo::Int>(data[i]));
+                }
+                type.Add(tmp, negAvg, dif);
+                type.Power(dif, 2, dif);
+                type.Add(sum, dif, sum);
+                if (notDoubleType) {
+                    type.Free(tmp);
+                }
             }
-            type.Add(value, nAvg, dif);
-            type.Power(dif, 2, dif);
-            type.Add(sum, dif, sum);
         }
-        std::byte* divisor = type.MakeValue(col.GetNumRows() - col.GetNumNulls() - 1);
-        std::byte* dispersion = type.Allocate();
-        type.Div(sum, divisor, dispersion);
-        return type.Power(dispersion, 0.5, dispersion);
+        std::byte* countOfNums = type.MakeValue(this->Count(index) - 1);
+        std::byte* result = type.Allocate();
+        type.Div(sum, countOfNums, result);
+        type.Power(result, 0.5, result);
+
+        type.Free(avg);
+        type.Free(negAvg);
+        type.Free(sum);
+        type.Free(dif);
+        type.Free(countOfNums);
+
+        return std::make_pair(result, &type);
     }
     return {};
 }
 
-std::byte* CsvStats::CountCentrlMomntOfDist(int index, int number) {
+std::optional<std::pair<std::byte const*, mo::Type const*>> CsvStats::CountCentrlMomntOfDist(
+    int index, int number) {
     mo::TypedColumnData const& col = col_data_[index];
     if (col.IsNumeric()) {
+        auto data = col.GetData();
+
         mo::DoubleType type;
-        mo::INumericType const& cType = static_cast<mo::INumericType const&>(col.GetType());
-        std::byte* avg = GetAvg(index);
-        std::byte* nAvg = type.Allocate();
-        type.Negate(avg, nAvg);
+        mo::INumericType const& colType = static_cast<mo::INumericType const&>(col.GetType());
+        std::byte const* avg = GetAvg(index)->first;
+        std::byte* negAvg = type.Allocate();
+        type.Negate(avg, negAvg);
         std::byte* sum = type.Allocate();
         std::byte* dif = type.Allocate();
-        for (std::byte const* value : col.GetData()) {
-            if(col.GetTypeId() != static_cast<char>(mo::TypeId::kDouble)) {
-                value = type.MakeValue(cType.GetValue<mo::Int>(value));
+        std::byte* tmp;
+        bool notDoubleType = col.GetType() != type;
+        for (unsigned i = 0; i < data.size(); ++i) {
+            if (!col.IsNullOrEmpty(i)) {
+                if (notDoubleType) {
+                    tmp = type.MakeValue(colType.GetValue<mo::Int>(data[i]));
+                }
+                type.Add(tmp, negAvg, dif);
+                type.Power(dif, number, dif);
+                type.Add(sum, dif, sum);
+                if (notDoubleType) {
+                    type.Free(tmp);
+                }
             }
-            type.Add(value, nAvg, dif);
-            type.Power(dif, number, dif);
-            type.Add(sum, dif, sum);
         }
-        std::byte* divisor = type.MakeValue(col.GetNumRows() - col.GetNumNulls());
-        return type.Div(sum, divisor, sum);
+        std::byte* countOfNums = type.MakeValue(this->Count(index));
+        std::byte* result = type.Allocate();
+        type.Div(sum, countOfNums, result);
+
+        type.Free(avg);
+        type.Free(negAvg);
+        type.Free(sum);
+        type.Free(dif);
+        type.Free(countOfNums);
+
+        return std::make_pair(result, &type);
     }
     return {};
 }
 
-std::byte* CsvStats::CountStndrtCentrlMomntOfDist(int index, int number) {
+std::optional<std::pair<std::byte const*, mo::Type const*>> CsvStats::CountStndrtCentrlMomntOfDist(
+    int index, int number) {
     mo::DoubleType type;
-    std::byte* left = type.Allocate();
-    left = CountCentrlMomntOfDist(index, number);
-    std::byte* right = type.Allocate();
-    right = GetSTD(index);
-    type.Power(right, number, right);
-    return type.Div(left, right, right);
+    std::byte const* left = CountCentrlMomntOfDist(index, number)->first;
+    std::byte const* right = GetSTD(index)->first;
+    std::byte* result(type.Allocate());
+    type.Power(right, number, result);
+    type.Div(left, result, result);
+
+    type.Free(left);
+    type.Free(right);
+
+    return std::make_pair(result, &type);
 }
 
-std::byte* CsvStats::GetSkewness(int index)
-{
+std::optional<std::pair<std::byte const*, mo::Type const*>> CsvStats::GetSkewness(int index) {
+    mo::TypedColumnData const& col = col_data_[index];
+    mo::TypeId type_id = col.GetTypeId();
+    if (type_id == +mo::TypeId::kEmpty || type_id == +mo::TypeId::kNull ||
+        type_id == +mo::TypeId::kUndefined) {
+        return {};
+    }
     return CountStndrtCentrlMomntOfDist(index, 3);
 }
 
-std::byte* CsvStats::GetKurtosis(int index)
-{
+std::optional<std::pair<std::byte const*, mo::Type const*>> CsvStats::GetKurtosis(int index) {
+    mo::TypedColumnData const& col = col_data_[index];
+    mo::TypeId type_id = col.GetTypeId();
+    if (type_id == +mo::TypeId::kEmpty || type_id == +mo::TypeId::kNull ||
+        type_id == +mo::TypeId::kUndefined) {
+        return {};
+    }
     return CountStndrtCentrlMomntOfDist(index, 4);
 }
 
@@ -138,3 +267,120 @@ int CsvStats::Count(int index) {
     mo::TypedColumnData const& col = col_data_[index];
     return col.GetNumRows() - col.GetNumNulls() - col.GetNumEmpties();
 };
+
+int CsvStats::Distinct(int index) {
+    mo::TypedColumnData const& col = col_data_[index];
+    mo::TypeId type_id = col.GetTypeId();
+    if (type_id == +mo::TypeId::kEmpty || type_id == +mo::TypeId::kNull ||
+        type_id == +mo::TypeId::kUndefined) {
+        return {};
+    }
+
+    std::unordered_set<std::byte> set;
+    auto data = col.GetData();
+
+    for (unsigned i = 0; i < data.size(); ++i) {
+        if (!col.IsNullOrEmpty(i)) {
+            set.insert(*data[i]);
+        }
+    }
+    return set.size();
+}
+
+void CsvStats::ShowSample(int start_row, int end_row, int start_col, int end_col, int str_len,
+                          int int_len, int double_len) {
+    // int quantity_of_headers = end_col - start_col + 1;
+    for (int i = start_row - 1; i <= end_row - 1; ++i) {
+        for (int j = start_col - 1; j <= end_col - 1; ++j) {
+            mo::TypedColumnData const& col = col_data_[j];
+
+            mo::Type const& type = static_cast<mo::Type const&>(col.GetType());
+
+            // mo::AbstractColumnData const& column = static_cast<mo::AbstractColumnData
+            // const&>(col); if (quantity_of_headers != 0) {
+            //     std::cout << std::setw(str_len) << column.GetColumn()->GetName();
+            //     --quantity_of_headers;
+            //     continue;
+            // }
+
+            switch (col.GetTypeId()) {
+            case static_cast<mo::TypeId>(mo::TypeId::kString):
+                std::cout << std::setw(str_len);
+                if (!col.IsNullOrEmpty(i)) {
+                    type.Print(col.GetData()[i], std::cout);
+                }
+                std::cout << "";
+                break;
+            case static_cast<mo::TypeId>(mo::TypeId::kInt):
+                std::cout << std::setw(int_len);
+                if (!col.IsNullOrEmpty(i)) {
+                    type.Print(col.GetData()[i], std::cout);
+                }
+                std::cout << "";
+                break;
+            case static_cast<mo::TypeId>(mo::TypeId::kDouble):
+                std::cout << std::setw(double_len);
+                if (!col.IsNullOrEmpty(i)) {
+                    type.Print(col.GetData()[i], std::cout);
+                }
+                std::cout << "";
+                break;
+            case static_cast<mo::TypeId>(mo::TypeId::kUndefined):
+                if (col.IsEmpty(i)) {
+                    mo::EmptyType empty;
+                    std::cout << std::setw(5);
+                    empty.Print(col.GetData()[i], std::cout);
+                }
+                if (col.IsNull(i)) {
+                    mo::NullType null(true);
+                    std::cout << std::setw(5);
+                    null.Print(col.GetData()[i], std::cout);
+                }
+                break;
+
+            default:
+                break;
+            }
+            std::cout << "\t";
+        }
+        std::cout << std::endl;
+    }
+}
+
+bool CsvStats::isCategorial(int index, int quantity) {
+    return this->Distinct(index) <= quantity ? true : false;
+}
+
+std::vector<std::byte const*> CsvStats::DeleteNullAndEmpties(int index) {
+    mo::TypedColumnData const& col = col_data_[index];
+    auto data = col.GetData();
+    std::vector<std::byte const*> vec = std::move(data);
+    for (unsigned i = 0; i < vec.size(); ++i) {
+        if (col.IsNullOrEmpty(i)) {
+            vec.erase(vec.begin() + i);
+        }
+    }
+    return vec;
+}
+
+std::optional<std::pair<std::byte const*, mo::Type const*>> CsvStats::GetQuantile(double part,
+                                                                                  int index) {
+    mo::TypedColumnData const& col = col_data_[index];
+    mo::TypeId type_id = col.GetTypeId();
+    if (type_id == +mo::TypeId::kEmpty || type_id == +mo::TypeId::kNull ||
+        type_id == +mo::TypeId::kUndefined) {
+        return {};
+    }
+
+    mo::Type const& type = static_cast<mo::Type const&>(col.GetType());
+    auto vec = col.GetData();
+    std::vector<std::byte const*> data;
+    for (unsigned i = 0; i < vec.size(); ++i) {
+        if (!col.IsNullOrEmpty(i)) {
+            data.insert(data.begin(), vec[i]);
+        }
+    }
+    int quantile = data.size() * part;
+    std::nth_element(data.begin(), data.begin() + quantile, data.end(), type.GetComparator());
+    return std::make_pair(data[quantile], &col.GetType());
+}
