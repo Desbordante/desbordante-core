f71df9e0df187ff50de717b58c1097caaf204ad6
Anna Rakhmukova <lessness19@gmail.com>
2022-08-02 12:55:36 +0300

    Refactor SearchTree
    - Divide Remove method into FindNode and CutLeaf methods
    - Create InsertLeafIntoEnd, InsertLeafIntoMiddle and FindNodeAndSetBits methods to shorten Add method
    - Get rid of extra copying
    - Add alias BitsetConsumer for std::function<void(const Bitset&)>
    - Make alias public
    - Modify headers order
    - Fix formatting
    - Make other changes to meet code style requirements and improve readability

--- src/algorithms/AIDFD/SearchTree.h
+++ src/algorithms/AIDFD/SearchTree.h
@@ -1,14 +1,16 @@
 #pragma once
 
+#include <unordered_map>
 #include <utility>
 
-#include "unordered_map"
-#include "boost/dynamic_bitset.hpp"
+#include <boost/dynamic_bitset.hpp>
 
 class SearchTree {
-private:
+public:
     using Bitset = boost::dynamic_bitset<>;
+    using BitsetConsumer = std::function<void(const Bitset&)>;
 
+private:
     struct Node {
         size_t bit_{};
 
@@ -22,22 +24,19 @@ private:
 
         [[nodiscard]] bool IsLeaf() const;
 
-        [[nodiscard]] Bitset GetUnion() const;
-        [[nodiscard]] Bitset GetInter() const;
+        [[nodiscard]] const Bitset& GetUnion() const;
+        [[nodiscard]] const Bitset& GetInter() const;
 
         // for inner nodes
-        Node(size_t bit, Bitset sets_union, Bitset sets_inter,
-             const std::shared_ptr<Node>& parent,
-             std::shared_ptr<Node> left = nullptr,
-             std::shared_ptr<Node> right = nullptr);
+        Node(size_t bit, Bitset sets_union, Bitset sets_inter, const std::shared_ptr<Node>& parent,
+             std::shared_ptr<Node> left = nullptr, std::shared_ptr<Node> right = nullptr);
 
         // for leaves
         Node(size_t bit, Bitset set, const std::shared_ptr<Node>& parent);
 
         // for both types of nodes
         Node(size_t bit, Bitset set, Bitset sets_union, Bitset sets_inter,
-             const std::shared_ptr<Node>& parent,
-             std::shared_ptr<Node> left = nullptr,
+             const std::shared_ptr<Node>& parent, std::shared_ptr<Node> left = nullptr,
              std::shared_ptr<Node> right = nullptr);
     };
 
@@ -48,22 +47,32 @@ private:
     void CreateSingleElementSets(const Bitset& set);
 
     void CollectSubsets(const Bitset& set, const std::shared_ptr<Node>& current_node,
-                        const std::function<void(const Bitset&)>& collect, bool& go_further) const;
+                        const BitsetConsumer& collect, bool& go_further) const;
 
-    void ForEach(const std::shared_ptr<Node>& current_node, const std::function<void (const Bitset&)>& collect) const;
+    void ForEach(const std::shared_ptr<Node>& current_node, const BitsetConsumer& collect) const;
+
+    std::shared_ptr<Node> FindNode(const Bitset& set);
+    void CutLeaf(const std::shared_ptr<Node>& node_to_remove);
+    void InsertLeafIntoEnd(const std::shared_ptr<Node>& current_node, const Bitset& set,
+                 size_t node_bit, size_t set_bit);
+    void InsertLeafIntoMiddle(const std::shared_ptr<Node>& current_node, const Bitset& set, size_t set_bit);
 
     static void UpdateInterAndUnion(const std::shared_ptr<Node>& node);
+
+    static std::pair<size_t, size_t> FindNodeAndSetBits(const Bitset& node_set, const Bitset& set);
 
 public:
     explicit SearchTree(size_t number_of_attributes);
     explicit SearchTree(const Bitset& set);
 
-    [[nodiscard]] size_t GetCardinality() const { return cardinality_; }
+    [[nodiscard]] size_t GetCardinality() const {
+        return cardinality_;
+    }
 
     bool Add(const Bitset& set);
     bool Remove(const Bitset& set);
 
-    void ForEach(const std::function<void (const Bitset&)>& collect) const;
-    void ForEachSubset(const Bitset& set, const std::function<void (const Bitset&)>& collect) const;
+    void ForEach(const BitsetConsumer& collect) const;
+    void ForEachSubset(const Bitset& set, const BitsetConsumer& collect) const;
     [[nodiscard]] bool ContainsAnySubsetOf(const Bitset& set) const;
 };
