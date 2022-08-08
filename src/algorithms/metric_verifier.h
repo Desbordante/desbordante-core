e7f217b5a92a3d0a6ece212217caba16e388cecc
Aviu00 <aviu00@protonmail.com>
2022-08-08 16:28:18 +0300

    Implement metric FD data highlights

--- src/algorithms/metric_verifier.h
+++ src/algorithms/metric_verifier.h
@@ -4,6 +4,7 @@
 #include <filesystem>
 #include <functional>
 #include <memory>
+#include <set>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -18,10 +19,34 @@
 namespace algos {
 
 class MetricVerifier : public algos::Primitive {
+public:
+    struct Highlight {
+        int data_index;
+        long double max_distance;
+
+        Highlight(int data_index, long double max_distance)
+            : data_index(data_index), max_distance(max_distance) {}
+
+        bool operator<(Highlight const& other) const {
+            return max_distance > other.max_distance;
+        }
+    };
+
 private:
     template <typename T>
     using DistanceFunction = std::function<long double(T, T)>;
     using CompareFunction = std::function<bool(util::PLI::Cluster const& cluster)>;
+    template <typename T>
+    using PointsHighlightsPair = std::pair<std::vector<T>, std::multiset<Highlight>>;
+
+    template <typename T>
+    struct IndexedPoint {
+        T point;
+        int index;
+        IndexedPoint() {}
+        IndexedPoint(T point, int index) : point(point), index(index) {}
+        IndexedPoint(IndexedPoint&& p) : point(std::move(p.point)), index(p.index) {}
+    };
 
     Metric metric_ = Metric::_values()[0];
     MetricAlgo algo_ = MetricAlgo::_values()[0];
@@ -33,6 +58,7 @@ private:
     config::EqNullsType is_null_equal_null_;
 
     bool metric_fd_holds_ = false;
+    std::vector<std::multiset<Highlight>> highlights_;
 
     static const config::OptionType<decltype(dist_to_null_infinity_)> DistToNullInfinityOpt;
     static const config::OptionType<decltype(parameter_)> ParameterOpt;
@@ -49,19 +75,22 @@ private:
         model::StringType const& type,
         std::unordered_map<std::string, util::QGramVector>& q_gram_map) const;
 
-    std::vector<std::byte const*> GetVectorOfPoints(util::PLI::Cluster const& cluster) const;
+    PointsHighlightsPair<IndexedPoint<std::byte const*>>
+    GetVectorOfPoints(util::PLI::Cluster const& cluster) const;
     template <typename T>
-    std::vector<T> GetVectorOfMultidimensionalPoints(
+    PointsHighlightsPair<T> GetVectorOfMultidimensionalPoints(
         util::PLI::Cluster const& cluster,
         std::function<void(T&, long double, size_t)> const& assignment_func) const;
 
-    bool CompareNumericValues(util::PLI::Cluster const& cluster) const;
+    bool CompareNumericValues(util::PLI::Cluster const& cluster);
     template <typename T>
-    bool BruteVerifyCluster(std::vector<T> const& points,
-                            DistanceFunction<T> const& dist_func) const;
-    bool CalipersCompareNumericValues(util::PLI::Cluster const& cluster) const;
-    CompareFunction GetCompareFunction() const;
-    bool VerifyMetricFD() const;
+    bool BruteVerifyCluster(std::vector<IndexedPoint<T>> const& indexed_points,
+                            std::multiset<Highlight>& cluster_highlights,
+                            DistanceFunction<T> const& dist_func);
+    bool CalipersCompareNumericValues(util::PLI::Cluster const& cluster);
+    CompareFunction GetCompareFunction();
+    void VerifyMetricFD();
+    void VisualizeHighlights() const;
     static_assert(std::is_same<decltype(MetricVerifier::lhs_indices_),
             decltype(MetricVerifier::rhs_indices_)>{}, "Types of indices must be the same");
     void ValidateIndices(decltype(MetricVerifier::lhs_indices_) const& indices) const;
@@ -76,6 +105,10 @@ protected:
 public:
     bool GetResult() const {
         return metric_fd_holds_;
+    }
+
+    std::vector<std::multiset<Highlight>> GetHighlights() const {
+        return highlights_;
     }
 
     void SetParameter(long double parameter) {
