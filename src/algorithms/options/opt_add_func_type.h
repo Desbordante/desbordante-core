41c5d377000d6f543a16d848db7adfa7a29b5e46
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-11-27 19:43:05 +0300

    Move Primitive to the new configuration procedure

--- src/algorithms/options/opt_add_func_type.h
+++ src/algorithms/options/opt_add_func_type.h
@@ -4,5 +4,5 @@
 #include <string_view>
 
 namespace algos::config {
-using OptAddFunc = std::function<void(std::string_view, std::vector<std::string_view> const &)>;
+using OptAddFunc = std::function<void(IOption *, std::vector<std::string_view> const &)>;
 }  // namespace algos::config
--- src/algorithms/primitive.cpp
+++ src/algorithms/primitive.cpp
@@ -1,26 +1,142 @@
-#include "primitive.h"
+#include "algorithms/primitive.h"
 
 #include <cassert>
 
-void algos::Primitive::AddProgress(double const val) noexcept {
+namespace algos {
+
+bool Primitive::HandleUnknownOption([[maybe_unused]] std::string_view const& option_name,
+                                    [[maybe_unused]] std::optional<boost::any> const& value) {
+    return false;
+}
+
+void Primitive::AddMoreNeededOptions(
+        [[maybe_unused]] std::unordered_set<std::string_view> &previous_options) const {}
+
+void Primitive::MakeExecuteOptsAvailable() {}
+
+config::OptAddFunc Primitive::GetOptAvailFunc() {
+    return [this](auto parent_opt, auto children) { MakeOptionsAvailable(parent_opt, children); };
+}
+
+void Primitive::ClearOptions() noexcept {
+    available_options.clear();
+    opt_parents_.clear();
+}
+
+void Primitive::ExecutePrepare() {
+    fit_completed_ = true;
+    ClearOptions();
+    MakeExecuteOptsAvailable();
+}
+
+Primitive::Primitive(std::vector<std::string_view> phase_names)
+        : phase_names_(std::move(phase_names)) {}
+
+void Primitive::MakeOptionsAvailable(config::IOption *parent,
+                                     std::vector<std::string_view> const &option_names) {
+    MakeOptionsAvailable(option_names);
+    auto it = possible_options_.find(parent->GetName());
+    assert(it != possible_options_.end());
+    for (const auto &option_name: option_names) {
+        opt_parents_[it->first].emplace_back(option_name);
+    }
+}
+
+void Primitive::ExcludeOptions(std::string_view parent_option) noexcept {
+    auto it = opt_parents_.find(parent_option);
+    if (it == opt_parents_.end()) return;
+
+    for (auto const &option_name: it->second) {
+        auto possible_opt_it = possible_options_.find(option_name);
+        assert(possible_opt_it != possible_options_.end());
+        available_options.erase(possible_opt_it->first);
+        UnsetOption(possible_opt_it->first);
+    }
+    opt_parents_.erase(it);
+}
+
+void Primitive::UnsetOption(std::string_view option_name) noexcept {
+    auto it = possible_options_.find(option_name);
+    if (it == possible_options_.end()
+        || available_options.find(it->first) == available_options.end())
+        return;
+    it->second->Unset();
+    ExcludeOptions(it->first);
+}
+
+void Primitive::AddProgress(double const val) noexcept {
     assert(val >= 0);
     std::scoped_lock lock(progress_mutex_);
     cur_phase_progress_ += val;
     assert(cur_phase_progress_ < 101);
 }
 
-void algos::Primitive::SetProgress(double const val) noexcept {
+void Primitive::SetProgress(double const val) noexcept {
     assert(0 <= val && val < 101);
     std::scoped_lock lock(progress_mutex_);
     cur_phase_progress_ = val;
 }
 
-std::pair<uint8_t, double> algos::Primitive::GetProgress() const noexcept {
+void Primitive::MakeOptionsAvailable(const std::vector<std::string_view> &option_names) {
+    for (std::string_view name: option_names) {
+        auto it = possible_options_.find(name);
+        assert(it != possible_options_.end());
+        available_options.insert(it->first);
+    }
+}
+
+void Primitive::Fit(model::IDatasetStream& data) {
+    if (!GetNeededOptions().empty()) throw std::logic_error(
+                "All options need to be set before starting processing.");
+    FitInternal(data);
+    ExecutePrepare();
+}
+
+unsigned long long Primitive::Execute() {
+    if (!fit_completed_) {
+        throw std::logic_error("Data must be processed before execution.");
+    }
+    if (!GetNeededOptions().empty())
+        throw std::logic_error("All options need to be set before execution.");
+    return ExecuteInternal();
+}
+
+void Primitive::SetOption(std::string_view const& option_name,
+                          std::optional<boost::any> const& value) {
+    auto it = possible_options_.find(option_name);
+    if (it == possible_options_.end()) {
+        if (!HandleUnknownOption(option_name, value)) {
+            throw std::invalid_argument("Unknown option \"" + std::string{option_name} + '"');
+        }
+        return;
+    } else if (available_options.find(it->first) == available_options.end()) {
+        throw std::invalid_argument("Invalid option \"" + std::string{option_name} + '"');
+    }
+
+    if (it->second->IsSet()) {
+        UnsetOption(it->first);
+    }
+
+    it->second->Set(value);
+}
+
+std::unordered_set<std::string_view> Primitive::GetNeededOptions() const {
+    std::unordered_set<std::string_view> needed{};
+    for (std::string_view name : available_options) {
+        if (!possible_options_.at(name)->IsSet()) {
+            needed.insert(name);
+        }
+    }
+    AddMoreNeededOptions(needed);
+    return needed;
+}
+
+std::pair<uint8_t, double> Primitive::GetProgress() const noexcept {
     std::scoped_lock lock(progress_mutex_);
     return std::make_pair(cur_phase_id_, cur_phase_progress_);
 }
 
-void algos::Primitive::ToNextProgressPhase() noexcept {
+void Primitive::ToNextProgressPhase() noexcept {
     /* Current phase done, ensure that this is displayed in the progress bar */
     SetProgress(kTotalProgressPercent);
 
@@ -29,3 +145,5 @@ void algos::Primitive::ToNextProgressPhase() noexcept {
     assert(cur_phase_id_ < phase_names_.size());
     cur_phase_progress_ = 0;
 }
+
+}  // namespace algos
