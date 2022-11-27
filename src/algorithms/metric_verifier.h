51aa61d69f323e29f575e530fad0a79fc251996a
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-11-28 01:58:40 +0300

    Split MetricVerifier enums

--- src/algorithms/metric_verifier.h
+++ src/algorithms/metric_verifier.h
@@ -8,31 +8,13 @@
 #include <unordered_map>
 #include <vector>
 
-#include "column_layout_relation_data.h"
-#include "column_layout_typed_relation_data.h"
-#include "primitive.h"
-#include "qgram_vector.h"
+#include "algorithms/metric_verifier_enums.h"
+#include "algorithms/primitive.h"
+#include "model/column_layout_relation_data.h"
+#include "model/column_layout_typed_relation_data.h"
+#include "util/qgram_vector.h"
 
 namespace algos {
-
-BETTER_ENUM(Metric, char,
-            euclidean = 0,  /* Standard metric for calculating the distance between numeric
-                             * values */
-            levenshtein,    /* Levenshtein distance between strings */
-            cosine          /* Represent strings as q-gram vectors and calculate cosine distance
-                             * between these vectors */
-)
-
-BETTER_ENUM(MetricAlgo, char,
-            brute = 0,      /* Brute force algorithm. Calculates distance between all possible pairs
-                             * of values and compares them with parameter */
-            approx,         /* 2-approximation brute force algorithm, that skips some distance
-                             * calculations, but may return false positive result */
-            calipers        /* Rotating calipers algorithm for 2d euclidean metric. Computes a
-                             * convex hull of the points and calculates distances between antipodal
-                             * pairs of convex hull, resulting in a significant reduction in the
-                             * number of comparisons */
-)
 
 class MetricVerifier : public algos::Primitive {
 private:
