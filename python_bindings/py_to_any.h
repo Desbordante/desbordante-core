c63e78ab35d1457f197b36b370c0ef83efb104f4
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-12-01 22:56:08 +0300

    Add PyToAny

---
+++ python_bindings/py_to_any.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <boost/any.hpp>
+#include <pybind11/pybind11.h>
+
+namespace python_bindings {
+boost::any PyToAny(std::type_index index, pybind11::object const& obj);
+}  // namespace python_bindings
