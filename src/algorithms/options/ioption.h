955de2fd904489e8f16a5d3c72b91019ecd8873c
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-12-01 22:54:24 +0300

    Add GetTypeIndex

--- src/algorithms/options/ioption.h
+++ src/algorithms/options/ioption.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include <optional>
 #include <string_view>
+#include <typeindex>
 
 #include "boost/any.hpp"
 
@@ -14,6 +15,7 @@ public:
     virtual void Unset() = 0;
     [[nodiscard]] virtual bool IsSet() const = 0;
     [[nodiscard]] virtual std::string_view GetName() const = 0;
+    [[nodiscard]] virtual std::type_index GetTypeIndex() const = 0;
     virtual ~IOption() = default;
 };
 
