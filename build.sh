16a313d255606a531cc6f4ea3f7e4f52e496108c
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-12-01 22:44:32 +0300

    Add pybind11

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ cd lib
 git clone https://github.com/google/googletest/ --branch release-1.10.0
 git clone https://github.com/amrayn/easyloggingpp/ --branch v9.97.0
 git clone https://github.com/aantron/better-enums.git --branch 0.11.3
+git clone https://github.com/pybind/pybind11.git --branch v2.10
 cd ..
 mkdir build
 cd build
