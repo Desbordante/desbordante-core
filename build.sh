f2c0748ff1fb8a02be710bace492a399c44a28dc
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2022-12-09 19:26:10 +0300

    Update googletest
    The old version did not build with `cmake` (as opposed to `make`).
    This allows Desbordante to be built using default CLion configuration.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir lib
 cd lib
-git clone https://github.com/google/googletest/ --branch release-1.10.0
+git clone https://github.com/google/googletest/ --branch release-1.12.1
 git clone https://github.com/amrayn/easyloggingpp/ --branch v9.97.0
 git clone https://github.com/aantron/better-enums.git --branch 0.11.3
 git clone https://github.com/pybind/pybind11.git --branch v2.10
