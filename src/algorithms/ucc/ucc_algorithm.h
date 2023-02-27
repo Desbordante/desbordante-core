3a1373de52544dd8ed5fba29e1d72255079024a6
Michael Polyntsov <Arno9148@gmail.com>
2023-02-27 19:54:40 +0300

    Implement base class for UCC mining algorithms

---
+++ src/algorithms/ucc/ucc_algorithm.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <list>
+#include <string_view>
+#include <vector>
+
+#include "algorithms/options/equal_nulls_opt.h"
+#include "algorithms/primitive.h"
+#include "model/ucc.h"
+#include "util/primitive_collection.h"
+
+namespace algos {
+
+// Base class for all algorithms that mine UCCs
+class UCCAlgorithm : public Primitive {
+private:
+    void ResetState() final {
+        ucc_collection_.Clear();
+        ResetUCCAlgorithmState();
+    }
+
+    virtual void ResetUCCAlgorithmState() = 0;
+
+protected:
+    // Collection of all mined UCCs. Every UCC mining algorithm must register found uccs here.
+    util::PrimitiveCollection<model::UCC> ucc_collection_;
+    config::EqNullsType is_null_equal_null_{};
+
+    // Pass this value as phase_names to the constructor if your algorithm has only one progress bar
+    // phase.
+    // If your algorithm has no progress bar implemented, pass an empty vector.
+    constexpr static std::string_view kDefaultPhaseName = "UCC mining";
+
+    explicit UCCAlgorithm(std::vector<std::string_view> phase_names)
+        : Primitive(std::move(phase_names)) {
+        RegisterOption(config::EqualNullsOpt.GetOption(&is_null_equal_null_));
+        MakeOptionsAvailable(config::GetOptionNames(config::EqualNullsOpt));
+    }
+
+public:
+    std::list<model::UCC> const& UCCList() const noexcept {
+        return ucc_collection_.AsList();
+    }
+
+    std::list<model::UCC>& UCCList() noexcept {
+        return ucc_collection_.AsList();
+    }
+};
+
+}  // namespace algos
