77ae677a08f4710102dcad478c58f9d9090622b3
Michael Polyntsov <Arno9148@gmail.com>
2023-02-27 19:52:39 +0300

    Define UCC alias

---
+++ src/model/ucc.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "vertical.h"
+
+namespace model {
+
+// UCC stands for Unique Column Colmbinations and denotes a list of columns whose projection
+// contains no duplicate entry. I.e. there is no tuples that have equal values in attributes
+// presenting in the UCC.
+// Defining UCC as Vertical, because we don't really need more functionality than Vertical provides.
+using UCC = Vertical;
+
+}  // namespace model
