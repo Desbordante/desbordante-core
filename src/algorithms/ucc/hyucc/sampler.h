66884b5747a71c0965665a145b8b739ecd4cb117
Michael Polyntsov <Arno9148@gmail.com>
2023-03-04 19:14:15 +0300

    Implement HyUCC Sampler and Preprocessor

---
+++ src/algorithms/ucc/hyucc/sampler.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "hycommon/sampler.h"
+#include "hycommon/types.h"
+#include "ucc/hyucc/structures/non_ucc_list.h"
+
+namespace algos {
+
+class Sampler {
+private:
+    hy::Sampler sampler_;
+
+public:
+    Sampler(hy::PLIsPtr plis, hy::RowsPtr pli_records)
+        : sampler_(std::move(plis), std::move(pli_records)) {}
+
+    NonUCCList GetNonUCCs(hy::IdPairs const& comparison_suggestions) {
+        return sampler_.GetAgreeSets(comparison_suggestions);
+    }
+};
+
+}  // namespace algos
