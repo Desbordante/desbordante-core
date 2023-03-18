0a2ac31dc7ddf219bba620fbb4b569eecce5b6c6
Aviu00 <aviu00@protonmail.com>
2023-03-18 16:56:47 +0300

    Implement python bindings for FDVerifier

---
+++ python_bindings/py_fd_verifier.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "algorithms/fd_verifier/fd_verifier.h"
+#include "py_primitive.h"
+
+namespace python_bindings {
+
+class PyFDVerifier : public PyPrimitive<algos::fd_verifier::FDVerifier> {
+public:
+    bool FDHolds() const {
+        return primitive_.FDHolds();
+    }
+
+    size_t GetNumErrorClusters() const {
+        return primitive_.GetNumErrorClusters();
+    }
+
+    size_t GetNumErrorRows() const {
+        return primitive_.GetNumErrorRows();
+    }
+
+    long double GetError() const {
+        return primitive_.GetError();
+    }
+};
+
+}  // namespace python_bindings
