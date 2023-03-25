54e0be78fecbcb3069fd0c030afa0f1b394dcaa0
xJoskiy <pavel5553-20@mail.ru>
2023-03-25 22:10:42 +0300

    Add mean absolute deviation in column statistic

--- src/algorithms/statistics/statistic.cpp
+++ src/algorithms/statistics/statistic.cpp
@@ -110,6 +110,7 @@ std::unordered_map<std::string, std::string> ColumnStats::ToKeyValueMap() const 
     try_add_stat(num_negatives, "num_negatives");
     try_add_stat(sum_of_squares, "sum_of_squares");
     try_add_stat(geometric_mean, "geometric_mean");
+    try_add_stat(mean_ad, "mean_ad");
 
     return res;
 }
