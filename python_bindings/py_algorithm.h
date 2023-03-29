278c231feb17babb97e467171a7c01ceed152528
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-03-29 23:05:50 +0300

    Allow setting options one by one in Python
    This is useful for configuring algorithms in Python scripts. Right now
    it would be impossible to do cleanly for an arbitrary algorithm, as the
    current way of configuring is only meant for the Python shell.

--- python_bindings/py_algorithm.h
+++ python_bindings/py_algorithm.h
@@ -20,6 +20,10 @@ protected:
     void Configure(pybind11::kwargs const& kwargs);
 
 public:
+    void SetOption(std::string const& option_name, pybind11::object const& option_value);
+
+    [[nodiscard]] std::unordered_set<std::string_view> GetNeededOptions() const;
+
     // For pandas dataframes
     void Fit(pybind11::object dataframe, std::string name, pybind11::kwargs const& kwargs);
 
