efddf7da9ea0e10e5713f6a26da6f04cfd1b6aea
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-03-31 22:47:39 +0300

    Add StringDataframeReader for special DataFrames
    The normal way of reading from a dataframe is rather slow because of
    the conversions to string, so this commit adds a new class for reading
    from string-only dataframes, which allows for faster reading.

---
+++ python_bindings/dataframe_reader.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <pybind11/pybind11.h>
+
+#include "model/idataset_stream.h"
+
+namespace python_bindings {
+
+class DataframeReaderBase : public model::IDatasetStream {
+protected:
+    pybind11::object dataframe_;
+    pybind11::iterator df_iter_;
+    std::string name_;
+    std::vector<std::string> column_names_;
+
+public:
+    explicit DataframeReaderBase(pybind11::object dataframe, std::string name = "Pandas dataframe");
+
+    void Reset() final;
+    std::string GetRelationName() const final;
+    std::string GetColumnName(int index) const final;
+    size_t GetNumberOfColumns() const final;
+    bool HasNextRow() const final;
+};
+
+class StringDataframeReader final : public DataframeReaderBase {
+public:
+    using DataframeReaderBase::DataframeReaderBase;
+
+    std::vector<std::string> GetNextRow() final;
+};
+
+class ArbitraryDataframeReader final : public DataframeReaderBase {
+private:
+    // Used in GetNextRow to check arbitrary dataframe values for nullity.
+    // Statically storing this may cause problems with garbage collection, but
+    // importing a module on every call may cause performance problems, and
+    // storing it as a field is a compromise, which is used here.
+    std::function<bool(pybind11::handle)> is_null_ =
+            [check_null = pybind11::module_::import("pandas").attr("isna")](pybind11::handle obj) {
+                return check_null(obj).cast<bool>();
+            };
+
+public:
+    using DataframeReaderBase::DataframeReaderBase;
+
+    std::vector<std::string> GetNextRow() final;
+};
+
+}  // namespace python_bindings
