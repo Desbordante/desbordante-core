f379409a57a9dfddc6bc4ee1cf6d32768dd02282
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-03-31 22:47:39 +0300

    Rewrite PandasReader
    The normal way of reading from a dataframe is rather slow because of
    the conversions, so this allows the user to pass a string-only
    dataframe for faster reading.

--- python_bindings/pandas_reader.cpp
+++ python_bindings/pandas_reader.cpp
@@ -4,7 +4,10 @@
 #include <utility>
 #include <vector>
 
+#include <Python.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/pytypes.h>
+#include <pybind11/stl.h>
 
 namespace python_bindings {
 
@@ -13,44 +16,58 @@ namespace py = pybind11;
 static std::vector<std::string> GetColumnNames(pybind11::object const& dataframe) {
     std::vector<std::string> names;
     py::list name_lst = dataframe.attr("columns").attr("to_list")();
-    for (auto element : name_lst) {
+    for (py::handle element : name_lst) {
         names.emplace_back(py::str(element));
     }
     return names;
 }
 
-PandasReader::PandasReader(pybind11::object dataframe, std::string name)
+PandasReaderBase::PandasReaderBase(pybind11::object dataframe, std::string name)
     : dataframe_(std::move(dataframe)),
+      df_iter_(dataframe_.attr("itertuples")(false, py::none{})),
       name_(std::move(name)),
       column_names_(GetColumnNames(dataframe_)) {}
 
-void PandasReader::Reset() {
-    row_ = 0;
+void PandasReaderBase::Reset() {
+    df_iter_ = dataframe_.attr("itertuples")(false, py::none{});
 }
 
-std::string PandasReader::GetRelationName() const {
+std::string PandasReaderBase::GetRelationName() const {
     return name_;
 }
 
-std::string PandasReader::GetColumnName(int index) const {
+std::string PandasReaderBase::GetColumnName(int index) const {
     return column_names_.at(index);
 }
 
-size_t PandasReader::GetNumberOfColumns() const {
+size_t PandasReaderBase::GetNumberOfColumns() const {
     return column_names_.size();
 }
 
-bool PandasReader::HasNextRow() const {
-    return row_ < pybind11::len(dataframe_);
+bool PandasReaderBase::HasNextRow() const {
+    return df_iter_ != py::iterator::sentinel();
 }
 
-std::vector<std::string> PandasReader::GetNextRow() {
+template <>
+std::vector<std::string> PandasReader<false>::GetNextRow() {
+    return py::cast<std::vector<std::string>>(*df_iter_++);
+}
+
+template <>
+std::vector<std::string> PandasReader<true>::GetNextRow() {
     std::vector<std::string> strings{};
-    auto list = pybind11::list(dataframe_.attr("iloc")[pybind11::int_(row_)]);
-    for (auto el : list) {
-        strings.emplace_back(pybind11::str(el));
+    auto list = *df_iter_++.cast<py::list>();
+    for (py::handle el : list) {
+        // When reading from .csv files, pandas treats some values as nulls,
+        // and empty values are among those values, which may cause some
+        // confusion here, since in Desbordante only the literal "NULL" string
+        // is interpreted as the null value.
+        if (py::isinstance<py::float_>(el) && std::isnan(el.cast<double>())) {
+            strings.emplace_back("NULL");
+        } else {
+            strings.emplace_back(pybind11::str(el));
+        }
     }
-    ++row_;
     return strings;
 }
 
--- python_bindings/pandas_reader.h
+++ python_bindings/pandas_reader.h
@@ -6,21 +6,29 @@
 
 namespace python_bindings {
 
-class PandasReader final : public model::IDatasetStream {
+class PandasReaderBase : public model::IDatasetStream {
+protected:
     pybind11::object dataframe_;
+    pybind11::iterator df_iter_;
     std::string name_;
     std::vector<std::string> column_names_;
-    pybind11::size_t row_ = 0;
 
 public:
-    PandasReader(pybind11::object dataframe, std::string name = "Pandas dataframe");
+    PandasReaderBase(pybind11::object dataframe, std::string name = "Pandas dataframe");
 
-    void Reset() override;
-    std::string GetRelationName() const override;
-    std::string GetColumnName(int index) const override;
-    size_t GetNumberOfColumns() const override;
-    bool HasNextRow() const override;
-    std::vector<std::string> GetNextRow() override;
+    void Reset() final;
+    std::string GetRelationName() const final;
+    std::string GetColumnName(int index) const final;
+    size_t GetNumberOfColumns() const final;
+    bool HasNextRow() const final;
+};
+
+template <bool transform_to_string>
+class PandasReader final : public PandasReaderBase {
+public:
+    using PandasReaderBase::PandasReaderBase;
+
+    std::vector<std::string> GetNextRow() final;
 };
 
 }  // namespace python_bindings
