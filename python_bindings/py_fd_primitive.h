1bcff47b3807ad7ecc4c9b5185bcd54f5d0f05d8
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-03-31 22:55:53 +0300

    Separate PyFD from PyFDAlgorithm

--- python_bindings/py_fd_primitive.h
+++ python_bindings/py_fd_primitive.h
@@ -1,48 +1,11 @@
 #pragma once
 
-#include <sstream>
-#include <utility>
 #include <vector>
 
-#include <pybind11/stl.h>
-
-#include "model/fd.h"
-#include "model/raw_fd.h"
+#include "py_fd.h"
 #include "py_primitive.h"
 
 namespace python_bindings {
-
-class PyFD {
-    std::vector<unsigned int> lhs_indices_{};
-    size_t rhs_index_;
-
-public:
-    explicit PyFD(RawFD const& fd) : rhs_index_(fd.rhs_) {
-        for (size_t index = fd.lhs_.find_first(); index != boost::dynamic_bitset<>::npos;
-             index = fd.lhs_.find_next(index)) {
-            lhs_indices_.emplace_back(index);
-        }
-    }
-
-    [[nodiscard]] std::string ToString() const {
-        std::stringstream stream;
-        stream << "( ";
-        for (auto index : lhs_indices_) {
-            stream << index << " ";
-        }
-        stream << ") -> ";
-        stream << rhs_index_;
-        return stream.str();
-    }
-
-    [[nodiscard]] pybind11::int_ GetRhs() const {
-        return rhs_index_;
-    }
-
-    [[nodiscard]] std::vector<unsigned int> GetLhs() const {
-        return lhs_indices_;
-    }
-};
 
 template <typename PrimitiveType>
 class PyFDPrimitive : public PyPrimitive<PrimitiveType> {
