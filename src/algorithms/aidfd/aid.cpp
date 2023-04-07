Desbordante/desbordante-core

commit 4dc1ff884050ecbed17c9614fd1ca2bb55ede612

Implement ResetStateFd method for Aid

src/algorithms/aidfd/aid.cpp
@@ -30,12 +30,14 @@ void Aid::FitFd(model::IDatasetStream& data_stream) {
         }
     }
     number_of_tuples_ = tuples_.size();
-    clusters_ = std::vector<std::unordered_map<size_t, Cluster>>(number_of_attributes_);
-    indices_in_clusters_ = std::vector<std::vector<size_t>>(
-            number_of_attributes_, std::vector<size_t>(number_of_tuples_));
-    constant_columns_ = boost::dynamic_bitset<>(number_of_attributes_);
-    prev_ratios_ = std::vector<double>(window_size_, 1.0);
-    sum_ = static_cast<double>(window_size_);
+}
+
+void Aid::ResetStateFd() {
+    clusters_.assign(number_of_attributes_, std::unordered_map<size_t, Cluster>{});
+    indices_in_clusters_.assign(number_of_attributes_, std::vector<size_t>{number_of_tuples_});
+    constant_columns_.reset();
+    prev_ratios_.assign(window_size_, 1.0);
+    sum_ = double{window_size_};
 }
 
 unsigned long long Aid::ExecuteInternal() {

src/algorithms/aidfd/aid.h
@@ -38,6 +38,8 @@ private:
 
     boost::dynamic_bitset<> constant_columns_;
 
+    void ResetStateFd() final;
+
     void FitFd(model::IDatasetStream &data_stream) final;
     unsigned long long ExecuteInternal() final;
 

commit 08c196c2505e1412e84fcb9e0b97aa336728339d

Change field initialization in Aid::ResetStateFd
Fixes a crash introduced in 4dc1ff884050ecbed17c9614fd1ca2bb55ede612

src/algorithms/aidfd/aid.cpp
@@ -30,11 +30,12 @@ void Aid::FitFd(model::IDatasetStream& data_stream) {
         }
     }
     number_of_tuples_ = tuples_.size();
+    constant_columns_ = boost::dynamic_bitset<>(number_of_attributes_);
 }
 
 void Aid::ResetStateFd() {
     clusters_.assign(number_of_attributes_, std::unordered_map<size_t, Cluster>{});
-    indices_in_clusters_.assign(number_of_attributes_, std::vector<size_t>{number_of_tuples_});
+    indices_in_clusters_.assign(number_of_attributes_, std::vector<size_t>(number_of_tuples_));
     constant_columns_.reset();
     prev_ratios_.assign(window_size_, 1.0);
     sum_ = double{window_size_};
08c196c2505e1412e84fcb9e0b97aa336728339d
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-04-06 23:57:00 +0300

    Change field initialization in Aid::ResetStateFd
    Fixes a crash introduced in 4dc1ff884050ecbed17c9614fd1ca2bb55ede612

--- src/algorithms/aidfd/aid.cpp
+++ src/algorithms/aidfd/aid.cpp
@@ -30,11 +30,12 @@ void Aid::FitFd(model::IDatasetStream& data_stream) {
         }
     }
     number_of_tuples_ = tuples_.size();
+    constant_columns_ = boost::dynamic_bitset<>(number_of_attributes_);
 }
 
 void Aid::ResetStateFd() {
     clusters_.assign(number_of_attributes_, std::unordered_map<size_t, Cluster>{});
-    indices_in_clusters_.assign(number_of_attributes_, std::vector<size_t>{number_of_tuples_});
+    indices_in_clusters_.assign(number_of_attributes_, std::vector<size_t>(number_of_tuples_));
     constant_columns_.reset();
     prev_ratios_.assign(window_size_, 1.0);
     sum_ = double{window_size_};
