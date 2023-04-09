9ca8a1b515a59fcf01786e8f06daee070487f589
Michael Polyntsov <Arno9148@gmail.com>
2023-04-09 17:13:19 +0300

    Add description to general hyfd (and hyucc) types

--- src/algorithms/hyfd/types.h
+++ src/algorithms/hyfd/types.h
@@ -10,11 +10,16 @@ class PositionListIndex;
 
 namespace algos::hyfd {
 
+// Represents a relation as a list of position list indexes. i-th PLI is a PLI built on i-th column
+// of the relation
 using PLIs = std::vector<util::PositionListIndex*>;
 using PLIsPtr = std::shared_ptr<PLIs>;
+// Represents a relation as a list of rows where each row is a list of row values
 using Rows = std::vector<std::vector<size_t>>;
+// Represents a relation as a list of column where each column is a list of column values
 using Columns = std::vector<std::vector<size_t>>;
 using RowsPtr = std::shared_ptr<Rows>;
+// Pair of row numbers
 using IdPairs = std::vector<std::pair<size_t, size_t>>;
 
 }  // namespace algos::hyfd
