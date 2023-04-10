547d42c84daffe7cf240438a1148aeda5588905c
Michael Polyntsov <Arno9148@gmail.com>
2023-04-11 02:05:48 +0300

    Implement UCCTree, a HyUCC's prefix tree of UCCs

---
+++ src/algorithms/ucc/hyucc/structures/ucc_tree.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <memory>
+
+#include <boost/dynamic_bitset.hpp>
+
+#include "ucc_tree_vertex.h"
+
+namespace algos {
+
+class UCCTree {
+private:
+    std::unique_ptr<UCCTreeVertex> root_;
+
+public:
+    explicit UCCTree(size_t num_attributes) : root_(UCCTreeVertex::Create(num_attributes, false)) {
+        root_->InitChildren(true);
+    }
+
+    [[nodiscard]] size_t GetNumAttributes() const noexcept {
+        return root_->GetNumAttributes();
+    }
+
+    [[nodiscard]] std::vector<boost::dynamic_bitset<>> GetUCCAndGeneralizations(
+            boost::dynamic_bitset<> const& ucc) {
+        return root_->GetUCCAndGeneralizationsRecursive(ucc);
+    }
+
+    void Remove(boost::dynamic_bitset<> const& ucc) {
+        root_->RemoveRecursive(ucc, ucc.find_first());
+    }
+
+    [[nodiscard]] bool FindUCCOrGeneralization(boost::dynamic_bitset<> const& ucc) {
+        return root_->FindUCCOrGeneralizationRecursive(ucc, ucc.find_first());
+    }
+
+    [[nodiscard]] std::vector<LhsPair> GetLevel(unsigned target_level) {
+        return root_->GetLevelRecursive(target_level);
+    }
+
+    UCCTreeVertex* AddUCC(boost::dynamic_bitset<> const& ucc, bool* is_new_out = nullptr);
+    [[nodiscard]] UCCTreeVertex* AddUCCGetIfNew(boost::dynamic_bitset<> const& ucc);
+    [[nodiscard]] std::vector<boost::dynamic_bitset<>> FillUCCs() const;
+};
+
+}  // namespace algos
