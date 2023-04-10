ff8a9e7d21b6eea5971dc3337c7a7e1098e48692
Michael Polyntsov <Arno9148@gmail.com>
2023-04-11 01:32:49 +0300

    Restore LHS after sorting in Preprocessor in separate function
    The same functionality is needed for HyUCC, so move it out to the
    separate function

--- src/algorithms/hyfd/preprocessor.h
+++ src/algorithms/hyfd/preprocessor.h
@@ -2,11 +2,15 @@
 
 #include <tuple>
 
+#include <boost/dynamic_bitset.hpp>
+
 #include "model/column_layout_relation_data.h"
 #include "types.h"
 
 namespace algos::hyfd {
 
 std::tuple<PLIs, Rows, std::vector<size_t>> Preprocess(ColumnLayoutRelationData *relation);
+boost::dynamic_bitset<> RestoreAgreeSet(boost::dynamic_bitset<> const& as,
+                                        std::vector<size_t> const& og_mapping, size_t num_cols);
 
 }  // namespace algos::hyfd
