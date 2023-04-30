c67cc402cfb65f4a7cf26c6748f3e29d5e1f6ae7
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-04-30 17:44:53 +0300

    Add DESBORDANTE_OPTION_USING macro
    Specifying full names for things in algos::config::{names,descriptions}
    namespaces can get annoying but `using namespace...` declarations are
    repetitive, so this macro should be used when an algorithm registers an
    option using the util::config::Option class directly.

---
+++ src/util/config/option_using.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define DESBORDANTE_OPTION_USING                \
+    using namespace util::config::names;        \
+    using namespace util::config::descriptions; \
+    using util::config::Option
