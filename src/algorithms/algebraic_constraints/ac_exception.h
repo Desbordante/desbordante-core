5ac9284b603a1c737accced26e3f7ed83f48d75e
Sched71 <scheka.dm@gmail.com>
2023-05-02 17:51:35 +0300

    Remove pairing_rule option from ACAlgorithm
    ACAlgorithm was implemented for trivial pairing rule. Non-trivial
    pairing rules seem non-informative, so they won`t be added

--- src/algorithms/algebraic_constraints/ac_exception.h
+++ src/algorithms/algebraic_constraints/ac_exception.h
@@ -6,7 +6,9 @@
 
 namespace algos {
 
-/* Row that has value pairs, that are exceptions */
+/* Row that has value pairs, that are exceptions. Exception is a (a_i, b_i) value pair
+ * from columns A and B, that has result of binary operation not belonging to any
+ * range discovered for (A, B) column pair */
 struct ACException {
     ACException(size_t row_i, std::pair<size_t, size_t> col_pair)
         : row_i(row_i), column_pairs{col_pair} {}
