3d237ad467ffc1a6d956ae71259bfe37edf44620
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-05-04 16:21:14 +0300

    Return `frozenset` of type tuples in GetPyType
    Allows to denote when multiple types can be accepted.

--- python_bindings/get_py_type.h
+++ python_bindings/get_py_type.h
@@ -5,5 +5,5 @@
 #include <pybind11/pybind11.h>
 
 namespace python_bindings {
-pybind11::tuple GetPyType(std::type_index type_index);
+pybind11::frozenset GetPyType(std::type_index type_index);
 }  // namespace python_bindings
