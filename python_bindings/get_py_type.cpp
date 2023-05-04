db80e859077ec98f5bdf8677baca34a1f3dac81c
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-05-04 16:21:14 +0300

    Return `frozenset` of type tuples in `GetPyType`
    Allows to denote when multiple types can be accepted.

--- python_bindings/get_py_type.cpp
+++ python_bindings/get_py_type.cpp
@@ -14,36 +14,54 @@ namespace py = pybind11;
 
 namespace python_bindings {
 
+constexpr static PyTypeObject* const py_int = &PyLong_Type;
+constexpr static PyTypeObject* const py_bool = &PyBool_Type;
+constexpr static PyTypeObject* const py_float = &PyFloat_Type;
+constexpr static PyTypeObject* const py_str = &PyUnicode_Type;
+constexpr static PyTypeObject* const py_list = &PyList_Type;
+
+static py::handle MakeType(PyTypeObject* py_type_ptr) {
+    // Does the same as `&py_type_ptr->ob_base.ob_base`: Python API simulates
+    // inheritance here. There is also the `_PyObject_CAST` macro that does a
+    // C-style cast, but, as indicated by the underscore, it's an internal
+    // Python macro, so it should be avoided.
+    return reinterpret_cast<PyObject*>(py_type_ptr);
+}
+
+static py::handle MakeType(PyObject* py_type_ptr) {
+    return py_type_ptr;
+}
+
+template <typename... TypePtrs>
+static py::tuple MakeTypeTuple(TypePtrs... type_ptrs) {
+    return py::make_tuple(MakeType(type_ptrs)...);
+}
+
 template <typename CppType, PyTypeObject*... PyTypes>
-static std::pair<std::type_index, std::function<py::tuple()>> const TypeTuplePair{
+static std::pair<std::type_index, std::function<py::frozenset()>> const SimplePyTypeInfoPair{
         std::type_index{typeid(CppType)},
-        []() { return py::make_tuple(py::handle{reinterpret_cast<PyObject*>(PyTypes)}...); }};
+        []() { return py::frozenset{py::make_tuple(MakeTypeTuple(PyTypes...))}; }};
 
-py::tuple GetPyType(std::type_index type_index) {
-    constexpr static PyTypeObject* const py_int = &PyLong_Type;
-    constexpr static PyTypeObject* const py_bool = &PyBool_Type;
-    constexpr static PyTypeObject* const py_float = &PyFloat_Type;
-    constexpr static PyTypeObject* const py_str = &PyUnicode_Type;
-    constexpr static PyTypeObject* const py_list = &PyList_Type;
-
-    // Type indexes are mapped to Python type tuples. The first element in the
-    // tuple is the type of the parameter itself, other elements are
-    // context-dependent.
-    // The tuples are created from Python API's raw pointers when needed as
-    // storing pybind11's objects themselves statically is unpredictable and can
-    // lead to errors related to garbage collection.
-    static const std::unordered_map<std::type_index, std::function<py::tuple()>> type_map{
-            TypeTuplePair<bool, py_bool>,
-            TypeTuplePair<ushort, py_int>,
-            TypeTuplePair<int, py_int>,
-            TypeTuplePair<unsigned int, py_int>,
-            TypeTuplePair<double, py_float>,
-            TypeTuplePair<long double, py_float>,
-            TypeTuplePair<algos::metric::Metric, py_str>,
-            TypeTuplePair<algos::metric::MetricAlgo, py_str>,
-            TypeTuplePair<algos::InputFormat, py_str>,
-            // The second type denotes the type of list elements.
-            TypeTuplePair<std::vector<unsigned int>, py_list, py_int>,
+py::frozenset GetPyType(std::type_index type_index) {
+    // Type indexes are mapped to frozensets of Python type tuples. The first
+    // element of all the tuples is the type of the parameter itself. If that
+    // element is `list`, the following element denotes the type of its
+    // elements. If that parameter is `tuple` then the following parameters
+    // denote the types of their respective elements.
+    // The tuples are created at runtime from Python API's raw pointers (when
+    // possible) as storing pybind11's objects themselves statically is
+    // unpredictable and can lead to errors related to garbage collection.
+    static const std::unordered_map<std::type_index, std::function<py::frozenset()>> type_map{
+            SimplePyTypeInfoPair<bool, py_bool>,
+            SimplePyTypeInfoPair<ushort, py_int>,
+            SimplePyTypeInfoPair<int, py_int>,
+            SimplePyTypeInfoPair<unsigned int, py_int>,
+            SimplePyTypeInfoPair<double, py_float>,
+            SimplePyTypeInfoPair<long double, py_float>,
+            SimplePyTypeInfoPair<algos::metric::Metric, py_str>,
+            SimplePyTypeInfoPair<algos::metric::MetricAlgo, py_str>,
+            SimplePyTypeInfoPair<algos::InputFormat, py_str>,
+            SimplePyTypeInfoPair<std::vector<unsigned int>, py_list, py_int>,
     };
     return type_map.at(type_index)();
 }
