Arrno/Desbordante

Implement HyUCC Validator

src/algorithms/ucc/hyucc/hyucc.cpp
@@ -2,10 +2,13 @@
 
 #include <chrono>
 
+#include <easylogging++.h>
+
 #include "hycommon/types.h"
 #include "inductor.h"
 #include "preprocessor.h"
 #include "sampler.h"
+#include "validator.h"
 
 namespace algos {
 
@@ -18,28 +21,57 @@ void HyUCC::FitInternal(model::IDatasetStream& data_stream) {
 }
 
 unsigned long long HyUCC::ExecuteInternal() {
+    using namespace hy;
     auto const start_time = std::chrono::system_clock::now();
 
     auto [plis, pli_records, og_mapping] = Preprocess(relation_.get());
-    auto const plis_shared = std::make_shared<hy::PLIs>(std::move(plis));
-    auto const pli_records_shared = std::make_shared<hy::Rows>(std::move(pli_records));
+    auto const plis_shared = std::make_shared<PLIs>(std::move(plis));
+    auto const pli_records_shared = std::make_shared<Rows>(std::move(pli_records));
 
-    Sampler sampler(plis_shared, pli_records_shared);
+    algos::Sampler sampler(plis_shared, pli_records_shared);
 
     auto ucc_tree = std::make_unique<UCCTree>(relation_->GetNumColumns());
     Inductor inductor(ucc_tree.get());
+    Validator validator(ucc_tree.get(), plis_shared, pli_records_shared);
 
-    hy::IdPairs comparison_suggestions;
+    IdPairs comparison_suggestions;
 
     while (true) {
-        auto non_uccs = sampler.GetNonUCCCandidates(comparison_suggestions);
+        LOG(DEBUG) << "Sampling...";
+        NonUCCList non_uccs = sampler.GetNonUCCCandidates(comparison_suggestions);
 
+        LOG(DEBUG) << "Inducing...";
         inductor.UpdateUCCTree(std::move(non_uccs));
+
+        LOG(DEBUG) << "Validating...";
+        comparison_suggestions = validator.ValidateAndExtendCandidates();
+
+        if (comparison_suggestions.empty()) {
+            break;
+        }
+    }
+
+    auto uccs = ucc_tree->FillUCCs();
+    RegisterUCCs(std::move(uccs), og_mapping);
+
+    LOG(DEBUG) << "Mined UCCs:";
+    for (model::UCC const& ucc : UCCList()) {
+        LOG(DEBUG) << ucc.ToString();
     }
 
     auto elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(
             std::chrono::system_clock::now() - start_time);
     return elapsed_milliseconds.count();
+}
+
+void HyUCC::RegisterUCCs(std::vector<boost::dynamic_bitset<>>&& uccs,
+                         const std::vector<size_t>& og_mapping) {
+    const auto* const schema = relation_->GetSchema();
+    for (auto&& ucc : uccs) {
+        boost::dynamic_bitset<> mapped_ucc =
+                hy::RestoreAgreeSet(ucc, og_mapping, schema->GetNumColumns());
+        ucc_collection_.Register(schema, std::move(mapped_ucc));
+    }
 }
 
 }  // namespace algos

src/algorithms/ucc/hyucc/validator.cpp
@@ -1,0 +1,152 @@
+#include "validator.h"
+
+#include "hycommon/validator_helpers.h"
+#include "hyfd/hyfd_config.h"
+#include "ucc/hyucc/structures/ucc_tree_vertex.h"
+
+namespace {
+
+using model::RawUCC;
+
+// Checkout comment for Validator::ValidateAndExtendCandidates, it's appropriate for this function
+// too
+size_t AddExtendedCandidatesFromInvalid(std::vector<algos::LhsPair>& next_level,
+                                        algos::UCCTree& ucc_tree,
+                                        std::vector<RawUCC> const& invalid_uccs,
+                                        size_t num_attributes) {
+    size_t candidates = 0;
+    for (auto const& ucc : invalid_uccs) {
+        for (size_t attr = 0; attr < num_attributes; ++attr) {
+            if (ucc.test(attr)) {
+                continue;
+            }
+
+            RawUCC ucc_ext = ucc;
+            ucc_ext.set(attr);
+
+            if (ucc_tree.FindUCCOrGeneralization(ucc_ext)) {
+                continue;
+            }
+
+            algos::UCCTreeVertex* child = ucc_tree.AddUCCGetIfNew(ucc_ext);
+            if (child == nullptr) {
+                continue;
+            }
+            next_level.emplace_back(child, std::move(ucc_ext));
+            candidates++;
+        }
+    }
+    return candidates;
+}
+
+}  // namespace
+
+namespace algos {
+
+using model::RawUCC;
+
+bool Validator::IsUnique(util::PLI const& pivot_pli, RawUCC const& ucc,
+                         hy::IdPairs& comparison_suggestions) {
+    std::vector<size_t> indices = util::BitsetToIndices<size_t>(ucc);
+    for (util::PLI::Cluster const& cluster : pivot_pli.GetIndex()) {
+        auto cluster_to_record =
+                hy::MakeClusterIdentifierToTMap<util::PLI::Cluster::value_type>(cluster.size());
+        for (auto const record_id : cluster) {
+            std::vector<size_t> cluster_id =
+                    hy::BuildClustersIdentifier((*compressed_records_)[record_id], indices);
+            if (cluster_id.empty()) {
+                continue;
+            }
+
+            auto const [it, inserted] = cluster_to_record.try_emplace(cluster_id, record_id);
+            if (!inserted) {
+                comparison_suggestions.emplace_back(record_id, it->second);
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+Validator::UCCValidations Validator::GetValidations(LhsPair const& vertex_and_ucc) {
+    auto [vertex, ucc] = vertex_and_ucc;
+    UCCValidations validations;
+    validations.set_count_validations(1);
+    validations.set_count_intersections(1);
+
+    size_t ucc_attr = ucc.find_first();
+    bool is_unique;
+    if (current_level_number_ == 1) {
+        assert(ucc_attr != boost::dynamic_bitset<>::npos);
+        is_unique = (*plis_)[ucc_attr]->AllValuesAreUnique();
+    } else {
+        ucc.reset(ucc_attr);
+        // It's guaranteed that the first cluster has the fewest records because of sorting
+        // in the Sampler
+        util::PLI const* pivot_pli = (*plis_)[ucc_attr];
+        is_unique = IsUnique(*pivot_pli, ucc, validations.comparison_suggestions());
+        ucc.set(ucc_attr);
+    }
+
+    if (!is_unique) {
+        vertex->SetIsUCC(false);
+        validations.invalid_instances().push_back(std::move(ucc));
+    }
+
+    return validations;
+}
+
+Validator::UCCValidations Validator::ValidateAndExtendSeq(
+        std::vector<LhsPair> const& current_level) {
+    UCCValidations result;
+    for (auto const& vertex_and_ucc : current_level) {
+        if (!vertex_and_ucc.first->IsUCC()) {
+            continue;
+        }
+        result.Add(GetValidations(vertex_and_ucc));
+    }
+    return result;
+}
+
+// This function is similar to hyfd::Validator::ValidateAndExtendCandidates(), but with some key
+// differences in the way it initializes the current_level and handles loop conditions. While there
+// is some shared structure, attempting to refactor the functions to eliminate duplication
+// introduced excessive complexity and reduced readability. As a result, it was decided to keep the
+// two functions separate and accept the minor duplication to maintain simplicity and clarity.
+hy::IdPairs Validator::ValidateAndExtendCandidates() {
+    size_t const num_attributes = plis_->size();
+    assert(num_attributes == tree_->GetNumAttributes());
+
+    size_t previous_num_invalid_uccs = 0;
+    std::vector<LhsPair> current_level = tree_->GetLevel(current_level_number_);
+    hy::IdPairs comparison_suggestions;
+    while (!current_level.empty()) {
+        UCCValidations result = ValidateAndExtendSeq(current_level);
+        comparison_suggestions.insert(comparison_suggestions.end(),
+                                      result.comparison_suggestions().begin(),
+                                      result.comparison_suggestions().end());
+        std::vector<LhsPair> next_level = hy::CollectCurrentChildren(current_level, num_attributes);
+
+        size_t candidates = AddExtendedCandidatesFromInvalid(
+                next_level, *tree_, result.invalid_instances(), num_attributes);
+
+        LogLevel(current_level, result, candidates, current_level_number_, "UCC");
+
+        size_t const num_invalid_uccs = result.invalid_instances().size();
+        size_t const num_valid_uccs = result.count_validations() - num_invalid_uccs;
+        current_level = std::move(next_level);
+        current_level_number_++;
+
+        if (num_invalid_uccs >
+                    (long double)hyfd::HyFDConfig::kEfficiencyThreshold * num_valid_uccs &&
+            previous_num_invalid_uccs < num_invalid_uccs) {
+            return comparison_suggestions;
+        }
+        previous_num_invalid_uccs = num_invalid_uccs;
+    }
+
+    return {};
+}
+
+}  // namespace algos

Desbordante/desbordante-core

src/algorithms/ucc/hyucc/hyucc.cpp
@@ -2,10 +2,13 @@
 
 #include <chrono>
 
+#include <easylogging++.h>
+
 #include "hycommon/types.h"
 #include "inductor.h"
 #include "preprocessor.h"
 #include "sampler.h"
+#include "validator.h"
 
 namespace algos {
 
@@ -18,28 +21,57 @@ void HyUCC::LoadDataInternal(model::IDatasetStream& data_stream) {
 }
 
 unsigned long long HyUCC::ExecuteInternal() {
+    using namespace hy;
     auto const start_time = std::chrono::system_clock::now();
 
     auto [plis, pli_records, og_mapping] = Preprocess(relation_.get());
-    auto const plis_shared = std::make_shared<hy::PLIs>(std::move(plis));
-    auto const pli_records_shared = std::make_shared<hy::Rows>(std::move(pli_records));
+    auto const plis_shared = std::make_shared<PLIs>(std::move(plis));
+    auto const pli_records_shared = std::make_shared<Rows>(std::move(pli_records));
 
-    Sampler sampler(plis_shared, pli_records_shared);
+    algos::Sampler sampler(plis_shared, pli_records_shared);
 
     auto ucc_tree = std::make_unique<UCCTree>(relation_->GetNumColumns());
     Inductor inductor(ucc_tree.get());
+    Validator validator(ucc_tree.get(), plis_shared, pli_records_shared);
 
-    hy::IdPairs comparison_suggestions;
+    IdPairs comparison_suggestions;
 
     while (true) {
-        auto non_uccs = sampler.GetNonUCCCandidates(comparison_suggestions);
+        LOG(DEBUG) << "Sampling...";
+        NonUCCList non_uccs = sampler.GetNonUCCs(comparison_suggestions);
 
+        LOG(DEBUG) << "Inducing...";
         inductor.UpdateUCCTree(std::move(non_uccs));
+
+        LOG(DEBUG) << "Validating...";
+        comparison_suggestions = validator.ValidateAndExtendCandidates();
+
+        if (comparison_suggestions.empty()) {
+            break;
+        }
+    }
+
+    auto uccs = ucc_tree->FillUCCs();
+    RegisterUCCs(std::move(uccs), og_mapping);
+
+    LOG(DEBUG) << "Mined UCCs:";
+    for (model::UCC const& ucc : UCCList()) {
+        LOG(DEBUG) << ucc.ToString();
     }
 
     auto elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(
             std::chrono::system_clock::now() - start_time);
     return elapsed_milliseconds.count();
+}
+
+void HyUCC::RegisterUCCs(std::vector<boost::dynamic_bitset<>>&& uccs,
+                         const std::vector<size_t>& og_mapping) {
+    const auto* const schema = relation_->GetSchema();
+    for (auto&& ucc : uccs) {
+        boost::dynamic_bitset<> mapped_ucc =
+                hy::RestoreAgreeSet(ucc, og_mapping, schema->GetNumColumns());
+        ucc_collection_.Register(schema, std::move(mapped_ucc));
+    }
 }
 
 }  // namespace algos

src/algorithms/ucc/hyucc/validator.cpp
@@ -1,0 +1,151 @@
+#include "validator.h"
+
+#include "hycommon/efficiency_threshold.h"
+#include "hycommon/validator_helpers.h"
+#include "ucc/hyucc/structures/ucc_tree_vertex.h"
+
+namespace {
+
+using model::RawUCC;
+
+// Check out comment for Validator::ValidateAndExtendCandidates, it's appropriate for this function
+// too
+size_t AddExtendedCandidatesFromInvalid(std::vector<algos::LhsPair>& next_level,
+                                        algos::UCCTree& ucc_tree,
+                                        std::vector<RawUCC> const& invalid_uccs,
+                                        size_t num_attributes) {
+    size_t candidates = 0;
+    for (auto const& ucc : invalid_uccs) {
+        for (size_t attr = 0; attr < num_attributes; ++attr) {
+            if (ucc.test(attr)) {
+                continue;
+            }
+
+            RawUCC ucc_ext = ucc;
+            ucc_ext.set(attr);
+
+            if (ucc_tree.FindUCCOrGeneralization(ucc_ext)) {
+                continue;
+            }
+
+            algos::UCCTreeVertex* child = ucc_tree.AddUCCGetIfNew(ucc_ext);
+            if (child == nullptr) {
+                continue;
+            }
+            next_level.emplace_back(child, std::move(ucc_ext));
+            candidates++;
+        }
+    }
+    return candidates;
+}
+
+}  // namespace
+
+namespace algos {
+
+using model::RawUCC;
+
+bool Validator::IsUnique(util::PLI const& pivot_pli, RawUCC const& ucc,
+                         hy::IdPairs& comparison_suggestions) {
+    std::vector<size_t> indices = util::BitsetToIndices<size_t>(ucc);
+    for (util::PLI::Cluster const& cluster : pivot_pli.GetIndex()) {
+        auto cluster_to_record =
+                hy::MakeClusterIdentifierToTMap<util::PLI::Cluster::value_type>(cluster.size());
+        for (auto const record_id : cluster) {
+            std::vector<size_t> cluster_id =
+                    hy::BuildClustersIdentifier((*compressed_records_)[record_id], indices);
+            if (cluster_id.empty()) {
+                continue;
+            }
+
+            auto const [it, inserted] = cluster_to_record.try_emplace(cluster_id, record_id);
+            if (!inserted) {
+                comparison_suggestions.emplace_back(record_id, it->second);
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+Validator::UCCValidations Validator::GetValidations(LhsPair const& vertex_and_ucc) {
+    auto [vertex, ucc] = vertex_and_ucc;
+    UCCValidations validations;
+    validations.set_count_validations(1);
+    validations.set_count_intersections(1);
+
+    size_t ucc_attr = ucc.find_first();
+    bool is_unique;
+    if (current_level_number_ == 1) {
+        assert(ucc_attr != boost::dynamic_bitset<>::npos);
+        is_unique = (*plis_)[ucc_attr]->AllValuesAreUnique();
+    } else {
+        ucc.reset(ucc_attr);
+        // It's guaranteed that the first cluster has the fewest records because of sorting
+        // in the Sampler
+        util::PLI const* pivot_pli = (*plis_)[ucc_attr];
+        is_unique = IsUnique(*pivot_pli, ucc, validations.comparison_suggestions());
+        ucc.set(ucc_attr);
+    }
+
+    if (!is_unique) {
+        vertex->SetIsUCC(false);
+        validations.invalid_instances().push_back(std::move(ucc));
+    }
+
+    return validations;
+}
+
+Validator::UCCValidations Validator::ValidateAndExtendSeq(
+        std::vector<LhsPair> const& current_level) {
+    UCCValidations result;
+    for (auto const& vertex_and_ucc : current_level) {
+        if (!vertex_and_ucc.first->IsUCC()) {
+            continue;
+        }
+        result.Add(GetValidations(vertex_and_ucc));
+    }
+    return result;
+}
+
+// This function is similar to hyfd::Validator::ValidateAndExtendCandidates(), but with some key
+// differences in the way it initializes the current_level and handles loop conditions. While there
+// is some shared structure, attempting to refactor the functions to eliminate duplication
+// introduced excessive complexity and reduced readability. As a result, it was decided to keep the
+// two functions separate and accept the minor duplication to maintain simplicity and clarity.
+hy::IdPairs Validator::ValidateAndExtendCandidates() {
+    size_t const num_attributes = plis_->size();
+    assert(num_attributes == tree_->GetNumAttributes());
+
+    size_t previous_num_invalid_uccs = 0;
+    std::vector<LhsPair> current_level = tree_->GetLevel(current_level_number_);
+    hy::IdPairs comparison_suggestions;
+    while (!current_level.empty()) {
+        UCCValidations result = ValidateAndExtendSeq(current_level);
+        comparison_suggestions.insert(comparison_suggestions.end(),
+                                      result.comparison_suggestions().begin(),
+                                      result.comparison_suggestions().end());
+        std::vector<LhsPair> next_level = hy::CollectCurrentChildren(current_level, num_attributes);
+
+        size_t candidates = AddExtendedCandidatesFromInvalid(
+                next_level, *tree_, result.invalid_instances(), num_attributes);
+
+        LogLevel(current_level, result, candidates, current_level_number_, "UCC");
+
+        size_t const num_invalid_uccs = result.invalid_instances().size();
+        size_t const num_valid_uccs = result.count_validations() - num_invalid_uccs;
+        current_level = std::move(next_level);
+        current_level_number_++;
+
+        if (num_invalid_uccs > (long double)hy::kEfficiencyThreshold * num_valid_uccs &&
+            previous_num_invalid_uccs < num_invalid_uccs) {
+            return comparison_suggestions;
+        }
+        previous_num_invalid_uccs = num_invalid_uccs;
+    }
+
+    return {};
+}
+
+}  // namespace algos
32f09c24bb0e349f582320a45b78af5d1f16903f
Michael Polyntsov <Arno9148@gmail.com>
2023-04-17 19:52:01 +0300

    Implement HyUCC Validator

--- src/algorithms/ucc/hyucc/hyucc.cpp
+++ src/algorithms/ucc/hyucc/hyucc.cpp
@@ -2,10 +2,13 @@
 
 #include <chrono>
 
+#include <easylogging++.h>
+
 #include "hycommon/types.h"
 #include "inductor.h"
 #include "preprocessor.h"
 #include "sampler.h"
+#include "validator.h"
 
 namespace algos {
 
@@ -18,28 +21,57 @@ void HyUCC::LoadDataInternal(model::IDatasetStream& data_stream) {
 }
 
 unsigned long long HyUCC::ExecuteInternal() {
+    using namespace hy;
     auto const start_time = std::chrono::system_clock::now();
 
     auto [plis, pli_records, og_mapping] = Preprocess(relation_.get());
-    auto const plis_shared = std::make_shared<hy::PLIs>(std::move(plis));
-    auto const pli_records_shared = std::make_shared<hy::Rows>(std::move(pli_records));
+    auto const plis_shared = std::make_shared<PLIs>(std::move(plis));
+    auto const pli_records_shared = std::make_shared<Rows>(std::move(pli_records));
 
-    Sampler sampler(plis_shared, pli_records_shared);
+    algos::Sampler sampler(plis_shared, pli_records_shared);
 
     auto ucc_tree = std::make_unique<UCCTree>(relation_->GetNumColumns());
     Inductor inductor(ucc_tree.get());
+    Validator validator(ucc_tree.get(), plis_shared, pli_records_shared);
 
-    hy::IdPairs comparison_suggestions;
+    IdPairs comparison_suggestions;
 
     while (true) {
-        auto non_uccs = sampler.GetNonUCCCandidates(comparison_suggestions);
+        LOG(DEBUG) << "Sampling...";
+        NonUCCList non_uccs = sampler.GetNonUCCs(comparison_suggestions);
 
+        LOG(DEBUG) << "Inducing...";
         inductor.UpdateUCCTree(std::move(non_uccs));
+
+        LOG(DEBUG) << "Validating...";
+        comparison_suggestions = validator.ValidateAndExtendCandidates();
+
+        if (comparison_suggestions.empty()) {
+            break;
+        }
+    }
+
+    auto uccs = ucc_tree->FillUCCs();
+    RegisterUCCs(std::move(uccs), og_mapping);
+
+    LOG(DEBUG) << "Mined UCCs:";
+    for (model::UCC const& ucc : UCCList()) {
+        LOG(DEBUG) << ucc.ToString();
     }
 
     auto elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(
             std::chrono::system_clock::now() - start_time);
     return elapsed_milliseconds.count();
+}
+
+void HyUCC::RegisterUCCs(std::vector<boost::dynamic_bitset<>>&& uccs,
+                         const std::vector<size_t>& og_mapping) {
+    const auto* const schema = relation_->GetSchema();
+    for (auto&& ucc : uccs) {
+        boost::dynamic_bitset<> mapped_ucc =
+                hy::RestoreAgreeSet(ucc, og_mapping, schema->GetNumColumns());
+        ucc_collection_.Register(schema, std::move(mapped_ucc));
+    }
 }
 
 }  // namespace algos
