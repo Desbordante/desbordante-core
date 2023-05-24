Arrno/Desbordante

Implement HyUCC Sampler and Preprocessor

src/algorithms/ucc/hyucc/hyucc.cpp
@@ -1,0 +1,33 @@
+#include "hyucc.h"
+
+#include <chrono>
+
+#include "hycommon/types.h"
+#include "preprocessor.h"
+#include "sampler.h"
+
+namespace algos {
+
+void HyUCC::FitInternal(model::IDatasetStream& data_stream) {
+    relation_ = ColumnLayoutRelationData::CreateFrom(data_stream, is_null_equal_null_);
+
+    if (relation_->GetColumnData().empty()) {
+        throw std::runtime_error("Got an empty dataset: UCC mining is meaningless.");
+    }
+}
+
+unsigned long long HyUCC::ExecuteInternal() {
+    auto const start_time = std::chrono::system_clock::now();
+
+    auto [plis, pli_records, og_mapping] = Preprocess(relation_.get());
+    auto const plis_shared = std::make_shared<hy::PLIs>(std::move(plis));
+    auto const pli_records_shared = std::make_shared<hy::Rows>(std::move(pli_records));
+
+    Sampler sampler(plis_shared, pli_records_shared);
+
+    auto elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::system_clock::now() - start_time);
+    return elapsed_milliseconds.count();
+}
+
+}  // namespace algos

src/algorithms/ucc/hyucc/hyucc.h
@@ -1,0 +1,23 @@
+#pragma once
+
+#include <memory>
+
+#include "model/column_layout_relation_data.h"
+#include "model/idataset_stream.h"
+#include "ucc/ucc_algorithm.h"
+
+namespace algos {
+
+class HyUCC : public UCCAlgorithm {
+private:
+    std::unique_ptr<ColumnLayoutRelationData> relation_;
+
+    void FitInternal(model::IDatasetStream& data_stream) override;
+    unsigned long long ExecuteInternal() override;
+    void ResetUCCAlgorithmState() override {}
+
+public:
+    HyUCC() : UCCAlgorithm({}) {}
+};
+
+}  // namespace algos

src/algorithms/ucc/hyucc/sampler.h
@@ -1,0 +1,22 @@
+#pragma once
+
+#include "hycommon/sampler.h"
+#include "hycommon/types.h"
+#include "ucc/hyucc/structures/non_ucc_list.h"
+
+namespace algos {
+
+class Sampler {
+private:
+    hy::Sampler sampler_;
+
+public:
+    Sampler(hy::PLIsPtr plis, hy::RowsPtr pli_records)
+        : sampler_(std::move(plis), std::move(pli_records)) {}
+
+    NonUCCList GetNonUCCCandidates(hy::IdPairs const& comparison_suggestions) {
+        return sampler_.GetAgreeSets(comparison_suggestions);
+    }
+};
+
+}  // namespace algos

Desbordante/desbordante-core

src/algorithms/ucc/hyucc/hyucc.cpp
@@ -1,0 +1,33 @@
+#include "hyucc.h"
+
+#include <chrono>
+
+#include "hycommon/types.h"
+#include "preprocessor.h"
+#include "sampler.h"
+
+namespace algos {
+
+void HyUCC::LoadDataInternal(model::IDatasetStream& data_stream) {
+    relation_ = ColumnLayoutRelationData::CreateFrom(data_stream, is_null_equal_null_);
+
+    if (relation_->GetColumnData().empty()) {
+        throw std::runtime_error("Got an empty dataset: UCC mining is meaningless.");
+    }
+}
+
+unsigned long long HyUCC::ExecuteInternal() {
+    auto const start_time = std::chrono::system_clock::now();
+
+    auto [plis, pli_records, og_mapping] = Preprocess(relation_.get());
+    auto const plis_shared = std::make_shared<hy::PLIs>(std::move(plis));
+    auto const pli_records_shared = std::make_shared<hy::Rows>(std::move(pli_records));
+
+    Sampler sampler(plis_shared, pli_records_shared);
+
+    auto elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::system_clock::now() - start_time);
+    return elapsed_milliseconds.count();
+}
+
+}  // namespace algos

src/algorithms/ucc/hyucc/hyucc.h
@@ -1,0 +1,23 @@
+#pragma once
+
+#include <memory>
+
+#include "model/column_layout_relation_data.h"
+#include "model/idataset_stream.h"
+#include "ucc/ucc_algorithm.h"
+
+namespace algos {
+
+class HyUCC : public UCCAlgorithm {
+private:
+    std::unique_ptr<ColumnLayoutRelationData> relation_;
+
+    void LoadDataInternal(model::IDatasetStream& data_stream) override;
+    unsigned long long ExecuteInternal() override;
+    void ResetUCCAlgorithmState() override {}
+
+public:
+    HyUCC() : UCCAlgorithm({}) {}
+};
+
+}  // namespace algos

src/algorithms/ucc/hyucc/sampler.h
@@ -1,0 +1,22 @@
+#pragma once
+
+#include "hycommon/sampler.h"
+#include "hycommon/types.h"
+#include "ucc/hyucc/structures/non_ucc_list.h"
+
+namespace algos {
+
+class Sampler {
+private:
+    hy::Sampler sampler_;
+
+public:
+    Sampler(hy::PLIsPtr plis, hy::RowsPtr pli_records)
+        : sampler_(std::move(plis), std::move(pli_records)) {}
+
+    NonUCCList GetNonUCCs(hy::IdPairs const& comparison_suggestions) {
+        return sampler_.GetAgreeSets(comparison_suggestions);
+    }
+};
+
+}  // namespace algos