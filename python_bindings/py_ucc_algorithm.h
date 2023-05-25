a023f79f6204aab1cca4c4e97b7fc86d2d910f08
Michael Polyntsov <Arno9148@gmail.com>
2023-05-25 15:59:23 +0300

    Implement python bindings base classes for ucc mining algorithms

---
+++ python_bindings/py_ucc_algorithm.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <list>
+
+#include "algorithms/ucc/ucc_algorithm.h"
+#include "get_algorithm.h"
+#include "model/ucc.h"
+#include "py_algorithm.h"
+#include "py_ucc.h"
+
+namespace python_bindings {
+
+class PyUCCAlgorithmBase : public PyAlgorithmBase {
+protected:
+    using PyAlgorithmBase::PyAlgorithmBase;
+
+public:
+    [[nodiscard]] std::vector<PyUCC> GetUCCs() const {
+        auto const& ucc_algo = GetAlgorithm<algos::UCCAlgorithm>(algorithm_);
+        std::list<model::UCC> const& ucc_list = ucc_algo.UCCList();
+        std::vector<PyUCC> py_uccs;
+        py_uccs.reserve(ucc_list.size());
+
+        for (model::UCC const& ucc : ucc_list) {
+            py_uccs.emplace_back(ucc);
+        }
+
+        return py_uccs;
+    }
+};
+
+template <typename T>
+using PyUCCAlgorithm = PyAlgorithm<T, PyUCCAlgorithmBase>;
+
+}  // namespace python_bindings
