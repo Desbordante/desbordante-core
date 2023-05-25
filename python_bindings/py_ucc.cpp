4d54ad1c02a89c18454630baae2d20ca7ef417cd
Michael Polyntsov <Arno9148@gmail.com>
2023-05-25 15:58:44 +0300

    Implement python bindings class for UCC

---
+++ python_bindings/py_ucc.cpp
@@ -0,0 +1,22 @@
+#include "py_ucc.h"
+
+#include <sstream>
+
+#include "util/bitset_utils.h"
+
+namespace python_bindings {
+
+PyUCC::PyUCC(model::RawUCC const& raw_ucc)
+    : ucc_indices_(util::BitsetToIndices<util::config::IndexType>(raw_ucc)) {}
+
+std::string PyUCC::ToString() const {
+    std::stringstream stream;
+    stream << "( ";
+    for (util::config::IndexType index : ucc_indices_) {
+        stream << index << " ";
+    }
+    stream << ")";
+    return stream.str();
+}
+
+}  // namespace python_bindings
---
+++ python_bindings/py_ucc.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "model/raw_ucc.h"
+#include "model/ucc.h"
+#include "util/config/indices/type.h"
+
+namespace python_bindings {
+
+class PyUCC {
+private:
+    util::config::IndicesType ucc_indices_;
+
+public:
+    explicit PyUCC(model::RawUCC const& raw_ucc);
+    explicit PyUCC(model::UCC const& ucc) : PyUCC(ucc.GetColumnIndices()) {}
+
+    [[nodiscard]] std::string ToString() const;
+
+    [[nodiscard]] util::config::IndicesType const& GetUCC() const noexcept {
+        return ucc_indices_;
+    }
+};
+
+}  // namespace python_bindings
