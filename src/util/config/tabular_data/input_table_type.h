ecd0a8e7d44151bec0540034a661c778ee2a15cf
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-06-05 12:04:52 +0300

    Add "table" option
    The files are structured this way because an algorithm may use several
    tables (HyMD) or even a list of tables (Spider).
    Those algorithms still need the `InputTable` type, but they will
    register different options, which may become common later and will thus
    be put in this directory as well.

---
+++ src/util/config/tabular_data/input_table_type.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <memory>
+
+#include "model/idataset_stream.h"
+
+namespace util::config {
+using InputTable = std::shared_ptr<model::IDatasetStream>;
+}  // namespace util::config
