fda19f3c7a630dadec187b50e25ae9fd2badb6bf
yakovypg <yasha-pmp@mail.ru>
2023-07-24 18:42:14 +0300

    Add timer

--- src/algorithms/fastod/timers/Timer.h
+++ src/algorithms/fastod/timers/Timer.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <chrono>
-#include <ctime>
 
 namespace algos::fastod {
 
@@ -9,11 +8,18 @@ typedef std::chrono::_V2::system_clock::time_point TimePoint;
 
 class Timer {
 private:
+    bool is_started_;
     TimePoint start_time_;
     TimePoint end_time_;
 
 public:
-    Timer();
+    Timer(bool start = true) noexcept;
+
+    void Start() noexcept;
+    void Stop() noexcept;
+
+    bool IsStarted() const noexcept;
+    double GetElapsedSeconds() const;
 };
 
 } // namespace algos::fastod
