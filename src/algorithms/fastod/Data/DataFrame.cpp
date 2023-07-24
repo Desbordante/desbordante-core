dc72db7d82779e400dfa3d6a514f3f519d6cbc9e
yakovypg <yasha-pmp@mail.ru>
2023-07-24 15:34:37 +0300

    Add methods for getting number of columns and tuples

--- src/algorithms/fastod/Data/DataFrame.cpp
+++ src/algorithms/fastod/Data/DataFrame.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <string>
+#include <algorithm>
 #include <filesystem>
 
 #include "parser/csv_parser.h"
@@ -16,6 +17,19 @@ DataFrame::DataFrame(std::vector<model::TypedColumnData> columns_data) noexcept
 
 SchemaValue const& DataFrame::GetValue(int tuple_index, int attribute_index) const noexcept {
     return SchemaValue(columns_data_.at(attribute_index), tuple_index);
+}
+
+size_t DataFrame::GetColumnCount() const noexcept {
+    return columns_data_.size();
+}
+
+size_t DataFrame::GetTupleCount() const noexcept {
+    auto minimum = std::min(columns_data_.cbegin(), columns_data_.cend(),
+        [](model::TypedColumnData const& x, model::TypedColumnData const& y) {
+            return x.GetNumRows() < y.GetNumRows();
+    });
+
+    return minimum->GetNumRows();
 }
 
 DataFrame DataFrame::FromCsv(std::filesystem::path const& path) {
