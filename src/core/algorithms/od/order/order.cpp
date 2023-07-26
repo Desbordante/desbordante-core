61c907d72f9327a6d89b0ab1846916c04bb5b079
Sched71 <scheka.dm@gmail.com>
2023-07-26 10:06:11 +0300

    Add Sorted partitions creation

--- src/core/algorithms/od/order/order.cpp
+++ src/core/algorithms/od/order/order.cpp
@@ -1,7 +1,10 @@
 #include "order.h"
 
+#include <algorithm>
+
 #include "config/names_and_descriptions.h"
 #include "config/tabular_data/input_table/option.h"
+#include "model/types/types.h"
 
 namespace algos::order {
 
@@ -23,6 +26,41 @@ void Order::LoadDataInternal() {
 }
 
 void Order::ResetState() {}
+
+void Order::CreateSortedPartitions() {
+    std::vector<model::TypedColumnData> const& data = typed_relation_->GetColumnData();
+    for (unsigned int i = 0; i < data.size(); ++i) {
+        if (!model::Type::IsOrdered(data[i].GetTypeId())) {
+            continue;
+        }
+        std::vector<std::pair<unsigned long, std::byte const*>> indexed_byte_data;
+        indexed_byte_data.reserve(data[i].GetNumRows());
+        std::vector<std::byte const*> const& byte_data = data[i].GetData();
+        for (size_t k = 0; k < byte_data.size(); ++k) {
+            indexed_byte_data.emplace_back(k, byte_data[k]);
+        }
+        model::Type const& type = data[i].GetType();
+        auto less = [&type](std::pair<unsigned long, std::byte const*> l,
+                            std::pair<unsigned long, std::byte const*> r) {
+            return type.Compare(l.second, r.second) == model::CompareResult::kLess;
+        };
+        std::sort(indexed_byte_data.begin(), indexed_byte_data.end(), less);
+        std::vector<std::unordered_set<unsigned long>> equivalence_classes;
+        equivalence_classes.push_back({indexed_byte_data.front().first});
+        auto equal = [&type](std::pair<unsigned long, std::byte const*> l,
+                             std::pair<unsigned long, std::byte const*> r) {
+            return type.Compare(l.second, r.second) == model::CompareResult::kEqual;
+        };
+        for (size_t k = 1; k < indexed_byte_data.size(); ++k) {
+            if (equal(indexed_byte_data[k - 1], indexed_byte_data[k])) {
+                equivalence_classes.back().insert(indexed_byte_data[k].first);
+            } else {
+                equivalence_classes.push_back({indexed_byte_data[k].first});
+            }
+        }
+        sorted_partitions_[{i}] = SortedPartition(std::move(equivalence_classes));
+    }
+}
 
 unsigned long long Order::ExecuteInternal() {}
 
--- src/core/algorithms/od/order/order.h
+++ src/core/algorithms/od/order/order.h
@@ -1,23 +1,31 @@
 #pragma once
 
 #include <memory>
+#include <unordered_map>
+
+#include <boost/container_hash/hash.hpp>
 
 #include "algorithms/algorithm.h"
 #include "model/table/column_layout_typed_relation_data.h"
 #include "config/tabular_data/input_table_type.h"
+#include "sorted_partitions.h"
 
 namespace algos::order {
 
 class Order : public Algorithm {
 private:
     using TypedRelation = model::ColumnLayoutTypedRelationData;
+    using SortedPartitions = std::unordered_map<std::vector<unsigned int>, SortedPartition,
+                                                boost::hash<std::vector<unsigned int>>>;
 
     config::InputTable input_table_;
     std::unique_ptr<TypedRelation> typed_relation_;
+    SortedPartitions sorted_partitions_;
 
     void RegisterOptions();
     void LoadDataInternal() override;
     void ResetState() override;
+    void CreateSortedPartitions();
     unsigned long long ExecuteInternal() final;
 
 public:
