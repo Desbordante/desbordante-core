d871a533edd4330624b4fb937d7afa9453caee2a
Sched71 <scheka.dm@gmail.com>
2023-07-26 21:16:29 +0300

    Add Sorted partitions production

--- src/core/algorithms/od/order/sorted_partitions.h
+++ src/core/algorithms/od/order/sorted_partitions.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
@@ -9,9 +10,13 @@ struct SortedPartition {
     using EquivalenceClasses = std::vector<std::unordered_set<unsigned long>>;
 
     EquivalenceClasses sorted_partition;
+    std::unordered_map<unsigned long, unsigned long> hash_partition;
 
     SortedPartition() = default;
     SortedPartition(EquivalenceClasses&& eq_classes) : sorted_partition(std::move(eq_classes)){};
+
+    void BuildHashTable();
+    SortedPartition operator*(SortedPartition const& other);
 };
 
 }  // namespace algos::order
