b769847cd6dc1bf3ff950bfc9fae34c003b6be45
Sched71 <scheka.dm@gmail.com>
2023-07-27 12:48:54 +0300

    Add OD validity check for Order

--- src/core/algorithms/od/order/order.cpp
+++ src/core/algorithms/od/order/order.cpp
@@ -62,6 +62,58 @@ void Order::CreateSortedPartitions() {
     }
 }
 
+bool SubsetSetDifference(std::unordered_set<unsigned long>& a,
+                                                        std::unordered_set<unsigned long>& b) {
+    auto const not_found = b.end();
+    for (auto const& element: a)
+        if (b.find(element) == not_found) {
+            return false;
+        } else {
+            b.erase(element);
+        }
+    return true;
+}
+
+ValidityType Order::CheckForSwap(SortedPartition const& l, SortedPartition const& r) {
+    ValidityType res = ValidityType::valid;
+    size_t l_i = 0, r_i = 0;
+    bool next_l = true, next_r = true;
+    std::unordered_set<unsigned long> l_eq_class;
+    std::unordered_set<unsigned long> r_eq_class;
+    while (l_i < l.sorted_partition.size() && r_i < r.sorted_partition.size()) {
+        if (next_l) {
+            l_eq_class = l.sorted_partition[l_i];
+        }    
+        if (next_r) {
+            r_eq_class = r.sorted_partition[r_i];
+        }
+        if (l_eq_class.size() < r_eq_class.size()) {
+            if(!SubsetSetDifference(l_eq_class, r_eq_class)) {
+                return ValidityType::swap;
+            } else {
+                res = ValidityType::merge;
+                ++l_i;
+                next_l = true;
+                next_r = false;
+            }
+        } else {
+            if (!SubsetSetDifference(r_eq_class, l_eq_class)) {
+                return ValidityType::swap;
+            } else {
+                ++r_i;
+                next_r = true;
+                if (l_eq_class.empty()) {
+                    ++l_i;
+                    next_l = true;
+                } else {
+                    next_l = false;
+                }
+            }
+        }
+    }
+    return res;
+}
+
 unsigned long long Order::ExecuteInternal() {}
 
 }  // namespace algos::order
