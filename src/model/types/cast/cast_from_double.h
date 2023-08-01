2312ddabe036a9cdc964b5a957b07089b6e53c56
Popov Dmitriy Ivanovich <PopovDmitriyStudy@yandex.ru>
2023-08-01 05:14:18 +0300

    Added spaces according to google code-style guide

--- src/model/types/cast/cast_from_double.h
+++ src/model/types/cast/cast_from_double.h
@@ -1,16 +1,15 @@
 #pragma once
 #include "cast_to_builtin_type.h"
 #include "icast_to_numeric_type.h"
-namespace model{
-class CastFromDoubleType :public CastToCppType<Double>{};
-class CastFromDoubleTypeToNumeric: public ICastToNumericType{
-    public:
-    std::byte* CastToDoubleType(const std::byte* data) override{
-        //model::DoubleType type = model::DoubleType();
-        return this->MakeValue<Double>(*reinterpret_cast<const Double*> (data));
+namespace model {
+class CastFromDoubleType : public CastToCppType<Double> {};
+class CastFromDoubleTypeToNumeric : public ICastToNumericType {
+public:
+    std::byte* CastToDoubleType(const std::byte* data) override {
+        return this->MakeValue<Double>(*reinterpret_cast<const Double*>(data));
     }
-    std::byte* CastToIntType(const std::byte* data) override{
-        return this->MakeValue<Int>(static_cast<Int>(*reinterpret_cast<const Double*> (data)));
+    std::byte* CastToIntType(const std::byte* data) override {
+        return this->MakeValue<Int>(static_cast<Int>(*reinterpret_cast<const Double*>(data)));
     }
 };
-}//namespace model
+} //namespace model
--- src/model/types/cast/icast_to_numeric_type.h
+++ src/model/types/cast/icast_to_numeric_type.h
@@ -2,14 +2,14 @@
 namespace model{
 class ICastToNumericType{
 public:
-    virtual std::byte* CastToDoubleType(const std::byte* data) =0;
-    virtual std::byte* CastToIntType(const std::byte* data)=0;
+    virtual std::byte* CastToDoubleType(const std::byte* data) = 0;
+    virtual std::byte* CastToIntType(const std::byte* data) = 0;
 protected:
-    template<typename T>
+    template <typename T>
     std::byte* MakeValue(T const literal) const {
         auto* buf = new std::byte[sizeof(T)];
         *reinterpret_cast<T*>(buf) = literal;
         return buf;
     }
 };
-}//namespace model
+} //namespace model
