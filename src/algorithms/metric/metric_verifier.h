b1656e7b71dcec73c1495f494b385264a7c9c4a6
Mstrutov <strutovsky.m.a@gmail.com>
2023-08-01 10:10:10 +0600

    Return convex hull and qgram vector to util
    Q-gram sequence is a general string functionality. At the moment it is only
    used in metric dependencies, but it may be used elsewhere. The same goes for
    the convex hull.

--- src/algorithms/metric/metric_verifier.h
+++ src/algorithms/metric/metric_verifier.h
@@ -18,10 +18,10 @@
 #include "config/equal_nulls/type.h"
 #include "config/indices/type.h"
 #include "config/tabular_data/input_table_type.h"
-#include "convex_hull.h"
 #include "model/column_layout_relation_data.h"
 #include "model/column_layout_typed_relation_data.h"
-#include "qgram_vector.h"
+#include "util/convex_hull.h"
+#include "util/qgram_vector.h"
 
 namespace algos::metric {
 
