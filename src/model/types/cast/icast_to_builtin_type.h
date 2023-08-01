3669f3dc1b5871f27661dd26bcd95700d901cc49
Popov Dmitriy Ivanovich <PopovDmitriyStudy@yandex.ru>
2023-08-01 06:28:06 +0300

    more changes in codestyle

--- src/model/types/cast/icast_to_builtin_type.h
+++ src/model/types/cast/icast_to_builtin_type.h
@@ -11,7 +11,7 @@ public:
         switch (inp_type) {
             case model::TypeId::kDouble: {
                 static_assert(std::is_same<return_type, Double>::value,
-                               "can only convert model::DoubleType to Double");
+                              "can only convert model::DoubleType to Double");
                 return *reinterpret_cast<Double*>(buf); 
             } break;
             case model::TypeId::kInt: {
