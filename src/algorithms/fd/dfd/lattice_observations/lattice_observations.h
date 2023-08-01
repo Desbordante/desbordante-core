63eb45ef85b13c5230607f8aa4bfe60d00cbe6d4
Mstrutov <strutovsky.m.a@gmail.com>
2023-08-01 11:22:34 +0600

    Remove redundant headers from CMakeLists

--- src/algorithms/fd/dfd/lattice_observations/lattice_observations.h
+++ src/algorithms/fd/dfd/lattice_observations/lattice_observations.h
@@ -2,15 +2,17 @@
 
 #include <unordered_map>
 
-#include "dfd/column_order/column_order.h"
-#include "dfd/node_category.h"
+#include "../column_order/column_order.h"
+#include "../node_category.h"
 #include "model/table/vertical.h"
 #include "util/custom_hashes.h"
 
 class LatticeObservations : public std::unordered_map<Vertical, NodeCategory> {
 public:
     bool IsCandidate(Vertical const& node) const;
-    bool IsVisited(Vertical const& node) const { return this->find(node) != this->end(); }
+    bool IsVisited(Vertical const& node) const {
+        return this->find(node) != this->end();
+    }
 
     NodeCategory UpdateDependencyCategory(Vertical const& node);
     NodeCategory UpdateNonDependencyCategory(Vertical const& node, unsigned int rhs_index);
