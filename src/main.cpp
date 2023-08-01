d752ed3d2fc8557400acf0410c9fda6615101b6b
yakovypg <yasha-pmp@mail.ru>
2023-08-01 20:47:54 +0300

    Change main

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,14 @@
 #include "util/config/all_options.h"
 #include "util/config/enum_to_available_values.h"
 
+#include "algorithms/fastod/fastod.h"
+#include "algorithms/fastod/data/DataFrame.h"
+#include <climits>
+#include <string>
+#include <filesystem>
+#include <vector>
+#include <fstream>
+
 INITIALIZE_EASYLOGGINGPP
 
 namespace {
@@ -25,6 +33,61 @@ boost::program_options::options_description InfoOptions() {
 }
 }  // namespace
 
+int main(int argc, char const* argv[]) {
+    std::string path_to_dataset = std::string("");
+    std::string output_path = std::string("");  
+    int max_time = INT_MAX;
+
+    if (argc <= 1) {
+        std::cout << "Error: Dataset not specified\n";
+        return 1; 
+    }
+    if (argc > 4) {
+        std::cout << "Error: Too many arguments entered\n";
+        return 1;
+    }
+
+    if (argc >= 2)
+        path_to_dataset = std::string(argv[1]);
+    
+    if (argc >= 3)
+        output_path = std::string(argv[2]);
+
+    if (argc >= 4) {
+        max_time = std::stoi(argv[3]);
+    }
+
+    algos::fastod::DataFrame data = algos::fastod::DataFrame::FromCsv(std::filesystem::path(path_to_dataset));
+    algos::fastod::Fastod fastod = algos::fastod::Fastod(data, max_time);
+    std::vector<algos::fastod::CanonicalOD> ods = fastod.Discover();
+
+    if (output_path.size() == 0) {
+        std::cout << "Found ODs: " << ods.size() << '\n';
+
+        for (int i = 0; i < ods.size(); ++i) {
+            std::string od = ods[i].ToString();
+            std::cout << i + 1 << ":\t" << od << '\n';
+        }
+    }
+    else {
+        std::ofstream file;
+        file.open(output_path);
+
+        if (!file.is_open()) {
+            std::cout << "Error: failed to open output file\n";
+            return 2;
+        }
+
+        for (int i = 0; i < ods.size(); ++i) {
+            std::string od = ods[i].ToString();
+            file << od << '\n';
+        }
+
+        file.close();
+    }
+}
+
+/*
 int main(int argc, char const* argv[]) {
     namespace po = boost::program_options;
     using namespace util::config;
@@ -77,3 +140,4 @@ int main(int argc, char const* argv[]) {
 
     return 0;
 }
+*/
