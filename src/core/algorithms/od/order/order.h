1e073fcabb4e5f4f19e0e4bc489c23aceac6f728
Sched71 <scheka.dm@gmail.com>
2023-08-03 12:18:48 +0300

    Add UpdateCandidateSets to Order

--- src/core/algorithms/od/order/order.h
+++ src/core/algorithms/od/order/order.h
@@ -30,6 +30,7 @@ public:
     config::InputTable input_table_;
     std::unique_ptr<TypedRelation> typed_relation_;
     SortedPartitions sorted_partitions_;
+    std::vector<AttributeList> single_attributes_;
     CandidateSets candidate_sets_;
     OrderDependencies valid_;
     OrderDependencies merge_invalidated_;
@@ -40,6 +41,9 @@ public:
     void CreateSortedPartitions();
     ValidityType CheckForSwap(SortedPartition const& l, SortedPartition const& r);
     void ComputeDependencies(LatticeLevel const& lattice_level);
+    std::vector<AttributeList> Extend(AttributeList const& lhs, AttributeList const& rhs);
+    bool IsMinimal(AttributeList const& a);
+    void UpdateCandidateSets(unsigned int level);
     void Prune(LatticeLevel& lattice_level);
     LatticeLevel GenerateNextLevel(LatticeLevel const& l);
     unsigned long long ExecuteInternal() final;
