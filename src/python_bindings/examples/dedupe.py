47dc277b88191bb1b347596dcb8add9859ed3543
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-08-25 01:26:08 +0300

    Improve naming in deduplication example, add comments

--- src/python_bindings/examples/dedupe.py
+++ src/python_bindings/examples/dedupe.py
@@ -1,28 +1,39 @@
 from collections import defaultdict, deque
 
-import desbordante as desb
-import pandas as pd
+import desbordante
+import pandas
 
-ALGORITHM = 'Pyro'
+# Algorithm that finds approximate FDs and its config
+ALGORITHM_TYPE = desbordante.Pyro
 ERROR = 0.001
 CONFIG = {'error': ERROR, 'max_lhs': 1}
+
+# Parameters for pandas.read_csv(...).
 DATASET_PATH = 'datasets/duplicates.csv'
 HEADER = 0
 SEPARATOR = ','
-OUTPUT_FILE = 'output.csv'
-WINDOW_SIZE = 4
 
+# File where the deduplicated dataset will be written.
+OUTPUT_FILE = 'output.csv'
+
+# Initial window size in sorted neighborhood method.
+INITIAL_WINDOW_SIZE = 4
+
+# Variable to simplify the configuration string construction below.
+ALGORITHM = ALGORITHM_TYPE.__name__
+
+# A message containing all variables used by this usage scenario, to be
+# displayed to the user.
 CONFIG_STRING = f"""Deduplication parameters:
 {ALGORITHM=}
 {ERROR=:.5f}
 {DATASET_PATH=}
 {SEPARATOR=}
-{WINDOW_SIZE=}
-"""
+{INITIAL_WINDOW_SIZE=}"""
 
 
-def get_1lhs_fds(df, algo_name, algo_config):
-    algo = getattr(desb, algo_name)()
+def get_1lhs_fds(df, algo_type, algo_config):
+    algo = algo_type()
     algo.load_data(df, **algo_config)
     algo.execute(**algo_config)
     return sorted((lhs_indices[0], fd.rhs_index) for fd in algo.get_fds()
@@ -43,13 +54,13 @@ def count_matches(row1, row2, rhs: list[int]):
     return sum(row1[index] == row2[index] for index in rhs)
 
 
-def setup_pandas_print():
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.width', None)
-    pd.set_option('display.max_colwidth', None)
+def configure_dataframe_print():
+    pandas.set_option('display.max_columns', None)
+    pandas.set_option('display.width', None)
+    pandas.set_option('display.max_colwidth', None)
 
 
-def print_fd_info(df: pd.DataFrame, fds: list[tuple[int, int]]):
+def print_fd_info(df: pandas.DataFrame, fds: list[tuple[int, int]]):
     fd_dict = defaultdict(list)
     for lhs, rhs in fds:
         fd_dict[lhs].append(df.columns[rhs])
@@ -75,7 +86,7 @@ def choose_index(col_name, distinct_values):
     return int(input('index: '))
 
 
-def merge_handler(df, new_rows, remaining_rows, used_rows):
+def merge_handler(df: pandas.DataFrame, new_rows, remaining_rows, used_rows):
     if not used_rows:
         return
     new_row = []
@@ -92,7 +103,7 @@ def unknown_handler(df, new_rows, remaining_rows, used_rows):
     print('Unknown command.')
 
 
-def ask_rows(df: pd.DataFrame, window: deque[tuple[int, object]]) -> list:
+def ask_rows(df: pandas.DataFrame, window: deque[tuple[int, object]]) -> list:
     commands = {
         'keepall': keepall_handler,
         'drop': drop_handler,
@@ -118,7 +129,7 @@ def is_similar(row_info, window, chosen_cols, matches_required):
                for prev_row_info in window)
 
 
-def get_deduped_rows(df: pd.DataFrame, chosen_cols: list[int], matches_required: int,
+def get_deduped_rows(df: pandas.DataFrame, chosen_cols: list[int], matches_required: int,
                      fds: list[tuple[int, int]]):
     df.sort_values([df.columns[rhs_col] for _, rhs_col in fds if rhs_col in chosen_cols],
                    inplace=True)
@@ -128,7 +139,7 @@ def get_deduped_rows(df: pd.DataFrame, chosen_cols: list[int], matches_required:
     new_rows = []
     has_duplicate = False
     for row_info in df.iterrows():
-        if len(window) < WINDOW_SIZE:
+        if len(window) < INITIAL_WINDOW_SIZE:
             if not has_duplicate:
                 has_duplicate = is_similar(row_info, window, chosen_cols, matches_required)
         elif not has_duplicate:
@@ -145,16 +156,18 @@ def get_deduped_rows(df: pd.DataFrame, chosen_cols: list[int], matches_required:
 
 
 def main():
-    setup_pandas_print()
+    configure_dataframe_print()
     print(CONFIG_STRING)
+    print()
 
-    df = pd.read_csv(DATASET_PATH, sep=SEPARATOR, header=HEADER, dtype='string', index_col=False, na_filter=False)
+    df = pandas.read_csv(DATASET_PATH, sep=SEPARATOR, header=HEADER,
+                         dtype='string', index_col=False, na_filter=False)
     print('Dataset sample:')
     print(df)
     print(f'Original records: {len(df)}')
     print()
 
-    fds = get_1lhs_fds(df, ALGORITHM, CONFIG)
+    fds = get_1lhs_fds(df, ALGORITHM_TYPE, CONFIG)
     print_fd_info(df, fds)
     lhs_column = int(input('LHS column index: '))
     fds = list(filter(lambda fd: fd[0] == lhs_column, fds))
@@ -170,7 +183,7 @@ def main():
     print()
 
     print(f'Resulting records: {len(new_rows)}. Duplicates found: {len(df) - len(new_rows)}')
-    new_df = pd.DataFrame(new_rows, columns=df.columns)
+    new_df = pandas.DataFrame(new_rows, columns=df.columns)
 
     print(new_df)
     new_df.to_csv(OUTPUT_FILE, index=False)
