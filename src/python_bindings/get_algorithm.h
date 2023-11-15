fc7f97e52c7b9a8668cf25be4d2e54ba3f463624
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-11-15 15:54:06 +0300

    Remove `const` in GetAlgorithm return type

--- src/python_bindings/get_algorithm.h
+++ src/python_bindings/get_algorithm.h
@@ -7,8 +7,8 @@
 namespace python_bindings {
 
 template <typename T>
-[[nodiscard]] T const& GetAlgorithm(std::unique_ptr<algos::Algorithm> const& ptr) {
-    return dynamic_cast<T const&>(*ptr);
+[[nodiscard]] T& GetAlgorithm(std::unique_ptr<algos::Algorithm> const& ptr) {
+    return dynamic_cast<T&>(*ptr);
 }
 
 }  // namespace python_bindings
