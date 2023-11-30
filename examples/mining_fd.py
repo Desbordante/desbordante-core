2f72dc364fa7061b2de629b93ad07d05f5ca77c0
DaniilGoncharov <daniilgoncharov08@gmail.com>
2023-11-30 22:39:50 +0300

    Add usage examples

---
+++ examples/mining_fd.py
@@ -0,0 +1,11 @@
+import desbordante
+
+TABLE = 'datasets/university_fd.csv'
+
+algo = desbordante.HyFD()
+algo.load_data(TABLE, ',', True)
+algo.execute()
+result = algo.get_fds()
+print('FDs:')
+for fd in result:
+    print(fd)
