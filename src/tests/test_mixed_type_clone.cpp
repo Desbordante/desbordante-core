9ab6dfc18f4675a452e9b42fa27e6d1acbf99607
FilippovD <65872165+Volga21@users.noreply.github.com>
2023-12-07 00:41:44 +0300

    Add files via upload

---
+++ src/tests/test_mixed_type_clone.cpp
@@ -0,0 +1,77 @@
+#include <memory>
+
+#include <gtest/gtest.h>
+
+#include "mixed_type.h"
+
+#include <typeinfo>
+
+
+namespace tests{
+class ClassMixedType : public ::testing::Test {
+
+    protected:
+
+    model::MixedType *mt;
+
+    void SetUp() { mt = new model::MixedType(false); }
+    void TearDown() { delete mt; } 
+
+};
+
+TEST_F(ClassMixedType, TestCloneIntType) {
+
+    model::IntType type_int_;
+    std::byte * value_;
+
+    value_=mt->MakeValue(12,&type_int_);
+    std::byte * new_value_=mt->Clone(value_);
+
+    EXPECT_EQ(mt->ValueToString(value_),mt->ValueToString(new_value_));
+}
+
+TEST_F(ClassMixedType, TestCloneDoubleType) {
+
+    model::DoubleType type_double_;
+    std::byte * value_;
+
+    value_=mt->MakeValue(2.7,&type_double_);
+    std::byte * new_value_=mt->Clone(value_);
+
+    EXPECT_EQ(mt->ValueToString(value_),mt->ValueToString(new_value_));
+}
+
+TEST_F(ClassMixedType, TestCloneStringType) {
+
+    model::StringType type_string_(model::TypeId::kString);
+    std::byte * value_;
+    std::string str_="Hello World!";
+
+    value_=mt->MakeValue(str_,&type_string_);
+    std::byte * new_value_=mt->Clone(value_);
+
+    EXPECT_EQ(mt->ValueToString(value_),mt->ValueToString(new_value_));
+}
+
+TEST_F(ClassMixedType, TestCloneNullType) {
+
+    model::NullType type_null_(true);
+    std::byte * value_;
+
+    value_=mt->MakeValue(0,&type_null_);
+    std::byte * new_value_=mt->Clone(value_);
+
+    EXPECT_EQ(mt->ValueToString(value_),mt->ValueToString(new_value_));
+}
+
+TEST_F(ClassMixedType, TestCloneEmptyType) {
+
+    model::EmptyType type_empty_;
+    std::byte * value_;
+
+    value_=mt->MakeValue(0,&type_empty_);
+    std::byte * new_value_=mt->Clone(value_);
+
+    EXPECT_EQ(mt->ValueToString(value_),mt->ValueToString(new_value_));
+}
+}
