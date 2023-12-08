b9aecaa4a2b01784ea28268023e4000847d269a3
Michael Polyntsov <Arno9148@gmail.com>
2023-12-08 22:06:58 +0300

    Remove program options from the algo factory code

--- src/core/algorithms/algo_factory.h
+++ src/core/algorithms/algo_factory.h
@@ -1,55 +1,18 @@
 #pragma once
 
-#include <type_traits>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <unordered_set>
 
 #include <boost/any.hpp>
-#include <boost/mp11/algorithm.hpp>
-#include <boost/program_options.hpp>
-#include <enum.h>
 
-#include "algorithms/algorithms.h"
-#include "algorithms/create_algorithm.h"
-#include "algorithms/pipelines/typo_miner/typo_miner.h"
-#include "config/names.h"
-#include "tabular_data/input_tables_type.h"
+#include "algorithms/algorithm.h"
+#include "algorithms/algorithm_types.h"
 
 namespace algos {
 
 using StdParamsMap = std::unordered_map<std::string, boost::any>;
-
-namespace details {
-
-inline boost::any GetAny(boost::program_options::variable_value const& value) {
-    return value.value();
-}
-
-inline boost::any GetAny(boost::any const& value) {
-    return value;
-}
-
-template <typename OptionMap>
-boost::any ExtractAnyFromMap(OptionMap& options, std::string_view option_name) {
-    using std::is_same_v, std::decay, boost::program_options::variables_map;
-    const std::string string_opt{option_name};
-    auto it = options.find(string_opt);
-    if (it == options.end()) {
-        throw std::out_of_range("No option named \"" + string_opt + "\" in parameters.");
-    }
-    return GetAny(options.extract(it).mapped());
-}
-
-template <typename T, typename OptionMap>
-T ExtractOptionValue(OptionMap&& options, std::string const& option_name) {
-    return boost::any_cast<T>(ExtractAnyFromMap(std::forward<OptionMap>(options), option_name));
-}
-
-template <typename OptionMap>
-boost::any GetOrEmpty(OptionMap const& options, std::string_view option_name) {
-    auto it = options.find(std::string{option_name});
-    return it == options.end() ? boost::any{} : GetAny(it->second);
-}
-
-}  // namespace details
 
 template <typename FuncType>
 void ConfigureFromFunction(Algorithm& algorithm, FuncType get_opt_value_by_name) {
@@ -61,78 +24,19 @@ void ConfigureFromFunction(Algorithm& algorithm, FuncType get_opt_value_by_name)
     }
 }
 
-template <typename OptionMap>
-void ConfigureFromMap(Algorithm& algorithm, OptionMap&& options) {
-    ConfigureFromFunction(algorithm, [&options](std::string_view option_name) {
-        return details::GetOrEmpty(options, option_name);
-    });
-}
+void ConfigureFromMap(Algorithm& algorithm, StdParamsMap const& options);
+void LoadAlgorithm(Algorithm& algorithm, StdParamsMap const& options);
 
-template <typename OptionMap>
-void LoadAlgorithm(Algorithm& algorithm, OptionMap&& options) {
-    ConfigureFromFunction(algorithm, [&options](std::string_view option_name) {
-        using namespace config::names;
-        namespace fs = std::filesystem;
-        if (option_name == kTable && options.find(std::string{kTable}) == options.end()) {
-            config::InputTable parser = std::make_shared<CSVParser>(
-                    details::ExtractOptionValue<fs::path>(options, kCsvPath),
-                    details::ExtractOptionValue<char>(options, kSeparator),
-                    details::ExtractOptionValue<bool>(options, kHasHeader));
-            return boost::any{parser};
-        } else if (option_name == kTables && options.find(std::string{kTables}) == options.end()) {
-            auto paths = details::ExtractOptionValue<std::vector<fs::path>>(options, kCsvPaths);
-            auto separator = details::ExtractOptionValue<char>(options, kSeparator);
-            auto has_header = details::ExtractOptionValue<bool>(options, kHasHeader);
-
-            if (paths.empty()) {
-                throw config::ConfigurationError("Expected collection of csv paths");
-            }
-
-            config::InputTables tables;
-            tables.reserve(paths.size());
-            for (auto const& path : paths) {
-                tables.push_back(std::make_shared<CSVParser>(path, separator, has_header));
-            }
-            return boost::any{tables};
-        }
-        return details::GetOrEmpty(options, option_name);
-    });
-    algorithm.LoadData();
-    ConfigureFromMap(algorithm, options);
-}
-
-template <typename T, typename OptionMap>
-std::unique_ptr<T> CreateAndLoadAlgorithm(OptionMap&& options) {
+template <typename T>
+std::unique_ptr<T> CreateAndLoadAlgorithm(StdParamsMap const& options) {
     std::unique_ptr<T> algorithm = std::make_unique<T>();
-    LoadAlgorithm(*algorithm, std::forward<OptionMap>(options));
+    LoadAlgorithm(*algorithm, options);
     return algorithm;
 }
 
-template <typename OptionMap>
-std::unique_ptr<Algorithm> CreateAlgorithm(AlgorithmType algorithm_enum, OptionMap&& options) {
-    std::unique_ptr<Algorithm> algorithm = CreateAlgorithmInstance(algorithm_enum);
-    LoadAlgorithm(*algorithm, std::forward<OptionMap>(options));
-    return algorithm;
-}
-
-template <typename OptionMap>
-std::unique_ptr<Algorithm> CreateTypoMiner(OptionMap&& options) {
-    using config::names::kPreciseAlgorithm, config::names::kApproximateAlgorithm;
-    using details::ExtractOptionValue;
-    AlgorithmType precise_algo = ExtractOptionValue<AlgorithmType>(options, kPreciseAlgorithm);
-    AlgorithmType approx_algo = ExtractOptionValue<AlgorithmType>(options, kApproximateAlgorithm);
-    std::unique_ptr<TypoMiner> typo_miner = std::make_unique<TypoMiner>(precise_algo, approx_algo);
-    LoadAlgorithm(*typo_miner, std::forward<OptionMap>(options));
-    return typo_miner;
-}
-
-template <typename OptionMap>
-std::unique_ptr<Algorithm> CreateAlgorithm(std::string const& algorithm_name, OptionMap&& options) {
-    if (algorithm_name == "typo_miner") {
-        return CreateTypoMiner(std::forward<OptionMap>(options));
-    }
-    AlgorithmType const algorithm_enum = AlgorithmType::_from_string_nocase(algorithm_name.c_str());
-    return CreateAlgorithm(algorithm_enum, std::forward<OptionMap>(options));
-}
-
+std::unique_ptr<Algorithm> CreateAlgorithm(AlgorithmType algorithm_enum,
+                                           StdParamsMap const& options);
+std::unique_ptr<Algorithm> CreateTypoMiner(StdParamsMap const& options);
+std::unique_ptr<Algorithm> CreateAlgorithm(std::string const& algorithm_name,
+                                           StdParamsMap const& options);
 }  // namespace algos
