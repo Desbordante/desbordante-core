8bca5d242a7a8577c1b2246728531ab92bac197e
xJoskiy <pavel5553-20@mail.ru>
2023-12-30 15:44:44 +0300

    Correct test cases for DataStats in Python

--- src/python_bindings/test_pybind_data_stats.py
+++ src/python_bindings/test_pybind_data_stats.py
@@ -3,7 +3,7 @@ import desbordante as db
 
 DATASET_PATH = "TestDataStats.csv"
 SEPARATOR = ','
-HAS_HEADER = True
+HAS_HEADER = False
 
 
 class TestDataStats(unittest.TestCase):
@@ -35,8 +35,8 @@ class TestDataStats(unittest.TestCase):
         self.assertIsNone(self.data_stats.get_median_ad(0))
 
     def test_get_number_of_values(self) -> None:
-        res = self.data_stats.get_number_of_values(1)
-        expected = 3
+        res = self.data_stats.get_number_of_values(0)
+        expected = 0
         self.assertEqual(expected, res)
 
     def test_get_number_of_columns(self) -> None:
@@ -56,11 +56,11 @@ class TestDataStats(unittest.TestCase):
 
     def test_get_columns_with_all_unique_values(self) -> None:
         res = self.data_stats.get_columns_with_all_unique_values()
-        expected = [6, 8, 9, 10]
+        expected = [8, 9, 10]
         self.assertEqual(expected, res)
 
     def test_get_number_of_distinct(self) -> None:
-        res = self.data_stats.get_number_of_distinct(7)
+        res = self.data_stats.get_number_of_distinct(5)
         expected = 6
         self.assertEqual(expected, res)
 
@@ -70,22 +70,22 @@ class TestDataStats(unittest.TestCase):
 
     def test_get_average(self) -> None:
         res = self.data_stats.get_average(2)
-        expected = 70.5133333
+        expected = 53.1525
         self.assertAlmostEqual(expected, res)
 
     def test_get_corrected_std(self) -> None:
         res = self.data_stats.get_corrected_std(7)
-        expected = 352.4417534
+        expected = 335.7594687731
         self.assertAlmostEqual(expected, res)
 
     def test_get_skewness(self) -> None:
         res = self.data_stats.get_skewness(7)
-        expected = 0.97232238
+        expected = 1.166303561798
         self.assertAlmostEqual(expected, res)
 
     def test_get_kurtosis(self) -> None:
         res = self.data_stats.get_kurtosis(8)
-        expected = 0.293771
+        expected = 1.05882994629
         self.assertAlmostEqual(expected, res)
 
     def test_get_min(self) -> None:
@@ -99,55 +99,58 @@ class TestDataStats(unittest.TestCase):
         self.assertEqual(expected, res)
 
     def test_get_sum(self) -> None:
-        res = self.data_stats.get_sum(9)
-        expected = 1142.32
+        res = self.data_stats.get_sum(2)
+        expected = 212.61
         self.assertAlmostEqual(expected, res)
 
     def test_get_quantile(self) -> None:
-        res = self.data_stats.get_quantile(0.25, 9)
-        expected = 10
-        self.assertEqual(expected, res)
+        quantile_0_25 = self.data_stats.get_quantile(0.25, 4)
+        quantile_0_5 = self.data_stats.get_quantile(0.5, 4)
+        quantile_0_75 = self.data_stats.get_quantile(0.75, 4)
+
+        self.assertEqual(2, quantile_0_25)
+        self.assertEqual(3, quantile_0_5)
+        self.assertEqual(4, quantile_0_75)
 
     def test_get_number_of_zeros(self) -> None:
         res = self.data_stats.get_number_of_zeros(7)
-        expected = 2
+        expected = 3
         self.assertEqual(expected, res)
 
     def test_get_number_of_negatives(self) -> None:
         res = self.data_stats.get_number_of_negatives(8)
-        expected = 2
+        expected = 3
         self.assertEqual(expected, res)
 
     def test_get_sum_of_squares(self) -> None:
-        res = self.data_stats.get_sum_of_squares(8)
-        expected = 105729737
-        self.assertEqual(expected, res)
+        res = self.data_stats.get_sum_of_squares(7)
+        expected = 1096089.607224
+        self.assertAlmostEqual(expected, res)
 
     def test_get_geometric_mean(self) -> None:
         res = self.data_stats.get_geometric_mean(9)
-        expected = 25.23013193
+        expected = 33.33024629230983
         self.assertAlmostEqual(expected, res)
 
     def test_get_mean_ad(self) -> None:
-        res = self.data_stats.get_mean_ad(4)
-        expected = 1.0
+        res = self.data_stats.get_mean_ad(7)
+        expected = 258.263
         self.assertAlmostEqual(expected, res)
 
     def test_get_median(self) -> None:
-        res = self.data_stats.get_median(4)
-        expected = 3.5
+        res = self.data_stats.get_median(9)
+        expected = 25.875
         self.assertAlmostEqual(expected, res)
 
     def test_get_median_ad(self) -> None:
-        res = self.data_stats.get_median_ad(2)
-        expected = 33.22
+        res = self.data_stats.get_median_ad(8)
+        expected = 123
         self.assertAlmostEqual(expected, res)
 
     def test_get_num_nulls(self) -> None:
         res = self.data_stats.get_num_nulls(0)
-        expected = 4
+        expected = 5
         self.assertEqual(expected, res)
-
 
 if __name__ == "__main__":
     unittest.main()
