50511710637ba740880bbc9ea60096db07d12afa
Ivan Morozko <ivan.morozko01@gmail.com>
2024-03-01 07:01:19 +0400

    Add test that checks that PredicateProvider works
    This simple test creates two predicates on a 2x2 table and evaluates them.
    We're checking for mo::GetPredicate function ability to correctly
    create a predicate

--- src/tests/all_csv_configs.cpp
+++ src/tests/all_csv_configs.cpp
@@ -120,4 +120,5 @@ CSVConfig const kTestDynamicFDUpdateBad3 =
 CSVConfig const kTestDynamicFDUpdateBad4 =
         CreateCsvConfig("dynamic_fd/TestDynamicUpdateBad4.csv", ',', true);
 CSVConfig const kLineItem = CreateCsvConfig("LineItem.csv", '|', true);
+CSVConfig const kTmpDC = CreateCsvConfig("tmp_dc.csv", ',', true);
 }  // namespace tests
