2a3dd662c747ea9a90c2b87fac715b0945b2fa89
Ivan Morozko <ivan.morozko01@gmail.com>
2024-03-02 21:51:24 +0400

    Implement IndexProvider class
    IndexProvider assigns unique indices to each distinct object of
    type T added to it. It will be used later for two main operations:
    1. Map out all predicates to numbers to use dynamic bitsets for quick
    intersection/etc.
    2. Hash all values in the table keeping their relative order
    (ignoring columns of DC-unsupported types. Only ints, doubles and
    strings are allowed). That is, the same values are substituited by
    the same integers, and higher values are replaced by larger integers

---
+++ src/core/algorithms/dc/index_provider.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <concepts>
+#include <unordered_map>
+#include <vector>
+
+#include "dc/base_provider.h"
+
+namespace model {
+
+/**
+ * @brief Singleton manager of unique indices for a collection of objects.
+ *
+ * This template class assigns a unique index to each distinct object added to it and maintains
+ * a bidirectional mapping between objects and their indices.
+ */
+template <std::totally_ordered<> T>
+class IndexProvider : public BaseProvider<IndexProvider<T>> {
+private:
+    size_t next_index_ = 0;
+    std::vector<T> objects_;
+    std::unordered_map<T, size_t> indexes_;
+
+    friend BaseProvider<IndexProvider<T>>;
+
+    static std::string ClassName() {
+        return "IndexProvider";
+    }
+
+    static void Clear() {
+        BaseProvider<IndexProvider<T>>::instance_->objects_.clear();
+        BaseProvider<IndexProvider<T>>::instance_->indexes_.clear();
+    }
+
+public:
+    size_t GetIndex(T object);
+
+    void AddAll(std::vector<T> const& objects);
+
+    T GetObject(size_t index) const;
+
+    size_t Size() const;
+
+    void Sort();
+};
+
+}  // namespace model
