daad4ac175138756434eada7984cce14f27916c6
Ivan Morozko <ivan.morozko01@gmail.com>
2024-03-02 21:51:24 +0400

    Implement IndexProvider class
    IndexProvider assigns unique indices to each distinct object of
    type T added to it. It will be used later for two main operations:
    1. Map out all predicates to numbers to use dynamic bitsets for quick
    intersection/etc.
    2. Hash all values in the table keeping their relative order
    (ignoring columns of DC-unsupported types. Only ints, doubles and
    strings are allowed). That is, the same values are substituited by
    the same integers, and higher values are replaced by larger integers

---
+++ src/core/algorithms/dc/index_provider.cpp
@@ -0,0 +1,42 @@
+#include "dc/index_provider.h"
+
+#include <algorithm>
+
+#include <easylogging++.h>
+
+namespace model {
+
+template <std::totally_ordered<> T>
+size_t IndexProvider<T>::GetIndex(T object) {
+    auto it = indexes_.find(object);
+    if (it == indexes_.end()) {
+        indexes_[object] = next_index_;
+        objects_.push_back(object);
+        return next_index_++;
+    }
+    return it->second;
+}
+
+template <std::totally_ordered<> T>
+void IndexProvider<T>::AddAll(std::vector<T> const& objects) {
+    for (auto object : objects) GetIndex(object);
+}
+
+template <std::totally_ordered<> T>
+T IndexProvider<T>::GetObject(size_t index) const {
+    return objects_.at(index);
+}
+
+template <std::totally_ordered<> T>
+size_t IndexProvider<T>::Size() const {
+    return objects_.size();
+}
+
+template <std::totally_ordered<> T>
+void IndexProvider<T>::Sort() {
+    std::sort(objects_.begin(), objects_.end());
+
+    for (size_t i = 0; i < objects_.size(); ++i) indexes_[objects_[i]] = i;
+}
+
+}  // namespace model
