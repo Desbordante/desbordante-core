0ebda7d70c56b8f66faa29252e9c280e3bcc2577
Ivan Morozko <ivan.morozko01@gmail.com>
2024-03-07 19:48:08 +0400

    Implement PrediateBuilder class
    Generates and categorizes predicates for the future evidence set construction

---
+++ src/core/algorithms/dc/predicate_builder.cpp
@@ -0,0 +1,131 @@
+#include "predicate_builder.h"
+
+#include <easylogging++.h>
+
+#include "index_provider.h"
+#include "operator.h"
+#include "predicate_provider.h"
+#include "typed_column_data_value_differences.h"
+
+namespace model {
+
+PredicateBuilder::PredicateBuilder(bool allow_different_columns, double minimum_shared_value,
+                                   double comparable_threshold)
+    : allow_cross_columns_(allow_different_columns),
+      minimum_shared_value_(minimum_shared_value),
+      comparable_threshold_(comparable_threshold) {
+    PredicateProvider::CreateInstance();
+    PredicateIndexProvider::CreateInstance();
+}
+
+PredicateBuilder::~PredicateBuilder() {
+    PredicateProvider::ClearInstance();
+    PredicateIndexProvider::ClearInstance();
+}
+
+void PredicateBuilder::BuildPredicateSpace(std::vector<TypedColumnData> const& input) {
+    BuildAndCategorizePredicates(input);
+
+    // Populate global PredicateIndexProvider with built predicates.
+    PredicateIndexProvider::GetInstance()->AddAll(predicates_);
+    BuildMutexMap();
+    BuildInverseMap();
+
+    LOG(DEBUG) <<" [Predicate] Predicate space size: " <<predicates_.size();
+}
+
+static size_t PredIdx(PredicatePtr const& p) {
+    return PredicateIndexProvider::GetInstance()->GetIndex(p);
+}
+
+void PredicateBuilder::BuildMutexMap() {
+    mutex_map_.resize(predicates_.size());
+
+    for (auto const& p1 : predicates_) {
+        // TODO: what if we use: if (index >= bitset.size()) bitset.resize(index + 1);
+        mutex_map_[PredIdx(p1)].resize(predicates_.size());
+
+        for (auto const& p2 : predicates_) {
+            if (p2->HasSameOperandsAs(p1)) mutex_map_[PredIdx(p1)].set(PredIdx(p2));
+        }
+    }
+}
+
+void PredicateBuilder::BuildInverseMap() {
+    inverse_map_.resize(predicates_.size());
+
+    for (auto const& p1 : predicates_) {
+        inverse_map_[PredIdx(p1)] = PredIdx(p1->GetInverse());
+    }
+}
+
+void PredicateBuilder::BuildAndCategorizePredicates(std::vector<TypedColumnData> const& input) {
+    size_t columns_num = input.size();
+    predicates_.reserve((columns_num * (columns_num + 1)) / 2);
+
+    for (size_t i = 0; i < columns_num; ++i) {
+        for (size_t j = i; j < columns_num; ++j) {
+            ProcessColumnPair(i, j, input);
+        }
+    }
+}
+
+void PredicateBuilder::ProcessColumnPair(size_t i, size_t j,
+                                         std::vector<TypedColumnData> const& input) {
+    bool joinable = IsJoinable(input[i], input[j]);
+    bool comparable = IsComparable(input[i], input[j]);
+
+    if (joinable || comparable) {
+        AddAndCategorizePredicate(ColumnOperand(input[i].GetColumn(), true),
+                                  ColumnOperand(input[j].GetColumn(), false), comparable);
+    }
+}
+
+void PredicateBuilder::CategorizeLastPredicate(bool comparable) {
+    auto const& pred = predicates_.back();
+
+    if (comparable) {
+        if (pred->IsCrossColumn())
+            num_cross_column_predicates_.push_back(pred);
+        else
+            num_single_column_predicates_.push_back(pred);
+    } else {
+        if (pred->IsCrossColumn())
+            str_cross_column_predicates_.push_back(pred);
+        else
+            str_single_column_predicates_.push_back(pred);
+    }
+}
+
+void PredicateBuilder::AddAndCategorizePredicate(ColumnOperand const& left,
+                                                 ColumnOperand const& right, bool comparable) {
+    for (auto op : kAllOperatorTypes) {
+        bool should_add_predicate =
+                comparable || op == OperatorType::kEqual || op == OperatorType::kUnequal;
+
+        if (should_add_predicate) {
+            predicates_.push_back(GetPredicate(Operator(op), left, right));
+            CategorizeLastPredicate(comparable);
+        }
+    }
+}
+
+bool PredicateBuilder::IsJoinable(TypedColumnData const& c1, TypedColumnData const& c2) {
+    if (!allow_cross_columns_) return c1.GetColumn() == c2.GetColumn();
+
+    if (c1.GetTypeId() != c2.GetTypeId()) return false;
+
+    // FIXME: diff > epsilon?
+    return GetSharedPercentage(c1, c2) > minimum_shared_value_;
+}
+
+bool PredicateBuilder::IsComparable(TypedColumnData const& c1, TypedColumnData const& c2) {
+    if (!allow_cross_columns_) return c1.GetColumn() == c2.GetColumn() && (c1.IsNumeric());
+
+    if (c1.GetTypeId() != c2.GetTypeId()) return false;
+
+    // FIXME: diff > epsilon?
+    return c1.IsNumeric() && GetAverageRatio(c1, c2) > comparable_threshold_;
+}
+
+}  // namespace model
---
+++ src/core/algorithms/dc/predicate_builder.h
@@ -0,0 +1,121 @@
+#pragma once
+
+#include <vector>
+
+#include "predicate.h"
+#include "table/typed_column_data.h"
+
+namespace model {
+
+/**
+ * @brief Constructs the predicate space for a given table and organizes predicates into categories.
+ *
+ * Generates set of predicates based on the provided table's columns. It evaluates each pair of
+ * columns to determine if meaningful predicates can be formed. It also categorizes them into four
+ * groups: numeric predicates on single columns, numeric predicates across columns, string
+ * predicates on single columns, and string predicates across columns. These groups will later be
+ * used for Evidence set building. Additionally, the builder constructs inverse maps and mutex maps
+ * to efficiently navigate between predicates and their relationships.
+ */
+class PredicateBuilder {
+private:
+    bool IsJoinable(TypedColumnData const& c1, TypedColumnData const& c2);
+    bool IsComparable(TypedColumnData const& c1, TypedColumnData const& c2);
+    void AddAndCategorizePredicate(ColumnOperand const& c1, ColumnOperand const& c2,
+                                   bool comparable);
+    void ProcessColumnPair(size_t i, size_t j, std::vector<TypedColumnData> const& input);
+    void CategorizeLastPredicate(bool comparable);
+
+    /**
+     * Fills internal predicate vectors with predicates.
+     *
+     * Goes through every pair of columns in the table, checks
+     * whether we can form menaningful predicates with the current pair
+     * by measuring how data in these columns are similar to each other.
+     */
+    void BuildAndCategorizePredicates(std::vector<TypedColumnData> const& input);
+    void BuildMutexMap();
+    void BuildInverseMap();
+
+    bool allow_cross_columns_;
+    double minimum_shared_value_;
+    double comparable_threshold_;
+
+    PredicatesVector predicates_;
+    PredicatesVector num_single_column_predicates_;
+    PredicatesVector num_cross_column_predicates_;
+    PredicatesVector str_single_column_predicates_;
+    PredicatesVector str_cross_column_predicates_;
+
+    // i (predicate num from PredicateIndexProvider) -> predicates with the same column pair as i
+    std::vector<boost::dynamic_bitset<>> mutex_map_;
+    // i -> index of predicate having inverse operator to predicate i
+    std::vector<size_t> inverse_map_;
+
+public:
+    PredicateBuilder(PredicateBuilder const& other) = delete;
+    PredicateBuilder& operator=(PredicateBuilder const& other) = delete;
+    PredicateBuilder(PredicateBuilder&& other) noexcept = default;
+    PredicateBuilder& operator=(PredicateBuilder&& other) noexcept = default;
+    ~PredicateBuilder();
+
+    /**
+     * Constructs a PredicateBuilder with specified configuration parameters.
+     *
+     * @param allow_cross_columns Determines whether predicates can be formed between different
+     * columns.
+     *
+     * @param minimum_shared_value The minimum percentage of shared values required for two columns
+     * to be considered joinable. This parameter is used to assess the similarity between columns
+     * based on the overlap in their values. A higher value indicates a stricter criterion for
+     * joinability, requiring a greater overlap in the values present in the two columns.
+     * It's expressed as a fraction between 0 and 1.
+     *
+     * @param comparable_threshold The threshold used to determine whether two columns are
+     * comparable based on their data characteristic. This value will be compared with the ratio of
+     * the columns average values. Columns with a ratio exceeding this threshold are considered
+     * sufficiently similar to be comparable, allowing for predicates that involve comparison
+     * operators (<, <=, >, >=) to be generated between them.
+     * It's expressed as a fraction between 0 and 1.
+     */
+    PredicateBuilder(bool allow_cross_columns, double minimum_shared_value = 0.3,
+                     double comparable_threshold = 0.1);
+
+    // TODO: can we pass just a vector of TypedColumnData, or there should be another table
+    // representation?
+    void BuildPredicateSpace(std::vector<TypedColumnData> const& input);
+
+    PredicatesVector const& GetNumSingleColumnPredicates() const noexcept {
+        return num_single_column_predicates_;
+    }
+
+    PredicatesVector const& GetNumCrossColumnPredicates() const noexcept {
+        return num_cross_column_predicates_;
+    }
+
+    PredicatesVector const& GetStrSingleColumnPredicates() const noexcept {
+        return str_single_column_predicates_;
+    }
+
+    PredicatesVector const& GetStrCrossColumnPredicates() const noexcept {
+        return str_cross_column_predicates_;
+    }
+
+    PredicatesVector const& GetPredicates() const noexcept {
+        return predicates_;
+    }
+
+    size_t PredicateCount() const noexcept {
+        return predicates_.size();
+    }
+
+    std::vector<boost::dynamic_bitset<>>&& GetMutexMap() noexcept {
+        return std::move(mutex_map_);
+    }
+
+    std::vector<size_t>&& GetInverseMap() noexcept {
+        return std::move(inverse_map_);
+    }
+};
+
+}  // namespace model
