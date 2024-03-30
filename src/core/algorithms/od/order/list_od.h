ba79e46d615c916e550f614975f1d707329b1dfb
Sched71 <scheka.dm@gmail.com>
2024-03-30 17:38:41 +0300

    Add python bindings for Order

---
+++ src/core/algorithms/od/order/list_od.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <vector>
+
+#include "order_utility.h"
+
+namespace algos::order {
+
+struct ListOD {
+    AttributeList lhs;
+    AttributeList rhs;
+};
+
+}  // namespace algos::order
