Sched71/Desbordante

Refactor ORDER algorithm structure
Order algorithm class became too big, I've decided to split it

src/core/algorithms/order/list_lattice.h
@@ -1,0 +1,42 @@
+#pragma once
+
+#include <unordered_set>
+#include <vector>
+
+#include "order_utility.h"
+
+namespace algos::order {
+
+class ListLattice {
+public:
+    using LatticeLevel = std::unordered_set<Node, Hash>;
+    using PrefixBlocks = std::unordered_map<AttributeList, std::vector<Node>, Hash>;
+
+private:
+    LatticeLevel lattice_level_;
+    unsigned int level_num_ = 1;
+
+    PrefixBlocks GetPrefixBlocks() const;
+    Node JoinNodes(Node const& l, Node const& r) const;
+
+public:
+    ListLattice(CandidateSets& candidate_sets, std::vector<AttributeList> single_attributes);
+
+    CandidatePairs ObtainCandidates(Node const& node) const;
+    void Prune(CandidateSets& candidate_sets);
+    void GenerateNextLevel(CandidateSets& candidate_sets);
+
+    LatticeLevel const& GetLatticeLevel() const {
+        return lattice_level_;
+    }
+
+    unsigned int GetLevelNumber() const {
+        return level_num_;
+    }
+
+    bool IsEmpty() const {
+        return lattice_level_.empty();
+    }
+};
+
+}  // namespace algos::order

src/core/algorithms/order/order_utility.h
@@ -1,0 +1,28 @@
+#pragma once
+
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include <boost/container_hash/hash.hpp>
+
+#include "model/table/column_index.h"
+
+namespace algos::order {
+using Node = std::vector<model::ColumnIndex>;
+using AttributeList = std::vector<model::ColumnIndex>;
+using CandidatePairs = std::vector<std::pair<AttributeList, AttributeList>>;
+using Hash = boost::hash<std::vector<unsigned int>>;
+using CandidateSets =
+        std::unordered_map<AttributeList, std::unordered_set<AttributeList, Hash>, Hash>;
+using OrderDependencies =
+        std::unordered_map<AttributeList, std::unordered_set<AttributeList, Hash>, Hash>;
+
+void PrintOD(AttributeList const& lhs, AttributeList const& rhs);
+std::vector<AttributeList> GetPrefixes(Node const& node);
+AttributeList MaxPrefix(AttributeList const& attribute_list);
+bool InUnorderedMap(OrderDependencies const& map, AttributeList const& lhs,
+                    AttributeList const& rhs);
+bool AreDisjoint(AttributeList const& a, AttributeList const& b);
+bool StartsWith(AttributeList const& rhs_candidate, AttributeList const& rhs);
+}  // namespace algos::order

Desbordante/desbordante-core

src/core/algorithms/od/order/list_lattice.h
@@ -1,0 +1,42 @@
+#pragma once
+
+#include <unordered_set>
+#include <vector>
+
+#include "order_utility.h"
+
+namespace algos::order {
+
+class ListLattice {
+public:
+    using LatticeLevel = std::unordered_set<Node, ListHash>;
+    using PrefixBlocks = std::unordered_map<AttributeList, std::vector<Node>, ListHash>;
+
+private:
+    LatticeLevel lattice_level_;
+    unsigned int level_num_ = 1;
+
+    PrefixBlocks GetPrefixBlocks() const;
+    Node JoinNodes(Node const& l, Node const& r) const;
+
+public:
+    ListLattice(CandidateSets& candidate_sets, std::vector<AttributeList> single_attributes);
+
+    CandidatePairs ObtainCandidates(Node const& node) const;
+    void Prune(CandidateSets& candidate_sets);
+    void GenerateNextLevel(CandidateSets& candidate_sets);
+
+    LatticeLevel const& GetLatticeLevel() const {
+        return lattice_level_;
+    }
+
+    unsigned int GetLevelNumber() const {
+        return level_num_;
+    }
+
+    bool IsEmpty() const {
+        return lattice_level_.empty();
+    }
+};
+
+}  // namespace algos::order

src/core/algorithms/od/order/order_utility.h
@@ -1,0 +1,28 @@
+#pragma once
+
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include <boost/container_hash/hash.hpp>
+
+#include "model/table/column_index.h"
+
+namespace algos::order {
+using Node = std::vector<model::ColumnIndex>;
+using AttributeList = std::vector<model::ColumnIndex>;
+using CandidatePairs = std::vector<std::pair<AttributeList, AttributeList>>;
+using ListHash = boost::hash<AttributeList>;
+using CandidateSets =
+        std::unordered_map<AttributeList, std::unordered_set<AttributeList, ListHash>, ListHash>;
+using OrderDependencies =
+        std::unordered_map<AttributeList, std::unordered_set<AttributeList, ListHash>, ListHash>;
+
+void PrintOD(AttributeList const& lhs, AttributeList const& rhs);
+std::vector<AttributeList> GetPrefixes(Node const& node);
+AttributeList MaxPrefix(AttributeList const& attribute_list);
+bool InUnorderedMap(OrderDependencies const& map, AttributeList const& lhs,
+                    AttributeList const& rhs);
+bool AreDisjoint(AttributeList const& a, AttributeList const& b);
+bool StartsWith(AttributeList const& rhs_candidate, AttributeList const& rhs);
+}  // namespace algos::order