125ea30bac10c33d99a922000a57fc5faa4f089a
Maksim <billsifer00@gmail.com>
2024-04-03 02:02:17 +0300

    Add python bindings for AR

--- src/python_bindings/ar/bind_ar.cpp
+++ src/python_bindings/ar/bind_ar.cpp
@@ -14,15 +14,33 @@ namespace py = pybind11;
 namespace python_bindings {
 void BindAr(py::module_& main_module) {
     using namespace algos;
+    using model::ArIDs;
     using model::ARStrings;
 
     auto ar_module = main_module.def_submodule("ar");
-    py::class_<ARStrings>(ar_module, "AssociativeRule")
+    py::class_<ARStrings>(ar_module, "ARStrings")
             .def("__str__", &ARStrings::ToString)
             .def_readonly("left", &ARStrings::left)
             .def_readonly("right", &ARStrings::right)
             .def_readonly("confidence", &ARStrings::confidence);
-    BindPrimitive<Apriori>(ar_module, &ARAlgorithm::GetArStringsList, "ArAlgorithm", "get_ars",
-                           {"Apriori"}, py::return_value_policy::move);
+
+    py::class_<ArIDs>(ar_module, "ArIDs")
+            .def_readonly("left", &ArIDs::left)
+            .def_readonly("right", &ArIDs::right)
+            .def_readonly("confidence", &ArIDs::confidence);
+
+    py::class_<ARAlgorithm, Algorithm>(ar_module, "ArAlgorithm")
+            .def("get_ars", &ARAlgorithm::GetArStringsList,
+                 py::return_value_policy::reference_internal)
+            .def("get_itemnames", &ARAlgorithm::GetItemNamesVector)
+            .def("get_ar_ids", &ARAlgorithm::GetArIDsList);
+
+    auto algos_module = ar_module.def_submodule("algorithms");
+    auto default_algorithm =
+            detail::RegisterAlgorithm<Apriori, ARAlgorithm>(algos_module, "Apriori");
+    algos_module.attr("Default") = default_algorithm;
+
+    // Perhaps in the future there will be a need for:
+    // default_algorithm.def("get_frequent_list", &Apriori::GetFrequentList);
 }
 }  // namespace python_bindings
