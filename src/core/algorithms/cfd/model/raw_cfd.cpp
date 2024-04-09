612dcac1e9a0698e60b39b6a984538f59132199f
Ivan Volgushev <ivo.volgushev@yandex.ru>
2024-04-09 17:03:04 +0300

    Add CLI, bindings and example for CFD discovery

--- src/core/algorithms/cfd/model/raw_cfd.cpp
+++ src/core/algorithms/cfd/model/raw_cfd.cpp
@@ -32,4 +32,30 @@ std::string RawCFD::ToJSON() const {
     return ss.str();
 }
 
+static std::string RawItemToString(RawCFD::RawItem const& item) {
+    std::stringstream ss;
+    ss << "(" << item.attribute;
+    ss << ", ";
+    if (item.value.has_value()) {
+        ss << item.value.value();
+    } else {
+        ss << "_";
+    }
+    ss << ")";
+    return ss.str();
+}
+
+std::string RawCFD::ToString() const {
+    std::stringstream ss;
+    ss << "{";
+    for (auto it = lhs_.begin(); it != lhs_.end(); ++it) {
+        if (it != lhs_.begin()) {
+            ss << ",";
+        }
+        ss << RawItemToString(*it);
+    }
+    ss << "} -> " << RawItemToString(rhs_);
+    return ss.str();
+}
+
 }  // namespace algos::cfd
--- src/core/algorithms/cfd/model/raw_cfd.h
+++ src/core/algorithms/cfd/model/raw_cfd.h
@@ -11,6 +11,14 @@ using AttributeIndex = int;
 class RawCFD {
 public:
     struct RawItem {
+    public:
+        AttributeIndex GetAttribute() const {
+            return attribute;
+        }
+
+        std::optional<std::string> GetValue() const {
+            return value;
+        }
         AttributeIndex attribute;         /* attribute column index */
         std::optional<std::string> value; /* pattern value is optional */
     };
@@ -25,6 +33,8 @@ public:
     explicit RawCFD(RawItems lhs, RawItem rhs) : lhs_(std::move(lhs)), rhs_(std::move(rhs)) {}
 
     std::string ToJSON() const;
+
+    std::string ToString() const;
 
     RawItems const& GetLhs() const {
         return lhs_;
