Mstrutov/Desbordante

Manage ColumnLayoutRelationData with a separate class
Algorithms don't ask for table options when they are passed an external
instance

src/core/algorithms/fd/fd_mine/fd_mine.cpp
@@ -10,7 +10,8 @@ namespace algos {
 
 using boost::dynamic_bitset;
 
-Fd_mine::Fd_mine() : PliBasedFDAlgorithm({kDefaultPhaseName}) {}
+Fd_mine::Fd_mine(std::optional<ColumnLayoutRelationDataManager> relation_manager)
+    : PliBasedFDAlgorithm({kDefaultPhaseName}, relation_manager) {}
 
 void Fd_mine::ResetStateFd() {
     candidate_set_.clear();

src/core/algorithms/fd/fd_mine/fd_mine.h
@@ -40,7 +40,7 @@ private:
     unsigned long long ExecuteInternal() override;
 
 public:
-    Fd_mine();
+    Fd_mine(std::optional<ColumnLayoutRelationDataManager> relation_manager = std::nullopt);
 };
 
 }  // namespace algos

Desbordante/desbordante-core

src/core/algorithms/fd/fd_mine/fd_mine.cpp
@@ -10,7 +10,8 @@ namespace algos {
 
 using boost::dynamic_bitset;
 
-FdMine::FdMine() : PliBasedFDAlgorithm({kDefaultPhaseName}) {}
+FdMine::FdMine(std::optional<ColumnLayoutRelationDataManager> relation_manager)
+    : PliBasedFDAlgorithm({kDefaultPhaseName}, relation_manager) {}
 
 void FdMine::ResetStateFd() {
     candidate_set_.clear();

src/core/algorithms/fd/fd_mine/fd_mine.h
@@ -40,7 +40,7 @@ private:
     unsigned long long ExecuteInternal() override;
 
 public:
-    FdMine();
+    FdMine(std::optional<ColumnLayoutRelationDataManager> relation_manager = std::nullopt);
 };
 
 }  // namespace algos