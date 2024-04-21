092ab5fa61ac7cb01d7b9a4f60a24fb27ef4accb
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2024-04-21 15:42:14 +0300

    Add GetColumnNames method to IDatasetStream

--- src/core/model/table/idataset_stream.h
+++ src/core/model/table/idataset_stream.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstddef>
 #include <string>
 #include <vector>
 
@@ -9,11 +10,21 @@ class IDatasetStream {
 public:
     virtual std::vector<std::string> GetNextRow() = 0;
     [[nodiscard]] virtual bool HasNextRow() const = 0;
-    [[nodiscard]] virtual size_t GetNumberOfColumns() const = 0;
-    [[nodiscard]] virtual std::string GetColumnName(size_t index) const = 0;
+    [[nodiscard]] virtual std::size_t GetNumberOfColumns() const = 0;
+    [[nodiscard]] virtual std::string GetColumnName(std::size_t index) const = 0;
     [[nodiscard]] virtual std::string GetRelationName() const = 0;
     virtual void Reset() = 0;
     virtual ~IDatasetStream() = default;
+
+    std::vector<std::string> GetColumnNames() const {
+        std::vector<std::string> column_names;
+        std::size_t const number_of_columns = GetNumberOfColumns();
+        column_names.reserve(number_of_columns);
+        for (std::size_t i = 0; i < number_of_columns; ++i) {
+            column_names.push_back(GetColumnName(i));
+        }
+        return column_names;
+    }
 };
 
 }  // namespace model
