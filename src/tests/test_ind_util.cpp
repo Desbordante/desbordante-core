162ef5fd5949f5d59f44ac4c40f0f3f011ab34e8
Chizhov Anton <zr9ihi@gmail.com>
2024-04-24 01:49:14 +0300

    Reorganizing IND mining algorithms tests
    The code for testing ind algorithms has been completely refactored. Thanks to
    this, we were able to reuse faida tests for other dependency mining algorithms.
    Add tests for Mind algorithm

--- src/tests/test_ind_util.cpp
+++ src/tests/test_ind_util.cpp
@@ -3,6 +3,11 @@
 #include <algorithm>
 #include <sstream>
 
+#include <gtest/gtest.h>
+
+#include "all_csv_configs.h"
+#include "ind/ind_algorithm.h"
+
 namespace tests {
 
 std::string TableNamesToString(CSVConfigs const& csv_configs) {
@@ -28,4 +33,69 @@ std::vector<INDTest> ToSortedINDTestVec(std::list<model::IND> const& inds) {
     return ind_test_vec;
 }
 
+void CheckINDsListsEquality(std::list<model::IND> const& actual, INDTestSet const& expected) {
+    ASSERT_EQ(actual.size(), expected.size())
+            << "count of generated dependencies does not match: expected " << expected.size()
+            << ", got " << actual.size();
+
+    for (auto const& dep : actual) {
+        if (expected.find(ToINDTest(dep)) == expected.end()) {
+            FAIL() << "generated dep '" << dep.ToShortString() << "' is not present in expected";
+        }
+    }
+    SUCCEED();
+}
+
+void CheckResultContainsINDs(std::list<model::IND> const& actual, INDTestSet expected_subset) {
+    ASSERT_NE(actual.size(), expected_subset.size())
+            << "count of generated dependencies must not not be equal to the subset size: got "
+            << actual.size();
+
+    for (auto const& dep : actual) {
+        auto iter = expected_subset.find(ToINDTest(dep));
+
+        if (iter != expected_subset.end()) {
+            expected_subset.erase(iter);
+        }
+    }
+    ASSERT_EQ(expected_subset.size(), 0);
+}
+
+void CheckINDsListsEqualityTest(std::unique_ptr<algos::INDAlgorithm> ind_algo,
+                                INDTestSet const& expected_inds) {
+    ind_algo->Execute();
+    CheckINDsListsEquality(ind_algo->INDList(), expected_inds);
+}
+
+void CheckINDsResultContainsINDsTest(std::unique_ptr<algos::INDAlgorithm> ind_algo,
+                                     INDTestSet const& expected_inds_subset,
+                                     size_t expected_result_size) {
+    ind_algo->Execute();
+    ASSERT_EQ(ind_algo->INDList().size(), expected_result_size);
+    CheckResultContainsINDs(ind_algo->INDList(), expected_inds_subset);
+}
+
+std::vector<INDEqualityTestConfig> const kINDEqualityTestConfigs{
+        {{kIndTestWide2},
+         INDTestSet{{{0, {2}}, {0, {0}}}, {{0, {3}}, {0, {1}}}, {{0, {2, 3}}, {0, {0, 1}}}}},
+        {{kIndTestPlanets},
+         INDTestSet{{{0, {0}}, {0, {1}}},
+                    {{0, {1}}, {0, {0}}},
+                    {{0, {2}}, {0, {3}}},
+                    {{0, {3}}, {0, {2}}},
+                    {{0, {1, 3}}, {0, {0, 2}}},
+                    {{0, {0, 2}}, {0, {1, 3}}},
+                    {{0, {0, 3}}, {0, {1, 2}}},
+                    {{0, {1, 2}}, {0, {0, 3}}}}},
+        {{kIndTest3aryInds},
+         INDTestSet{{{0, {3}}, {0, {0}}},
+                    {{0, {4}}, {0, {1}}},
+                    {{0, {5}}, {0, {2}}},
+                    {{0, {2}}, {0, {5}}},
+                    {{0, {3, 4}}, {0, {0, 1}}},
+                    {{0, {4, 5}}, {0, {1, 2}}},
+                    {{0, {3, 5}}, {0, {0, 2}}},
+                    {{0, {3, 4, 5}}, {0, {0, 1, 2}}},
+                    {{0, {3, 4, 5}}, {0, {0, 1, 2}}}}},
+};
 }  // namespace tests
--- src/tests/test_ind_util.h
+++ src/tests/test_ind_util.h
@@ -5,15 +5,40 @@
 #include <vector>
 
 #include "algorithms/ind/ind.h"
+#include "algorithms/ind/ind_algorithm.h"
 #include "csv_config_util.h"
 
 namespace tests {
 
 using CCTest = std::pair<model::TableIndex, std::vector<model::ColumnIndex>>;
 using INDTest = std::pair<CCTest, CCTest>;
+using INDTestSet = std::set<INDTest>;
 
 std::string TableNamesToString(CSVConfigs const& csv_configs);
 INDTest ToINDTest(model::IND const& ind);
 std::vector<INDTest> ToSortedINDTestVec(std::list<model::IND> const& inds);
+
+void CheckINDsListsEquality(std::list<model::IND> const& actual, INDTestSet const& expected);
+void CheckResultContainsINDs(std::list<model::IND> const& actual, INDTestSet expected_subset);
+
+void CheckINDsListsEqualityTest(std::unique_ptr<algos::INDAlgorithm> ind_algo,
+                                INDTestSet const& expected_inds);
+
+void CheckINDsResultContainsINDsTest(std::unique_ptr<algos::INDAlgorithm> ind_algo,
+                                     INDTestSet const& expected_inds_subset,
+                                     size_t expected_result_size);
+
+struct INDEqualityTestConfig {
+    CSVConfigs csv_configs;
+    INDTestSet expected_inds;
+};
+
+/*
+ * Test configurations for which both Faida and other IND algorithms produce the same results.
+ * If a config is found for which Faida produces a different number of dependencies (due to
+ * the fact that Faida’s algorithm is approximate), then you will need to add this config to
+ * a separate vector.
+ */
+extern std::vector<INDEqualityTestConfig> const kINDEqualityTestConfigs;
 
 }  // namespace tests
