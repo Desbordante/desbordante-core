7d8f95bb8c3f86191390ac8c4ed87275deb0385a
Chizhov Anton <zr9ihi@gmail.com>
2024-04-24 01:46:44 +0300

    Implement `Mind` algorithm
    See the paper "Unary and n-ary inclusion dependency discovery in relational databases"

---
+++ src/core/algorithms/ind/mind/raw_ind.h
@@ -0,0 +1,37 @@
+/** \file
+ * \brief Mind algorithm
+ *
+ * Mind-specific IND representation.
+ */
+#pragma once
+
+#include <sstream>
+
+#include "model/table/column_combination.h"
+
+namespace algos::mind {
+
+struct RawIND {
+    model::ColumnCombination lhs;
+    model::ColumnCombination rhs;
+
+    bool operator==(RawIND const& other) const noexcept {
+        return lhs == other.lhs && rhs == other.rhs;
+    }
+
+    bool operator!=(RawIND const& other) const noexcept {
+        return !(*this == other);
+    }
+};
+
+}  // namespace algos::mind
+
+template <>
+struct std::hash<algos::mind::RawIND> {
+    size_t operator()(algos::mind::RawIND const& ind) const {
+        size_t hash = 0;
+        hash ^= ind.lhs.GetHash();
+        hash = std::rotl(hash, 11) ^ ind.rhs.GetHash();
+        return hash;
+    }
+};
