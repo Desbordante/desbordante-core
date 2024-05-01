6de7b1af089abf087ee1088765d0520201665cbe
Ivan Morozko <ivan.morozko01@gmail.com>
2024-05-01 19:10:35 +0400

    Add test that checks static fields of CommonClueSetBuilder
    Validates the number of bits in the clue, the structure of the predicate packs.
    And the correction map which stores predicate-to-bitset mappings

--- src/tests/test_dc_structures_correct_results.h
+++ src/tests/test_dc_structures_correct_results.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <bitset>
 #include <string>
 #include <vector>
 
@@ -31,3 +32,26 @@ std::vector<std::string> str_single_column_predicate_group_expected = {"t.F == s
                                                                        "t.G == s.G", "t.G != s.G"};
 
 std::vector<std::string> str_cross_column_predicate_group_expected = {};
+
+std::vector<std::pair<size_t, size_t>> expected_column_indices = {
+        {5, 5}, {6, 6}, {0, 0}, {1, 1}, {2, 2}, {3, 3}, {4, 4}, {0, 2}, {0, 3}, {2, 3}};
+
+std::vector<std::vector<size_t>> expected_eq_masks = {{0}, {1},  {2},  {4},  {6},
+                                                      {8}, {10}, {12}, {14}, {16}};
+
+std::vector<std::vector<size_t>> expected_gt_masks = {{},  {},   {3},  {5},  {7},
+                                                      {9}, {11}, {13}, {15}, {17}};
+
+std::vector<std::vector<size_t>> expected_correction_map = {
+        {48, 49},         {50, 51},         {0, 1, 3, 4},     {2, 3, 4, 5},     {18, 19, 21, 22},
+        {20, 21, 22, 23}, {24, 25, 27, 28}, {26, 27, 28, 29}, {36, 37, 39, 40}, {38, 39, 40, 41},
+        {42, 43, 45, 46}, {44, 45, 46, 47}, {6, 7, 9, 10},    {8, 9, 10, 11},   {12, 13, 15, 16},
+        {14, 15, 16, 17}, {30, 31, 33, 34}, {32, 33, 34, 35}};
+
+std::bitset<64> VectorToBitset(std::vector<size_t> const& positions) {
+    std::bitset<64> bset;
+    for (auto pos : positions) {
+        bset.set(pos);
+    }
+    return bset;
+}
