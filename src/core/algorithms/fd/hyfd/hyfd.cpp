48fc8164c489324cbc71d30e5f6395f84e552064
Michael Tsukanov <tsukanovWOW@ya.ru>
2024-05-06 00:00:40 +0300

    Multi-threaded implementation of HyFD algorithm
    The HyFD algorithm was originally single-threaded,
    which goes against its intended implementation.
    If ThreadNumber option is specified in the algorithm configuration step,
    the algorithm will utilize the specified number of threads.

--- src/core/algorithms/fd/hyfd/hyfd.cpp
+++ src/core/algorithms/fd/hyfd/hyfd.cpp
@@ -12,6 +12,8 @@
 
 #include "algorithms/fd/hycommon/preprocessor.h"
 #include "algorithms/fd/hycommon/util/pli_util.h"
+#include "config/names.h"
+#include "config/thread_number/option.h"
 #include "inductor.h"
 #include "sampler.h"
 #include "validator.h"
@@ -19,7 +21,13 @@
 namespace algos::hyfd {
 
 HyFD::HyFD(std::optional<ColumnLayoutRelationDataManager> relation_manager)
-    : PliBasedFDAlgorithm({}, relation_manager) {}
+    : PliBasedFDAlgorithm({}, relation_manager) {
+    RegisterOption(config::kThreadNumberOpt(&threads_num_));
+}
+
+void HyFD::MakeExecuteOptsAvailable() {
+    MakeOptionsAvailable({config::names::kThreads});
+}
 
 unsigned long long HyFD::ExecuteInternal() {
     using namespace hy;
@@ -30,12 +38,12 @@ unsigned long long HyFD::ExecuteInternal() {
     auto const plis_shared = std::make_shared<PLIs>(std::move(plis));
     auto const pli_records_shared = std::make_shared<Rows>(std::move(pli_records));
 
-    Sampler sampler(plis_shared, pli_records_shared);
+    Sampler sampler(plis_shared, pli_records_shared, threads_num_);
 
     auto const positive_cover_tree =
             std::make_shared<fd_tree::FDTree>(GetRelation().GetNumColumns());
     Inductor inductor(positive_cover_tree);
-    Validator validator(positive_cover_tree, plis_shared, pli_records_shared);
+    Validator validator(positive_cover_tree, plis_shared, pli_records_shared, threads_num_);
 
     IdPairs comparison_suggestions;
 
