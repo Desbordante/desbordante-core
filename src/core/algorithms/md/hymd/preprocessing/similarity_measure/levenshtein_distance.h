c05c8a6ef9cd5d7afe3a96ffd6688c31a0e8d412
Daniil Nikolaev <quanttlight@gmail.com>
2024-05-06 14:52:24 +0300

    Update LevenshteinDistance

--- src/core/algorithms/md/hymd/preprocessing/similarity_measure/levenshtein_distance.h
+++ src/core/algorithms/md/hymd/preprocessing/similarity_measure/levenshtein_distance.h
@@ -1,3 +1,3 @@
 #include <string_view>
 
-unsigned LevenshteinDistance(std::string_view r, std::string_view l) noexcept;
+unsigned LevenshteinDistance(std::string_view l, std::string_view r) noexcept;
