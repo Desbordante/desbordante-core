b9f1597ef9d22fd19ba4b825cf617f6d6cf84742
Daniil Nikolaev <quanttlight@gmail.com>
2024-05-07 23:52:13 +0300

    Add Creator for every metric

--- src/core/algorithms/md/hymd/preprocessing/similarity_measure/date_dif_similarity_measure.h
+++ src/core/algorithms/md/hymd/preprocessing/similarity_measure/date_dif_similarity_measure.h
@@ -2,6 +2,8 @@
 #include <cstdlib>
 
 #include "algorithms/md/hymd/preprocessing/similarity_measure/distance_similarity_measure.h"
+#include "algorithms/md/hymd/similarity_measure_creator.h"
+#include "config/exceptions.h"
 #include "model/types/date_type.h"
 
 size_t DateDifference(model::Date const& left, model::Date const& right) {
@@ -10,6 +12,25 @@ size_t DateDifference(model::Date const& left, model::Date const& right) {
 
 namespace algos::hymd::preprocessing::similarity_measure {
 class DateSimilarityMeasure : public DistanceSimilarityMeasure {
+    static constexpr auto kName = "date_similarity";
+
+public:
+    class Creator final : public SimilarityMeasureCreator {
+        model::md::DecisionBoundary const min_sim_;
+
+    public:
+        Creator(model::md::DecisionBoundary min_sim)
+            : SimilarityMeasureCreator(kName), min_sim_(min_sim) {
+            if (!(0.0 <= min_sim_ && min_sim_ <= 1.0)) {
+                throw config::ConfigurationError("Minimum similarity out of range");
+            }
+        }
+
+        std::unique_ptr<SimilarityMeasure> MakeMeasure() const final {
+            return std::make_unique<DateSimilarityMeasure>(min_sim_);
+        }
+    };
+
 public:
     DateSimilarityMeasure(model::md::DecisionBoundary min_sim)
         : DistanceSimilarityMeasure(
