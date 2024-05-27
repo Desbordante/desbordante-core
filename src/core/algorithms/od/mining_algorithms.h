33202dfe3b4094b9b21d26a6b9abd54d372fd9be
Ivan Khromov <ivankhromov05@mail.ru>
2024-05-28 00:41:12 +0300

    Fix clang-format 2

--- src/core/algorithms/od/mining_algorithms.h
+++ src/core/algorithms/od/mining_algorithms.h
@@ -1,5 +1,5 @@
 #pragma once
 
 #include "algorithms/od/fastod/fastod.h"
-#include "algorithms/od/order/order.h"
 #include "algorithms/od/od_verifier/od_verifier.h"
+#include "algorithms/od/order/order.h"
