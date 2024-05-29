ebcb2ad8bf61815ffe136ef23aa62c2102bb884d
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2024-05-29 16:03:22 +0300

    Move to boost flat collections for indexes

--- src/core/algorithms/md/hymd/indexes/similarity_index.h
+++ src/core/algorithms/md/hymd/indexes/similarity_index.h
@@ -1,15 +1,15 @@
 #pragma once
 
-#include <map>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
+
+#include <boost/container/flat_map.hpp>
+#include <boost/unordered/unordered_flat_set.hpp>
 
 #include "algorithms/md/hymd/preprocessing/similarity.h"
 #include "algorithms/md/hymd/table_identifiers.h"
 
 namespace algos::hymd::indexes {
-using RecSet = std::unordered_set<RecordIdentifier>;
-using MatchingRecsMapping = std::map<preprocessing::Similarity, RecSet>;
+using RecSet = boost::unordered::unordered_flat_set<RecordIdentifier>;
+using MatchingRecsMapping = boost::container::flat_map<preprocessing::Similarity, RecSet>;
 using SimilarityIndex = std::vector<MatchingRecsMapping>;
 }  // namespace algos::hymd::indexes
