1ad8dcb3ec592ca131a3c5e658f0659202e72364
xJoskiy <pavel5553-20@mail.ru>
2024-06-05 17:14:46 +0300

    Add DC verification examples

--- examples/verifying_dc.py
+++ examples/verifying_dc.py
@@ -1,4 +1,7 @@
 import desbordante as db
 
-verificator = db.DCVerification()
+verificator = db.dc_verification.algorithms.Default()
 
+verificator.load_data(table=("TestFD.csv", ',', True))
+
+verificator.execute(denial_constraint="!(j.Col0 == s.Col0 and t.C <= t.B)")
