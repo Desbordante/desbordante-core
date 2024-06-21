0c8af8be41a8f20afa489dffdca5448872a7260e
Senichenkov <Petua41@yandex.ru>
2024-06-21 10:03:21 +0300

    Add ND verification utils
    Implement utility classes for ND verification algorithm

---
+++ src/core/algorithms/nd/nd_verifier/util/value_combination.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <cstddef>
+#include <memory>
+#include <ostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "model/types/builtin.h"
+
+namespace algos::nd_verifier::util {
+
+class ValueCombination {
+private:
+    using TypedValue = std::pair<model::TypeId, std::byte const*>;
+
+    std::vector<TypedValue> typed_data_;
+
+    bool CompareValues(TypedValue a, TypedValue b);
+
+public:
+    ValueCombination(std::vector<TypedValue> typed_data) : typed_data_(typed_data) {}
+
+    std::string ToString() const;
+
+    bool operator==(ValueCombination const& other) {
+        return !(*this != other);
+    }
+
+    bool operator!=(ValueCombination const& other) {
+        if (typed_data_.size() != other.typed_data_.size()) {
+            return true;
+        }
+
+        for (size_t i{0}; i < typed_data_.size(); ++i) {
+            auto first_pair = typed_data_[i];
+            auto second_pair = other.typed_data_[i];
+
+            if (!CompareValues(first_pair, second_pair)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+std::ostream& operator<<(std::ostream& os, ValueCombination const& vc);
+
+}  // namespace algos::nd_verifier::util
---
+++ src/core/algorithms/nd/nd_verifier/util/vector_to_string.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace algos::nd_verifier::util {
+
+template <typename T>
+concept Printable = requires(std::stringstream& sstream, T& t) {
+    sstream << t;
+};
+
+template <Printable T>
+std::string VectorToString(std::vector<T> const& vect) {
+    std::stringstream sstream;
+    sstream << '[';
+    for (auto pt{vect.begin()}; pt != vect.end(); ++pt) {
+        if (pt != vect.begin()) {
+            sstream << ", ";
+        }
+        sstream << *pt;
+    }
+    sstream << ']';
+    return sstream.str();
+}
+
+}  // namespace algos::nd_verifier::util
