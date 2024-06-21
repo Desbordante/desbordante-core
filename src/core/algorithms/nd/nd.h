c4cc6e963462fcc004a8721b9f5466629c2e4f43
Senichenkov <Petua41@yandex.ru>
2024-06-21 10:03:21 +0300

    Add ND verification utils
    Implement utility classes for ND verification algorithm

---
+++ src/core/algorithms/nd/nd.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "model/table/column.h"
+#include "model/table/vertical.h"
+
+namespace model {
+
+using WeightType = unsigned int;
+
+class ND {
+private:
+    Vertical lhs_;
+    Vertical rhs_;
+    WeightType weight_;
+
+public:
+    ND(Vertical const& lhs, Vertical const& rhs, WeightType weight)
+        : lhs_(lhs), rhs_(rhs), weight_(weight) {}
+
+    [[nodiscard]] Vertical const& GetLhs() const {
+        return lhs_;
+    }
+
+    [[nodiscard]] Vertical const& GetRhs() const {
+        return rhs_;
+    }
+
+    [[nodiscard]] WeightType GetWeight() const {
+        return weight_;
+    }
+
+    [[nodiscard]] std::vector<ColumnIndex> GetLhsIndices() const {
+        return lhs_.GetColumnIndicesAsVector();
+    }
+
+    [[nodiscard]] std::vector<ColumnIndex> GetRhsIndices() const {
+        return rhs_.GetColumnIndicesAsVector();
+    }
+
+    [[nodiscard]] std::string ToShortString() const;
+
+    [[nodiscard]] std::string ToLongString() const;
+};
+
+}  // namespace model
---
+++ src/core/algorithms/nd/nd_verifier/util/value_combination.cpp
@@ -0,0 +1,54 @@
+#include "algorithms/nd/nd_verifier/util/value_combination.h"
+
+#include <ostream>
+#include <sstream>
+#include <string>
+
+#include "model/types/create_type.h"
+#include "model/types/mixed_type.h"
+
+namespace algos::nd_verifier::util {
+
+using namespace model;
+
+bool ValueCombination::CompareValues(ValueCombination::TypedValue a,
+                                     ValueCombination::TypedValue b) {
+    if (a.first != b.first) {
+        return false;
+    }
+    if (a.second == nullptr && b.second == nullptr) {
+        return true;
+    } else if (a.second == nullptr || b.second == nullptr) {
+        return false;
+    }
+    auto type = CreateType(a.first, false);
+    if (a.first == +(TypeId::kMixed)) {
+        if (MixedType::RetrieveTypeId(a.second) != MixedType::RetrieveTypeId(b.second)) {
+            return false;
+        }
+    }
+    return type->Compare(a.second, b.second) == CompareResult::kEqual;
+}
+
+std::string ValueCombination::ToString() const {
+    std::stringstream sstream;
+    sstream << '(';
+    for (auto pt{typed_data_.begin()}; pt != typed_data_.end(); ++pt) {
+        if (pt != typed_data_.begin()) {
+            sstream << ", ";
+        }
+        if (pt->second != nullptr) {
+            auto type = CreateType(pt->first, false);
+            sstream << type->ValueToString(pt->second);
+        }
+    }
+    sstream << ')';
+    return sstream.str();
+}
+
+std::ostream& operator<<(std::ostream& os, ValueCombination const& vc) {
+    os << vc.ToString();
+    return os;
+}
+
+}  // namespace algos::nd_verifier::util
