9f963ee4d8c1ec5f457c5588edf834b5b0c6b65a
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2024-07-15 19:13:39 +0700

    Remove support for nulls and empty in DataInfo
    Similarity measures should handle them by themselves.

--- src/core/algorithms/md/hymd/preprocessing/data_info.cpp
+++ src/core/algorithms/md/hymd/preprocessing/data_info.cpp
@@ -5,56 +5,36 @@
 namespace algos::hymd::preprocessing {
 
 auto DataInfo::MakeDataPtr(std::unique_ptr<std::byte[]> data, model::Type::Destructor destructor,
-                           std::size_t const elements, std::size_t const type_size,
-                           std::unordered_set<ValueIdentifier> const& nulls,
-                           std::unordered_set<ValueIdentifier> const& empty)
+                           std::size_t const elements, std::size_t const type_size)
         -> std::unique_ptr<std::byte[], DataDeleter> {
-    DataDeleter deleter = [destructor = std::move(destructor), elements, type_size, &nulls,
-                           &empty](std::byte* data) {
+    DataDeleter deleter = [destructor = std::move(destructor), elements,
+                           type_size](std::byte* data) {
         if (destructor) {
             for (ValueIdentifier value_id = 0; value_id < elements; ++value_id) {
-                if (nulls.contains(value_id) || empty.contains(value_id)) continue;
                 destructor(data + value_id * type_size);
             }
         }
         delete[] data;
     };
-    std::unique_ptr<std::byte[], DataDeleter> ptr{nullptr, std::move(deleter)};
-    ptr.reset(data.release());
-    return ptr;
+    return {data.release(), std::move(deleter)};
 }
 
-DataInfo::DataInfo(std::size_t elements, std::size_t type_size,
-                   std::unordered_set<ValueIdentifier> nulls,
-                   std::unordered_set<ValueIdentifier> empty, model::Type::Destructor destructor,
+DataInfo::DataInfo(std::size_t elements, std::size_t type_size, model::Type::Destructor destructor,
                    std::unique_ptr<std::byte[]> data)
     : elements_(elements),
       type_size_(type_size),
-      nulls_(std::move(nulls)),
-      empty_(std::move(empty)),
-      data_(MakeDataPtr(std::move(data), std::move(destructor), elements_, type_size_, nulls_,
-                        empty_)) {}
+      data_(MakeDataPtr(std::move(data), std::move(destructor), elements_, type_size_)) {}
 
 std::shared_ptr<DataInfo> DataInfo::MakeFrom(indexes::KeyedPositionListIndex const& pli,
                                              model::Type const& type) {
     std::size_t const value_number = pli.GetClusters().size();
     std::size_t const type_size = type.GetSize();
     auto data = std::unique_ptr<std::byte[]>(type.Allocate(value_number));
-    std::unordered_set<ValueIdentifier> nulls;
-    std::unordered_set<ValueIdentifier> empty;
     for (auto const& [string, value_id] : pli.GetMapping()) {
-        if (string.empty()) {
-            empty.insert(value_id);
-            continue;
-        }
-        if (string == model::Null::kValue) {
-            nulls.insert(value_id);
-            continue;
-        }
         type.ValueFromStr(&data[value_id * type_size], string);
     }
-    return std::make_shared<DataInfo>(value_number, type_size, std::move(nulls), std::move(empty),
-                                      type.GetDestructor(), std::move(data));
+    return std::make_shared<DataInfo>(value_number, type_size, type.GetDestructor(),
+                                      std::move(data));
 }
 
 }  // namespace algos::hymd::preprocessing
--- src/core/algorithms/md/hymd/preprocessing/data_info.h
+++ src/core/algorithms/md/hymd/preprocessing/data_info.h
@@ -19,19 +19,14 @@ private:
 
     std::size_t const elements_;
     std::size_t const type_size_;
-    std::unordered_set<ValueIdentifier> const nulls_;
-    std::unordered_set<ValueIdentifier> const empty_;
     std::unique_ptr<std::byte[], DataDeleter> const data_;
 
     static std::unique_ptr<std::byte[], DataDeleter> MakeDataPtr(
             std::unique_ptr<std::byte[]> data, model::Type::Destructor destructor,
-            std::size_t const elements, std::size_t const type_size,
-            std::unordered_set<ValueIdentifier> const& nulls,
-            std::unordered_set<ValueIdentifier> const& empty);
+            std::size_t const elements, std::size_t const type_size);
 
 public:
-    DataInfo(std::size_t elements, std::size_t type_size, std::unordered_set<ValueIdentifier> nulls,
-             std::unordered_set<ValueIdentifier> empty, model::Type::Destructor destructor,
+    DataInfo(std::size_t elements, std::size_t type_size, model::Type::Destructor destructor,
              std::unique_ptr<std::byte[]> data);
 
     std::byte const* GetAt(ValueIdentifier const value_id) const noexcept {
@@ -42,21 +37,12 @@ public:
         return elements_;
     }
 
-    std::unordered_set<ValueIdentifier> const& GetNulls() const noexcept {
-        return nulls_;
-    }
-
-    std::unordered_set<ValueIdentifier> const& GetEmpty() const noexcept {
-        return empty_;
-    }
-
     template <typename Comparator>
     std::vector<std::pair<std::byte*, ValueIdentifier>> MakeSortedInfo(
             Comparator const& comparator) const {
         std::vector<std::pair<std::byte*, ValueIdentifier>> sorted_info;
-        sorted_info.reserve(elements_ - nulls_.size() - empty_.size());
+        sorted_info.reserve(elements_);
         for (ValueIdentifier value_id = 0; value_id < elements_; ++value_id) {
-            if (nulls_.contains(value_id) || empty_.contains(value_id)) continue;
             sorted_info.emplace_back(&data_[value_id * type_size_], value_id);
         }
         std::sort(sorted_info.begin(), sorted_info.end(),
