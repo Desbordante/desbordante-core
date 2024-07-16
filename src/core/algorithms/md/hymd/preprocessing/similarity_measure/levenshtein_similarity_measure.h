6daedfcd093f31d9ed23dee5ee30d0a6eb7729ab
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2024-07-16 20:21:33 +0700

    Remove mentions of nulls in Levenshtein

--- src/core/algorithms/md/hymd/preprocessing/similarity_measure/levenshtein_similarity_measure.h
+++ src/core/algorithms/md/hymd/preprocessing/similarity_measure/levenshtein_similarity_measure.h
@@ -8,7 +8,6 @@
 namespace algos::hymd::preprocessing::similarity_measure {
 
 class LevenshteinSimilarityMeasure final : public SimilarityMeasure {
-    bool const is_null_equal_null_;
     model::md::DecisionBoundary const min_sim_;
     std::size_t const size_limit_;
     static constexpr auto kName = "levenshtein_similarity";
@@ -16,16 +15,13 @@ class LevenshteinSimilarityMeasure final : public SimilarityMeasure {
 public:
     class Creator final : public SimilarityMeasureCreator {
         model::md::DecisionBoundary const min_sim_;
-        bool const is_null_equal_null_;
         std::size_t const size_limit_;
 
     public:
-        Creator(model::md::DecisionBoundary min_sim = 0.7, bool is_null_equal_null = true,
-                std::size_t size_limit = 0);
+        Creator(model::md::DecisionBoundary min_sim = 0.7, std::size_t size_limit = 0);
 
         std::unique_ptr<SimilarityMeasure> MakeMeasure() const final {
-            return std::make_unique<LevenshteinSimilarityMeasure>(min_sim_, is_null_equal_null_,
-                                                                  size_limit_);
+            return std::make_unique<LevenshteinSimilarityMeasure>(min_sim_, size_limit_);
         }
     };
 
@@ -35,8 +31,7 @@ public:
             std::vector<indexes::PliCluster> const& clusters_right,
             util::WorkerThreadPool& thread_pool) const final;
 
-    LevenshteinSimilarityMeasure(model::md::DecisionBoundary min_sim, bool is_null_equal_null,
-                                 std::size_t size_limit);
+    LevenshteinSimilarityMeasure(model::md::DecisionBoundary min_sim, std::size_t size_limit);
 };
 
 }  // namespace algos::hymd::preprocessing::similarity_measure
