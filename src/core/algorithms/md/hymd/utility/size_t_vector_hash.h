e6057cb618e56bb965b5e5a7ea0086e349c7502d
Michael Sinelnikov <sinelnikov.michael@yandex.ru>
2024-07-31 20:44:04 +0300

    Change CCV ID type

--- src/core/algorithms/md/hymd/utility/size_t_vector_hash.h
+++ src/core/algorithms/md/hymd/utility/size_t_vector_hash.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdint>
 #include <functional>
 #include <vector>
 
@@ -12,6 +13,17 @@ struct hash<std::vector<std::size_t>> {
     std::size_t operator()(std::vector<std::size_t> const& p) const noexcept {
         util::PyTupleHash hasher{p.size()};
         for (std::size_t el : p) {
+            hasher.AddValue(el);
+        }
+        return hasher.GetResult();
+    }
+};
+
+template <>
+struct hash<std::vector<std::uint16_t>> {
+    std::size_t operator()(std::vector<std::uint16_t> const& p) const noexcept {
+        util::PyTupleHash hasher{p.size()};
+        for (std::uint16_t el : p) {
             hasher.AddValue(el);
         }
         return hasher.GetResult();
