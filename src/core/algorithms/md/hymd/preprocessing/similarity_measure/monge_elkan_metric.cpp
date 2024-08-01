dfe1f2f6c64887ef36d551a139cfaa4c64256538
Daniil Nikolaev <quanttlight@gmail.com>
2024-08-01 10:43:00 +0300

    Move MongeElkan implementation

--- src/core/algorithms/md/hymd/preprocessing/similarity_measure/monge_elkan_metric.cpp
+++ src/core/algorithms/md/hymd/preprocessing/similarity_measure/monge_elkan_metric.cpp
@@ -2,23 +2,6 @@
 
 #include <algorithm>
 
-template <typename SimilarityFunction>
-double MongeElkan(std::vector<std::string> const& a, std::vector<std::string> const& b,
-                  SimilarityFunction similarityFunction) {
-    if (a.empty() && b.empty()) return 1.0;
-    double sum = 0.0;
-    for (auto const& s : a) {
-        double max_sim = 0.0;
-        for (auto const& q : b) {
-            double similarity = similarityFunction(s, q);
-            max_sim = std::max(max_sim, similarity);
-        }
-        sum += max_sim;
-    }
-
-    return a.empty() ? 0 : sum / a.size();
-}
-
 double MongeElkan(std::vector<std::string> const& a, std::vector<std::string> const& b) {
     return MongeElkan(a, b, [](std::string const& s1, std::string const& s2) {
         return NormalizedSmithWatermanGotoh(s1, s2);
