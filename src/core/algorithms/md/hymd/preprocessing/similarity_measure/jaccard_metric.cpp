db82dda89bafa43d54780886d1e66cb6a4bf807c
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2024-08-04 14:44:35 +0700

    Metrics cleanup

--- src/core/algorithms/md/hymd/preprocessing/similarity_measure/jaccard_metric.cpp
+++ src/core/algorithms/md/hymd/preprocessing/similarity_measure/jaccard_metric.cpp
@@ -3,15 +3,16 @@
 #include <algorithm>
 #include <iterator>
 #include <sstream>
+#include <unordered_set>
 
 namespace algos::hymd::preprocessing::similarity_measure {
 
 double JaccardIndex(std::string const& s1, std::string const& s2) {
     std::istringstream iss1(s1), iss2(s2);
-    std::set<std::string> set1{std::istream_iterator<std::string>{iss1},
-                               std::istream_iterator<std::string>{}};
-    std::set<std::string> set2{std::istream_iterator<std::string>{iss2},
-                               std::istream_iterator<std::string>{}};
+    std::unordered_set<std::string> set1{std::istream_iterator<std::string>{iss1},
+                                         std::istream_iterator<std::string>{}};
+    std::unordered_set<std::string> set2{std::istream_iterator<std::string>{iss2},
+                                         std::istream_iterator<std::string>{}};
 
     return JaccardIndex(set1, set2);
 }
--- src/core/algorithms/md/hymd/preprocessing/similarity_measure/smith_waterman_gotoh.cpp
+++ src/core/algorithms/md/hymd/preprocessing/similarity_measure/smith_waterman_gotoh.cpp
@@ -5,17 +5,17 @@
 
 namespace {
 double SubstitutionCompare(char a, char b) {
-    return (a == b) ? 1.0 : -2.0;
+    return a == b ? 1.0 : -2.0;
 }
 
 double SmithWatermanGotoh(std::string const& s, std::string const& t, double gapValue = -0.5) {
-    std::vector<double> v0(t.length() + 1, 0.0);
-    std::vector<double> v1(t.length() + 1, 0.0);
+    std::vector<double> v0(t.size() + 1, 0.0);
+    std::vector<double> v1(t.size() + 1, 0.0);
 
     double max = 0.0;
 
-    for (size_t i = 1; i <= s.length(); ++i) {
-        for (size_t j = 1; j <= t.length(); ++j) {
+    for (size_t i = 1; i <= s.size(); ++i) {
+        for (size_t j = 1; j <= t.size(); ++j) {
             double match = v0[j - 1] + SubstitutionCompare(s[i - 1], t[j - 1]);
             double delete_from_s = v0[j] + gapValue;
             double delete_from_t = v1[j - 1] + gapValue;
@@ -31,15 +31,10 @@ double SmithWatermanGotoh(std::string const& s, std::string const& t, double gap
 
 namespace algos::hymd::preprocessing::similarity_measure {
 double NormalizedSmithWatermanGotoh(std::string const& s, std::string const& t, double gapValue) {
-    if (s.empty() && t.empty()) {
-        return 1.0;
-    }
+    if (s.empty() && t.empty()) return 1.0;
+    if (s.empty() || t.empty()) return 0.0;
 
-    if (s.empty() || t.empty()) {
-        return 0.0;
-    }
-
-    double max_distance = std::min(s.length(), t.length()) * std::max(1.0, gapValue);
+    double max_distance = std::min(s.size(), t.size()) * std::max(1.0, gapValue);
     return SmithWatermanGotoh(s, t, gapValue) / max_distance;
 }
 }  // namespace algos::hymd::preprocessing::similarity_measure
