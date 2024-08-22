8ad381954f5d8a1833c96925c82a6a6a1e4374b8
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2024-08-22 21:34:54 +0300

    Simplify util::ArgumentType

--- src/core/util/argument_type.h
+++ src/core/util/argument_type.h
@@ -5,10 +5,11 @@
 #include <tuple>
 
 namespace util {
+namespace detail {
 template <std::size_t N, typename R, typename... Args>
-auto GetParam(std::function<R(Args...)>) -> std::tuple_element_t<N, std::tuple<Args...>>;
+std::tuple_element_t<N, std::tuple<Args...>> GetParam(std::function<R(Args...)>&&) {}
+}  // namespace detail
 
 template <typename Func, std::size_t N>
-using ArgumentType =
-        decltype(GetParam<N>(std::declval<decltype(std::function{std::declval<Func>()})>()));
+using ArgumentType = decltype(detail::GetParam<N>(std::function(std::declval<Func>())));
 }  // namespace util
