619f26a6da9611ae22f7fdaea6d27b8889a88d1e
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2024-08-24 20:13:40 +0300

    Work around gcc 11.4.0 compiler bug

--- src/core/util/argument_type.h
+++ src/core/util/argument_type.h
@@ -8,8 +8,14 @@ namespace util {
 namespace detail {
 template <std::size_t N, typename R, typename... Args>
 std::tuple_element_t<N, std::tuple<Args...>> GetParam(std::function<R(Args...)>&&) {}
+
+// GCC 11.4.0 workaround
+template <typename T>
+auto GetFunction(T&& obj) {
+    return std::function{obj};
+}
 }  // namespace detail
 
 template <typename Func, std::size_t N>
-using ArgumentType = decltype(detail::GetParam<N>(std::function(std::declval<Func>())));
+using ArgumentType = decltype(detail::GetParam<N>(detail::GetFunction(std::declval<Func>())));
 }  // namespace util
