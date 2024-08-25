88be7623a73ffa55d90b6f4e9f406e30c46ef470
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2024-08-25 10:38:23 +0300

    Remove Levenshtein distance function
    Levenshtein distance is already available in util.

--- src/core/algorithms/md/hymd/preprocessing/similarity_measure/levenshtein_distance.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace algos::hymd::preprocessing::similarity_measure {
-double LevenshteinDistance(std::string const& l, std::string const& r) noexcept;
-}  // namespace algos::hymd::preprocessing::similarity_measure
