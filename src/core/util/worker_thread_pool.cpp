1fb56291b7a956112da4074729c2da33e74e6fbc
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2024-09-10 17:57:34 +0300

    Rethrow exceptions in WorkerThreadPool

--- src/core/util/worker_thread_pool.cpp
+++ src/core/util/worker_thread_pool.cpp
@@ -6,10 +6,13 @@ namespace util {
 WorkerThreadPool::WorkerThreadPool(std::size_t thread_num)
     : barrier_(thread_num, Completion{*this}) {
     assert(thread_num > 1);
+    tasks_.reserve(thread_num);
+    tasks_.emplace_back(DoWork{});  // main thread task
     worker_threads_.reserve(--thread_num);
     try {
         while (thread_num--) {
-            worker_threads_.emplace_back(&WorkerThreadPool::Work, this);
+            worker_threads_.emplace_back(&WorkerThreadPool::Work, this,
+                                         std::ref(tasks_.emplace_back(DoWork{})));
         }
     } catch (std::system_error&) {
         Terminate();
