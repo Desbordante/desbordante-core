01f7873d4b3cb7006cb589d4207bd7d3576f5e43
Nikita Nemakin <nikita.nemakin@yandex.ru>
2024-09-16 22:05:40 +0300

    Implement PFDVerifier class

--- src/core/algorithms/algorithm_types.h
+++ src/core/algorithms/algorithm_types.h
@@ -11,7 +11,7 @@ using AlgorithmTypes =
                    Apriori, metric::MetricVerifier, DataStats, fd_verifier::FDVerifier, HyUCC,
                    PyroUCC, cfd::FDFirstAlgorithm, ACAlgorithm, UCCVerifier, Faida, Spider, Mind,
                    Fastod, GfdValidation, EGfdValidation, NaiveGfdValidation, order::Order,
-                   dd::Split>;
+                   dd::Split, PFDVerifier>;
 
 // clang-format off
 /* Enumeration of all supported non-pipeline algorithms. If you implement a new
@@ -76,7 +76,10 @@ BETTER_ENUM(AlgorithmType, char,
     order,
 
 /* Differential dependencies mining algorithm */
-    split
+    split,
+
+/* PFD verifier algorithm */
+    pfd_verifier
 )
 // clang-format on
 
