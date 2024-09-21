c5ba8ac1f7d602f7afda766360a5f780a73bd2e8
Ivan Morozko <ivan.morozko01@gmail.com>
2024-09-22 01:33:41 +0400

    Implement SingleClueSetBuilder
    Inherits from CommonClueSetBuilder and builds clues based from
    one PLI shard

---
+++ src/core/algorithms/dc/single_clue_set_builder.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <vector>
+
+#include "dc/common_clue_set_builder.h"
+#include "dc/pli_shard.h"
+
+namespace model {
+
+/**
+ * Constructs a clue set for a single PLI shard.
+ */
+class SingleClueSetBuilder : public CommonClueSetBuilder {
+public:
+    SingleClueSetBuilder(PredicateBuilder const& pbuilder, PliShard const& shard);
+
+    ClueSet BuildClueSet() override;
+
+private:
+    std::vector<Pli> const& plis_;
+    size_t tid_beg_;
+    size_t tid_range_;
+    size_t evidence_count_;
+
+    void SetSingleEQ(std::vector<Clue>& clues, Pli::Cluster const& cluster, Clue const& mask);
+    void CorrectStrSingle(std::vector<Clue>& clues, Pli const& pli, Clue const& mask);
+    void SetCrossEQ(std::vector<Clue>& clues, Pli::Cluster const& pivotCluster,
+                    Pli::Cluster const& probeCluster, Clue const& mask);
+    void CorrectStrCross(std::vector<Clue>& clues, Pli const& pivotPli, Pli const& probePli,
+                         Clue const& mask);
+    void SetGT(std::vector<Clue>& clues, Pli::Cluster const& pivotCluster, Pli const& probePli,
+               size_t from, Clue const& mask);
+    void CorrectNumSingle(std::vector<Clue>& clues, Pli const& pli, Clue const& eqMask,
+                          Clue const& gtMask);
+    void CorrectNumCross(std::vector<Clue>& clues, Pli const& pivotPli, Pli const& probePli,
+                         Clue const& eqMask, Clue const& gtMask);
+};
+
+}  // namespace model
