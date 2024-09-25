ee61aa115febb691ec283b09cc936c31937be1e1
shalasheg <shalasheg@gmail.com>
2024-09-25 03:20:34 +0300

    Integrate Cords into Desbordante codebase

--- src/core/algorithms/fd/mining_algorithms.h
+++ src/core/algorithms/fd/mining_algorithms.h
@@ -9,5 +9,6 @@
 #include "algorithms/fd/fun/fun.h"
 #include "algorithms/fd/hyfd/hyfd.h"
 #include "algorithms/fd/pyro/pyro.h"
+#include "algorithms/fd/sfd/cords.h"
 #include "algorithms/fd/tane/pfdtane.h"
 #include "algorithms/fd/tane/tane.h"
