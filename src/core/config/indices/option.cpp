f037cd8c09e5a2b2a4a0666ba9c91924b72cfacd
Michael Polyntsov <Arno9148@gmail.com>
2024-09-28 22:23:45 +0300

    Add parameter to allow empty list of indices in IndicesOption

--- src/core/config/indices/option.cpp
+++ src/core/config/indices/option.cpp
@@ -15,10 +15,15 @@ void IndicesOption::NormalizeIndices(config::IndicesType& indices) {
 
 IndicesOption::IndicesOption(std::string_view name, std::string_view description,
                              typename Option<config::IndicesType>::NormalizeFunc normalize_func,
-                             typename Option<config::IndicesType>::DefaultFunc calculate_default)
+                             typename Option<config::IndicesType>::DefaultFunc calculate_default,
+                             bool allow_empty)
     : normalize_(normalize_func != nullptr),
       common_option_(name, description, std::move(calculate_default), std::move(normalize_func),
-                     nullptr) {}
+                     nullptr),
+      allow_empty_(allow_empty) {}
+
+IndicesOption::IndicesOption(std::string_view name, std::string_view description, bool allow_empty)
+    : IndicesOption(name, description, NormalizeIndices, nullptr, allow_empty) {}
 
 std::string_view IndicesOption::GetName() const {
     return common_option_.GetName();
@@ -30,10 +35,11 @@ Option<config::IndicesType> IndicesOption::operator()(
     assert(get_col_count);
     Option<config::IndicesType> option = common_option_(value_ptr);
     option.SetValueCheck([get_col_count = std::move(get_col_count),
-                          value_check_func = std::move(value_check_func),
-                          normalize = normalize_](config::IndicesType const& indices) {
-        if (indices.empty()) {
-            throw ConfigurationError("Indices cannot be empty");
+                          value_check_func = std::move(value_check_func), normalize = normalize_,
+                          option_name = common_option_.GetName(),
+                          allow_empty = allow_empty_](config::IndicesType const& indices) {
+        if (!allow_empty && indices.empty()) {
+            throw ConfigurationError(std::string{option_name} + " cannot be empty");
         }
         static_assert(std::is_unsigned_v<config::IndexType>);
 
