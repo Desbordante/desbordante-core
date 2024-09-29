cf74991a92f807498f9925900326490223e41277
Ivan Morozko <ivan.morozko01@gmail.com>
2024-09-29 20:46:54 +0400

    Implement Evidence
    This is class that maps 1to1 with Clue. The ApproximateEvidenceInversion algorithm
    (AEI) that will build approximate denial constraints is using Evidences
    as it's input

---
+++ src/core/algorithms/dc/evidence.h
@@ -0,0 +1,32 @@
+#include <vector>
+#include <easylogging++.h>
+
+#include "dc/common_clue_set_builder.h"
+
+namespace model {
+
+struct Evidence {
+    int64_t count;
+    Clue clue;
+    Clue evidence;
+
+    Evidence(Clue satisfied, int64_t count, Clue const& cardinalityMask,
+             std::vector<Clue> const& correctionMap)
+        : count(count), clue(satisfied) {
+        evidence = cardinalityMask;
+
+        Clue tmp = clue;
+        size_t pos = 0;
+        while (tmp.any()) {
+            if (tmp.test(0)) {
+                evidence ^= correctionMap[pos];
+            }
+            tmp >>= 1;
+            pos++;
+        }
+    }
+
+    Evidence(Clue bitSet, int64_t count) : count(count), evidence(bitSet) {}
+};
+
+}  // namespace model
