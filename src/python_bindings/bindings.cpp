055a9988c6e699fe3df2974e319f57f59e834ea8
AntonChern <anton_chernikov1@list.ru>
2024-10-01 14:10:52 +0300

    Add python bindings for GFD mining

--- src/python_bindings/bindings.cpp
+++ src/python_bindings/bindings.cpp
@@ -12,6 +12,7 @@
 #include "dynamic/bind_dynamic_fd_verification.h"
 #include "fd/bind_fd.h"
 #include "fd/bind_fd_verification.h"
+#include "gfd/bind_gfd_mining.h"
 #include "gfd/bind_gfd_verification.h"
 #include "ind/bind_ind.h"
 #include "mfd/bind_mfd_verification.h"
@@ -38,10 +39,26 @@ PYBIND11_MODULE(desbordante, module, pybind11::mod_gil_not_used()) {
         el::Loggers::reconfigureAllLoggers(conf);
     }
 
-    for (auto bind_func :
-         {BindMainClasses, BindDataTypes, BindFd, BindCfd, BindAr, BindUcc, BindAc, BindOd, BindNd,
-          BindFdVerification, BindMfdVerification, BindUccVerification, BindStatistics, BindInd,
-          BindGfdVerification, BindSplit, BindDynamicFdVerification, BindNdVerification, BindSFD}) {
+    for (auto bind_func : {BindMainClasses,
+                           BindDataTypes,
+                           BindFd,
+                           BindCfd,
+                           BindAr,
+                           BindUcc,
+                           BindAc,
+                           BindOd,
+                           BindNd,
+                           BindFdVerification,
+                           BindMfdVerification,
+                           BindUccVerification,
+                           BindStatistics,
+                           BindInd,
+                           BindGfdVerification,
+                           BindSplit,
+                           BindDynamicFdVerification,
+                           BindNdVerification,
+                           BindSFD,
+                           BindGfdMining}) {
         bind_func(module);
     }
 }
