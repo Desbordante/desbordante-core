457801c2b446715f06a81afcf1158bb97e8f8bdf
Ivan Morozko <ivan.morozko01@gmail.com>
2024-10-05 17:49:17 +0400

    Move cardinality mask building from Evidence set to a new structure
    This structure is renamed PackAndCorrectionMapBuilder, as it now
    builds not only packs and correction map, but also a correction map.
    So renamed that to EvidenceAuxStructuresBuilder

--- src/core/algorithms/dc/FastADC/util/evidence_set_builder.h
+++ src/core/algorithms/dc/FastADC/util/evidence_set_builder.h
@@ -14,10 +14,8 @@ class EvidenceSetBuilder {
 public:
     EvidenceSet evidence_set;
 
-    EvidenceSetBuilder(PredicateBuilder const& pbuilder, std::vector<PliShard> const& pli_shards,
-                       PredicatePacks const& packs) {
+    EvidenceSetBuilder(std::vector<PliShard> const& pli_shards, PredicatePacks const& packs) {
         clue_set_ = BuildClueSet(pli_shards, packs);
-        BuildCardinalityMask(pbuilder);
     }
 
     EvidenceSetBuilder(EvidenceSetBuilder const& other) = delete;
@@ -25,27 +23,20 @@ public:
     EvidenceSetBuilder(EvidenceSetBuilder&& other) noexcept = default;
     EvidenceSetBuilder& operator=(EvidenceSetBuilder&& other) noexcept = delete;
 
-    void BuildEvidenceSet(std::vector<PredicateBitset> const& correction_map) {
+    void BuildEvidenceSet(std::vector<PredicateBitset> const& correction_map,
+                          PredicateBitset const& cardinality_mask) {
         evidence_set.Reserve(clue_set_.size());
 
         for (auto const& [clue, count] : clue_set_) {
-            evidence_set.EmplaceBack(clue, count, cardinality_mask_, correction_map);
+            evidence_set.EmplaceBack(clue, count, cardinality_mask, correction_map);
         }
 
         LOG(DEBUG) << " [Evidence] # of evidences: " << evidence_set.Size();
         LOG(DEBUG) << " [Evidence] Accumulated evidence count: " << evidence_set.GetTotalCount();
     }
 
-    // For tests
-    PredicateBitset const& GetCardinalityMask() const {
-        return cardinality_mask_;
-    }
-
 private:
-    void BuildCardinalityMask(PredicateBuilder const& pbuilder);
-
     ClueSet clue_set_;
-    PredicateBitset cardinality_mask_;
 };
 
 }  // namespace algos::fastadc
