1530350729ff5fa15a58190cd3d8107243b48af1
Ivan Morozko <ivan.morozko01@gmail.com>
2024-10-05 20:11:08 +0400

    Optimize AccumulateClues by hashing clue with zero value
    The zero clue presented a lot in the clues vector

--- src/core/algorithms/dc/FastADC/util/common_clue_set_builder.h
+++ src/core/algorithms/dc/FastADC/util/common_clue_set_builder.h
@@ -8,8 +8,16 @@ namespace algos::fastadc {
 
 /* Maximum supported number of bits in clue is 64 */
 using Clue = std::bitset<64>;
-using ClueSet = std::unordered_map<Clue, int64_t>;
 
+struct ClueHash {
+    std::size_t operator()(Clue const& clue) const noexcept {
+        return clue.to_ullong();
+    }
+};
+
+using ClueSet = std::unordered_map<Clue, int64_t, ClueHash>;
+
+#if 0
 template <typename... Vectors>
 inline ClueSet AccumulateClues(Vectors const&... vectors) {
     ClueSet clue_set;
@@ -21,4 +29,29 @@ inline ClueSet AccumulateClues(Vectors const&... vectors) {
     (insert_clues(vectors), ...);
     return clue_set;
 }
+#else
+template <typename... Vectors>
+ClueSet AccumulateClues(Vectors const&... vectors) {
+    ClueSet clue_set;
+    int64_t clue_zero_count = 0;
+
+    auto insert_clues = [&](std::vector<Clue> const& clues) {
+        for (auto const& clue : clues) {
+            if (clue.none()) {
+                ++clue_zero_count;
+            } else {
+                clue_set[clue]++;
+            }
+        }
+    };
+
+    (insert_clues(vectors), ...);
+
+    if (clue_zero_count > 0) {
+        clue_set[Clue(0)] = clue_zero_count;
+    }
+
+    return clue_set;
+}
+#endif
 }  // namespace algos::fastadc
