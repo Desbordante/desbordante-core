86c06ec8c8cf66ed86e2b3189c0bd24d0158b7e7
Chizhov Anton <zr9ihi@gmail.com>
2024-10-27 16:08:43 +0300

    Add raw indices options without normalization

--- src/core/config/indices/option.cpp
+++ src/core/config/indices/option.cpp
@@ -8,14 +8,17 @@
 
 namespace config {
 
-static void NormalizeIndices(config::IndicesType& indices) {
+void IndicesOption::NormalizeIndices(config::IndicesType& indices) {
     std::sort(indices.begin(), indices.end());
     indices.erase(std::unique(indices.begin(), indices.end()), indices.end());
 }
 
 IndicesOption::IndicesOption(std::string_view name, std::string_view description,
+                             typename Option<config::IndicesType>::NormalizeFunc normalize_func,
                              typename Option<config::IndicesType>::DefaultFunc calculate_default)
-    : common_option_(name, description, std::move(calculate_default), NormalizeIndices, nullptr) {}
+    : normalize_(normalize_func != nullptr),
+      common_option_(name, description, std::move(calculate_default), std::move(normalize_func),
+                     nullptr) {}
 
 std::string_view IndicesOption::GetName() const {
     return common_option_.GetName();
@@ -26,23 +29,33 @@ Option<config::IndicesType> IndicesOption::operator()(
         typename Option<config::IndicesType>::ValueCheckFunc value_check_func) const {
     assert(get_col_count);
     Option<config::IndicesType> option = common_option_(value_ptr);
-    option.SetValueCheck(
-            [get_col_count = std::move(get_col_count),
-             value_check_func = std::move(value_check_func)](config::IndicesType const& indices) {
-                if (indices.empty()) {
-                    throw ConfigurationError("Indices cannot be empty");
-                }
-                static_assert(std::is_unsigned_v<config::IndexType>);
-                assert(std::is_sorted(indices.begin(), indices.end()));
-                config::ValidateIndex(indices.back(), get_col_count());
-                if (value_check_func) value_check_func(indices);
-            });
+    option.SetValueCheck([get_col_count = std::move(get_col_count),
+                          value_check_func = std::move(value_check_func),
+                          normalize = normalize_](config::IndicesType const& indices) {
+        if (indices.empty()) {
+            throw ConfigurationError("Indices cannot be empty");
+        }
+        static_assert(std::is_unsigned_v<config::IndexType>);
+
+        auto const get_max_id = [&]() {
+            assert(!normalize || std::is_sorted(indices.begin(), indices.end()));
+            return normalize ? indices.back() : *std::max_element(indices.begin(), indices.end());
+        };
+
+        config::ValidateIndex(get_max_id(), get_col_count());
+
+        if (value_check_func) value_check_func(indices);
+    });
     return option;
 }
 
 using config::names::kLhsIndices, config::descriptions::kDLhsIndices;
 using config::names::kRhsIndices, config::descriptions::kDRhsIndices;
+
 extern IndicesOption const kLhsIndicesOpt{kLhsIndices, kDLhsIndices};
 extern IndicesOption const kRhsIndicesOpt{kRhsIndices, kDRhsIndices};
+
+extern IndicesOption const kLhsRawIndicesOpt{kLhsIndices, kDLhsIndices, nullptr};
+extern IndicesOption const kRhsRawIndicesOpt{kRhsIndices, kDRhsIndices, nullptr};
 
 }  // namespace config
