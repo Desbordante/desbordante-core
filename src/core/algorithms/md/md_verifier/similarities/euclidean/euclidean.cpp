03d664cec9abe7521233127ac55f10e0ceb08f97
Ilya Syresenkov <ilya.syresenkov.04@gmail.com>
2024-11-22 18:09:25 +0300

    refactor: change similarity measures and naming

--- src/core/algorithms/md/md_verifier/similarities/euclidean/euclidean.cpp
+++ src/core/algorithms/md/md_verifier/similarities/euclidean/euclidean.cpp
@@ -1,12 +1,11 @@
-#include "core/algorithms/md/md_verifier/similarities/euclidean/euclidean.h"
+#include "algorithms/md/md_verifier/similarities/euclidean/euclidean.h"
 
 #include "util/convex_hull.h"
 
 namespace algos::md {
-class EuclideanSimilarity : public AbstractSimilarityMeasure<long double> {
-public:
-    long double operator()(long double left, long double right) override {
-        return 1 / (1 + util::EuclideanDistance({left}, {right}));
-    }
-};
+
+long double EuclideanSimilarity::operator()(long double left, long double right) {
+    return 1.0 / (1.0 + util::EuclideanDistance({left}, {right}));
+}
+
 }  // namespace algos::md
