2308f243ff5692ccf1d7e6e8e66f0aa23004d1d5
Ivan Morozko <ivan.morozko01@gmail.com>
2024-12-01 22:53:03 +0400

    No need to define hash_value, declaration is enough to use boost::hash_combine

--- src/core/algorithms/dc/FastADC/model/column_operand.cpp
+++
@@ -1,9 +0,0 @@
-#include "column_operand.h"
-
-namespace algos::fastadc {
-
-size_t hash_value(ColumnOperand const& col_op) noexcept {
-    return std::hash<ColumnOperand>()(col_op);
-}
-
-}  // namespace algos::fastadc
