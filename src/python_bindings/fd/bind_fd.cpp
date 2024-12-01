c64ad11a08774b5e709a8aa94ca90414c61e884c
Maksim <billchiper00@mail.ru>
2024-12-01 21:26:28 +0300

    Copy `FD` objects when returning results in Python

--- src/python_bindings/fd/bind_fd.cpp
+++ src/python_bindings/fd/bind_fd.cpp
@@ -59,7 +59,8 @@ void BindFd(py::module_& main_module) {
                           PFDTane>(fd_module, py::overload_cast<>(&FDAlgorithm::FdList, py::const_),
                                    "FdAlgorithm", "get_fds",
                                    {"HyFD", "Aid", "Depminer", "DFD", "FastFDs", "FDep", "FdMine",
-                                    "FUN", kPyroName, kTaneName, kPFDTaneName});
+                                    "FUN", kPyroName, kTaneName, kPFDTaneName},
+                                   pybind11::return_value_policy::copy);
 
     auto define_submodule = [&fd_algos_module, &main_module](char const* name,
                                                              std::vector<char const*> algorithms) {
