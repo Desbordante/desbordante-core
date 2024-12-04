a41caa7f94b78444814fe12c989faeceb203e35f
yrii <st117326@student.spbu.com>
2024-12-04 17:41:58 +0300

    Created ierarchy of class DDVerifier
    # Conflicts:
    #	src/core/algorithms/dd/split/split.cpp
    #	src/core/algorithms/dd/split/split.h

--- src/core/algorithms/dd/dd_verifier/dd_verifier.cpp
+++ src/core/algorithms/dd/dd_verifier/dd_verifier.cpp
@@ -7,181 +7,45 @@
 #include "option_using.h"
 #include "tabular_data/input_table/option.h"
 
-#include <easylogging++.h>
-
-#include "table/vertical.h"
-
 namespace algos::dd {
-    DDVerifier::DDVerifier() : Algorithm({}) {
+    DDVerifier::DDVerifier(DD dd) : Algorithm({}), dd_(std::move(dd)) {
         RegisterOptions();
         MakeOptionsAvailable({config::kTableOpt.GetName()});
     }
 
     void DDVerifier::RegisterOptions() {
         DESBORDANTE_OPTION_USING;
-        const auto default_dd = DDs();
+        config::InputTable default_table;
         RegisterOption(config::kTableOpt(&input_table_));
-        RegisterOption(Option{&dd_, kDDString, kDDDString, default_dd});
+        RegisterOption(Option{&num_rows_, kNumRows, kDNumRows, 0U});
+        RegisterOption(Option{&num_columns_, kNumColumns, kDNUmColumns, 0U});
     }
-
-    double DDVerifier::GetError() const {
-        return error_;
-    }
-
-    void DDVerifier::VisualizeHighlights() {
-        for (const auto &[col_index, pair]: highlights_) {
-            auto &col_data = typed_relation_->GetColumnData(col_index);
-            const auto col_schema = typed_relation_->GetSchema();
-            LOG(INFO) << "DD not Holds in " << col_schema->GetColumn(col_index)->GetName() << " in " << pair
-                    .first << " and " << pair.second << " rows with values " << col_data.
-                    GetDataAsString(pair.first) << ", " << col_data.GetDataAsString(pair.second) <<
-                    '\n';
-        }
-    }
-
-
-    std::size_t DDVerifier::GetNumErrorRhs() const {
-        return num_error_rhs_;
-    }
-
-    double DDVerifier::CalculateDistance(const model::ColumnIndex column_index,
-                                         const std::pair<std::size_t, std::size_t> &tuple_pair) const {
-        model::TypedColumnData const &column = typed_relation_->GetColumnData(column_index);
-        const model::TypeId type_id = column.GetTypeId();
-
-        if (type_id == +model::TypeId::kUndefined) {
-            throw std::invalid_argument("Column with index \"" + std::to_string(column_index) +
-                                        "\" type undefined.");
-        }
-        if (type_id == +model::TypeId::kMixed) {
-            throw std::invalid_argument("Column with index \"" + std::to_string(column_index) +
-                                        "\" contains values of different types.");
-        }
-        if (column.IsNull(tuple_pair.first) || column.IsNull(tuple_pair.second)) {
-            throw std::runtime_error("Some of the value coordinates are nulls.");
-        }
-        if (column.IsEmpty(tuple_pair.first) || column.IsEmpty(tuple_pair.second)) {
-            throw std::runtime_error("Some of the value coordinates are empty.");
-        }
-        double dif = 0;
-        if (column.GetType().IsMetrizable()) {
-            std::byte const *first_value = column.GetValue(tuple_pair.first);
-            std::byte const *second_value = column.GetValue(tuple_pair.second);
-            auto const &type = static_cast<model::IMetrizableType const &>(column.GetType());
-            dif = type.Dist(first_value, second_value);
-        }
-        return dif;
-    }
-
-    std::vector<std::pair<int, int> > DDVerifier::GetRowsWhereLhsHolds(
-        const std::list<model::DFStringConstraint> &constraints) const {
-        std::vector<std::pair<int, int> > result;
-        std::vector<model::ColumnIndex> columns;
-        for (auto const &constraint: constraints) {
-            model::ColumnIndex column_index = relation_->GetSchema()->GetColumn(constraint.column_name)->GetIndex();
-            columns.push_back(column_index);
-        }
-        auto curr_constraint = constraints.cbegin();
-        for (const auto column_index: columns) {
-            if (result.empty()) {
-                for (size_t i = 0; i < num_rows_; i++) {
-                    for (size_t j = i; j < num_rows_; j++) {
-                        if (const auto dif = CalculateDistance(column_index, {i, j});
-                            dif <= curr_constraint->upper_bound && dif >= curr_constraint->lower_bound) {
-                            result.emplace_back(i, j);
-                        }
-                    }
-                }
-            } else {
-                for (std::size_t i = 0; i < result.size(); i++) {
-                    if (const double dif = CalculateDistance(column_index, result[i]);
-                        dif > curr_constraint->upper_bound || dif < curr_constraint->lower_bound) {
-                        result.erase(result.cbegin() + static_cast<int>(i));
-                        --i;
-                    }
-                }
-            }
-            ++curr_constraint;
-        }
-        return result;
-    }
-
-
     void DDVerifier::LoadDataInternal() {
-        relation_ = ColumnLayoutRelationData::CreateFrom(*input_table_, false);
+        relation_ = ColumnLayoutRelationData::CreateFrom(*input_table_, false);  // nulls are
+        // ignored
         input_table_->Reset();
-        typed_relation_ = model::ColumnLayoutTypedRelationData::CreateFrom(*input_table_, false);
+        typed_relation_ = model::ColumnLayoutTypedRelationData::CreateFrom(*input_table_,
+                                                                           false);  // nulls are ignored
     }
-
     void DDVerifier::MakeExecuteOptsAvailable() {
         using namespace config::names;
-        MakeOptionsAvailable({kDDString});
+        MakeOptionsAvailable({kNumRows, kNumColumns});
     }
 
-    unsigned long long DDVerifier::ExecuteInternal() {
-        LOG(INFO) << dd_.ToString();
-        num_rows_ = typed_relation_->GetNumRows();
-        num_columns_ = typed_relation_->GetNumColumns();
+    /*VerifyDD(){
+     *get tuple of pairs, which holds lhs
+     *Check this pairs on holding rhs{
+     *if not hold{
+     *break;
+     *NOT HOLDS
+     *}
+     *}
+     *return OK;
+     *
+     *}
+     *
+     *
+     *
+     */
 
-        auto start_time = std::chrono::system_clock::now();
-
-        VerifyDD();
-
-        PrintStatistics();
-
-        auto elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(
-            std::chrono::system_clock::now() - start_time);
-
-        return elapsed_milliseconds.count();
-    }
-
-    void DDVerifier::CheckDFOnRhs(const std::vector<std::pair<int, int> > &lhs) {
-        std::vector<model::ColumnIndex> columns;
-        for (const auto &dd: dd_.right) {
-            model::ColumnIndex column_index = relation_->GetSchema()->GetColumn(dd.column_name)->GetIndex();
-            columns.push_back(column_index);
-        }
-        for (std::pair pair: lhs) {
-            auto curr_constraint = dd_.right.cbegin();
-            bool is_error = false;
-            for (const auto column_index: columns) {
-                if (const double dif = CalculateDistance(column_index, pair); !(
-                    dif >= curr_constraint->lower_bound && dif <= curr_constraint->upper_bound)) {
-                    std::pair<std::size_t, std::pair<int, int> > incorrect_pair = {column_index, pair};
-                    highlights_.push_back(incorrect_pair);
-                    is_error = true;
-                }
-                ++curr_constraint;
-            }
-            if (is_error) {
-                ++num_error_rhs_;
-            }
-        }
-    }
-
-
-    void DDVerifier::VerifyDD() {
-        const std::vector<std::pair<int, int> > lhs = GetRowsWhereLhsHolds(dd_.left);
-        CheckDFOnRhs(lhs);
-        error_ = static_cast<double>(num_error_rhs_) / static_cast<double>(lhs.size());
-    }
-
-    std::vector<std::pair<std::size_t, std::pair<int, int>>> DDVerifier::GetHighlights() const {
-        return highlights_;
-    }
-
-    bool DDVerifier::DDHolds() const {
-        return !num_error_rhs_;
-    }
-
-    void DDVerifier::PrintStatistics() {
-        if (DDHolds()) {
-            LOG(INFO) << "DD holds.";
-        } else {
-            LOG(INFO) << "DD does not hold.";
-            LOG(INFO) << "Number of rhs rows with errors: " << GetNumErrorRhs();
-            LOG(INFO) << "DD error threshold: " << GetError();
-            VisualizeHighlights();
-        }
-    }
 }
--- src/core/algorithms/dd/dd_verifier/dd_verifier.h
+++ src/core/algorithms/dd/dd_verifier/dd_verifier.h
@@ -6,56 +6,29 @@
 #include "tabular_data/input_table_type.h"
 
 namespace algos::dd {
-    using DFs = model::DFStringConstraint;
-    using DDs = model::DDString;
+    using DF = model::DFStringConstraint;
+    using DD = model::DDString;
 
-    struct DDVerifier : Algorithm {
+    class DDVerifier : public Algorithm {
     private:
-        DDs dd_;
+        DD dd_;
         config::InputTable input_table_;
-        std::size_t num_rows_{};
-        std::size_t num_columns_{};
-        std::size_t num_error_rhs_{};
-        double error_ = 0.;
         std::shared_ptr<ColumnLayoutRelationData> relation_;
         std::shared_ptr<model::ColumnLayoutTypedRelationData> typed_relation_;
-        std::vector<std::pair<std::size_t, std::pair<int, int> > > highlights_;
-
+        unsigned num_rows_;
+        model::ColumnIndex num_columns_;
         void RegisterOptions();
-
-        void VisualizeHighlights();
-
-        void PrintStatistics();
-
-        std::vector<std::pair<int, int> > GetRowsWhereLhsHolds(
-            const std::list<model::DFStringConstraint> &constraints) const;
-
-        double CalculateDistance(model::ColumnIndex column_index,
-                                 const std::pair<std::size_t, std::size_t> &tuple_pair) const;
-
-        void CheckDFOnRhs(const std::vector<std::pair<int, int> > &lhs);
-
-        void ResetState() {
-            num_rows_ = 0;
-            num_columns_ = 0;
-        }
-
     protected:
         void LoadDataInternal() override;
-
         void MakeExecuteOptsAvailable() override;
-
         unsigned long long ExecuteInternal() override;
-
     public:
-        DDVerifier();
-        double GetError() const;
-        std::size_t GetNumErrorRhs() const;
-        bool DDHolds() const;
+        DDVerifier(DD dd);
         void VerifyDD();
-        DDs GetDD() const {
-            return dd_;
-        };
-        std::vector<std::pair<std::size_t, std::pair<int, int> > > GetHighlights() const;
+
+
+
     };
+
+
 }
