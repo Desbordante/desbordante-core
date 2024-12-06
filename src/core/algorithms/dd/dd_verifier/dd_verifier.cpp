cd8e37260e40a249b365655d40af98cbe7d9e456
yrii <st117326@student.spbu.com>
2024-12-06 22:55:52 +0300

    Copied func Calculate Distance

--- src/core/algorithms/dd/dd_verifier/dd_verifier.cpp
+++ src/core/algorithms/dd/dd_verifier/dd_verifier.cpp
@@ -20,6 +20,43 @@ namespace algos::dd {
         RegisterOption(Option{&num_rows_, kNumRows, kDNumRows, 0U});
         RegisterOption(Option{&num_columns_, kNumColumns, kDNUmColumns, 0U});
     }
+    //question -- Can I copy this func from Split algorithm
+    double DDVerifier::CalculateDistance(const model::ColumnIndex column_index,
+                                         const std::pair<std::size_t, std::size_t> &tuple_pair) const {
+        model::TypedColumnData const& column = typed_relation_->GetColumnData(column_index);
+        const model::TypeId type_id = column.GetTypeId();
+
+        if (type_id == +model::TypeId::kUndefined) {
+            throw std::invalid_argument("Column with index \"" + std::to_string(column_index) +
+                                        "\" type undefined.");
+        }
+        if (type_id == +model::TypeId::kMixed) {
+            throw std::invalid_argument("Column with index \"" + std::to_string(column_index) +
+                                        "\" contains values of different types.");
+        }
+        if (column.IsNull(tuple_pair.first) || column.IsNull(tuple_pair.second)) {
+            throw std::runtime_error("Some of the value coordinates are nulls.");
+        }
+        if (column.IsEmpty(tuple_pair.first) || column.IsEmpty(tuple_pair.second)) {
+            throw std::runtime_error("Some of the value coordinates are empty.");
+        }
+        double dif = 0;
+        if (column.GetType().IsMetrizable()) {
+            std::byte const* first_value = column.GetValue(tuple_pair.first);
+            std::byte const* second_value = column.GetValue(tuple_pair.second);
+            auto const& type = static_cast<model::IMetrizableType const&>(column.GetType());
+            dif = type.Dist(first_value, second_value);
+        }
+        return dif;
+    }
+
+    std::vector<model::PLI::Cluster> DDVerifier::GetRowsHolds(DF const constraint) {
+        model::ColumnIndex column_index = relation_->GetSchema()->GetColumn(constraint.column_name)->GetIndex();
+        std::shared_ptr<model::PLI const> pli =
+                relation_->GetColumnData(column_index).GetPliOwnership();
+        std::deque<model::PLI::Cluster> const& index = pli->GetIndex();
+    }
+
     void DDVerifier::LoadDataInternal() {
         relation_ = ColumnLayoutRelationData::CreateFrom(*input_table_, false);  // nulls are
         // ignored
@@ -31,6 +68,8 @@ namespace algos::dd {
         using namespace config::names;
         MakeOptionsAvailable({kNumRows, kNumColumns});
     }
+
+
 
     /*VerifyDD(){
      *get tuple of pairs, which holds lhs
