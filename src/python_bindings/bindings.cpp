6d3d3e7505389c3c516c5bc0dbae25ee29290b4e
Ilya Syresenkov <ilya.syresenkov.04@gmail.com>
2025-01-05 03:14:42 +0300

    feat(MD): add python bindings

--- src/python_bindings/bindings.cpp
+++ src/python_bindings/bindings.cpp
@@ -19,6 +19,7 @@
 #include "ind/bind_ind.h"
 #include "ind/bind_ind_verification.h"
 #include "md/bind_md.h"
+#include "md/bind_md_verifier.h"
 #include "mfd/bind_mfd_verification.h"
 #include "nar/bind_nar.h"
 #include "nd/bind_nd.h"
@@ -69,7 +70,8 @@ PYBIND11_MODULE(desbordante, module, pybind11::mod_gil_not_used()) {
                            BindMd,
                            BindDCVerification,
                            BindPfdVerification,
-                           BindFastADC}) {
+                           BindFastADC BindMd,
+                           BindMDVerifier}) {
         bind_func(module);
     }
 }
