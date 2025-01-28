ol-imorozko/Desbordante

Implement tests for predicate space building
This test builds predicate space from the provided data (CSV file)
and compares the list of predicates that will be used later for DC
discovery with the expected one.

The expected list of predicates was built manually from running
the FastADC Java implementation.

The next test check that inverse and mutex maps are being built
correctly.

src/tests/all_csv_configs.cpp
@@ -92,4 +92,5 @@ CSVConfig const kProbeTest2 = CreateCsvConfig("ProbeTest2.csv", ',', true);
 CSVConfig const kTestParse = CreateCsvConfig("TestParse.csv", ',', false);
 CSVConfig const kODnorm6 = CreateCsvConfig("OD_norm6.csv", ',', true);
 CSVConfig const kTmpDC = CreateCsvConfig("tmp_dc.csv", ',', true);
+CSVConfig const kTestDC = CreateCsvConfig("dc_test_dataset.csv", ',', true);
 }  // namespace tests

Desbordante/desbordante-core

src/tests/all_csv_configs.cpp
@@ -130,4 +130,5 @@ CSVConfig const kLineItem = CreateCsvConfig("LineItem.csv", '|', true);
 CSVConfig const kAnimalsBeverages = CreateCsvConfig("animals_beverages.csv", ',', true);
 CSVConfig const kTestDC = CreateCsvConfig("TestDC.csv", ',', true);
 CSVConfig const kTestDC1 = CreateCsvConfig("TestDC1.csv", ',', true);
+CSVConfig const kTmpDC = CreateCsvConfig("tmp_dc.csv", ',', true);
 }  // namespace tests