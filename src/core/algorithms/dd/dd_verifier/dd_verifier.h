19e843bc8b51f81387c977e3bcc9ad8eaeacf16b
bmwproexpert <st117326@student.spbu.com>
2025-02-03 19:19:21 +0300

    fixed algo of Veritying

--- src/core/algorithms/dd/dd_verifier/dd_verifier.h
+++ src/core/algorithms/dd/dd_verifier/dd_verifier.h
@@ -17,28 +17,33 @@ namespace algos::dd {
         std::size_t num_columns_;
         std::shared_ptr<ColumnLayoutRelationData> relation_;
         std::shared_ptr<model::ColumnLayoutTypedRelationData> typed_relation_;
+
         void RegisterOptions();
-        std::vector<std::pair<int, int>> GetRowsHolds(const std::list<model::DFStringConstraint> &constraints) const;
+
+        std::vector<std::pair<int, int> > GetRowsHolds(const std::list<model::DFStringConstraint> &constraints) const;
+
         double CalculateDistance(model::ColumnIndex column_index,
-                                         const std::pair<std::size_t, std::size_t> &tuple_pair) const;
-        std::vector<std::pair<int, int>> CheckDFOnRhs(const std::vector<std::pair<int, int>>& lhs) const;
+                                 const std::pair<std::size_t, std::size_t> &tuple_pair) const;
+
+        std::vector<std::pair<int, int> > CheckDFOnRhs(const std::vector<std::pair<int, int> > &lhs) const;
+
         void ResetState() final {
             dd_.left.clear();
             dd_.right.clear();
         }
+
         void SetLimits();
+
     protected:
         void LoadDataInternal() override;
+
         void MakeExecuteOptsAvailable() override;
+
         unsigned long long ExecuteInternal() override;
+
     public:
         DDVerifier();
-        bool VerifyDD() const;
 
-
-
-
+        bool VerifyDD();
     };
-
-
 }
