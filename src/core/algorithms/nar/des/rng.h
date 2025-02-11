449271226c75619a5f1168ee3e2cfca4ea7dab12
Ivan Volgushev <ivo.volgushev@yandex.ru>
2025-02-11 14:31:13 +0300

    Add RNG seed option to DES

--- src/core/algorithms/nar/des/rng.h
+++ src/core/algorithms/nar/des/rng.h
@@ -6,12 +6,15 @@ namespace algos::des {
 
 class RNG {
 private:
-    long unsigned const kSeed_ = 2;
+    long unsigned const kDefaultSeed_ = 2;
     // result_type can differ on different STL implementations and data models
-    std::mt19937 rng_{static_cast<std::mt19937::result_type>(kSeed_)};
+    std::mt19937 rng_{static_cast<std::mt19937::result_type>(kDefaultSeed_)};
     std::uniform_real_distribution<double> uni_{0.0, 1.0};
 
 public:
+    void SetSeed(long unsigned seed) {
+        rng_ = std::mt19937(static_cast<std::mt19937::result_type>(seed));
+    }
     double Next() {
         return uni_(rng_);
     }
