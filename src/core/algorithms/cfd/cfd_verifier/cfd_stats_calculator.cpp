095049aa463793bdd85d35a324f8372ab69b89d4
Fedoseev Dmitry <godrik2003@mail.ru>
2025-02-25 18:01:02 +0300

    feat(CFD): added Verifier

---
+++ src/core/algorithms/cfd/cfd_verifier/cfd_stats_calculator.cpp
@@ -0,0 +1,110 @@
+#include "cfd_stats_calculator.h"
+
+#include <iostream>
+
+#include "cfd/util/cfd_output_util.h"
+
+namespace algos::cfd_verifier {
+void CFDStatsCalculator::CreateSupportMask() {
+    support_mask_.resize(relation_->GetNumRows(), true);
+
+    for (size_t i = 0; i < relation_->GetNumRows(); i++) {
+        bool supports_cfd = true;
+
+        std::vector<int> row_values = relation_->GetRow(i);
+
+        for (auto expected_value : rule_.first) {
+            if (std::find(row_values.begin(), row_values.end(), expected_value) ==
+                        row_values.end() &&
+                expected_value > 0) {
+                supports_cfd = false;
+                break;
+            }
+        }
+
+        support_mask_[i] = supports_cfd;
+    }
+}
+
+void CFDStatsCalculator::MakeLhsToRowNums() {
+    for (size_t row_idx = 0; row_idx < relation_->GetNumRows(); ++row_idx) {
+        if (!support_mask_[row_idx]) {
+            continue;
+        }
+
+        cfd::Itemset lhs_values;
+        auto const& row_values = relation_->GetRow(row_idx);
+        for (int attr_idx : lhs_attrs_) {
+            lhs_values.push_back(row_values[attr_idx]);
+        }
+
+        lhs_to_row_nums_[lhs_values].push_back(row_idx);
+    }
+}
+
+void CFDStatsCalculator::DetermineMostFrequentRHS() {
+    for (auto const& [lhs_values, row_indices] : lhs_to_row_nums_) {
+        std::map<cfd::Item, size_t> rhs_count;
+
+        for (size_t row_index : row_indices) {
+            auto row = relation_->GetRow(row_index);
+            cfd::Item rhs_value = row[rhs_attr_index_];
+            rhs_count[rhs_value]++;
+        }
+
+        if (!rhs_count.empty()) {
+            most_frequent_rhs_[lhs_values] = std::max_element(rhs_count.begin(), rhs_count.end(),
+                                                              [](auto const& a, auto const& b) {
+                                                                  return a.second < b.second;
+                                                              })
+                                                     ->first;
+        }
+    }
+}
+
+void CFDStatsCalculator::CalculateSupportAndConfidence() {
+    size_t total_supported = 0;
+    size_t valid_tuples = 0;
+
+    for (auto const& [lhs_values, row_indices] : lhs_to_row_nums_) {
+        auto it = most_frequent_rhs_.find(lhs_values);
+        if (it == most_frequent_rhs_.end()) {
+            continue;
+        }
+
+        cfd::Item most_frequent_rhs = it->second;
+
+        for (size_t row_index : row_indices) {
+            auto row = relation_->GetRow(row_index);
+            if (row[rhs_attr_index_] == most_frequent_rhs) {
+                valid_tuples++;
+            } else {
+                violating_rows_.push_back(row_index);
+            }
+        }
+        total_supported += row_indices.size();
+    }
+
+    std::sort(violating_rows_.begin(), violating_rows_.end());
+    support_ = total_supported;
+    confidence_ = total_supported > 0 ? static_cast<double>(valid_tuples) / total_supported : 0.0;
+}
+
+void CFDStatsCalculator::ResetState() {
+    support_mask_.clear();
+    lhs_to_row_nums_.clear();
+    most_frequent_rhs_.clear();
+    support_ = 0;
+    confidence_ = 0.0;
+    violating_rows_.clear();
+}
+
+void CFDStatsCalculator::CalculateStatistics() {
+    ResetState();
+    CreateSupportMask();
+    MakeLhsToRowNums();
+    DetermineMostFrequentRHS();
+    CalculateSupportAndConfidence();
+}
+
+}  // namespace algos::cfd_verifier
---
+++ src/core/algorithms/cfd/cfd_verifier/cfd_verifier.cpp
@@ -0,0 +1,94 @@
+#include "cfd/cfd_verifier/cfd_verifier.h"
+
+#include <iostream>
+
+#include <easylogging++.h>
+
+#include "cfd/model/cfd_relation_data.h"
+#include "cfd/util/cfd_output_util.h"
+#include "cfd_verifier.h"
+#include "config/names_and_descriptions.h"
+#include "option_using.h"
+#include "tabular_data/input_table/option.h"
+#include "util/timed_invoke.h"
+
+namespace algos::cfd_verifier {
+
+CFDVerifier::CFDVerifier() : Algorithm({}) {
+    RegisterOptions();
+    MakeOptionsAvailable({config::kTableOpt.GetName()});
+}
+
+void CFDVerifier::RegisterOptions() {
+    DESBORDANTE_OPTION_USING;
+    RegisterOption(config::kTableOpt(&input_table_));
+    RegisterOption(Option{&string_rule_left_, kCFDRuleLeft, kDCFDRuleLeft,
+                          std::vector<std::pair<std::string, std::string>>{}});
+    RegisterOption(Option{&string_rule_right_, kCFDRuleRight, kDCFDRuleRight,
+                          std::pair<std::string, std::string>{}});
+}
+
+void CFDVerifier::LoadDataInternal() {
+    relation_ = cfd::CFDRelationData::CreateFrom(*input_table_);
+
+    if (relation_->GetColumnData().empty()) {
+        throw std::runtime_error("Got an empty dataset: CFD verifying is meaningless.");
+    }
+}
+
+void CFDVerifier::MakeExecuteOptsAvailable() {
+    using namespace config::names;
+    MakeOptionsAvailable({kCFDRuleLeft, kCFDRuleRight});
+}
+
+unsigned long long CFDVerifier::ExecuteInternal() {
+    auto get_attr_id = [this](std::string const& attr_name) -> int {
+        int attr_id = relation_->GetAttr(attr_name);
+        if (attr_id == -1) {
+            throw std::runtime_error("Attribute not found: " + attr_name);
+        }
+        return attr_id;
+    };
+
+    auto extract_item_ids =
+            [this, &get_attr_id](std::vector<std::pair<std::string, std::string>> const& rule_part)
+            -> std::vector<int> {
+        std::vector<int> item_ids(relation_->GetAttrsNumber());
+        for (auto const& [attr_name, item_name] : rule_part) {
+            int attr_id = get_attr_id(attr_name);
+
+            if (item_name != "_") {
+                int item_id = relation_->GetItem(attr_id, item_name);
+                if (item_id == -1) {
+                    throw std::runtime_error("Item not found in item universe: " + item_name);
+                }
+                item_ids.push_back(item_id);
+            } else {
+                item_ids.push_back(-1 - attr_id);
+            }
+        }
+        return item_ids;
+    };
+
+    std::vector<int> cfd_left_id = extract_item_ids(string_rule_left_);
+    std::vector<int> cfd_right_id = extract_item_ids({string_rule_right_});
+
+    cfd_ = std::make_pair(std::move(cfd_left_id), cfd_right_id.back());
+
+    LOG(INFO) << "Starting CFD verification...";
+    LOG(INFO) << "\tRule to verify: " << cfd::Output::CFDToString(cfd_, relation_);
+
+    auto verification_time = ::util::TimedInvoke(&CFDVerifier::VerefyCFD, this);
+    LOG(DEBUG) << "CFD verification took " << std::to_string(verification_time) << "ms";
+
+    auto stats_calculation_time = ::util::TimedInvoke(&CFDVerifier::CalculateStatistics, this);
+    LOG(DEBUG) << "Statistics calculation took " << std::to_string(stats_calculation_time) << "ms";
+
+    return verification_time + stats_calculation_time;
+}
+
+void CFDVerifier::VerefyCFD() {
+    stats_calculator_ = CFDStatsCalculator(relation_, std::move(cfd_));
+}
+
+}  // namespace algos::cfd_verifier
