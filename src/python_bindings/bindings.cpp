8786f65739c4985a160eb5de376b2bc3d3dd35db
Ilya Syresenkov <ilya.syresenkov.04@gmail.com>
2025-03-04 21:02:31 +0300

    fix(MD): fix more issues after cherry-pick

--- src/python_bindings/bindings.cpp
+++ src/python_bindings/bindings.cpp
@@ -70,6 +70,7 @@ PYBIND11_MODULE(desbordante, module, pybind11::mod_gil_not_used()) {
                            BindNdVerification,
                            BindSFD,
                            BindMd,
+                           BindMDVerifier,
                            BindDCVerification,
                            BindPfdVerification,
                            BindFastADC,
