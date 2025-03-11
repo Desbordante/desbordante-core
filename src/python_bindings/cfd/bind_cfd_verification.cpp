ed61d080f2b86c6a2c98800afdb3bd6ba9aa4026
Fedoseev Dmitry <godrik2003@mail.ru>
2025-03-11 22:32:31 +0300

    feat(CFD): add clustering and highlight

--- src/python_bindings/cfd/bind_cfd_verification.cpp
+++ src/python_bindings/cfd/bind_cfd_verification.cpp
@@ -2,6 +2,7 @@
 #include <pybind11/stl.h>
 
 #include "algorithms/cfd/cfd_verifier/cfd_verifier.h"
+#include "algorithms/cfd/cfd_verifier/highlight.h"
 #include "py_util/bind_primitive.h"
 
 namespace {
@@ -14,13 +15,17 @@ void BindCFDVerification(py::module_& main_module) {
     using namespace algos::cfd_verifier;
 
     auto cfd_verification_module = main_module.def_submodule("cfd_verification");
+    py::class_<Highlight>(cfd_verification_module, "Highlight")
+            .def_property_readonly("cluster", &Highlight::GetCluster)
+            .def_property_readonly("get_violating_rows", &Highlight::GetViolatingRows);
 
     BindPrimitiveNoBase<CFDVerifier>(cfd_verification_module, "CFDVerifier")
             .def("cfd_holds", &CFDVerifier::CFDHolds)
             .def("get_real_support", &CFDVerifier::GetRealSupport)
             .def("get_real_confidence", &CFDVerifier::GetRealConfidence)
-            .def("get_rows_satisfying_cfd", &CFDVerifier::GetRowsSatisfyingCFD)
-            .def("get_rows_violating_cfd", &CFDVerifier::GetRowsViolatingCFD);
+            .def("get_highlights", &CFDVerifier::GetHighlights)
+            .def("get_num_clusters_violating_cfd", &CFDVerifier::GetNumClustersViolatingCFD)
+            .def("get_num_rows_violating_cfd", &CFDVerifier::GetNumRowsViolatingCFD);
 
     main_module.attr("cfd_verification") = cfd_verification_module;
 }
