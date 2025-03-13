1ac1816aa1db44b0238a2f6beb1ccf2d3938080c
bmwproexpert <st117326@student.spbu.com>
2025-03-13 18:31:55 +0300

    Reformated code of bindings

--- src/python_bindings/dd/bind_dd_verification.cpp
+++ src/python_bindings/dd/bind_dd_verification.cpp
@@ -10,18 +10,18 @@
 
 namespace python_bindings {
 namespace py = pybind11;
-    void BindDDVerification(py::module_& main_module) {
-        using namespace algos;
-        auto dd_verification_module = main_module.def_submodule("dd_verification");
-        dd_verification_module.def("create_df", &util::dd::CreateDf);
-        dd_verification_module.def("create_dd", &util::dd::CreateDd);
-        py::class_<model::DFStringConstraint>(dd_verification_module, "DF");
 
+void BindDDVerification(py::module_& main_module) {
+    using namespace algos;
+    auto dd_verification_module = main_module.def_submodule("dd_verification");
+    dd_verification_module.def("create_df", &util::dd::CreateDf);
+    dd_verification_module.def("create_dd", &util::dd::CreateDd);
+    py::class_<model::DFStringConstraint>(dd_verification_module, "DF");
 
-        BindPrimitiveNoBase<dd::DDVerifier>(dd_verification_module, "DDVerifier")
-                .def("dd_holds", &dd::DDVerifier::DDHolds)
-                .def("get_error", &dd::DDVerifier::GetError)
-                .def("get_num_error_pairs", &dd::DDVerifier::GetNumErrorRhs)
-                .def("get_highlights", &dd::DDVerifier::GetHighlights);
-    }
+    BindPrimitiveNoBase<dd::DDVerifier>(dd_verification_module, "DDVerifier")
+            .def("dd_holds", &dd::DDVerifier::DDHolds)
+            .def("get_error", &dd::DDVerifier::GetError)
+            .def("get_num_error_pairs", &dd::DDVerifier::GetNumErrorRhs)
+            .def("get_highlights", &dd::DDVerifier::GetHighlights);
+}
 }  // namespace python_bindings
