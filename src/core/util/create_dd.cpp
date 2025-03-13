9e4f385330d499f84dda274ff6b5529674ba0cb3
bmwproexpert <st117326@student.spbu.com>
2025-03-13 17:53:54 +0300

    Final reformat code with ClangFormat

--- src/core/util/create_dd.cpp
+++ src/core/util/create_dd.cpp
@@ -1,20 +1,20 @@
 #include "create_dd.h"
 
 namespace util {
-    model::DFStringConstraint dd::CreateDf(const std::string &col_name, const double lower_bound, const double upper_bound) {
-        model::DFStringConstraint dd;
-        dd.column_name = col_name;
-        dd.constraint.lower_bound = lower_bound;
-        dd.constraint.upper_bound = upper_bound;
-        return dd;
-    }
-
-    model::DDString dd::CreateDd(const std::list<model::DFStringConstraint>& lhs,
-                                  const std::list<model::DFStringConstraint>& rhs) {
-        model::DDString dd;
-        dd.left = lhs;
-        dd.right = rhs;
-        return dd;
-
-    }
+model::DFStringConstraint dd::CreateDf(std::string const& col_name, double const lower_bound,
+                                       double const upper_bound) {
+    model::DFStringConstraint dd;
+    dd.column_name = col_name;
+    dd.constraint.lower_bound = lower_bound;
+    dd.constraint.upper_bound = upper_bound;
+    return dd;
 }
+
+model::DDString dd::CreateDd(std::list<model::DFStringConstraint> const& lhs,
+                             std::list<model::DFStringConstraint> const& rhs) {
+    model::DDString dd;
+    dd.left = lhs;
+    dd.right = rhs;
+    return dd;
+}
+}  // namespace util
--- src/core/util/create_dd.h
+++ src/core/util/create_dd.h
@@ -1,11 +1,10 @@
 #pragma once
 #include "algorithms/dd/dd.h"
 
+namespace util::dd {
+model::DFStringConstraint CreateDf(std::string const& col_name, double lower_bound,
+                                   double upper_bound);
 
-    namespace util::dd {
-        model::DFStringConstraint CreateDf(const std::string &col_name, double lower_bound, double upper_bound);
-
-        model::DDString CreateDd(const std::list<model::DFStringConstraint>& lhs,
-                                  const std::list<model::DFStringConstraint>& rhs);
-    }
-
+model::DDString CreateDd(std::list<model::DFStringConstraint> const& lhs,
+                         std::list<model::DFStringConstraint> const& rhs);
+}  // namespace util::dd
