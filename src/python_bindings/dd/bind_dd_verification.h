f9c341afd6f3f7835c95d99b16dac0bdfd5e68e3
bmwproexpert <st117326@student.spbu.com>
2025-03-13 18:31:55 +0300

    Reformated code of bindings

--- src/python_bindings/dd/bind_dd_verification.h
+++ src/python_bindings/dd/bind_dd_verification.h
@@ -3,5 +3,5 @@
 #include <pybind11/pybind11.h>
 
 namespace python_bindings {
-    void BindDDVerification(pybind11::module_& main_module);
+void BindDDVerification(pybind11::module_& main_module);
 }  // namespace python_bindings
