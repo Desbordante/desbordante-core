f122733b653a928bdc4798faf48a6f38fdb85052
Yuriy Litvinov <bmwproexpert@gmail.com>
2025-04-06 16:23:06 +0300

    Added tabulations in opt_to_py.cpp

--- src/python_bindings/py_util/opt_to_py.cpp
+++ src/python_bindings/py_util/opt_to_py.cpp
@@ -23,29 +23,29 @@ using ConvFunction = std::function<py::object(boost::any)>;
 
 template <typename T>
 std::pair<std::type_index, ConvFunction> normal_conv_pair{
-    std::type_index(typeid(T)),
-    [](boost::any value) { return py::cast(boost::any_cast<T>(value)); }};
+        std::type_index(typeid(T)),
+        [](boost::any value) { return py::cast(boost::any_cast<T>(value)); }};
 
 template <typename T>
 std::pair<std::type_index, ConvFunction> enum_conv_pair{
-    std::type_index(typeid(T)),
-    [](boost::any value) { return py::cast(boost::any_cast<T>(value)._to_string()); }};
+        std::type_index(typeid(T)),
+        [](boost::any value) { return py::cast(boost::any_cast<T>(value)._to_string()); }};
 std::unordered_map<std::type_index, ConvFunction> const kConverters{
-    normal_conv_pair<int>,
-    normal_conv_pair<double>,
-    normal_conv_pair<long double>,
-    normal_conv_pair<unsigned int>,
-    normal_conv_pair<bool>,
-    normal_conv_pair<config::ThreadNumType>,
-    normal_conv_pair<config::CustomRandomSeedType>,
-    normal_conv_pair<config::MaxLhsType>,
-    normal_conv_pair<config::ErrorType>,
-    normal_conv_pair<config::IndicesType>,
-    normal_conv_pair<model::DDString>,
-    enum_conv_pair<algos::metric::MetricAlgo>,
-    enum_conv_pair<algos::metric::Metric>,
-    enum_conv_pair<algos::InputFormat>,
-    enum_conv_pair<algos::hymd::LevelDefinition>};
+        normal_conv_pair<int>,
+        normal_conv_pair<double>,
+        normal_conv_pair<long double>,
+        normal_conv_pair<unsigned int>,
+        normal_conv_pair<bool>,
+        normal_conv_pair<config::ThreadNumType>,
+        normal_conv_pair<config::CustomRandomSeedType>,
+        normal_conv_pair<config::MaxLhsType>,
+        normal_conv_pair<config::ErrorType>,
+        normal_conv_pair<config::IndicesType>,
+        normal_conv_pair<model::DDString>,
+        enum_conv_pair<algos::metric::MetricAlgo>,
+        enum_conv_pair<algos::metric::Metric>,
+        enum_conv_pair<algos::InputFormat>,
+        enum_conv_pair<algos::hymd::LevelDefinition>};
 }  // namespace
 
 namespace python_bindings {
