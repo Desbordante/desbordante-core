9ee1146ea08d50ef3179ab6ff401fe3531394a0f
Yuriy Litvinov <bmwproexpert@gmail.com>
2025-04-06 16:25:33 +0300

    Fixed codestyle in highlight.h

--- src/core/algorithms/dd/dd_verifier/highlight.h
+++ src/core/algorithms/dd/dd_verifier/highlight.h
@@ -7,18 +7,18 @@
 
 class Highlight {
 private:
-    model::ColumnIndex attribute_index;
-    std::pair<std::size_t, std::size_t> pair_rows;
+    model::ColumnIndex attribute_index_;
+    std::pair<std::size_t, std::size_t> pair_rows_;
 
 public:
     Highlight(model::ColumnIndex attribute_index, std::pair<std::size_t, std::size_t> rows_pair)
-        : attribute_index(attribute_index), pair_rows(std::move(rows_pair)) {}
+        : attribute_index_(attribute_index), pair_rows_(std::move(rows_pair)) {}
 
     [[nodiscard]] model::ColumnIndex GetAttributeIndex() const {
-        return attribute_index;
+        return attribute_index_;
     }
 
     [[nodiscard]] std::pair<std::size_t, std::size_t> GetPairRows() const {
-        return pair_rows;
+        return pair_rows_;
     }
 };
