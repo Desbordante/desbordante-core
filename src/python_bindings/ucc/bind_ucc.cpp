3b318f6c9e3db94d32837f9f8d38d06886a927fa
DaniilGoncharov <daniilgoncharov08@gmail.com>
2025-04-15 10:44:40 +0300

    Add serialization support for most algorithms
    Add serialization support for AC, AR, CFD, DC, DD, FD, IND, MD, NAR, OD, SFD and UCC algorithms

--- src/python_bindings/ucc/bind_ucc.cpp
+++ src/python_bindings/ucc/bind_ucc.cpp
@@ -11,6 +11,7 @@
 #include "config/indices/type.h"
 #include "model/table/column.h"
 #include "py_util/bind_primitive.h"
+#include "py_util/table_serialization.h"
 #include "util/bitset_utils.h"
 
 namespace {
@@ -38,7 +39,25 @@ void BindUcc(py::module_& main_module) {
             .def("to_long_string", &UCC::ToString)
             .def_property_readonly("indices", &UCC::GetColumnIndicesAsVector)
             .def("__eq__", [](UCC const& a, UCC const& b) { return a == b; })
-            .def("__hash__", [](UCC const& ucc) { return py::hash(MakeUCCNameTuple(ucc)); });
+            .def("__hash__", [](UCC const& ucc) { return py::hash(MakeUCCNameTuple(ucc)); })
+            .def(py::pickle(
+                // __getstate__
+                [](UCC const& ucc) {
+                    auto schema_state = table_serialization::SerializeRelationalSchema(ucc.GetSchema());
+                    auto vertical_state = table_serialization::SerializeVertical(ucc);
+                    return py::make_tuple(schema_state, vertical_state);
+                },
+                // __setstate__
+                [](py::tuple t) {
+                    if (t.size() != 2) {
+                        throw std::runtime_error("Invalid state for UCC pickle!");
+                    }
+                    auto schema_state = t[0].cast<py::tuple>();
+                    auto schema_ptr = table_serialization::DeserializeRelationalSchema(schema_state);
+                    auto vertical_state = t[1].cast<py::tuple>();
+                    Vertical vert = table_serialization::DeserializeVertical(vertical_state, schema_ptr);
+                    return UCC(schema_ptr, std::move(vert));
+                }));
     BindPrimitive<HPIValid, HyUCC, PyroUCC>(
             ucc_module, py::overload_cast<>(&UCCAlgorithm::UCCList, py::const_), "UccAlgorithm",
             "get_uccs", {"HPIValid", "HyUCC", "PyroUCC"}, pybind11::return_value_policy::copy);
