a7f6e5e97597d94e76ba0caf51cbd69cb96d3dfb
m4xxx1m <mi4469228@yandex.ru>
2025-05-07 19:50:11 +0300

    Add DFS strategy to DynFD

---
+++ src/core/algorithms/fd/dynfd/non_fd_inductor.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vector>
+#include "model/FDTrees/fd_tree.h"
+#include "model/non_fd_tree.h"
+#include "validator.h"
+
+namespace algos::dynfd {
+class NonFDInductor {
+    std::shared_ptr<model::FDTree> positive_cover_tree_;
+    std::shared_ptr<NonFDTree> negative_cover_tree_;
+    std::shared_ptr<Validator> validator_;
+
+    void Dfs(RawFD fd);
+
+    void DeduceNonFds(RawFD fd);
+
+public:
+    NonFDInductor(std::shared_ptr<model::FDTree> positive_cover_tree,
+              std::shared_ptr<NonFDTree> negative_cover_tree,
+              std::shared_ptr<Validator> validator) noexcept
+        : positive_cover_tree_(std::move(positive_cover_tree)),
+          negative_cover_tree_(std::move(negative_cover_tree)),
+          validator_(std::move(validator)) {}
+
+    void FindFds(const std::vector<RawFD> &valid_fds);
+};
+} // namespace algos::dynfd
