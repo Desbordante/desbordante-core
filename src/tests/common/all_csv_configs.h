p-senichenkov/Desbordante

Add "cropDatasets" target and sort all_csv_configs
Add CMake target that takes only first X lines from datasets
Sort all_csv_configs.{h,cpp} lexicographically

Edit cropDatasets

src/tests/all_csv_configs.h
@@ -3,85 +3,84 @@
 #include "parser/csv_parser/csv_parser.h"
 
 namespace tests {
-extern CSVConfig const kWdcAstronomical;
-extern CSVConfig const kWdcSymbols;
-extern CSVConfig const kWdcScience;
-extern CSVConfig const kWdcSatellites;
-extern CSVConfig const kWdcAppearances;
-extern CSVConfig const kWdcAstrology;
-extern CSVConfig const kWdcGame;
-extern CSVConfig const kWdcKepler;
-extern CSVConfig const kWdcPlanetz;
-extern CSVConfig const kWdcAge;
-extern CSVConfig const kTestWide;
 extern CSVConfig const kAbalone;
-extern CSVConfig const kIris;
+extern CSVConfig const kACShippingDates;
 extern CSVConfig const kAdult;
+extern CSVConfig const kAdult60k;
+extern CSVConfig const kAnimalsBeverages;
+extern CSVConfig const kBernoulliRelation;
 extern CSVConfig const kBreastCancer;
+extern CSVConfig const kCIPublicHighway;
 extern CSVConfig const kCIPublicHighway10k;
+extern CSVConfig const kCIPublicHighway200k;
+extern CSVConfig const kCIPublicHighway10attr;
+extern CSVConfig const kCIPublicHighway5attr;
+extern CSVConfig const kCIPublicHighway700;
+extern CSVConfig const kEpicMeds;
+extern CSVConfig const kEpicVitals;
+extern CSVConfig const kFdReduced30;
+extern CSVConfig const kFlight1k;
+extern CSVConfig const kIndTest3aryInds;
+extern CSVConfig const kIndTestEmpty;
+extern CSVConfig const kIndTestNulls;
+extern CSVConfig const kIndTestPlanets;
+extern CSVConfig const kIndTestTableFirst;
+extern CSVConfig const kIndTestTableSecond;
+extern CSVConfig const kIndTestTypos;
+extern CSVConfig const kIndTestWide2;
+extern CSVConfig const kIowa1kk;
+extern CSVConfig const kIowa25attr1kk;
+extern CSVConfig const kIowa500k;
+extern CSVConfig const kIowa600k;
+extern CSVConfig const kIowa900k;
+extern CSVConfig const kIris;
+extern CSVConfig const kLegacyPayors;
+extern CSVConfig const kLetter;
+extern CSVConfig const kLineItem;
+extern CSVConfig const kMushroom;
+extern CSVConfig const kMushroom2k;
 extern CSVConfig const kNeighbors10k;
 extern CSVConfig const kNeighbors50k;
 extern CSVConfig const kNeighbors100k;
-extern CSVConfig const kCIPublicHighway700;
-extern CSVConfig const kEpicVitals;
-extern CSVConfig const kEpicMeds;
-extern CSVConfig const kIowa1kk;
-extern CSVConfig const kFdReduced30;
-extern CSVConfig const kFlight1k;
-extern CSVConfig const kPlista1k;
-extern CSVConfig const kLetter;
-extern CSVConfig const kCIPublicHighway;
-extern CSVConfig const kLegacyPayors;
-extern CSVConfig const kTestEmpty;
-extern CSVConfig const kTestSingleColumn;
-extern CSVConfig const kTestLong;
-extern CSVConfig const kTestFD;
-extern CSVConfig const kTestND;
-extern CSVConfig const kOdTestNormOd;
-extern CSVConfig const kOdTestNormSmall2x3;
-extern CSVConfig const kOdTestNormSmall3x3;
+extern CSVConfig const kNeighbors120k;
+extern CSVConfig const kNeighbors130k;
+extern CSVConfig const kNullEmpty;
+extern CSVConfig const kODnorm6;
 extern CSVConfig const kOdTestNormAbalone;
 extern CSVConfig const kOdTestNormBalanceScale;
 extern CSVConfig const kOdTestNormBreastCancerWisconsin;
 extern CSVConfig const kOdTestNormClassification;
 extern CSVConfig const kOdTestNormEchocardiogram;
+extern CSVConfig const kOdTestNormHepatitis;
 extern CSVConfig const kOdTestNormHepatitis1;
 extern CSVConfig const kOdTestNormHepatitis2;
 extern CSVConfig const kOdTestNormHepatitis3;
 extern CSVConfig const kOdTestNormHepatitis4;
 extern CSVConfig const kOdTestNormHepatitis5;
-extern CSVConfig const kOdTestNormHepatitis;
 extern CSVConfig const kOdTestNormHorse10c;
 extern CSVConfig const kOdTestNormIris;
-extern CSVConfig const kIndTestWide2;
-extern CSVConfig const kIndTestEmpty;
-extern CSVConfig const kIndTestPlanets;
-extern CSVConfig const kIndTest3aryInds;
-extern CSVConfig const kIndTestTableFirst;
-extern CSVConfig const kIndTestTableSecond;
-extern CSVConfig const kIndTestNulls;
-extern CSVConfig const kIndTestTypos;
-extern CSVConfig const kTestZeros;
-extern CSVConfig const kNullEmpty;
-extern CSVConfig const kSimpleTypes;
-extern CSVConfig const kRulesBook;
-extern CSVConfig const kRulesBookRows;
-extern CSVConfig const kRulesPresentationExtended;
-extern CSVConfig const kRulesPresentation;
-extern CSVConfig const kRulesSynthetic2;
-extern CSVConfig const kRulesKaggleRows;
-extern CSVConfig const kTennis;
-extern CSVConfig const kMushroom;
-extern CSVConfig const kTestDataStats;
-extern CSVConfig const kTestMetric;
-extern CSVConfig const kBernoulliRelation;
-extern CSVConfig const kACShippingDates;
-extern CSVConfig const kSimpleTypos;
-extern CSVConfig const kTest1;
+extern CSVConfig const kOdTestNormOd;
+extern CSVConfig const kOdTestNormSmall2x3;
+extern CSVConfig const kOdTestNormSmall3x3;
+extern CSVConfig const kPlista1k;
 extern CSVConfig const kProbeTest1;
 extern CSVConfig const kProbeTest2;
-extern CSVConfig const kTestParse;
-extern CSVConfig const kODnorm6;
+extern CSVConfig const kRulesBook;
+extern CSVConfig const kRulesBookRows;
+extern CSVConfig const kRulesKaggleRows;
+extern CSVConfig const kRulesPresentation;
+extern CSVConfig const kRulesPresentationExtended;
+extern CSVConfig const kRulesSynthetic2;
+extern CSVConfig const kSimpleTypes;
+extern CSVConfig const kSimpleTypes1;
+extern CSVConfig const kSimpleTypos;
+extern CSVConfig const kTennis;
+extern CSVConfig const kTest1;
+extern CSVConfig const kTestDataStats;
+extern CSVConfig const kTestDC;
+extern CSVConfig const kTestDC1;
+extern CSVConfig const kTestDC2;
+extern CSVConfig const kTestDC3;
 extern CSVConfig const kTestDD;
 extern CSVConfig const kTestDD1;
 extern CSVConfig const kTestDD2;
@@ -94,9 +93,8 @@ extern CSVConfig const kTestDif2;
 extern CSVConfig const kTestDif3;
 extern CSVConfig const kTestDif4;
 extern CSVConfig const kTestDif5;
-extern CSVConfig const kSimpleTypes1;
-extern CSVConfig const kTestDynamicFDInit;
 extern CSVConfig const kTestDynamicFDEmpty;
+extern CSVConfig const kTestDynamicFDInit;
 extern CSVConfig const kTestDynamicFDInsert;
 extern CSVConfig const kTestDynamicFDInsertBad1;
 extern CSVConfig const kTestDynamicFDInsertBad2;
@@ -105,10 +103,23 @@ extern CSVConfig const kTestDynamicFDUpdateBad1;
 extern CSVConfig const kTestDynamicFDUpdateBad2;
 extern CSVConfig const kTestDynamicFDUpdateBad3;
 extern CSVConfig const kTestDynamicFDUpdateBad4;
-extern CSVConfig const kLineItem;
-extern CSVConfig const kAnimalsBeverages;
-extern CSVConfig const kTestDC;
-extern CSVConfig const kTestDC1;
-extern CSVConfig const kTestDC2;
-extern CSVConfig const kTestDC3;
+extern CSVConfig const kTestEmpty;
+extern CSVConfig const kTestFD;
+extern CSVConfig const kTestLong;
+extern CSVConfig const kTestMetric;
+extern CSVConfig const kTestND;
+extern CSVConfig const kTestParse;
+extern CSVConfig const kTestSingleColumn;
+extern CSVConfig const kTestWide;
+extern CSVConfig const kTestZeros;
+extern CSVConfig const kWdcAge;
+extern CSVConfig const kWdcAppearances;
+extern CSVConfig const kWdcAstrology;
+extern CSVConfig const kWdcAstronomical;
+extern CSVConfig const kWdcGame;
+extern CSVConfig const kWdcKepler;
+extern CSVConfig const kWdcPlanetz;
+extern CSVConfig const kWdcSatellites;
+extern CSVConfig const kWdcScience;
+extern CSVConfig const kWdcSymbols;
 }  // namespace tests

Desbordante/desbordante-core

src/tests/common/all_csv_configs.h
@@ -3,85 +3,84 @@
 #include "parser/csv_parser/csv_parser.h"
 
 namespace tests {
-extern CSVConfig const kWdcAstronomical;
-extern CSVConfig const kWdcSymbols;
-extern CSVConfig const kWdcScience;
-extern CSVConfig const kWdcSatellites;
-extern CSVConfig const kWdcAppearances;
-extern CSVConfig const kWdcAstrology;
-extern CSVConfig const kWdcGame;
-extern CSVConfig const kWdcKepler;
-extern CSVConfig const kWdcPlanetz;
-extern CSVConfig const kWdcAge;
-extern CSVConfig const kTestWide;
 extern CSVConfig const kAbalone;
-extern CSVConfig const kIris;
+extern CSVConfig const kACShippingDates;
 extern CSVConfig const kAdult;
+extern CSVConfig const kAdult9attr;
+extern CSVConfig const kAnimalsBeverages;
+extern CSVConfig const kBernoulliRelation;
 extern CSVConfig const kBreastCancer;
+extern CSVConfig const kCIPublicHighway;
 extern CSVConfig const kCIPublicHighway10k;
+extern CSVConfig const kCIPublicHighway20attr55k;
+extern CSVConfig const kCIPublicHighway700;
+extern CSVConfig const kEpicMeds;
+extern CSVConfig const kEpicVitals;
+extern CSVConfig const kFdReduced30;
+extern CSVConfig const kFlight1k;
+extern CSVConfig const kIndTest3aryInds;
+extern CSVConfig const kIndTestEmpty;
+extern CSVConfig const kIndTestNulls;
+extern CSVConfig const kIndTestPlanets;
+extern CSVConfig const kIndTestTableFirst;
+extern CSVConfig const kIndTestTableSecond;
+extern CSVConfig const kIndTestTypos;
+extern CSVConfig const kIndTestWide2;
+extern CSVConfig const kIowa1kk;
+extern CSVConfig const kIowa1kkNoDate;
+extern CSVConfig const kIowa25attr1kk;
+extern CSVConfig const kIowa450k;
+extern CSVConfig const kIowa550k;
+extern CSVConfig const kIowa650k;
+extern CSVConfig const kIris;
+extern CSVConfig const kLegacyPayors;
+extern CSVConfig const kLetter;
+extern CSVConfig const kLineItem;
+extern CSVConfig const kMushroom;
+extern CSVConfig const kMushroomPlus3attr1500;
+extern CSVConfig const kMushroomPlus4attr1300;
 extern CSVConfig const kNeighbors10k;
 extern CSVConfig const kNeighbors50k;
 extern CSVConfig const kNeighbors100k;
-extern CSVConfig const kCIPublicHighway700;
-extern CSVConfig const kEpicVitals;
-extern CSVConfig const kEpicMeds;
-extern CSVConfig const kIowa1kk;
-extern CSVConfig const kFdReduced30;
-extern CSVConfig const kFlight1k;
-extern CSVConfig const kPlista1k;
-extern CSVConfig const kLetter;
-extern CSVConfig const kCIPublicHighway;
-extern CSVConfig const kLegacyPayors;
-extern CSVConfig const kTestEmpty;
-extern CSVConfig const kTestSingleColumn;
-extern CSVConfig const kTestLong;
-extern CSVConfig const kTestFD;
-extern CSVConfig const kTestND;
-extern CSVConfig const kOdTestNormOd;
-extern CSVConfig const kOdTestNormSmall2x3;
-extern CSVConfig const kOdTestNormSmall3x3;
+extern CSVConfig const kNullEmpty;
+extern CSVConfig const kODnorm6;
 extern CSVConfig const kOdTestNormAbalone;
 extern CSVConfig const kOdTestNormBalanceScale;
 extern CSVConfig const kOdTestNormBreastCancerWisconsin;
 extern CSVConfig const kOdTestNormClassification;
 extern CSVConfig const kOdTestNormEchocardiogram;
+extern CSVConfig const kOdTestNormHepatitis;
 extern CSVConfig const kOdTestNormHepatitis1;
 extern CSVConfig const kOdTestNormHepatitis2;
 extern CSVConfig const kOdTestNormHepatitis3;
 extern CSVConfig const kOdTestNormHepatitis4;
 extern CSVConfig const kOdTestNormHepatitis5;
-extern CSVConfig const kOdTestNormHepatitis;
 extern CSVConfig const kOdTestNormHorse10c;
 extern CSVConfig const kOdTestNormIris;
-extern CSVConfig const kIndTestWide2;
-extern CSVConfig const kIndTestEmpty;
-extern CSVConfig const kIndTestPlanets;
-extern CSVConfig const kIndTest3aryInds;
-extern CSVConfig const kIndTestTableFirst;
-extern CSVConfig const kIndTestTableSecond;
-extern CSVConfig const kIndTestNulls;
-extern CSVConfig const kIndTestTypos;
-extern CSVConfig const kTestZeros;
-extern CSVConfig const kNullEmpty;
-extern CSVConfig const kSimpleTypes;
-extern CSVConfig const kRulesBook;
-extern CSVConfig const kRulesBookRows;
-extern CSVConfig const kRulesPresentationExtended;
-extern CSVConfig const kRulesPresentation;
-extern CSVConfig const kRulesSynthetic2;
-extern CSVConfig const kRulesKaggleRows;
-extern CSVConfig const kTennis;
-extern CSVConfig const kMushroom;
-extern CSVConfig const kTestDataStats;
-extern CSVConfig const kTestMetric;
-extern CSVConfig const kBernoulliRelation;
-extern CSVConfig const kACShippingDates;
-extern CSVConfig const kSimpleTypos;
-extern CSVConfig const kTest1;
+extern CSVConfig const kOdTestNormOd;
+extern CSVConfig const kOdTestNormSmall2x3;
+extern CSVConfig const kOdTestNormSmall3x3;
+extern CSVConfig const kPlista1k;
 extern CSVConfig const kProbeTest1;
 extern CSVConfig const kProbeTest2;
-extern CSVConfig const kTestParse;
-extern CSVConfig const kODnorm6;
+extern CSVConfig const kRulesBook;
+extern CSVConfig const kRulesBookRows;
+extern CSVConfig const kRulesKaggleRows;
+extern CSVConfig const kRulesPresentation;
+extern CSVConfig const kRulesPresentationExtended;
+extern CSVConfig const kRulesSynthetic2;
+extern CSVConfig const kSimpleTypes;
+extern CSVConfig const kSimpleTypes1;
+extern CSVConfig const kSimpleTypos;
+extern CSVConfig const kTennis;
+extern CSVConfig const kTest1;
+extern CSVConfig const kTestDataStats;
+extern CSVConfig const kTestDC;
+extern CSVConfig const kTestDC1;
+extern CSVConfig const kTestDC2;
+extern CSVConfig const kTestDC3;
+extern CSVConfig const kTestDC4;
+extern CSVConfig const kTestDC5;
 extern CSVConfig const kTestDD;
 extern CSVConfig const kTestDD1;
 extern CSVConfig const kTestDD2;
@@ -94,9 +93,8 @@ extern CSVConfig const kTestDif2;
 extern CSVConfig const kTestDif3;
 extern CSVConfig const kTestDif4;
 extern CSVConfig const kTestDif5;
-extern CSVConfig const kSimpleTypes1;
-extern CSVConfig const kTestDynamicFDInit;
 extern CSVConfig const kTestDynamicFDEmpty;
+extern CSVConfig const kTestDynamicFDInit;
 extern CSVConfig const kTestDynamicFDInsert;
 extern CSVConfig const kTestDynamicFDInsertBad1;
 extern CSVConfig const kTestDynamicFDInsertBad2;
@@ -105,12 +103,23 @@ extern CSVConfig const kTestDynamicFDUpdateBad1;
 extern CSVConfig const kTestDynamicFDUpdateBad2;
 extern CSVConfig const kTestDynamicFDUpdateBad3;
 extern CSVConfig const kTestDynamicFDUpdateBad4;
-extern CSVConfig const kLineItem;
-extern CSVConfig const kAnimalsBeverages;
-extern CSVConfig const kTestDC;
-extern CSVConfig const kTestDC1;
-extern CSVConfig const kTestDC2;
-extern CSVConfig const kTestDC3;
-extern CSVConfig const kTestDC4;
-extern CSVConfig const kTestDC5;
+extern CSVConfig const kTestEmpty;
+extern CSVConfig const kTestFD;
+extern CSVConfig const kTestLong;
+extern CSVConfig const kTestMetric;
+extern CSVConfig const kTestND;
+extern CSVConfig const kTestParse;
+extern CSVConfig const kTestSingleColumn;
+extern CSVConfig const kTestWide;
+extern CSVConfig const kTestZeros;
+extern CSVConfig const kWdcAge;
+extern CSVConfig const kWdcAppearances;
+extern CSVConfig const kWdcAstrology;
+extern CSVConfig const kWdcAstronomical;
+extern CSVConfig const kWdcGame;
+extern CSVConfig const kWdcKepler;
+extern CSVConfig const kWdcPlanetz;
+extern CSVConfig const kWdcSatellites;
+extern CSVConfig const kWdcScience;
+extern CSVConfig const kWdcSymbols;
 }  // namespace tests