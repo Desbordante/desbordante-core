cb09ffd89ca5ec473fc46e32399fa5d3eaffab3e
Senichenkov <Petua41@yandex.ru>
2025-09-21 12:49:34 +0300

    Implement base model classes
    Implement ComparableTupleType class to compare fixed-sized tuples of
    values.
    Implement IDomain class to represent an ordered domain in a metric space
    of values.
    Implement default domain classes: Ball and Parallelepiped.
    
    Implement PAC and DomainPAC classes.

---
+++ src/core/algorithms/pac/model/comparable_tuple_type.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <cstddef>
+#include <functional>
+#include <string>
+#include <vector>
+
+#include "algorithms/pac/model/tuple.h"
+#include "builtin.h"
+#include "model/types/type.h"
+
+namespace pac::model {
+using Comparer = std::function<bool(Tuple const&, Tuple const&)>;
+
+/// @brief Provides operations to compare fixed-sized tuples of values
+class ComparableTupleType {
+private:
+    std::vector<::model::Type const*> types_;
+    Comparer comparer_;
+
+public:
+    ComparableTupleType(std::vector<::model::Type const*>&& types, Comparer&& comparer)
+        : types_(std::move(types)), comparer_(std::move(comparer)) {}
+
+    /// @brief Convert @c std::byte* of type @c types[type_num] to string.
+    /// Consider using this function rather than directly calling @c types[type_num]->ValueToString,
+    /// because it correctly handles NULLs
+    std::string ByteToString(std::size_t type_num, std::byte const* value) const {
+        if (value == nullptr) {
+            return "NULL";
+        }
+        return types_[type_num]->ValueToString(value);
+    }
+
+    /// @brief Compare two @c std::byte pointers of type @c types[type_num].
+    /// Consider using this function rather than directly call @c types[type_num]->Compare, because
+    /// it correctly handles NULLs (NULL is considered less than any value).
+    ::model::CompareResult CompareBytes(std::size_t const type_num, std::byte const* x,
+                                        std::byte const* y);
+
+    bool Less(Tuple const& x, Tuple const& y) const {
+        assert(x.size() >= types_.size());
+        assert(y.size() >= types_.size());
+
+        return comparer_(x, y);
+    }
+
+    std::string ValueToString(Tuple const& value) const;
+
+    std::vector<::model::Type const*> const& GetTypes() const {
+        return types_;
+    }
+};
+
+/// @brief Converts vector of strings of the same format as ComparableTupleType::ValueToString()
+/// does
+std::string StringValueToString(std::vector<std::string> const& strings);
+}  // namespace pac::model
---
+++ src/core/algorithms/pac/model/default_domains/untyped_domain.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <cassert>
+#include <cstddef>
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "algorithms/pac/model/comparable_tuple_type.h"
+#include "algorithms/pac/model/idomain.h"
+#include "algorithms/pac/model/tuple.h"
+
+namespace pac::model {
+using StringValues = std::vector<std::string>;
+using StringCompare = std::function<bool(StringValues const&, StringValues const&)>;
+using StringDistFromDomain = std::function<double(StringValues const&)>;
+
+/// @brief Domain that uses string value representations instead of typed values.
+/// Useful when original type information is not needed (e. g. values to some specific type)
+/// or cannot be obtained (e. g. Python bindings)
+class UntypedDomain final : public IDomain {
+private:
+    StringCompare string_compare_;
+    StringDistFromDomain string_dist_;
+    std::string name_;
+
+    StringValues ValuesToStrings(Tuple const& values) const {
+        StringValues result(values.size());
+        for (std::size_t i = 0; i < values.size(); ++i) {
+            result[i] = tuple_type_->ByteToString(i, values[i]);
+        }
+        return result;
+    }
+
+public:
+    /// @param string_compare -- function that returns true if first argument is less than second.
+    /// @param string_dist_from_domain -- function that returns from this domain to its argument;
+    /// second argument is dist_from_null_is_infinity.
+    /// @param name -- displayed name of this domain; should be short, but informative.
+    UntypedDomain(StringCompare&& string_compare, StringDistFromDomain&& string_dist_from_domain,
+                  std::string&& name = "Untyped Domain")
+        : string_compare_(std::move(string_compare)),
+          string_dist_(std::move(string_dist_from_domain)),
+          name_(std::move(name)) {}
+
+    virtual double DistFromDomain(Tuple const& value) const override {
+        return string_dist_(ValuesToStrings(value));
+    }
+
+    virtual std::string ToString() const override {
+        return name_;
+    }
+
+    virtual void SetTypes(std::vector<::model::Type const*>&& types) override {
+        tuple_type_ = std::make_shared<ComparableTupleType>(
+                std::move(types), [this](Tuple const& a, Tuple const& b) {
+                    return string_compare_(ValuesToStrings(a), ValuesToStrings(b));
+                });
+    }
+};
+}  // namespace pac::model
