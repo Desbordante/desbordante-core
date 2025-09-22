f9afbbf711902c47260ce8d460cd992bf85fc549
Tydik42 <tgaraev653@gmail.com>
2025-09-22 04:52:09 +0300

    Refactor logging setup and remove unused log statements

--- src/tests/benchmark/main.cpp
+++ src/tests/benchmark/main.cpp
@@ -25,7 +25,7 @@ namespace po = boost::program_options;
 int main(int argc, char* argv[]) {
     using namespace benchmark;
 
-    util::logging::Initialize();
+    ::util::logging::Initialize();
 
     BenchmarkRunner bm_runner;
     BenchmarkComparer bm_comparer;
