c6a63809aa36b4d4d68f6ebc582f50cc04ba3125
Tydik42 <tgaraev653@gmail.com>
2025-09-22 04:52:09 +0300

    Refactor logging setup and remove unused log statements

--- src/core/util/logger.h
+++ src/core/util/logger.h
@@ -1,23 +1,34 @@
 #pragma once
 
 #include <memory>
+#include <string>
+#include <vector>
 
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
 namespace util::logging {
 
-inline void Initialize(std::string const& logger_name = "desbordante") {
+inline void Initialize(std::string const& logger_name = "desbordante",
+                       std::vector<spdlog::sink_ptr> sinks = {}) {
     if (spdlog::get(logger_name)) {
         return;
     }
-    auto const logger = spdlog::stderr_color_mt(logger_name);
-    logger->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] %v");
+
+    if (sinks.empty()) {
+        sinks.push_back(std::make_shared<spdlog::sinks::stderr_color_sink_mt>());
+    }
+
+    auto logger = std::make_shared<spdlog::logger>(logger_name, sinks.begin(), sinks.end());
+    logger->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%^%l%$] %v");
+
 #ifndef NDEBUG
     logger->set_level(spdlog::level::debug);
 #else
-    logger->set_level(spdlog::level::off);
+    logger->set_level(spdlog::level::info);
 #endif
+
+    spdlog::register_logger(logger);
 }
 
 inline std::shared_ptr<spdlog::logger> GetLogger(std::string const& logger_name = "desbordante") {
@@ -26,9 +37,16 @@ inline std::shared_ptr<spdlog::logger> GetLogger(std::string const& logger_name 
 
 }  // namespace util::logging
 
-#define LOG_TRACE(...) SPDLOG_LOGGER_TRACE(::util::logging::GetLogger(), __VA_ARGS__)
-#define LOG_DEBUG(...) SPDLOG_LOGGER_DEBUG(::util::logging::GetLogger(), __VA_ARGS__)
-#define LOG_INFO(...) SPDLOG_LOGGER_INFO(::util::logging::GetLogger(), __VA_ARGS__)
-#define LOG_WARN(...) SPDLOG_LOGGER_WARN(::util::logging::GetLogger(), __VA_ARGS__)
-#define LOG_ERROR(...) SPDLOG_LOGGER_ERROR(::util::logging::GetLogger(), __VA_ARGS__)
-#define LOG_CRITICAL(...) SPDLOG_LOGGER_CRITICAL(::util::logging::GetLogger(), __VA_ARGS__)
+#define GET_CACHED_LOGGER()                                     \
+    ([]() -> std::shared_ptr<spdlog::logger>& {                 \
+        static thread_local std::shared_ptr<spdlog::logger>     \
+            s_logger = ::util::logging::GetLogger();            \
+        return s_logger;                                        \
+    }())
+
+#define LOG_TRACE(...) SPDLOG_LOGGER_TRACE(GET_CACHED_LOGGER(), __VA_ARGS__)
+#define LOG_DEBUG(...) SPDLOG_LOGGER_DEBUG(GET_CACHED_LOGGER(), __VA_ARGS__)
+#define LOG_INFO(...) SPDLOG_LOGGER_INFO(GET_CACHED_LOGGER(), __VA_ARGS__)
+#define LOG_WARN(...) SPDLOG_LOGGER_WARN(GET_CACHED_LOGGER(), __VA_ARGS__)
+#define LOG_ERROR(...) SPDLOG_LOGGER_ERROR(GET_CACHED_LOGGER(), __VA_ARGS__)
+#define LOG_CRITICAL(...) SPDLOG_LOGGER_CRITICAL(GET_CACHED_LOGGER(), __VA_ARGS__)
