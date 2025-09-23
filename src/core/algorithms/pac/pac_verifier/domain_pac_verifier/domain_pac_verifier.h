e6e1bf7d55d2fd63a17317ebe533ac2f1ba50678
Senichenkov <Petua41@yandex.ru>
2025-09-23 10:54:22 +0300

    Implement Domain PAC verifier
    Implement verifier for Domain Probabilistic Approximate Constraints

---
+++ src/core/algorithms/pac/pac_verifier/domain_pac_verifier/domain_pac_verifier.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstddef>
+#include <vector>
+
+#include "algorithms/pac/pac_verifier/domain_pac_verifier/domain_pac_verifier_base.h"
+#include "descriptions.h"
+#include "names.h"
+#include "option_using.h"
+
+namespace algos::pac_verifier {
+/// @brief Domain Probabilistic Approximate Constraint verifier.
+/// Takes pointer to IDomain as domain option.
+/// This version is considered to be used in C++.
+class DomainPACVerifier final : public DomainPACVerifierBase {
+public:
+    DomainPACVerifier() : DomainPACVerifierBase() {
+        DESBORDANTE_OPTION_USING;
+
+        RegisterOption(Option(&domain_, kDomain, kDDomain));
+        MakeOptionsAvailable({kDomain});
+    }
+};
+}  // namespace algos::pac_verifier
