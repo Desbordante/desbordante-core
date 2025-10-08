6189427b4ed753660d3a222419373d398ac08480
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2025-10-08 21:17:44 +0300

    Remove FDAlgorithm::GetKeys
    This method is no longer used anywhere. The Web interface now uses
    Python, where there are no bindings for this method.

--- src/core/algorithms/fd/fd_algorithm.cpp
+++ src/core/algorithms/fd/fd_algorithm.cpp
@@ -55,44 +55,4 @@ unsigned int FDAlgorithm::Fletcher16() {
     return (sum2 << 8) | sum1;
 }
 
-/* Attribute A contains only unique values (i.e. A is the key) iff [A]->[B]
- * holds for every attribute B. So to determine if A is a key, we count
- * number of fds with lhs==[A] and if it equals the total number of attributes
- * minus one (the attribute A itself) then A is the key.
- */
-std::vector<Column const*> FDAlgorithm::GetKeys() const {
-    std::vector<Column const*> keys;
-    std::map<Column const*, size_t> fds_count_per_col;
-    unsigned int cols_of_equal_values = 0;
-
-    for (FD const& fd : FdList()) {
-        Vertical const& lhs = fd.GetLhs();
-
-        if (lhs.GetArity() == 0) {
-            /* We separately count columns consisting of only equal values,
-             * because they cannot be on the right side of the minimal fd.
-             * And obviously for every attribute A true: [A]->[B] holds
-             * if []->[B] holds.
-             */
-            cols_of_equal_values++;
-        } else if (lhs.GetArity() == 1) {
-            fds_count_per_col[lhs.GetColumns().front()]++;
-        }
-    }
-
-    if (fds_count_per_col.empty()) return keys;
-    assert(fds_count_per_col.begin()->first->GetSchema() != nullptr);
-    size_t const number_of_columns = fds_count_per_col.begin()->first->GetSchema()->GetNumColumns();
-    [[maybe_unused]] RelationalSchema const* first_schema =
-            fds_count_per_col.begin()->first->GetSchema();
-    for (auto const& [col, num] : fds_count_per_col) {
-        assert(col->GetSchema() == first_schema);
-        if (num + 1 + cols_of_equal_values == number_of_columns) {
-            keys.push_back(col);
-        }
-    }
-
-    return keys;
-}
-
 }  // namespace algos
