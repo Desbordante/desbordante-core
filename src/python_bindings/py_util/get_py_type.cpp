0d5094f3825c1096c76fa36069eaeef777fac9cf
Senichenkov <Petua41@yandex.ru>
2025-10-10 18:48:43 +0300

    Implement Domain PAC bindings
    Implement Python bindigns for Domain PAC verifier

--- src/python_bindings/py_util/get_py_type.cpp
+++ src/python_bindings/py_util/get_py_type.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 
 #include <Python.h>
+#include <pybind11/pytypes.h>
 #include <pybind11/stl/filesystem.h>
 
 #include "algorithms/cfd/enums.h"
@@ -14,12 +15,14 @@
 #include "algorithms/md/hymd/hymd.h"
 #include "algorithms/md/md_verifier/column_similarity_classifier.h"
 #include "algorithms/metric/enums.h"
+#include "algorithms/pac/model/default_domains/domain_type.h"
 #include "association_rules/ar_algorithm_enums.h"
 #include "config/custom_random_seed/type.h"
 #include "config/error_measure/type.h"
 #include "config/tabular_data/input_table_type.h"
 #include "config/tabular_data/input_tables_type.h"
 #include "model/table/column_combination.h"
+#include "pac/model/idomain.h"
 
 namespace py = pybind11;
 
@@ -114,6 +117,11 @@ py::tuple GetPyType(std::type_index type_index) {
             PyTypePair<std::vector<std::filesystem::path>, kPyList, kPyStr>,
             PyTypePair<std::unordered_set<size_t>, kPySet, kPyInt>,
             PyTypePair<std::string, kPyStr>,
+            {typeid(std::vector<std::string>), []() { return MakeTypeTuple(kPyList, kPyStr); }},
+            {typeid(std::vector<double>), []() { return MakeTypeTuple(kPyList, kPyFloat); }},
+            PyTypePair<pac::model::DomainType, kPyStr>,
+            {typeid(std::shared_ptr<pac::model::IDomain>),
+             []() { return MakeTypeTuple(py::type::of<pac::model::IDomain>()); }},
     };
     return type_map.at(type_index)();
 }
