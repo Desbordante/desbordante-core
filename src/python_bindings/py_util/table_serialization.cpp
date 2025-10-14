b2b67abc1f77e46acfce4bb3059bcf808aa7e8af
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2025-10-14 17:40:35 +0300

    Remove `RelationalSchema::IndicesToBitset`
    This is an uncommon and simple method, the effects of which can be
    recreated from the outside.

--- src/python_bindings/py_util/table_serialization.cpp
+++ src/python_bindings/py_util/table_serialization.cpp
@@ -42,7 +42,7 @@ Vertical DeserializeVertical(py::tuple t, RelationalSchema const* schema) {
         throw std::runtime_error("Invalid state for Vertical pickle!");
     }
     auto idx_vec = t[0].cast<std::vector<unsigned int>>();
-    boost::dynamic_bitset<> bitset = schema->IndicesToBitset(idx_vec);
+    boost::dynamic_bitset<> bitset = util::IndicesToBitset(idx_vec, schema->GetNumColumns());
     return Vertical(schema, std::move(bitset));
 }
 
