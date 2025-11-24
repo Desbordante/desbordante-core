302d6157fd6f0f2d9aff8564e0957da52521b3a3
PodushkaPIR <seterovas@yandex.ru>
2025-11-24 11:48:05 +0300

    Fix pyfiles with unused imports and an extra formatting print

--- examples/expert/data_cleaning_dc.py
+++ examples/expert/data_cleaning_dc.py
@@ -156,7 +156,7 @@ def main():
     print_table(TABLE_2)
 
     print("Now we can check if the constraint holds by running the algorithm again.\n\n"
-    f"DC " + DC + " holds: " + str(dc_holds), end='\n\n'
+    "DC " + DC + " holds: " + str(dc_holds), end='\n\n'
     "After fixing the typos in the initial dataset, the constraint holds.\n")
 
 
