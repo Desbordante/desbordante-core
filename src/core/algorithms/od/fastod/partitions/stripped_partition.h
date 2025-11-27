104109bfcaebbe3276847971403c637c73fcdca1
Artyom Gavrilov <artyom.e.gavrilov@gmail.com>
2025-11-27 20:51:39 +0300

    Add missing include
    Due to the normalisation of includes and their subsequent formatting, the
    order in which they're included has changed, and the build has broken.
    Adding this include solves the build problem.

--- src/core/algorithms/od/fastod/partitions/stripped_partition.h
+++ src/core/algorithms/od/fastod/partitions/stripped_partition.h
@@ -6,6 +6,7 @@
 #include <vector>
 
 #include "core/algorithms/od/fastod/partitions/range_based_stripped_partition.h"
+#include "core/algorithms/od/fastod/storage/data_frame.h"
 
 namespace algos::fastod {
 
