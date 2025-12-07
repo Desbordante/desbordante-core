8661561cf7b59bf146c599e6b36f992a4944ffaa
Artyom Gavrilov <artyom.e.gavrilov@gmail.com>
2025-12-07 17:46:01 +0300

    Replace library "..." includes with <...>
    Replace library #include "..." with <...> for consistency with project
    style and proper formatting.

--- src/core/algorithms/dc/FastADC/model/operator.h
+++ src/core/algorithms/dc/FastADC/model/operator.h
@@ -4,9 +4,10 @@
 #include <cstddef>
 #include <string>
 
+#include <frozen/string.h>
+#include <frozen/unordered_map.h>
+
 #include "core/model/types/type.h"
-#include "frozen/string.h"
-#include "frozen/unordered_map.h"
 
 namespace algos::fastadc {
 
