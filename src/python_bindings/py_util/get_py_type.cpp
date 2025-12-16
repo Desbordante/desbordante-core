277b62c8e3e830dbffafb7b8d2b582e1fb0c94b2
Tydik42 <tgaraev653@gmail.com>
2025-12-16 19:22:39 +0300

    Improve and unify configuration for AR and data models
    - Standardize AR option names with "Ar" prefix to avoid conflicts.
    - Centralize min_support/min_confidence into shared CommonOption
    objects.
    - Refactor TransactionalData to use a Params struct for cleaner
    instantiation.
    - Add strict [0, 1] range validation for support/confidence options.
    - Switch AR rule storage from std::list to std::vector.
    - Adapt AR miner and bindings to all related changes.
    - Rename association_rulses to ar for consistency

--- src/python_bindings/py_util/get_py_type.cpp
+++ src/python_bindings/py_util/get_py_type.cpp
@@ -11,7 +11,6 @@
 #include <boost/core/demangle.hpp>
 #include <pybind11/stl/filesystem.h>
 
-#include "core/algorithms/association_rules/ar_algorithm_enums.h"
 #include "core/algorithms/cfd/enums.h"
 #include "core/algorithms/dd/dd.h"
 #include "core/algorithms/md/hymd/enums.h"
@@ -24,6 +23,7 @@
 #include "core/config/tabular_data/input_table_type.h"
 #include "core/config/tabular_data/input_tables_type.h"
 #include "core/model/table/column_combination.h"
+#include "core/model/transaction/input_format_type.h"
 
 namespace py = pybind11;
 
@@ -89,7 +89,7 @@ py::tuple GetPyType(std::type_index type_index) {
             PyTypePair<algos::metric::MetricAlgo, kPyStr>,
             PyTypePair<config::PfdErrorMeasureType, kPyStr>,
             PyTypePair<config::AfdErrorMeasureType, kPyStr>,
-            PyTypePair<algos::InputFormat, kPyStr>,
+            PyTypePair<model::InputFormatType, kPyStr>,
             PyTypePair<algos::cfd::Substrategy, kPyStr>,
             PyTypePair<algos::hymd::LevelDefinition, kPyStr>,
             PyTypePair<algos::od::Ordering, kPyStr>,
