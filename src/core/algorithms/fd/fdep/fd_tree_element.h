c8eeee7a7b894b5326e2ce14fd351470c91b4bed
Alexey Shlyonskikh <59949541+BUYT-1@users.noreply.github.com>
2026-02-23 20:18:22 +0300

    Move FDep to the new FD architecture
    Also removed the printing dependencies to file functions, they are
    unused and I don't want to reimplement them.

--- src/core/algorithms/fd/fdep/fd_tree_element.h
+++ src/core/algorithms/fd/fdep/fd_tree_element.h
@@ -4,12 +4,9 @@
 #include <memory>
 #include <vector>
 
-// For printing Dependencies
-#include <fstream>
-#include <string>
+#include <boost/dynamic_bitset.hpp>
 
-#include "core/algorithms/fd/fd.h"
-#include "core/model/table/relational_schema.h"
+#include "core/algorithms/fd/bitset_pair_result_reporter.h"
 #include "core/model/types/bitset.h"
 
 class FDTreeElement {
@@ -41,15 +38,12 @@ public:
     [[nodiscard]] bool ContainsGeneralization(model::Bitset<kMaxAttrNum> const& lhs,
                                               size_t attr_num, size_t current_attr) const;
 
-    // Printing found dependencies in output file.
-    void PrintDep(std::string const& file, std::vector<std::string>& column_names) const;
-
-    void FillFdCollection(std::shared_ptr<RelationalSchema> const& scheme,
-                          std::list<FD>& fd_collection,
-                          unsigned int max_lhs = std::numeric_limits<unsigned int>::max()) const;
+    void CreateAnswer(std::size_t attr_num, algos::fd::BitsetPairResultReporter const& report_fd,
+                      unsigned int max_lhs = std::numeric_limits<unsigned int>::max()) const;
 
 private:
     std::vector<std::unique_ptr<FDTreeElement>> children_;
+    // TODO: why is this not dynamic_bitset?
     model::Bitset<kMaxAttrNum> rhs_attributes_;
     size_t max_attribute_number_;
     model::Bitset<kMaxAttrNum> is_fd_;
@@ -70,12 +64,7 @@ private:
     void FilterSpecializationsHelper(FDTreeElement& filtered_tree,
                                      model::Bitset<kMaxAttrNum>& active_path);
 
-    // Helper function for PrintDep.
-    void PrintDependencies(model::Bitset<kMaxAttrNum>& active_path, std::ofstream& file,
-                           std::vector<std::string>& column_names) const;
-
     void TransformTreeFdCollection(
-            model::Bitset<kMaxAttrNum>& active_path, std::list<FD>& fd_collection,
-            std::shared_ptr<RelationalSchema> const& scheme,
+            boost::dynamic_bitset<>& lhs, algos::fd::BitsetPairResultReporter const& report_fd,
             unsigned int max_lhs = std::numeric_limits<unsigned int>::max()) const;
 };
