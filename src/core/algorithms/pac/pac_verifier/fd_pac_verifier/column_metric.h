0d96ed789b6470e8704bc45772dd41315ec2af17
Senichenkov <Petua41@yandex.ru>
2026-03-26 11:30:20 +0300

    Implement FD PAC verifier

---
+++ src/core/algorithms/pac/pac_verifier/fd_pac_verifier/column_metric.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <cstddef>
+#include <functional>
+#include <limits>
+#include <string>
+#include <variant>
+
+#include "core/config/exceptions.h"
+#include "core/model/types/imetrizable_type.h"
+#include "core/model/types/type.h"
+
+namespace algos::pac_verifier {
+using RawDataMetric = std::function<double(std::byte const*, std::byte const*, model::Type const*)>;
+using StringDataMetric = std::function<double(std::string&&, std::string&&)>;
+// Order matters: pybind11 tries types left-to-right
+using ValueMetric = std::variant<std::nullptr_t, StringDataMetric, RawDataMetric>;
+
+namespace detail {
+// pybind11::type::of, which is used by our GetPyType, cannot work with standard types.
+// So we need this wrapper around ValueMetric (which is std::variant).
+// The only place this class is used is `get_opt_type` method of FDPACVerifier<true>.
+class FakeValueMetric {
+private:
+    ValueMetric value_metric_;
+
+public:
+    FakeValueMetric(ValueMetric&& value_metric) : value_metric_(std::move(value_metric)) {}
+
+    operator ValueMetric() {
+        return value_metric_;
+    }
+};
+}  // namespace detail
+
+/// @brief Provides a convenient way to calculate distance between values in some column, either
+/// using user-provided metric or a default one, and taking into account @c dist_from_null_is_infty
+/// option
+class ColumnMetric {
+private:
+    model::Type const* type_;
+    // Either metr_type_ or raw_data_metric_ or string_data_metric_ is used.
+    // It's invariant that exactly one of them is not nullptr.
+    model::IMetrizableType const* metr_type_;
+    RawDataMetric raw_data_metric_;
+    StringDataMetric string_data_metric_;
+    bool dist_from_null_is_infty_;
+
+public:
+    ColumnMetric() = default;
+    ColumnMetric(ColumnMetric const&) = default;
+    ColumnMetric& operator=(ColumnMetric const&) = default;
+    ColumnMetric(ColumnMetric&&) = default;
+    ColumnMetric& operator=(ColumnMetric&&) = default;
+    ~ColumnMetric() = default;
+
+    ColumnMetric(model::Type const* type, std::nullptr_t, bool dist_from_null_is_infty)
+        : type_(type), dist_from_null_is_infty_(dist_from_null_is_infty) {
+        metr_type_ = dynamic_cast<model::IMetrizableType const*>(type_);
+        if (!metr_type_) {
+            throw config::ConfigurationError("Cannot use default metric, because column type " +
+                                             type_->ToString() + " is not metrizable");
+        }
+    }
+
+    ColumnMetric(model::Type const* type, RawDataMetric&& raw_data_metric,
+                 bool dist_from_null_is_infty)
+        : type_(type),
+          raw_data_metric_(std::move(raw_data_metric)),
+          dist_from_null_is_infty_(dist_from_null_is_infty) {}
+
+    ColumnMetric(model::Type const* type, StringDataMetric&& string_data_metric,
+                 bool dist_from_null_is_infty)
+        : type_(type),
+          string_data_metric_(std::move(string_data_metric)),
+          dist_from_null_is_infty_(dist_from_null_is_infty) {}
+
+    double Dist(std::byte const* x, std::byte const* y) const {
+        if (!x || !y) {
+            return dist_from_null_is_infty_ ? std::numeric_limits<double>::infinity() : 0;
+        }
+        if (raw_data_metric_) {
+            return raw_data_metric_(x, y, type_);
+        }
+        if (string_data_metric_) {
+            return string_data_metric_(type_->ValueToString(x), type_->ValueToString(y));
+        }
+        assert(metr_type_);
+        return metr_type_->Dist(x, y);
+    }
+};
+}  // namespace algos::pac_verifier
---
+++ src/core/algorithms/pac/pac_verifier/fd_pac_verifier/fd_pac_highlight.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#include <algorithm>
+#include <cstddef>
+#include <functional>
+#include <iterator>
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "core/algorithms/pac/model/tuple.h"
+#include "core/algorithms/pac/pac_verifier/fd_pac_verifier/tuple_pair.h"
+#include "core/model/types/type.h"
+
+namespace algos::pac_verifier {
+class FDPACHighlight {
+private:
+    using Pairs = std::vector<TuplePair>;
+    using Tuples = pac::model::Tuples;
+    using Types = std::vector<model::Type const*>;
+
+    using PairsIt = Pairs::const_iterator;
+
+    using LhsRhsData = std::pair<pac::model::Tuple, pac::model::Tuple>;
+    using RawPair = std::pair<LhsRhsData, LhsRhsData>;
+    using LhsRhsString = std::pair<std::string, std::string>;
+    using StringPair = std::pair<LhsRhsString, LhsRhsString>;
+
+    std::shared_ptr<Tuples> lhs_tuples_;
+    std::shared_ptr<Tuples> rhs_tuples_;
+    std::shared_ptr<Types> lhs_types_;
+    std::shared_ptr<Types> rhs_types_;
+    // Pairs are kept here to extend their lifetime
+    std::shared_ptr<Pairs> pairs_;
+    // Range of pairs that form the highlight
+    PairsIt begin_, end_;
+
+    template <typename Res>
+    std::vector<Res> TransformPairs(std::function<Res(TuplePair const&)> const& op) const {
+        std::vector<Res> result;
+        result.reserve(std::distance(begin_, end_));
+        std::ranges::transform(begin_, end_, std::back_inserter(result), op);
+        return result;
+    }
+
+public:
+    // Empty highlight
+    FDPACHighlight()
+        : lhs_tuples_(std::make_shared<Tuples>()),
+          rhs_tuples_(std::make_shared<Tuples>()),
+          lhs_types_(std::make_shared<Types>()),
+          rhs_types_(std::make_shared<Types>()),
+          pairs_(std::make_shared<Pairs>()),
+          begin_(pairs_->begin()),
+          end_(pairs_->end()) {}
+
+    FDPACHighlight(std::shared_ptr<Tuples> lhs_tuples, std::shared_ptr<Tuples> rhs_tuples,
+                   std::shared_ptr<Types> lhs_types, std::shared_ptr<Types> rhs_types,
+                   std::shared_ptr<Pairs> pairs, PairsIt begin, PairsIt end)
+        : lhs_tuples_(std::move(lhs_tuples)),
+          rhs_tuples_(std::move(rhs_tuples)),
+          lhs_types_(std::move(lhs_types)),
+          rhs_types_(std::move(rhs_types)),
+          pairs_(std::move(pairs)),
+          begin_(begin),
+          end_(end) {}
+
+    /// @brief Get pairs of row indices
+    std::vector<std::pair<std::size_t, std::size_t>> RowIndices() const;
+
+    /// @brief Get total number of pairs highlighted
+    std::size_t NumPairs() const {
+        // Each pair has a counterpart
+        return 2 * std::distance(begin_, end_);
+    }
+
+    /// @brief Value @c types associated with left-hand side columns
+    std::vector<model::Type const*> const& LhsTypes() const {
+        return *lhs_types_;
+    }
+
+    /// @brief Value @c types associated with right-hand side columns
+    std::vector<model::Type const*> const& RhsTypes() const {
+        return *rhs_types_;
+    }
+
+    /// @brief Get pairs as pointers to @c std::byte that can be used with types (see @c LhsTypes,
+    /// @c RhsTypes)
+    std::vector<RawPair> RawData() const;
+
+    /// @brief Get data as pairs of strings
+    std::vector<StringPair> StringData() const;
+
+    std::string ToString() const;
+};
+}  // namespace algos::pac_verifier
