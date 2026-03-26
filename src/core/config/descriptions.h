b7141790c8d62c6747f2ee1c604fe182f61aa52e
Senichenkov <Petua41@yandex.ru>
2026-03-26 11:27:17 +0300

    Add FD PAC-related names and descriptions

--- src/core/config/descriptions.h
+++ src/core/config/descriptions.h
@@ -2,6 +2,10 @@
 
 #include <string_view>
 
+#define DEFAULT_METRIC_NOTE                                                                \
+    "You can pass nullptr for some columns or leave the entire list empty to use default " \
+    "metrics, deduced from column type."
+
 namespace config::descriptions {
 // Common
 constexpr auto kDCustomMetric = "user-defined metric";
@@ -182,6 +186,11 @@ constexpr auto kDDiagonalThreshold =
         "horizontal, i. e. maximal ratio m/N such that an addition of m values on a table "
         "containing N rows is not considered a change (default is 1e-5).";
 constexpr auto kDDomain = "Ordered domain for Domain PAC.";
+constexpr auto kDLhsDeltas =
+        "Maximum distances between values in left-hand side of FD PAC. "
+        "Default is 0 for each column.";
+constexpr auto kDLhsMetrics = "Metrics for left-hand side columns. " DEFAULT_METRIC_NOTE;
+constexpr auto kDRhsMetrics = "Metrics for right-hand side columns. " DEFAULT_METRIC_NOTE;
 constexpr auto kDMaxEpsilon =
         "Maximal value of epsilon, which shows how much values deviate from exact dependency "
         "(default is +infinity).";
@@ -245,3 +254,5 @@ constexpr auto kDSdG1 = "Lower bound of the sequential interval (inclusive).";
 constexpr auto kDSdG2 = "Upper bound of the sequential interval (inclusive). Set -1 for Infinity.";
 constexpr auto kDSdIndices = "Subset of row indices to validate the SD on.";
 }  // namespace config::descriptions
+
+#undef DEFAULT_METRIC_NOTE
