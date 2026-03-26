d3f1f7d7b052dc0183294824df032ba306dd31aa
Senichenkov <Petua41@yandex.ru>
2026-03-26 11:31:14 +0300

    Implement FD PAC verifier bindings

--- src/python_bindings/pac/bind_pac_verification.cpp
+++ src/python_bindings/pac/bind_pac_verification.cpp
@@ -8,13 +8,19 @@
 #include <utility>
 #include <vector>
 
+#include <pybind11/stl.h>
+
 #include "core/algorithms/algo_factory.h"
+#include "core/algorithms/algorithm.h"
 #include "core/algorithms/pac/pac_verifier/domain_pac_verifier/domain_pac_highlight.h"
 #include "core/algorithms/pac/pac_verifier/domain_pac_verifier/domain_pac_verifier.h"
+#include "core/algorithms/pac/pac_verifier/fd_pac_verifier/fd_pac_highlight.h"
+#include "core/algorithms/pac/pac_verifier/fd_pac_verifier/fd_pac_verifier.h"
 #include "core/config/names.h"
 #include "python_bindings/py_util/bind_primitive.h"
 
 namespace py = pybind11;
+using namespace algos::pac_verifier;
 
 namespace python_bindings {
 void BindPACVerification(py::module_& main_module) {
@@ -22,6 +28,7 @@ void BindPACVerification(py::module_& main_module) {
     auto algos_module = pac_verification_module.def_submodule("algorithms");
 
     BindDomainPACVerification(pac_verification_module);
+    BindFDPACVerification(pac_verification_module, algos_module);
 }
 
 void BindDomainPACVerification(py::module_& pac_verification_module) {
@@ -68,5 +75,34 @@ void BindDomainPACVerification(py::module_& pac_verification_module) {
                                 verifier.Execute();
                             },
                             "Verify PAC with given delta", "epsilon"_a);
+}
+
+void BindFDPACVerification(py::module_& pac_verification_module, py::module_& algorithms_module) {
+    using namespace py::literals;
+    using namespace config::names;
+
+    py::class_<FDPACHighlight>(pac_verification_module, "FDPACHighlight")
+            .def_property_readonly("row_indices", &FDPACHighlight::RowIndices)
+            .def_property_readonly("num_pairs", &FDPACHighlight::NumPairs)
+            .def_property_readonly("string_data", &FDPACHighlight::StringData)
+            .def("__str__", &FDPACHighlight::ToString)
+            .doc() = "A set of tuple pairs that violate FD PAC.";
+
+    // Have to use low-level facility, because BindPrimitiveNoBase would overwrite ".default"
+    // attribute on each call
+    detail::RegisterAlgorithm<FDPACVerifier, algos::Algorithm>(algorithms_module, "FDPACVerifier")
+            .def("get_pac", &FDPACVerifier::GetPAC)
+            .def("get_highlights", &FDPACVerifier::GetHighlights, "eps_1"_a = 0, "eps_2"_a = -1)
+            .def(
+                    "verify",
+                    [](FDPACVerifier& verifier, double epsilon, double delta) {
+                        algos::ConfigureFromMap(verifier, {
+                                                                  {kMinEpsilon, epsilon},
+                                                                  {kMaxEpsilon, epsilon},
+                                                                  {kMinDelta, delta},
+                                                          });
+                        verifier.Execute();
+                    },
+                    "Verify PAC with given epsilon or delta", "epsilon"_a = -1, "delta"_a = -1);
 }
 }  // namespace python_bindings
--- src/python_bindings/pac/bind_pac_verification.h
+++ src/python_bindings/pac/bind_pac_verification.h
@@ -5,4 +5,6 @@
 namespace python_bindings {
 void BindPACVerification(pybind11::module_& main_module);
 void BindDomainPACVerification(pybind11::module_& pac_verification_module);
+void BindFDPACVerification(pybind11::module_& pac_verification_module,
+                           pybind11::module_& algorithms_module);
 }  // namespace python_bindings
