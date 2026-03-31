b1f0ee581dcba643fe693f88d62a82dbad202134
Alexey Shlyonskikh <59949541+BUYT-1@users.noreply.github.com>
2026-04-01 00:04:55 +0300

    Rename PliBasedFdAlgorithm
    Mark it as legacy code.

--- src/core/algorithms/fd/pyro/pyro.h
+++ src/core/algorithms/fd/pyro/pyro.h
@@ -10,7 +10,7 @@
 namespace algos {
 
 /* Class for mining FD with pyro algorithm */
-class Pyro : public DependencyConsumer, public PliBasedFDAlgorithm {
+class Pyro : public DependencyConsumer, public LegacyPliBasedFDAlgorithm {
 private:
     std::list<std::unique_ptr<SearchSpace>> search_spaces_;
 
