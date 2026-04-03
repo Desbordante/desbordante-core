ec35f048b659d3487af2dbe1719d1f7ad1e046b5
Alexey Shlyonskikh <59949541+BUYT-1@users.noreply.github.com>
2026-04-03 16:00:58 +0300

    Use != in VectorToTuple iteration
    For consistency with other code.

--- src/python_bindings/py_util/vector_to_tuple.h
+++ src/python_bindings/py_util/vector_to_tuple.h
@@ -9,7 +9,7 @@ namespace python_bindings {
 template <typename ElementType, typename Func>
 pybind11::tuple VectorToTuple(std::vector<ElementType> const& vec, Func&& func) {
     pybind11::tuple tup(vec.size());
-    for (std::size_t i = 0; i < vec.size(); ++i) {
+    for (std::size_t i = 0; i != vec.size(); ++i) {
         tup[i] = std::invoke(func, vec[i]);
     }
     return tup;
@@ -18,7 +18,7 @@ pybind11::tuple VectorToTuple(std::vector<ElementType> const& vec, Func&& func) 
 template <typename ElementType>
 pybind11::tuple VectorToTuple(std::vector<ElementType> const& vec) {
     pybind11::tuple tup(vec.size());
-    for (std::size_t i = 0; i < vec.size(); i++) {
+    for (std::size_t i = 0; i != vec.size(); i++) {
         tup[i] = vec[i];
     }
     return tup;
