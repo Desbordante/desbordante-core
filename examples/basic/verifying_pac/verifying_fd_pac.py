c2a781a51b2e43862d8982a7357187126ce4c7ef
Senichenkov <Petua41@yandex.ru>
2026-04-04 19:07:07 +0300

    Edit example

--- examples/basic/verifying_pac/verifying_fd_pac.py
+++ examples/basic/verifying_pac/verifying_fd_pac.py
@@ -1,14 +1,17 @@
-from tabulate import tabulate
-from csv import reader
+import logging
+from sys import stdout
+
+logging.basicConfig(stream=stdout)
+logging.getLogger('desbordante').setLevel(logging.DEBUG)
 
 import desbordante
 
 from util import *
 
-MARINE_URCHINS = 'examples/datasets/verifying_pac/marine_urchins.csv'
+MARINE_URCHINS = "examples/datasets/verifying_pac/marine_urchins.csv"
 
 print(
-    '''This example illustrates the usage of Functional Dependency Probabilistic Approximate Constraints (FD PACs).
+    """This example illustrates the usage of Functional Dependency Probabilistic Approximate Constraints (FD PACs).
 Given columns sets X and Y, and a set of numbers {Δᵢ}, an FD PAC X → Y with parameters {εᵢ}, {δᵢ} means that
     if, for some tuples t₁, t₂, d(t₁[Aᵢ], t₂[Aᵢ]) < Δᵢ for each Aᵢ ∈ X, then
     Pr(d(t₁[Bᵢ], t₂[Bᵢ]) ≤ εᵢ) ≥ δᵢ for each Bᵢ ∈ Y
@@ -23,27 +26,26 @@ For each individual, we measured:
  - Age (in days)
  - Size (diameter in millimeters)
  - Water quality in individual's habitat, labeled from A (excellent) to D (poor)
-The following table contains measurement results:''')
-print(f'{BOLD}{csv_to_str(MARINE_URCHINS)}{ENDC}')
+The following table contains measurement results:"""
+)
+print(f"{BOLD}{csv_to_str(MARINE_URCHINS)}{ENDC}")
 
-print(f'''
+print(f"""
 We can assume that the size of marine urchin roughly depends on its age.
 In terms of FD PACs, this means that FD PAC {BLUE}{{Age}} → {{Size}}{ENDC} should hold with some reasonable parameters.
 
 Let's run FD PAC verifier to check this hypothesis. We use the following parameters: lhs_indices={BLUE}[0]{ENDC},
 rhs_indices={BLUE}[1]{ENDC}, lhs_deltas={BLUE}[10]{ENDC}.
-''')
+""")
 
 algo = desbordante.pac_verification.algorithms.FDPACVerifier()
-algo.load_data(table=(MARINE_URCHINS, ',', True),
-               lhs_indices=[0],
-               rhs_indices=[1])
+algo.load_data(table=(MARINE_URCHINS, ",", True), lhs_indices=[0], rhs_indices=[1])
 algo.execute(lhs_deltas=[10])
 
 pac = algo.get_pac()
-print(f'Algorithm result: {RED}{pac}{ENDC}.')
+print(f"Algorithm result: {RED}{pac}{ENDC}.")
 
-print(f'''
+print(f"""
 This FD PAC means that our hypothesis holds only for {pac.deltas[0] / 100:.2f}% of urchins.
 
 Intuitively, we can suggest, that urchins which live in poor water, should be smaller, than those
@@ -53,26 +55,29 @@ In terms of FD PACs, this means that we need to validate FD PAC {BLUE}{{Age, Wat
 Let's run the algorithm with the following parameters: lhs_indices={BLUE}[0, 2]{ENDC}, rhs_indices={BLUE}[1]{ENDC},
 lhs_deltas={BLUE}[10]{ENDC}.
 
-TODO: сказать про метрику
-''')
+{RED}TODO: сказать про метрику{ENDC}
+""")
 
 
-def abs_dist(a: float, b: float) -> float:
-    return abs(a - b)
-
-
+# Note that all custom metrics must work with strings.
+# If you need type info, consider using FD PAC verifier from C++ library
 def alphabet_dist(a: str, b: str) -> float:
     return abs(ord(a[0]) - ord(b[0]))
 
 
 algo = desbordante.pac_verification.algorithms.FDPACVerifier()
-algo.load_data(table=(MARINE_URCHINS, ',', True),
-               lhs_indices=[0, 2],
-               rhs_indices=[1],
-               lhs_metrics={abs_dist, alphabet_dist})
-algo.execute(lhs_deltas=[10])
+algo.load_data(
+    table=(MARINE_URCHINS, ",", True),
+    lhs_indices=[0, 2],
+    rhs_indices=[1],
+    # None means "default metric for this column"
+    lhs_metrics=[None, alphabet_dist],
+)
+algo.execute(lhs_deltas=[10, 1])
+
+alphabet_dist('abc', 'cba')
 
 pac = algo.get_pac()
-print(f'Algorithm result: {GREEN}{pac}{ENDC}.')
+print(f"Algorithm result: {GREEN}{pac}{ENDC}.")
 
-print('''This PAC means that our refined hypothesis holds.''')
+print("""This PAC means that our refined hypothesis holds.""")
