208b1897628565628fd7a2feb32adc35a85ad5c6
PodushkaPIR <seterovas@yandex.ru>
2026-04-08 16:13:43 +0300

    Add IWYU pragma keep for critical includes
    Added // IWYU pragma: keep comments to protect includes that are
    required transitively through option_using.h macro (names_and_descriptions.h).
    
    This prevents IWYU from incorrectly suggesting removal of these
    critical includes which would break compilation.

--- src/core/algorithms/gfd/gfd_validator/egfd_validator.h
+++ src/core/algorithms/gfd/gfd_validator/egfd_validator.h
@@ -1,7 +1,15 @@
 #pragma once
+#include <map>
+#include <set>
+#include <utility>
+#include <vector>
+
+// IWYU pragma: keep - needed for config namespaces
+#include "core/config/names_and_descriptions.h"
+
 #include "core/algorithms/gfd/gfd.h"
 #include "core/algorithms/gfd/gfd_validator/gfd_handler.h"
-#include "core/config/names_and_descriptions.h"
+#include "core/algorithms/gfd/graph_descriptor.h"
 
 namespace algos {
 
