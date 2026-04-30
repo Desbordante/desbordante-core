BUYT-1/Desbordante

Remove time accounting in HPIValid
The user is not going to need any of this aside from the timeout,
possibly. None of the timers were properly exposed anyway.

src/core/algorithms/ucc/hpivalid/result_collector.cpp
@@ -22,7 +22,6 @@ ResultCollector::ResultCollector(double timeout)
     : timeout_(timeout),
       ucc_count_(0),
       diff_sets_final_(0),
-      timers_(timer::TimerName::num_of_timers),
       diff_sets_(0),
       diff_sets_initial_(0),
       tree_complexity_(0),
@@ -37,7 +36,7 @@ void ResultCollector::AddUCC(Edge const& ucc) {
 
 bool ResultCollector::TimedOut() {
     return std::chrono::duration_cast<std::chrono::duration<double>>(
-                   clock::now() - timers_[timer::TimerName::total].begin)
+                   std::chrono::high_resolution_clock::now() - exec_start_)
                    .count() > timeout_;
 }
 
@@ -57,19 +56,14 @@ void ResultCollector::FinalHypergraph(Hypergraph const& hg) {
     diff_sets_final_ = hg.NumEdges();
 }
 
-void ResultCollector::StartTimer(timer::TimerName timer) {
-    timers_[timer].begin = clock::now();
+void ResultCollector::SetStartTime() {
+    exec_start_ = std::chrono::high_resolution_clock::now();
 }
 
-void ResultCollector::StopTimer(timer::TimerName timer) {
-    timers_[timer].end = clock::now();
-    timers_[timer].elapsed += std::chrono::duration_cast<std::chrono::milliseconds>(
-                                      timers_[timer].end - timers_[timer].begin)
-                                      .count();
-}
-
-unsigned long long ResultCollector::Time(timer::TimerName timer) const {
-    return timers_[timer].elapsed;
+unsigned long long ResultCollector::GetTimeSinceStart() const {
+    return std::chrono::duration_cast<std::chrono::seconds>(
+                   std::chrono::high_resolution_clock::now() - exec_start_)
+            .count();
 }
 
 void ResultCollector::CountDiffSets(unsigned number) {

Desbordante/desbordante-core

src/core/algorithms/ucc/hpivalid/result_collector.cpp
@@ -11,18 +11,10 @@
 
 namespace algos::hpiv {
 
-namespace timer {
-std::vector<std::string> description = {"Total Execution Time", "Cluster-Structures Construction",
-                                        "Total Enumeration Algo Time"
-                                        "Difference Set Sampling (all)",
-                                        "Cluster Intersection (validation)"};
-}  // namespace timer
-
 ResultCollector::ResultCollector(double timeout)
     : timeout_(timeout),
       ucc_count_(0),
       diff_sets_final_(0),
-      timers_(timer::TimerName::num_of_timers),
       diff_sets_(0),
       diff_sets_initial_(0),
       tree_complexity_(0),
@@ -37,7 +29,7 @@ void ResultCollector::AddUCC(Edge const& ucc) {
 
 bool ResultCollector::TimedOut() const {
     return std::chrono::duration_cast<std::chrono::duration<double>>(
-                   clock::now() - timers_[timer::TimerName::total].begin)
+                   std::chrono::high_resolution_clock::now() - exec_start_)
                    .count() > timeout_;
 }
 
@@ -57,19 +49,14 @@ void ResultCollector::FinalHypergraph(Hypergraph const& hg) {
     diff_sets_final_ = hg.NumEdges();
 }
 
-void ResultCollector::StartTimer(timer::TimerName timer) {
-    timers_[timer].begin = clock::now();
+void ResultCollector::SetStartTime() {
+    exec_start_ = std::chrono::high_resolution_clock::now();
 }
 
-void ResultCollector::StopTimer(timer::TimerName timer) {
-    timers_[timer].end = clock::now();
-    timers_[timer].elapsed += std::chrono::duration_cast<std::chrono::milliseconds>(
-                                      timers_[timer].end - timers_[timer].begin)
-                                      .count();
-}
-
-unsigned long long ResultCollector::Time(timer::TimerName timer) const {
-    return timers_[timer].elapsed;
+unsigned long long ResultCollector::GetTimeSinceStart() const {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(
+                   std::chrono::high_resolution_clock::now() - exec_start_)
+            .count();
 }
 
 void ResultCollector::CountDiffSets(unsigned number) {