92be10eb661c0dc593ec2da3ce17952638cb0350
Alexey Shlyonskikh <59949541+BUYT-1@users.noreply.github.com>
2026-05-25 21:21:43 +0300

    Rewrite includes in algo_factory.cpp

--- src/core/algorithms/algo_factory.cpp
+++ src/core/algorithms/algo_factory.cpp
@@ -2,13 +2,18 @@
 
 #include <algorithm>
 #include <stdexcept>
+#include <string>
+#include <string_view>
 #include <type_traits>
 #include <vector>
 
-#include "core/algorithms/algorithms.h"
-#include "core/algorithms/create_algorithm.h"
+#include <boost/any.hpp>
+
+#include "core/algorithms/algorithm.h"
 #include "core/config/names.h"
+#include "core/config/tabular_data/input_table_type.h"
 #include "core/config/tabular_data/input_tables_type.h"
+#include "core/parser/csv_parser/csv_parser.h"
 
 namespace algos {
 
